ad921e14baf8363cb4b057da19d7f790663f1b47
Jerry Shao <jerryshao@datastrato.com>
2023-05-25 17:13:50 +0800

    [#21] feat(schema): add Protobuf serialization and deserialization support for schema system (#26)
    ### What changes were proposed in this pull request?
    
    This PR proposes to add protobuf SerDe support for schema support. The
    protobuf SerDe support will be mainly used in schema persistence and RPC
    communication.
    
    ### Why are the changes needed?
    
    The adds of protobuf SerDe support will be used in schema persistence
    and RPC communication.
    
    Fix: #21
    
    ### Does this PR introduce _any_ user-facing change?
    
    N/A
    
    ### How was this patch tested?
    
    Adds new UT to cover the codes.

---
+++ core/src/main/java/com/datastrato/unified_catalog/schema/proto/TenantSerDe.java
@@ -0,0 +1,47 @@
+package com.datastrato.unified_catalog.schema.proto;
+
+class TenantSerDe implements ProtoSerDe<com.datastrato.unified_catalog.schema.Tenant, Tenant> {
+  @Override
+  public Tenant serialize(com.datastrato.unified_catalog.schema.Tenant tenant) {
+    Tenant.Builder builder =
+        Tenant.newBuilder()
+            .setId(tenant.getId())
+            .setName(tenant.getName())
+            .setAuditInfo(new AuditInfoSerDe().serialize(tenant.auditInfo()));
+
+    if (tenant.getComment() != null) {
+      builder.setComment(tenant.getComment());
+    }
+
+    SchemaVersion version =
+        SchemaVersion.newBuilder()
+            .setMajorNumber(tenant.getVersion().majorVersion)
+            .setMinorNumber(tenant.getVersion().minorVersion)
+            .build();
+
+    builder.setVersion(version);
+
+    return builder.build();
+  }
+
+  @Override
+  public com.datastrato.unified_catalog.schema.Tenant deserialize(Tenant p) {
+    com.datastrato.unified_catalog.schema.Tenant.Builder builder =
+        new com.datastrato.unified_catalog.schema.Tenant.Builder();
+    builder
+        .withId(p.getId())
+        .withName(p.getName())
+        .withAuditInfo(new AuditInfoSerDe().deserialize(p.getAuditInfo()));
+
+    if (p.hasComment()) {
+      builder.withComment(p.getComment());
+    }
+
+    com.datastrato.unified_catalog.schema.SchemaVersion version =
+        com.datastrato.unified_catalog.schema.SchemaVersion.forValues(
+            p.getVersion().getMajorNumber(), p.getVersion().getMinorNumber());
+    builder.withVersion(version);
+
+    return builder.build();
+  }
+}
