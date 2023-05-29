8f3d1d684bd47b66f60833ffd80782df69fa4810
Jerry Shao <jerryshao@datastrato.com>
2023-05-29 22:53:08 +0800

    [#27] feat(core): add config system (#29)
    ### What changes were proposed in this pull request?
    
    This PR introduces a Spark-style like strong typed config system for the
    project.
    
    ### Why are the changes needed?
    
    The config system is a cornerstone of the project. By comparing
    different config system implementations, Spark's one is strong type
    verified, and also can extend to other functions, so introducing a
    Spark-style like config system for the project.
    
    Fix: #27 
    
    ### Does this PR introduce _any_ user-facing change?
    
    N/A
    
    ### How was this patch tested?
    
    Add new UTs to cover the test

---
+++ core/src/test/java/com/datastrato/graviton/config/TestConfigEntry.java
@@ -0,0 +1,124 @@
+package com.datastrato.graviton.config;
+
+import com.google.common.collect.Lists;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestConfigEntry {
+
+  private final ConcurrentMap<String, String> configMap = new ConcurrentHashMap<>();
+
+  @BeforeEach
+  public void initializeConfigMap() {
+    configMap.put("graviton.test.string", "test-string");
+    configMap.put("graviton.test.string.alt1", "test-string1");
+    configMap.put("graviton.test.string.alt2", "test-string2");
+  }
+
+  @AfterEach
+  public void clearConfigMap() {
+    configMap.clear();
+  }
+
+  @Test
+  public void testConfWithDefaultValue() {
+    ConfigEntry<String> testConf =
+        new ConfigBuilder("graviton.test.string")
+            .doc("test")
+            .internal()
+            .stringConf()
+            .createWithDefault("test");
+    String value = testConf.readFrom(configMap);
+    Assertions.assertEquals("test-string", value);
+
+    ConfigEntry<Integer> testConf1 =
+        new ConfigBuilder("graviton.test.int")
+            .doc("test")
+            .version("1.0")
+            .intConf()
+            .createWithDefault(10);
+    int value1 = testConf1.readFrom(configMap);
+    Assertions.assertEquals(10, value1);
+
+    ConfigEntry<Boolean> testConf2 =
+        new ConfigBuilder("graviton.test.boolean").booleanConf().createWithDefault(true);
+    boolean value2 = testConf2.readFrom(configMap);
+    Assertions.assertTrue(value2);
+  }
+
+  @Test
+  public void testConfWithoutDefaultValue() {
+    ConfigEntry<String> testConf =
+        new ConfigBuilder("graviton.test.string").doc("test").internal().stringConf();
+    String value = testConf.readFrom(configMap);
+    Assertions.assertEquals("test-string", value);
+
+    ConfigEntry<Integer> testConf1 = new ConfigBuilder("graviton.test.int.no-exist").intConf();
+    Throwable exception =
+        Assertions.assertThrows(NoSuchElementException.class, () -> testConf1.readFrom(configMap));
+    Assertions.assertEquals(
+        "No configuration found for key graviton.test.int.no-exist", exception.getMessage());
+  }
+
+  @Test
+  public void testConfWithOptionalValue() {
+    ConfigEntry<Optional<String>> testConf =
+        new ConfigBuilder("graviton.test.no-exist-string").stringConf().createWithOptional();
+
+    Optional<String> value = testConf.readFrom(configMap);
+    Assertions.assertEquals(Optional.empty(), value);
+
+    ConfigEntry<Optional<Integer>> testConf1 =
+        new ConfigBuilder("graviton.test.no-exist-int").intConf().createWithOptional();
+
+    Optional<Integer> value1 = testConf1.readFrom(configMap);
+    Assertions.assertEquals(Optional.empty(), value1);
+  }
+
+  @Test
+  public void testConfWithAlternatives() {
+    ConfigEntry<String> testConf =
+        new ConfigBuilder("graviton.test.string")
+            .alternatives(
+                Lists.newArrayList("graviton.test.string.alt1", "graviton.test.string.alt1"))
+            .stringConf()
+            .createWithDefault("test");
+
+    String value = testConf.readFrom(configMap);
+    Assertions.assertEquals("test-string", value);
+
+    ConfigEntry<String> testConf1 =
+        new ConfigBuilder("graviton.test.string.no-exist")
+            .alternatives(
+                Lists.newArrayList("graviton.test.string.alt1", "graviton.test.string.alt1"))
+            .stringConf()
+            .createWithDefault("test");
+
+    String value1 = testConf1.readFrom(configMap);
+    Assertions.assertEquals("test-string1", value1);
+  }
+
+  @Test
+  public void testSetConf() {
+    ConfigEntry<Integer> testConf =
+        new ConfigBuilder("graviton.test.int").intConf().createWithDefault(1);
+
+    testConf.writeTo(configMap, 10);
+    Assertions.assertEquals("10", configMap.get("graviton.test.int"));
+
+    ConfigEntry<Optional<Integer>> testConf1 =
+        new ConfigBuilder("graviton.test.int1").intConf().createWithOptional();
+
+    testConf1.writeTo(configMap, Optional.of(11));
+    Assertions.assertEquals("11", configMap.get("graviton.test.int1"));
+
+    testConf1.writeTo(configMap, Optional.empty());
+    Assertions.assertEquals("11", configMap.get("graviton.test.int1"));
+  }
+}
