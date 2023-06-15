eae1c3decbf5b65460691cbfebeadb65ea1e1f2c
Jerry Shao <jerryshao@datastrato.com>
2023-06-15 17:11:51 +0800

    Add entity serde interface for Graviton

---
+++ core/src/main/java/com/datastrato/graviton/EntitySerDeFactory.java
@@ -0,0 +1,34 @@
+package com.datastrato.graviton;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EntitySerDeFactory {
+
+  private static final Logger LOG = LoggerFactory.getLogger(EntitySerDe.class);
+
+  // Register EntitySerDe's short name to its full qualified class name in the map. So that user
+  // don't need to specify the full qualified class name when creating an EntitySerDe.
+  private static final Map<String, String> ENTITY_SERDES =
+      ImmutableMap.of("proto", "com.datastrato.graviton.proto.ProtoEntitySerDe");
+
+  private EntitySerDeFactory() {}
+
+  public static EntitySerDe createEntitySerDe(Config config) {
+    String name = config.get(configs.ENTITY_SERDE);
+    return createEntitySerDe(name);
+  }
+
+  public static EntitySerDe createEntitySerDe(String name) {
+    String className = ENTITY_SERDES.getOrDefault(name, name);
+
+    try {
+      return (EntitySerDe) Class.forName(className).newInstance();
+    } catch (Exception e) {
+      LOG.error("Failed to create EntitySerDe by name {}.", name, e);
+      throw new RuntimeException("Failed to create EntitySerDe: " + name, e);
+    }
+  }
+}
---
+++ core/src/main/java/com/datastrato/graviton/proto/ProtoEntitySerDe.java
@@ -0,0 +1,130 @@
+package com.datastrato.graviton.proto;
+
+import com.datastrato.graviton.Entity;
+import com.datastrato.graviton.EntitySerDe;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.google.protobuf.Any;
+import com.google.protobuf.Message;
+import com.google.protobuf.MessageLite;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Optional;
+
+public class ProtoEntitySerDe implements EntitySerDe {
+
+  // The implementation of different entities should also register its class to this map,
+  // otherwise ProtoEntitySerDe will not be able to deserialize the entity.
+  private static final Map<String, String> ENTITY_TO_SERDE =
+      ImmutableMap.of(
+          "com.datastrato.graviton.meta.AuditInfo", "com.datastrato.graviton.proto.AuditInfoSerDe",
+          "com.datastrato.graviton.meta.Lakehouse", "com.datastrato.graviton.proto.LakehouseSerDe");
+
+  private static final Map<String, String> ENTITY_TO_PROTO =
+      ImmutableMap.of(
+          "com.datastrato.graviton.meta.AuditInfo", "com.datastrato.graviton.proto.AuditInfo",
+          "com.datastrato.graviton.meta.Lakehouse", "com.datastrato.graviton.proto.Lakehouse");
+
+  private final Map<Class<? extends Entity>, ProtoSerDe<? extends Entity, ? extends Message>>
+      entityToSerDe;
+
+  private final Map<Class<? extends Entity>, Class<? extends Message>> entityToProto;
+
+  private final Map<Class<? extends Message>, Class<? extends Entity>> protoToEntity;
+
+  public ProtoEntitySerDe() throws IOException {
+    ClassLoader loader =
+        Optional.ofNullable(Thread.currentThread().getContextClassLoader())
+            .orElse(getClass().getClassLoader());
+
+    this.entityToSerDe = Maps.newHashMap();
+    for (Map.Entry<String, String> entry : ENTITY_TO_SERDE.entrySet()) {
+      String key = entry.getKey();
+      String s = entry.getValue();
+      Class<? extends Entity> entityClass = (Class<? extends Entity>) loadClass(key, loader);
+      Class<? extends ProtoSerDe<? extends Entity, ? extends Message>> serdeClass =
+          (Class<? extends ProtoSerDe<? extends Entity, ? extends Message>>) loadClass(s, loader);
+
+      try {
+        ProtoSerDe<? extends Entity, ? extends MessageLite> serde = serdeClass.newInstance();
+        entityToSerDe.put(entityClass, serde);
+      } catch (Exception exception) {
+        throw new IOException("Failed to instantiate serde class " + s, exception);
+      }
+    }
+
+    this.entityToProto = Maps.newHashMap();
+    for (Map.Entry<String, String> entry : ENTITY_TO_PROTO.entrySet()) {
+      String e = entry.getKey();
+      String p = entry.getValue();
+      Class<? extends Entity> entityClass = (Class<? extends Entity>) loadClass(e, loader);
+      Class<? extends Message> protoClass = (Class<? extends Message>) loadClass(p, loader);
+      entityToProto.put(entityClass, protoClass);
+    }
+
+    this.protoToEntity = Maps.newHashMap();
+    entityToProto.forEach((e, p) -> protoToEntity.put(p, e));
+  }
+
+  @Override
+  public <T extends Entity> byte[] serialize(T t) throws IOException {
+    Any any = Any.pack(toProto(t));
+    return any.toByteArray();
+  }
+
+  @Override
+  public <T extends Entity> T deserialize(byte[] bytes, Class<T> clazz, ClassLoader classLoader)
+      throws IOException {
+    Any any = Any.parseFrom(bytes);
+
+    if (!entityToSerDe.containsKey(clazz) || !entityToProto.containsKey(clazz)) {
+      throw new IOException("No proto and serde class found for entity " + clazz.getName());
+    }
+
+    if (!any.is(entityToProto.get(clazz))) {
+      throw new IOException("Invalid proto for entity " + clazz.getName());
+    }
+
+    ProtoSerDe<T, Message> protoSerDe = (ProtoSerDe<T, Message>) entityToSerDe.get(clazz);
+    Class<? extends Message> protoClazz = entityToProto.get(clazz);
+
+    try {
+      Message anyMessage = any.unpack(protoClazz);
+      return protoSerDe.deserialize(anyMessage);
+    } catch (Exception e) {
+      throw new IOException("Failed to deserialize entity " + clazz.getName(), e);
+    }
+  }
+
+  public <T extends Entity, M extends Message> M toProto(T t) throws IOException {
+    if (!entityToSerDe.containsKey(t.getClass())) {
+      throw new IOException("No serde found for entity " + t.getClass().getName());
+    }
+
+    ProtoSerDe<T, M> protoSerDe = (ProtoSerDe<T, M>) entityToSerDe.get(t.getClass());
+
+    return protoSerDe.serialize(t);
+  }
+
+  public <T extends Entity, M extends Message> T fromProto(M m) throws IOException {
+    if (!protoToEntity.containsKey(m.getClass())) {
+      throw new IOException("No entity class found for proto " + m.getClass().getName());
+    }
+    Class<? extends Entity> entityClass = protoToEntity.get(m.getClass());
+
+    if (!entityToSerDe.containsKey(entityClass)) {
+      throw new IOException("No serde found for entity " + entityClass.getName());
+    }
+
+    ProtoSerDe<T, M> protoSerDe = (ProtoSerDe<T, M>) entityToSerDe.get(entityClass);
+    return protoSerDe.deserialize(m);
+  }
+
+  private Class<?> loadClass(String className, ClassLoader classLoader) throws IOException {
+    try {
+      return Class.forName(className, true, classLoader);
+    } catch (Exception e) {
+      throw new IOException("Failed to load class " + className, e);
+    }
+  }
+}
