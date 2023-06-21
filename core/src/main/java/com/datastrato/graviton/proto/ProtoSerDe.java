6473a5a9359de9d7a54f737c8b2a1efc24a3039d
Jerry Shao <jerryshao@datastrato.com>
2023-06-21 14:10:54 +0800

    [#51] feat(core): add SerDe interface for graviton (#52)
    ### What changes were proposed in this pull request?
    
    This PR adds the `EntitySerDe` interface and Protobuf implementation for
    Graviton.
    
    ### Why are the changes needed?
    
    The SerDe interface will be used for storage system to serialize and
    deserialize entity objects when interacting with underlying storage.
    
    Fix: #51 
    
    ### Does this PR introduce _any_ user-facing change?
    
    N/A
    
    ### How was this patch tested?
    
    Existing UTs to cover the code.

--- core/src/main/java/com/datastrato/graviton/proto/ProtoSerDe.java
+++ core/src/main/java/com/datastrato/graviton/proto/ProtoSerDe.java
@@ -1,8 +1,8 @@
 package com.datastrato.graviton.proto;
 
-import com.google.protobuf.MessageLite;
+import com.google.protobuf.Message;
 
-public interface ProtoSerDe<T, M extends MessageLite> {
+public interface ProtoSerDe<T, M extends Message> {
 
   M serialize(T t);
 
