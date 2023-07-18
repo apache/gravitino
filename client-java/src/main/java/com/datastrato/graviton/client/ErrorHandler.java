d8bf0e1f3f6c2425e1971e468f78079e15c137b8
Justin Mclean <justin@datastrato.com>
2023-07-18 13:48:19 +1000

    Add minimal headers and update LICENSE and NOTICE

--- client-java/src/main/java/com/datastrato/graviton/client/ErrorHandler.java
+++ client-java/src/main/java/com/datastrato/graviton/client/ErrorHandler.java
@@ -1,3 +1,8 @@
+/*
+* Copyright 2023 Datastrato.
+* This software is licensed under the Apache License version 2.
+*/
+
 package com.datastrato.graviton.client;
 
 import com.datastrato.graviton.dto.responses.ErrorResponse;
--- core/src/main/java/com/datastrato/graviton/meta/AuditInfo.java
+++ core/src/main/java/com/datastrato/graviton/meta/AuditInfo.java
@@ -1,3 +1,8 @@
+/*
+* Copyright 2023 Datastrato.
+* This software is licensed under the Apache License version 2.
+*/
+
 package com.datastrato.graviton.meta;
 
 import com.datastrato.graviton.Audit;
