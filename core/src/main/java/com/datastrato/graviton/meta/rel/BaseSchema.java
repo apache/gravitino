7a82305be307042469bf7641ec6976fd0a07c8ff
Justin Mclean <justin@classsoftware.com>
2023-07-19 16:53:48 +1000

    [#101] Added minimal header to java files missing headers (#97)
    ### What changes were proposed in this pull request?
    
    So we know what files Datastrato have created vs 3rd party files a small
    header has been added. This will help if we decide to donate this code
    base to a foundation.
    
    ### Why are the changes needed?
    
    Fix: #101 
    
    ### Does this PR introduce _any_ user-facing change?
    
    N/A
    
    ### How was this patch tested?
    
    N/A

--- core/src/main/java/com/datastrato/graviton/meta/rel/BaseSchema.java
+++ core/src/main/java/com/datastrato/graviton/meta/rel/BaseSchema.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2023 Datastrato.
+ * This software is licensed under the Apache License version 2.
+ */
 package com.datastrato.graviton.meta.rel;
 
 import com.datastrato.graviton.Entity;
