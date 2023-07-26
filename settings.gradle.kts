ee84ae901e697e72ef9de3b78e792abd5d732c95
yuqi <yuqi@datastrato.com>
2023-07-26 21:23:45 +0800

    [Improvement] Remove all wailcard import, fix again

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@
  * Copyright 2023 Datastrato.
  * This software is licensed under the Apache License version 2.
  */
-rootProject.name = "graviton"
+rootProject.name = "Graviton"
 
 include("api", "client-java", "common", "core", "meta", "server", "catalog-hive")
 include("storage")
