7dc0df7634e4037edde8244859d29bf6a2282237
Justin Mclean <justin@classsoftware.com>
2023-07-28 19:11:37 +1000

    [#134] Increased test coverage on API module (#135)
    ### What changes were proposed in this pull request?
    
    Added more tests to increase test coverage.
    
    ### Why are the changes needed?
    
    To increase test coverage.
    
    Fix: #134
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Build and test pass locally.
    
    Co-authored-by: Justin Mclean <justin@datastrato.com>

--- api/src/test/java/com/datastrato/graviton/TestCatalogChange.java
+++ api/src/test/java/com/datastrato/graviton/TestCatalogChange.java
@@ -47,4 +47,54 @@ public class TestCatalogChange {
 
     assertEquals(property, change.getProperty());
   }
+
+  @Test
+  void testRenameEqualsAndHashCode() {
+    String nameA = "Catalog";
+    RenameCatalog change1 = (RenameCatalog) CatalogChange.rename(nameA);
+    String nameB = "Catalog";
+    RenameCatalog change2 = (RenameCatalog) CatalogChange.rename(nameB);
+
+    assertTrue(change1.equals(change2));
+    assertTrue(change2.equals(change1));
+    assertEquals(change1.hashCode(), change2.hashCode());
+  }
+
+  @Test
+  void testRenameNotEqualsAndHashCode() {
+    String nameA = "Catalog";
+    RenameCatalog change1 = (RenameCatalog) CatalogChange.rename(nameA);
+    String nameB = "New Catalog";
+    RenameCatalog change2 = (RenameCatalog) CatalogChange.rename(nameB);
+
+    assertFalse(change1.equals(null));
+    assertFalse(change1.equals(change2));
+    assertFalse(change2.equals(change1));
+    assertNotEquals(change1.hashCode(), change2.hashCode());
+  }
+
+  @Test
+  void testUpdateEqualsAndHashCode() {
+    String commentA = "a comment";
+    UpdateCatalogComment update1 = (UpdateCatalogComment) CatalogChange.updateComment(commentA);
+    String commentB = "a comment";
+    UpdateCatalogComment update2 = (UpdateCatalogComment) CatalogChange.updateComment(commentB);
+
+    assertTrue(update1.equals(update2));
+    assertTrue(update2.equals(update1));
+    assertEquals(update1.hashCode(), update2.hashCode());
+  }
+
+  @Test
+  void testUpdateNotEqualsAndHashCode() {
+    String commentA = "a comment";
+    UpdateCatalogComment update1 = (UpdateCatalogComment) CatalogChange.updateComment(commentA);
+    String commentB = "a new comment";
+    UpdateCatalogComment update2 = (UpdateCatalogComment) CatalogChange.updateComment(commentB);
+
+    assertFalse(commentA.equals(null));
+    assertFalse(commentA.equals(commentB));
+    assertFalse(commentB.equals(commentA));
+    assertNotEquals(commentA.hashCode(), commentB.hashCode());
+  }
 }
