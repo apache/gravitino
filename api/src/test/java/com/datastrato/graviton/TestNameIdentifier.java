8bcc6cf81b21687c6af928c99cabc777737348c3
Justin Mclean <justin@classsoftware.com>
2023-07-28 19:11:37 +1000

    [#134] Increased test coverage on API module (#135)
    ### What changes were proposed in this pull request?
    
    Added more tests to increase test coverage.
    
    ### Why are the changes needed?
    
    To increase test coverage.
    
    Fix: #134
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Build and test pass locally.
    
    Co-authored-by: Justin Mclean <justin@datastrato.com>

--- api/src/test/java/com/datastrato/graviton/TestNameIdentifier.java
+++ api/src/test/java/com/datastrato/graviton/TestNameIdentifier.java
@@ -4,7 +4,8 @@
  */
 package com.datastrato.graviton;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 public class TestNameIdentifier {
@@ -12,42 +13,76 @@ public class TestNameIdentifier {
   @Test
   public void testCreateNameIdentifier() {
     NameIdentifier id = NameIdentifier.of("a", "b", "c");
-    Assertions.assertEquals(Namespace.of("a", "b"), id.namespace());
-    Assertions.assertEquals("c", id.name());
+    assertEquals(Namespace.of("a", "b"), id.namespace());
+    assertEquals("c", id.name());
 
     NameIdentifier id1 = NameIdentifier.of(Namespace.of("a", "b"), "c");
-    Assertions.assertEquals(Namespace.of("a", "b"), id1.namespace());
-    Assertions.assertEquals("c", id1.name());
+    assertEquals(Namespace.of("a", "b"), id1.namespace());
+    assertEquals("c", id1.name());
 
     NameIdentifier id2 = NameIdentifier.parse("a.b.c");
-    Assertions.assertEquals(Namespace.of("a", "b"), id2.namespace());
-    Assertions.assertEquals("c", id2.name());
+    assertEquals(Namespace.of("a", "b"), id2.namespace());
+    assertEquals("c", id2.name());
 
     NameIdentifier id3 = NameIdentifier.parse("a");
-    Assertions.assertEquals(Namespace.empty(), id3.namespace());
-    Assertions.assertEquals("a", id3.name());
+    assertEquals(Namespace.empty(), id3.namespace());
+    assertEquals("a", id3.name());
   }
 
   @Test
   public void testCreateWithInvalidArgs() {
-    Assertions.assertThrows(IllegalArgumentException.class, NameIdentifier::of);
-    Assertions.assertThrows(IllegalArgumentException.class, () -> NameIdentifier.of("a", null));
-    Assertions.assertThrows(IllegalArgumentException.class, () -> NameIdentifier.of("a", ""));
+    assertThrows(IllegalArgumentException.class, NameIdentifier::of);
+    assertThrows(IllegalArgumentException.class, () -> NameIdentifier.of("a", null));
+    assertThrows(IllegalArgumentException.class, () -> NameIdentifier.of("a", ""));
 
-    Assertions.assertThrows(IllegalArgumentException.class, () -> NameIdentifier.of(null, "a"));
-    Assertions.assertThrows(
-        IllegalArgumentException.class, () -> NameIdentifier.of(Namespace.empty(), null));
-    Assertions.assertThrows(
-        IllegalArgumentException.class, () -> NameIdentifier.of(Namespace.empty(), ""));
+    assertThrows(IllegalArgumentException.class, () -> NameIdentifier.of(null, "a"));
+    assertThrows(IllegalArgumentException.class, () -> NameIdentifier.of(Namespace.empty(), null));
+    assertThrows(IllegalArgumentException.class, () -> NameIdentifier.of(Namespace.empty(), ""));
 
-    Assertions.assertThrows(IllegalArgumentException.class, () -> NameIdentifier.parse(null));
-    Assertions.assertThrows(IllegalArgumentException.class, () -> NameIdentifier.parse(""));
-    Assertions.assertThrows(IllegalArgumentException.class, () -> NameIdentifier.parse("a."));
-    Assertions.assertThrows(IllegalArgumentException.class, () -> NameIdentifier.parse("a.."));
-    Assertions.assertThrows(IllegalArgumentException.class, () -> NameIdentifier.parse(".a"));
-    Assertions.assertThrows(IllegalArgumentException.class, () -> NameIdentifier.parse("..a"));
-    Assertions.assertThrows(IllegalArgumentException.class, () -> NameIdentifier.parse("a..b"));
-    Assertions.assertThrows(IllegalArgumentException.class, () -> NameIdentifier.parse("a.b."));
-    Assertions.assertThrows(IllegalArgumentException.class, () -> NameIdentifier.parse(".a.b"));
+    assertThrows(IllegalArgumentException.class, () -> NameIdentifier.parse(null));
+    assertThrows(IllegalArgumentException.class, () -> NameIdentifier.parse(""));
+    assertThrows(IllegalArgumentException.class, () -> NameIdentifier.parse("a."));
+    assertThrows(IllegalArgumentException.class, () -> NameIdentifier.parse("a.."));
+    assertThrows(IllegalArgumentException.class, () -> NameIdentifier.parse(".a"));
+    assertThrows(IllegalArgumentException.class, () -> NameIdentifier.parse("..a"));
+    assertThrows(IllegalArgumentException.class, () -> NameIdentifier.parse("a..b"));
+    assertThrows(IllegalArgumentException.class, () -> NameIdentifier.parse("a.b."));
+    assertThrows(IllegalArgumentException.class, () -> NameIdentifier.parse(".a.b"));
+  }
+
+  @Test
+  public void testEqualsAndHashNameIdentifier() {
+    NameIdentifier id1 = NameIdentifier.parse("a.b.c");
+    NameIdentifier id2 = NameIdentifier.parse("a.b.c");
+
+    assertTrue(id1.equals(id2));
+    assertTrue(id2.equals(id1));
+    assertEquals(id1.hashCode(), id2.hashCode());
+  }
+
+  @Test
+  public void testNotEqualsAndHashNameIdentifier() {
+    NameIdentifier id1 = NameIdentifier.parse("a.b.c");
+    NameIdentifier id2 = NameIdentifier.parse("a.b.z");
+
+    assertFalse(id1.equals(null));
+    assertFalse(id1.equals(id2));
+    assertFalse(id2.equals(id1));
+    assertNotEquals(id1.hashCode(), id2.hashCode());
+  }
+
+  @Test
+  public void testHasNamespace() {
+    NameIdentifier id = NameIdentifier.parse("a.b.c");
+    assertTrue(id.hasNamespace());
+  }
+
+  @Test
+  public void testToString() {
+    NameIdentifier id1 = NameIdentifier.parse("a");
+    NameIdentifier id2 = NameIdentifier.parse("a.b.c");
+
+    assertEquals(id1.toString(), "a");
+    assertEquals(id2.toString(), "a.b.c");
   }
 }
--- api/src/test/java/com/datastrato/graviton/TestSchemaChange.java
+++ api/src/test/java/com/datastrato/graviton/TestSchemaChange.java
@@ -30,4 +30,58 @@ public class TestSchemaChange {
 
     assertEquals(property, change.getProperty());
   }
+
+  @Test
+  void testRemovePropertyEqualsAndHashCode() {
+    String propertyA = "property A";
+    RemoveProperty changeA = (RemoveProperty) SchemaChange.removeProperty(propertyA);
+    String propertyB = "property A";
+    RemoveProperty changeB = (RemoveProperty) SchemaChange.removeProperty(propertyB);
+
+    assertTrue(changeA.equals(changeB));
+    assertTrue(changeB.equals(changeA));
+    assertEquals(changeA.hashCode(), changeB.hashCode());
+  }
+
+  @Test
+  void testRemovePropertyNotEqualsAndHashCode() {
+    String propertyA = "property A";
+    RemoveProperty changeA = (RemoveProperty) SchemaChange.removeProperty(propertyA);
+    String propertyB = "property B";
+    RemoveProperty changeB = (RemoveProperty) SchemaChange.removeProperty(propertyB);
+
+    assertFalse(changeA.equals(null));
+    assertFalse(changeA.equals(changeB));
+    assertFalse(changeB.equals(changeA));
+    assertNotEquals(changeA.hashCode(), changeB.hashCode());
+  }
+
+  @Test
+  void testSetPropertyEqualsAndHashCode() {
+    String propertyA = "property A";
+    String valueA = "A";
+    SetProperty changeA = (SetProperty) SchemaChange.setProperty(propertyA, valueA);
+    String propertyB = "property A";
+    String valueB = "A";
+    SetProperty changeB = (SetProperty) SchemaChange.setProperty(propertyB, valueB);
+
+    assertTrue(changeA.equals(changeB));
+    assertTrue(changeB.equals(changeA));
+    assertEquals(changeA.hashCode(), changeB.hashCode());
+  }
+
+  @Test
+  void testSetPropertyNotEqualsAndHashCode() {
+    String propertyA = "property A";
+    String valueA = "A";
+    SetProperty changeA = (SetProperty) SchemaChange.setProperty(propertyA, valueA);
+    String propertyB = "property B";
+    String valueB = "B";
+    SetProperty changeB = (SetProperty) SchemaChange.setProperty(propertyB, valueB);
+
+    assertFalse(changeA.equals(null));
+    assertFalse(changeA.equals(changeB));
+    assertFalse(changeB.equals(changeA));
+    assertNotEquals(changeA.hashCode(), changeB.hashCode());
+  }
 }
