ffa81f095950dc552974558c6f7224dfd97f86df
Justin Mclean <justin@classsoftware.com>
2023-08-03 12:00:21 +1000

    [#156] docs: improve English and add JavaDocs for java client module (#157)
    ### What changes were proposed in this pull request?
    
    Improve English and add JavaDocs for the java client module.
    
    ### Why are the changes needed?
    
    To improve documentation and understanding of the code.
    
    Fix: #156
    
    ### Does this PR introduce _any_ user-facing change?
    
    No code was changed.
    
    ### How was this patch tested?
    
    Build and tests passed locally.
    
    Co-authored-by: Justin Mclean <justin@datastrato.com>

--- client-java/src/main/java/com/datastrato/graviton/GravitonMetaLake.java
+++ client-java/src/main/java/com/datastrato/graviton/GravitonMetaLake.java
@@ -14,10 +14,23 @@ import com.datastrato.graviton.exceptions.NoSuchMetalakeException;
 import com.google.common.base.Preconditions;
 import java.util.Map;
 
+/**
+ * GravitonMetaLake is a specific implementation of MetalakeDTO that also supports Catalog
+ * operations.
+ */
 public class GravitonMetaLake extends MetalakeDTO implements SupportsCatalogs {
 
   private final RESTClient restClient;
 
+  /**
+   * Constructs a new Graviton MetaLake instance with the provided details.
+   *
+   * @param name The name of the Graviton MetaLake.
+   * @param comment The comment for the Graviton MetaLake.
+   * @param properties The properties of the Graviton MetaLake.
+   * @param auditDTO The audit information for the Graviton MetaLake.
+   * @param restClient The REST client used for API communication.
+   */
   GravitonMetaLake(
       String name,
       String comment,
@@ -28,16 +41,42 @@ public class GravitonMetaLake extends MetalakeDTO implements SupportsCatalogs {
     this.restClient = restClient;
   }
 
+  /**
+   * Retrieves an array of Catalog names associated with this Graviton MetaLake.
+   *
+   * @param namespace The namespace to filter Catalogs.
+   * @return An array of name identifier's representing the Catalogs.
+   * @throws NoSuchMetalakeException If the Graviton MetaLake does not exist.
+   */
   @Override
   public NameIdentifier[] listCatalogs(Namespace namespace) throws NoSuchMetalakeException {
     throw new UnsupportedOperationException("Not implemented yet");
   }
 
+  /**
+   * Loads a specific Catalog associated with this Graviton MetaLake.
+   *
+   * @param ident The identifier of the Catalog to be loaded.
+   * @return A Catalog instance representing the loaded Catalog.
+   * @throws NoSuchCatalogException If the specified Catalog does not exist.
+   */
   @Override
   public Catalog loadCatalog(NameIdentifier ident) throws NoSuchCatalogException {
     throw new UnsupportedOperationException("Not implemented yet");
   }
 
+  /**
+   * Creates a new Catalog associated with this Graviton MetaLake.
+   *
+   * @param ident The identifier of the new Catalog.
+   * @param type The type of the new Catalog.
+   * @param comment The comment for the new Catalog.
+   * @param properties The properties of the new Catalog.
+   * @return A Catalog instance representing the newly created Catalog.
+   * @throws NoSuchMetalakeException If the Graviton MetaLake does not exist.
+   * @throws CatalogAlreadyExistsException If a Catalog with the specified identifier already
+   *     exists.
+   */
   @Override
   public Catalog createCatalog(
       NameIdentifier ident, Catalog.Type type, String comment, Map<String, String> properties)
@@ -45,25 +84,54 @@ public class GravitonMetaLake extends MetalakeDTO implements SupportsCatalogs {
     throw new UnsupportedOperationException("Not implemented yet");
   }
 
+  /**
+   * Alters a specific Catalog associated with this GravitonMetaLake.
+   *
+   * @param ident The identifier of the Catalog to be altered.
+   * @param changes The changes to be applied to the Catalog.
+   * @return A Catalog instance representing the updated Catalog.
+   * @throws NoSuchCatalogException If the specified Catalog does not exist.
+   * @throws IllegalArgumentException If the provided changes are invalid.
+   */
   @Override
   public Catalog alterCatalog(NameIdentifier ident, CatalogChange... changes)
       throws NoSuchCatalogException, IllegalArgumentException {
     throw new UnsupportedOperationException("Not implemented yet");
   }
 
+  /**
+   * Drops a specific Catalog associated with this Graviton MetaLake.
+   *
+   * @param ident The identifier of the Catalog to be dropped.
+   * @return True if the drop operation was successful, false otherwise.
+   */
   @Override
   public boolean dropCatalog(NameIdentifier ident) {
     throw new UnsupportedOperationException("Not implemented yet");
   }
 
+  /** The Builder class for GravitonMetaLake. */
   static class Builder extends MetalakeDTO.Builder<Builder> {
     private RESTClient restClient;
 
+    /**
+     * Sets the RESTClient for the GravitonMetaLake being built.
+     *
+     * @param restClient The REST client for API communication.
+     * @return The Builder instance with the updated REST client.
+     */
     Builder withRestClient(RESTClient restClient) {
       this.restClient = restClient;
       return this;
     }
 
+    /**
+     * Builds the GravitonMetaLake instance.
+     *
+     * @return A new Graviton MetaLake instance with the specified details.
+     * @throws NullPointerException If the REST client is not set.
+     * @throws IllegalArgumentException If the name is null or empty, or the audit is null.
+     */
     @Override
     public GravitonMetaLake build() {
       Preconditions.checkNotNull(restClient, "restClient must be set");
