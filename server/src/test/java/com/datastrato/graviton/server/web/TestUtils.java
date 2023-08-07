f3d32572b0087225638a7631d9b060ee0f75e34e
Justin Mclean <justin@classsoftware.com>
2023-08-07 17:40:56 +1000

    [#180] test(server): add server tests. (#182)
    ### What changes were proposed in this pull request?
    
    Add unit tests for the server module. Some of the tests for
    GravitonServer and JettyServer are not the best as the classes are not
    easily testable.
    
    ### Why are the changes needed?
    
    To increase code coverage and understanding and to document how to use
    some code.
    
    Fix: #180
    
    ### Does this PR introduce _any_ user-facing change?
    
    No code changes made.
    
    ### How was this patch tested?
    
    Build test pass locally.
    
    Co-authored-by: Justin Mclean <justin@datastrato.com>

---
+++ server/src/test/java/com/datastrato/graviton/server/web/TestUtils.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2023 Datastrato.
+ * This software is licensed under the Apache License version 2.
+ */
+package com.datastrato.graviton.server.web;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import com.datastrato.graviton.dto.responses.ErrorResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.junit.jupiter.api.Test;
+
+/*
+ * Copyright 2023 Datastrato.
+ * This software is licensed under the Apache License version 2.
+ */
+public class TestUtils {
+
+  @Test
+  public void testRemoteUser() {
+    HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+    when(mockRequest.getRemoteUser()).thenReturn("user");
+    String remoteUser = Utils.remoteUser(mockRequest);
+    assertEquals("user", remoteUser);
+  }
+
+  @Test
+  public void testRemoteUserDefault() {
+    HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+    when(mockRequest.getRemoteUser()).thenReturn(null);
+    String remoteUser = Utils.remoteUser(mockRequest);
+    assertEquals("graviton", remoteUser);
+  }
+
+  @Test
+  public void testOkWithData() {
+    Response response = Utils.ok("data");
+    assertNotNull(response);
+    assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+    assertEquals("data", response.getEntity());
+    assertEquals(MediaType.APPLICATION_JSON, response.getMediaType().toString());
+  }
+
+  @Test
+  public void testOkWithoutData() {
+    Response response = Utils.ok();
+    assertNotNull(response);
+    assertEquals(Response.Status.NO_CONTENT.getStatusCode(), response.getStatus());
+    assertEquals(MediaType.APPLICATION_JSON, response.getMediaType().toString());
+  }
+
+  @Test
+  public void testIllegalArguments() {
+    Response response = Utils.illegalArguments("Invalid argument");
+    assertNotNull(response);
+    assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), response.getStatus());
+    assertEquals(MediaType.APPLICATION_JSON, response.getMediaType().toString());
+    ErrorResponse errorResponse = (ErrorResponse) response.getEntity();
+    assertEquals("Invalid argument", errorResponse.getMessage());
+  }
+
+  @Test
+  public void testInternalError() {
+    Response response = Utils.internalError("Internal error");
+    assertNotNull(response);
+    assertEquals(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), response.getStatus());
+    assertEquals(MediaType.APPLICATION_JSON, response.getMediaType().toString());
+    ErrorResponse errorResponse = (ErrorResponse) response.getEntity();
+    assertEquals("Internal error", errorResponse.getMessage());
+  }
+
+  @Test
+  public void testNotFoundWithType() {
+    Response response = Utils.notFound("Resource", "Not found");
+    assertNotNull(response);
+    assertEquals(Response.Status.NOT_FOUND.getStatusCode(), response.getStatus());
+    assertEquals(MediaType.APPLICATION_JSON, response.getMediaType().toString());
+    ErrorResponse errorResponse = (ErrorResponse) response.getEntity();
+    assertEquals("Resource", errorResponse.getType());
+    assertEquals("Not found", errorResponse.getMessage());
+  }
+
+  @Test
+  public void testNotFoundWithThrowable() {
+    Throwable throwable = new RuntimeException("Some error");
+    Response response = Utils.notFound("Resource", throwable);
+    assertNotNull(response);
+    assertEquals(Response.Status.NOT_FOUND.getStatusCode(), response.getStatus());
+    assertEquals(MediaType.APPLICATION_JSON, response.getMediaType().toString());
+    ErrorResponse errorResponse = (ErrorResponse) response.getEntity();
+    assertEquals("RuntimeException", errorResponse.getType());
+    assertEquals("Resource", errorResponse.getMessage());
+  }
+
+  @Test
+  public void testAlreadyExistsWithType() {
+    Response response = Utils.alreadyExists("Resource", "Already exists");
+    assertNotNull(response);
+    assertEquals(Response.Status.CONFLICT.getStatusCode(), response.getStatus());
+    assertEquals(MediaType.APPLICATION_JSON, response.getMediaType().toString());
+    ErrorResponse errorResponse = (ErrorResponse) response.getEntity();
+    assertEquals("Resource", errorResponse.getType());
+    assertEquals("Already exists", errorResponse.getMessage());
+  }
+
+  @Test
+  public void testAlreadyExistsWithThrowable() {
+    Throwable throwable = new RuntimeException("Already exists");
+    Response response = Utils.alreadyExists("New message", throwable);
+    assertNotNull(response);
+    assertEquals(Response.Status.CONFLICT.getStatusCode(), response.getStatus());
+    assertEquals(MediaType.APPLICATION_JSON, response.getMediaType().toString());
+    ErrorResponse errorResponse = (ErrorResponse) response.getEntity();
+    assertEquals("RuntimeException", errorResponse.getType());
+    assertEquals("New message", errorResponse.getMessage());
+  }
+}
