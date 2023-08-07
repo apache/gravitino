b3b9b2ad085286f55d9befaef5f833e61c03d2b5
Qi Yu <yuqi@datastrato.com>
2023-08-07 20:09:59 +0800

    [#185] <impro>(all): Disable static wildcard import (#186)
    ### What changes were proposed in this pull request?
    
    - Add a check rule that forbids wildcard static imports
    - Remove all wildcard static imports
    - Make the version of Gradle and JDK consistent with that of local
    environment
    
    
    ### Why are the changes needed?
    
    Please see #185 
    
    Fix: #185 
    
    ### Does this PR introduce _any_ user-facing change?
    
    No
    
    ### How was this patch tested?
    
    No

--- api/src/test/java/com/datastrato/graviton/TestNameIdentifier.java
+++ api/src/test/java/com/datastrato/graviton/TestNameIdentifier.java
@@ -4,7 +4,11 @@
  */
 package com.datastrato.graviton;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
