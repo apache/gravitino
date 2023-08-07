7c74fbe49f7565ea84199da0c5fcad805b9ec13f
Justin Mclean <justin@classsoftware.com>
2023-08-07 17:40:56 +1000

    [#180] test(server): add server tests. (#182)
    ### What changes were proposed in this pull request?
    
    Add unit tests for the server module. Some of the tests for
    GravitonServer and JettyServer are not the best as the classes are not
    easily testable.
    
    ### Why are the changes needed?
    
    To increase code coverage and understanding and to document how to use
    some code.
    
    Fix: #180
    
    ### Does this PR introduce _any_ user-facing change?
    
    No code changes made.
    
    ### How was this patch tested?
    
    Build test pass locally.
    
    Co-authored-by: Justin Mclean <justin@datastrato.com>

---
+++ server/src/test/java/com/datastrato/graviton/server/web/TestJettyServer.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2023 Datastrato.
+ * This software is licensed under the Apache License version 2.
+ */
+package com.datastrato.graviton.server.web;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import com.datastrato.graviton.server.GravitonServerException;
+import com.datastrato.graviton.server.ServerConfig;
+import javax.servlet.Filter;
+import javax.servlet.Servlet;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestJettyServer {
+
+  private JettyServer jettyServer;
+
+  @BeforeEach
+  public void setUp() {
+    jettyServer = new JettyServer();
+  }
+
+  @AfterEach
+  public void tearDown() {
+    if (jettyServer != null) {
+      jettyServer.stop();
+    }
+  }
+
+  @Test
+  public void testInitialize() {
+    ServerConfig config = new ServerConfig();
+
+    jettyServer.initialize(config);
+
+    // TODO might be nice to have an isInitalised method or similar?
+  }
+
+  @Test
+  public void testStartAndStop() throws GravitonServerException {
+    ServerConfig config = new ServerConfig();
+
+    jettyServer.initialize(config);
+    jettyServer.start();
+    // TODO might be nice to have an IsRunning method or similar?
+    jettyServer.stop();
+  }
+
+  @Test
+  public void testAddServletAndFilter() throws GravitonServerException {
+    ServerConfig config = new ServerConfig();
+
+    jettyServer.initialize(config);
+    jettyServer.start();
+
+    Servlet mockServlet = mock(Servlet.class);
+    Filter mockFilter = mock(Filter.class);
+    jettyServer.addServlet(mockServlet, "/test");
+    jettyServer.addFilter(mockFilter, "/filter");
+
+    // TODO add asserts
+
+    jettyServer.stop();
+  }
+
+  @Test
+  public void testStopWithNullServer() {
+    assertDoesNotThrow(() -> jettyServer.stop());
+  }
+
+  @Test
+  public void testStartWithoutInitialise() throws InterruptedException {
+    assertThrows(GravitonServerException.class, () -> jettyServer.start());
+  }
+}
