fe68ff8fdfb80e3b499cd589fe0629eab0b6b939
Justin Mclean <justin@classsoftware.com>
2023-08-08 15:57:08 +1000

    [#171] Impove test coverage (#172)
    ### What changes were proposed in this pull request?
    
    Added more tests.
    
    ### Why are the changes needed?
    
    To increase code coverage and document how to use code to help users
    understand how to use it.
    
    Fix: #171
    
    ### Does this PR introduce _any_ user-facing change?
    
    No code changes were made.
    
    ### How was this patch tested?
    
    Build and test pass on local machine.
    
    ---------
    
    Co-authored-by: Justin Mclean <justin@datastrato.com>

---
+++ common/src/test/java/com/datastrato/graviton/rest/TestRESTUtils.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2023 Datastrato.
+ * This software is licensed under the Apache License version 2.
+ */
+package com.datastrato.graviton.rest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+class TestRESTUtils {
+
+  @Test
+  void testStripTrailingSlash() {
+    assertEquals("/path/to/resource", RESTUtils.stripTrailingSlash("/path/to/resource/"));
+    assertEquals("/path/to/resource", RESTUtils.stripTrailingSlash("/path/to/resource////"));
+    assertEquals("", RESTUtils.stripTrailingSlash(""));
+    assertEquals(null, RESTUtils.stripTrailingSlash(null));
+  }
+
+  @Test
+  void testEncodeFormData() {
+    Map<Object, Object> formData =
+        ImmutableMap.builder().put("key1", "value1").put("key2", "value2").build();
+
+    String expected = "key1=value1&key2=value2";
+    assertEquals(expected, RESTUtils.encodeFormData(formData));
+
+    Map<Object, Object> emptyFormData = ImmutableMap.of();
+    assertEquals("", RESTUtils.encodeFormData(emptyFormData));
+  }
+
+  @Test
+  void testDecodeFormData() {
+    String formString = "key1=value1&key2=value2";
+    Map<Object, Object> expectedFormData =
+        ImmutableMap.builder().put("key1", "value1").put("key2", "value2").build();
+
+    assertEquals(expectedFormData, RESTUtils.decodeFormData(formString));
+
+    String emptyFormString = "";
+    Map<String, String> emptyFormData = ImmutableMap.of();
+    /* This may not be behaviour we want? */
+    assertThrows(IllegalArgumentException.class, () -> RESTUtils.decodeFormData(emptyFormString));
+  }
+
+  @Test
+  void testEncodeString() {
+    assertEquals("test", RESTUtils.encodeString("test"));
+    assertEquals("", RESTUtils.encodeString(""));
+    /* not %20 as you might expect */
+    assertEquals("hello+world", RESTUtils.encodeString("hello world"));
+    assertThrows(IllegalArgumentException.class, () -> RESTUtils.encodeString(null));
+  }
+
+  @Test
+  void testDecodeString() {
+    assertEquals("test", RESTUtils.decodeString("test"));
+    assertEquals("", RESTUtils.decodeString(""));
+    assertEquals("hello world", RESTUtils.decodeString("hello%20world"));
+    assertThrows(IllegalArgumentException.class, () -> RESTUtils.decodeString(null));
+  }
+}
