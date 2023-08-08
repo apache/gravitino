51af61b5db90ebd61276592ab789dbc531aa873d
Justin Mclean <justin@classsoftware.com>
2023-08-08 12:41:52 +1000

    [#181] Improve core tests (#187)
    ### What changes were proposed in this pull request?
    
    Added tests for some classes that were missing tests.
    
    ### Why are the changes needed?
    
    To improve code coverage and document code.
    
    Fix: #181
    
    ### Does this PR introduce _any_ user-facing change?
    
    No code changes.
    
    ### How was this patch tested?
    
    Build and test pass locally. Merged main as it contained changes that
    would cause original tests to fail.
    
    ---------
    
    Co-authored-by: Justin Mclean <justin@datastrato.com>

---
+++ core/src/test/java/com/datastrato/graviton/TestField.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2023 Datastrato.
+ * This software is licensed under the Apache License version 2.
+ */
+package com.datastrato.graviton;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+class FieldTest {
+
+  @Test
+  void testRequiredFieldCreation() {
+    Field field = Field.required("age", Integer.class, "Age of the person");
+    assertNotNull(field);
+  }
+
+  @Test
+  void testOptionalFieldCreation() {
+    Field field = Field.optional("email", String.class);
+    assertNotNull(field);
+  }
+
+  @Test
+  void testFieldValidation() {
+    Field field = Field.required("name", String.class);
+
+    assertDoesNotThrow(() -> field.validate("John Doe"));
+
+    IllegalArgumentException ex =
+        assertThrows(IllegalArgumentException.class, () -> field.validate(null));
+    assertEquals("Field name is required", ex.getMessage());
+
+    ex = assertThrows(IllegalArgumentException.class, () -> field.validate(42));
+    assertEquals("Field name is not of type java.lang.String", ex.getMessage());
+  }
+
+  @Test
+  void testBuilderMissingName() {
+    IllegalArgumentException ex =
+        assertThrows(IllegalArgumentException.class, () -> Field.required(null, Integer.class));
+    assertEquals("Field name is required", ex.getMessage());
+  }
+
+  @Test
+  void testBuilderMissingTypeClass() {
+    IllegalArgumentException ex =
+        assertThrows(IllegalArgumentException.class, () -> Field.optional("weight", null));
+    assertEquals("Field type class is required", ex.getMessage());
+  }
+}
