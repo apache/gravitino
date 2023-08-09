64dbd05c529dcb54a6cee102afb0f3ddcb23793a
xunliu <xun@datastrato.com>
2023-08-09 10:36:58 +0800

    rollback change to the rootProject.name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,6 @@
  * Copyright 2023 Datastrato.
  * This software is licensed under the Apache License version 2.
  */
-rootProject.name = "graviton"
+rootProject.name = "Graviton"
 
 include("api", "client-java", "common", "core", "meta", "server", "catalog-hive")
