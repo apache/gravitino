46df750bed0564c02ea36ede2eedc47c3574df2c
Justin Mclean <justin@datastrato.com>
2023-08-10 17:02:07 +1000

    remove removed method

--- core/src/main/java/com/datastrato/graviton/storage/kv/CustomEntityKeyEncoder.java
+++ core/src/main/java/com/datastrato/graviton/storage/kv/CustomEntityKeyEncoder.java
@@ -242,23 +242,6 @@ public class CustomEntityKeyEncoder implements EntityKeyEncoder {
   }
 
   /**
-   * Encodes a NameIdentifier object into a byte array for use as a key in a key-value store.
-   *
-   * @param entity The NameIdentifier object to be encoded.
-   * @return The byte array representing the encoded key.
-   */
-  @Override
-  public byte[] encode(NameIdentifier entity) {
-    // A simple approach is taken, utilizing the identifier
-    // of the entity as the key. However. This will be improved in future PRs.
-    // TODO (yuqi) While the NameIdentifier provides key information, it may
-    // be insufficient for key encoding. An improvement
-    // involves inferring the object's class from the key, which would help
-    //  deserialization when retrieving it from the key-value store.
-    return entity.toString().getBytes();
-  }
-
-  /**
    * Encodes a Namespace object into a byte array for use as a key in a key-value store.
    *
    * @param namespace The Namespace object to be encoded.
