fb45d5a605f3841a688f71e5a56248a05213d953
xunliu <xun@datastrato.com>
2023-08-11 10:31:33 +0800

    Add comment about JDK 8u151

--- bin/common.sh
+++ bin/common.sh
@@ -41,6 +41,8 @@ function check_java_version() {
     JVM_VERSION=$(echo "$jvmver"|sed -e 's|^1\.\([0-9][0-9]*\)\..*$|\1|')
   fi
 
+  # JDK 8u151 version fixed a number of security vulnerabilities and issues to improve system stability and security.
+  # https://www.oracle.com/java/technologies/javase/8u151-relnotes.html
   if [ "$JVM_VERSION" -lt 8 ] || { [ "$JVM_VERSION" -eq 8 ] && [ "${jvmver#*_}" -lt 151 ]; } ; then
     echo "Graviton requires either Java 8 update 151 or newer"
     exit 1;
