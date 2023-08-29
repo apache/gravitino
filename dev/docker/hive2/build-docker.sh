35c581c7d812cc0880a5b2d406ba30ae9f55d326
Xun Liu <xun@datastrato.com>
2023-08-29 22:35:53 +0800

    [#241] Hive2 docker runtime environment (#267)
    ### What changes were proposed in this pull request?
    
    Build a hive runtime environment docker image including:
    These versions match the Hadoop and Hive that Graviton depends on.
    
    1. Hadoop-2.7.3
    2. Hive-2.3.9
    3. Mysql
    4. openJDK-8u382
    5. Support `linux/ARM64` and `linux/amd86` architecture
    
    ### Why are the changes needed?
    
    The hive catalog integration test need to connect really Hive runtime
    environment
    
    Fix: #241 
    
    ### Does this PR introduce _any_ user-facing change?
    
    N/A
    
    ### How was this patch tested?
    
    Success build and push to the docker hub
    +
    https://hub.docker.com/repository/docker/datastrato/hive2/tags?page=1&ordering=last_updated
    
    <img width="791" alt="image"
    src="https://github.com/datastrato/graviton/assets/3677382/7aee52b5-44f4-4f7c-ae4a-8703c7daca94">

---
+++ dev/docker/hive2/build-docker.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Copyright 2023 Datastrato.
+# This software is licensed under the Apache License version 2.
+#
+set -ex
+bin="$(dirname "${BASH_SOURCE-$0}")"
+bin="$(cd "${bin}">/dev/null; pwd)"
+
+# Environment variables definition
+IMAGE_NAME="datastrato/hive2:0.1.0"
+HADOOP_VERSION="2.7.3"
+HIVE_VERSION="2.3.9"
+
+HADOOP_PACKAGE_NAME="hadoop-${HADOOP_VERSION}.tar.gz"
+HADOOP_DOWNLOAD_URL="http://archive.apache.org/dist/hadoop/core/hadoop-${HADOOP_VERSION}/${HADOOP_PACKAGE_NAME}"
+
+HIVE_PACKAGE_NAME="apache-hive-${HIVE_VERSION}-bin.tar.gz"
+HIVE_DOWNLOAD_URL="https://archive.apache.org/dist/hive/hive-${HIVE_VERSION}/${HIVE_PACKAGE_NAME}"
+
+# Prepare download packages
+if [[ ! -d "${bin}/packages" ]]; then
+  mkdir -p "${bin}/packages"
+fi
+
+if [ ! -f "${bin}/packages/${HADOOP_PACKAGE_NAME}" ]; then
+  curl -s -o "${bin}/packages/${HADOOP_PACKAGE_NAME}" ${HADOOP_DOWNLOAD_URL}
+fi
+
+if [ ! -f "${bin}/packages/${HIVE_PACKAGE_NAME}" ]; then
+  curl -s -o "${bin}/packages/${HIVE_PACKAGE_NAME}" ${HIVE_DOWNLOAD_URL}
+fi
+
+# Create multi-arch builder
+BUILDER_NAME="hive2"
+builders=$(docker buildx ls)
+if echo "${builders}" | grep -q "${BUILDER_NAME}"; then
+  echo "BuildKit builder '${BUILDER_NAME}' already exists."
+else
+  echo "BuildKit builder '${BUILDER_NAME}' does not exist."
+  docker buildx create --platform linux/amd64,linux/arm64 --use --name hive2
+fi
+
+# Option params --no-cache --push
+docker buildx build --platform=linux/amd64,linux/arm64 --build-arg HADOOP_PACKAGE_NAME=${HADOOP_PACKAGE_NAME} --build-arg HIVE_PACKAGE_NAME=${HIVE_PACKAGE_NAME} --output type=docker --progress plain -t ${IMAGE_NAME} .
