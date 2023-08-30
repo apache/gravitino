b1d57903c6a8f2555a24eef05dfe48001df1c28c
Xun Liu <xun@datastrato.com>
2023-08-30 18:08:41 +0800

    [#216][#217] test: add e2e integration test framework & metalake integration test (#235)
    ### What changes were proposed in this pull request?
    
    e2e integration test framework implementation below function.
    
    + Create a new integration module, you can execute each integration test
    code in it.
    + Each test set can start/stop a Graviton Server instance in the local
    or Github action environment.
    + You can through Graviton Client or HttpClient to connect the Graviton
    Server instance to test.
    
    #### Test operation flow
    
    1. Deploy integration test environment
        ```
        ./gradlew clean build
        ./gradlew clean compileDistribution 
    # The `compileDistribution` command will create
    `distribution/package/...` directory in the Graviton root directory.
        ```
    2. Execute test units in the `integration` module.
        + Auto start Graviton Server before the test.
        + Execute your test code at this time.
        + Auto stop Graviton Server after the test.
    
    3. Manual execution of tests units in GravitonServer debug mode
    + Enable `GRAVITON_DEBUG_OPTS` envronirment in the graviton-env.conf
    file.
        + Manual execution `./distribution/package/bin/graviton.sh start`
        + Remote debug Graviton Server in the 8000 port (default).
        + Set breakpoint in the Graviton project.
        + Manual execution integration tests unit.
    
    ### Why are the changes needed?
    
    e2e test provides a real deployment environment to execute integration
    tests.
    
    + It can validity test Graviton startup scripts, graviton.conf,
    graviton-env.sh, and more properties files.
    + It can validity test Catalogs JAR isolated.
    
    Fix: #216, #217 
    
    ### Does this PR introduce _any_ user-facing change?
    
    N/A
    
    ### How was this patch tested?
    
    Tested in the integration module

---
+++ integration-test/src/test/java/com/datastrato/graviton/integration/util/GravitonITUtils.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2023 Datastrato.
+ * This software is licensed under the Apache License version 2.
+ */
+package com.datastrato.graviton.integration.util;
+
+import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GravitonITUtils {
+  public static final Logger LOG = LoggerFactory.getLogger(GravitonITUtils.class);
+
+  public static void startGravitonServer() {
+    CommandExecutor.executeCommandLocalHost(
+        System.getenv("GRAVITON_HOME") + "/bin/graviton.sh start",
+        false,
+        ProcessData.TypesOfData.OUTPUT);
+    // wait for server to start.
+    sleep(100, false);
+  }
+
+  public static void stopGravitonServer() {
+    CommandExecutor.executeCommandLocalHost(
+        System.getenv("GRAVITON_HOME") + "/bin/graviton.sh stop",
+        false,
+        ProcessData.TypesOfData.OUTPUT);
+    // wait for server to stop.
+    sleep(100, false);
+  }
+
+  public static void sleep(long millis, boolean logOutput) {
+    if (logOutput) {
+      LOG.info("Starting sleeping for " + millis + " milliseconds ...");
+      LOG.info("Caller: " + Thread.currentThread().getStackTrace()[2]);
+    }
+    try {
+      Thread.sleep(millis);
+    } catch (InterruptedException e) {
+      LOG.error("Exception in sleep() ", e);
+    }
+    if (logOutput) {
+      LOG.info("Finished.");
+    }
+  }
+
+  public static String genRandomName() {
+    return UUID.randomUUID().toString().replace("-", "");
+  }
+}
