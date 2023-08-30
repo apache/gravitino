c70b5f761b8d97e775df2733988fbf1a9ef6c3c7
mchades <liminghuang@datastrato.com>
2023-08-30 15:31:17 +0800

    [#289]feat(api, core): Add a universal spec for partition semantics (#249)
    ### What changes were proposed in this pull request?
    
    Propose `Transform` interface to represent a table partitioning, more
    details refers to [Partitioning Design
    Doc](https://docs.google.com/document/d/1qgadcecPxwRQpHO_L0h6XPBqOiWduYYPupCuTadLN28/edit?usp=sharing)
    
    ### Why are the changes needed?
    - Base on the `Transform` interface, we can define partitions for table.
    - We can use `LogicalExpressions` to create a `Transform` in Graviton.
    
    Fix: #289 
    
    ### Does this PR introduce _any_ user-facing change?
    
    Not yet, user api will change in another PR.
    
    ### How was this patch tested?
    UTs added.

---
+++ core/src/main/java/com/datastrato/graviton/meta/rel/transforms/LiteralTransform.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2023 Datastrato.
+ * This software is licensed under the Apache License version 2.
+ */
+package com.datastrato.graviton.meta.rel.transforms;
+
+import io.substrait.expression.Expression;
+
+/**
+ * A LiteralTransform is a transform that references a {@link
+ * io.substrait.expression.Expression.Literal}.
+ */
+public abstract class LiteralTransform implements RefTransform<Expression.Literal> {
+
+  private static final String LITERAL_REFERENCE = "literal_reference";
+
+  @Override
+  public String name() {
+    return LITERAL_REFERENCE;
+  }
+}
---
+++ core/src/test/java/com/datastrato/graviton/meta/rel/TestTransforms.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2023 Datastrato.
+ * This software is licensed under the Apache License version 2.
+ */
+package com.datastrato.graviton.meta.rel;
+
+import static com.datastrato.graviton.meta.rel.transforms.Transforms.field;
+import static com.datastrato.graviton.meta.rel.transforms.Transforms.function;
+import static com.datastrato.graviton.meta.rel.transforms.Transforms.literal;
+
+import com.datastrato.graviton.meta.rel.transforms.FieldTransform;
+import com.datastrato.graviton.meta.rel.transforms.LiteralTransform;
+import com.datastrato.graviton.meta.rel.transforms.RefTransform;
+import com.datastrato.graviton.meta.rel.transforms.Transforms;
+import com.datastrato.graviton.rel.Column;
+import io.substrait.expression.ImmutableExpression;
+import io.substrait.type.Type;
+import io.substrait.type.TypeCreator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestTransforms {
+  @Test
+  public void testNamedReference() {
+    Column column =
+        new Column() {
+          @Override
+          public String name() {
+            return "col_1";
+          }
+
+          @Override
+          public Type dataType() {
+            return TypeCreator.NULLABLE.I8;
+          }
+
+          @Override
+          public String comment() {
+            return null;
+          }
+        };
+    Transforms.NamedReference expected =
+        new Transforms.NamedReference(new String[] {column.name()});
+
+    Assertions.assertEquals(expected, field(column));
+  }
+
+  @Test
+  public void testLiteralTransform() {
+    ImmutableExpression.I32Literal num = ImmutableExpression.I32Literal.builder().value(64).build();
+    Transforms.LiteralReference expected = new Transforms.LiteralReference(num);
+
+    Assertions.assertEquals(expected, literal(num));
+  }
+
+  @Test
+  public void testFunctionTransform() {
+    Column column =
+        new Column() {
+          @Override
+          public String name() {
+            return "col_1";
+          }
+
+          @Override
+          public Type dataType() {
+            return TypeCreator.NULLABLE.I8;
+          }
+
+          @Override
+          public String comment() {
+            return null;
+          }
+        };
+    FieldTransform arg1 = new Transforms.NamedReference(new String[] {column.name()});
+    LiteralTransform arg2 = literal(ImmutableExpression.StrLiteral.builder().value("bar").build());
+    Transforms.FunctionTrans expected =
+        new Transforms.FunctionTrans("foo", new RefTransform[] {arg1, arg2});
+
+    Assertions.assertEquals(expected, function("foo", new RefTransform[] {arg1, arg2}));
+  }
+}
