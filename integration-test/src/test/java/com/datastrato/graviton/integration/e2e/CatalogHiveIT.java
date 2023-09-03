b60f530cffaf677a0a86643bed81eaedcdc88a54
xunliu <xun@datastrato.com>
2023-09-03 23:50:16 +0800

    Java checked

--- integration-test/src/test/java/com/datastrato/graviton/integration/e2e/CatalogHiveIT.java
+++ integration-test/src/test/java/com/datastrato/graviton/integration/e2e/CatalogHiveIT.java
@@ -31,8 +31,6 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CatalogHiveIT extends AbstractIT {
