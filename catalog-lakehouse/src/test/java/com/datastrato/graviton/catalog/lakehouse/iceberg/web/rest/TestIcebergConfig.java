b959f05cc0de59e67fb75fd3421d45789329f1bc
fanng <xiaojing@datastrato.com>
2023-09-04 12:17:22 +0800

    [#314] feat(iceberg): support table interface

--- catalog-lakehouse/src/test/java/com/datastrato/graviton/catalog/lakehouse/iceberg/web/rest/TestIcebergConfig.java
+++ catalog-lakehouse/src/test/java/com/datastrato/graviton/catalog/lakehouse/iceberg/web/rest/TestIcebergConfig.java
@@ -5,30 +5,23 @@
 
 package com.datastrato.graviton.catalog.lakehouse.iceberg.web.rest;
 
-import com.datastrato.graviton.catalog.lakehouse.iceberg.web.IcebergObjectMapperProvider;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import org.apache.iceberg.rest.responses.ConfigResponse;
-import org.glassfish.jersey.test.JerseyTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class TestIcebergConfig extends JerseyTest {
+public class TestIcebergConfig extends IcebergTestBase {
 
   @Override
   protected Application configure() {
-    return IcebergRestTestUtil.getIcebergResourceConfig(IcebergConfig.class);
+    return IcebergRestTestUtil.getIcebergResourceConfig(IcebergConfig.class, false);
   }
 
   @Test
   public void testConfig() {
-    Response resp =
-        target(IcebergRestTestUtil.CONFIG_PATH)
-            .register(IcebergObjectMapperProvider.class)
-            .request(MediaType.APPLICATION_JSON_TYPE)
-            .accept(MediaType.APPLICATION_JSON_TYPE)
-            .get();
+    Response resp = getConfigClientBuilder().get();
     Assertions.assertEquals(Response.Status.OK.getStatusCode(), resp.getStatus());
     Assertions.assertEquals(MediaType.APPLICATION_JSON_TYPE, resp.getMediaType());
 
