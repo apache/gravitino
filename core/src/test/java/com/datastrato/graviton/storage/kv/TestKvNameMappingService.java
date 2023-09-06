71c090ae4ae37143aefb98100fb6da03025cf3c1
Qi Yu <yuqi@datastrato.com>
2023-09-06 22:54:58 +0800

    [#338] improvement(core): Fix some typo problem in kv package(minor change) (#339)
    ### What changes were proposed in this pull request?
    
    Fix some spelling errors.
     
    ### Why are the changes needed?
    
    To make code more tidy. 
    
    Fix: #338 
    
    ### Does this PR introduce _any_ user-facing change?
    
    N/A
    
    ### How was this patch tested?
    
    Do not need to test the changes

--- core/src/test/java/com/datastrato/graviton/storage/kv/TestKvNameMappingService.java
+++ core/src/test/java/com/datastrato/graviton/storage/kv/TestKvNameMappingService.java
@@ -5,7 +5,7 @@
 
 package com.datastrato.graviton.storage.kv;
 
-import static com.datastrato.graviton.Configs.DEFUALT_ENTITY_KV_STORE;
+import static com.datastrato.graviton.Configs.DEFAULT_ENTITY_KV_STORE;
 import static com.datastrato.graviton.Configs.ENTITY_KV_STORE;
 import static com.datastrato.graviton.Configs.ENTITY_STORE;
 import static com.datastrato.graviton.Configs.ENTRY_KV_ROCKSDB_BACKEND_PATH;
@@ -44,7 +44,7 @@ public class TestKvNameMappingService {
   private KvNameMappingService createNameMappingService(String kvPath) throws IOException {
     Config config = Mockito.mock(Config.class);
     Mockito.when(config.get(ENTITY_STORE)).thenReturn("kv");
-    Mockito.when(config.get(ENTITY_KV_STORE)).thenReturn(DEFUALT_ENTITY_KV_STORE);
+    Mockito.when(config.get(ENTITY_KV_STORE)).thenReturn(DEFAULT_ENTITY_KV_STORE);
     Mockito.when(config.get(Configs.ENTITY_SERDE)).thenReturn("proto");
     Mockito.when(config.get(ENTRY_KV_ROCKSDB_BACKEND_PATH)).thenReturn(kvPath);
 
