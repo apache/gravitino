fe0470392a6c99f04726aa6e48f1b6cf4d8cc0ee
yuqi <yuqi@datastrato.com>
2023-09-06 19:11:02 +0800

    Add comments

--- api/src/main/java/com/datastrato/graviton/rel/Bucket.java
+++ api/src/main/java/com/datastrato/graviton/rel/Bucket.java
@@ -7,10 +7,27 @@ package com.datastrato.graviton.rel;
 
 public interface Bucket {
 
+  /**
+   * Bucket transforms. These are the transforms that are used to create the bucket. Say we have a
+   * table with 3 columns: a, b, c. We want to bucket on a and b. Then the bucket transforms are [a,
+   * b].
+   *
+   * @return
+   */
   Transform[] transforms();
 
+  /**
+   * Number of buckets. Default is 0.
+   *
+   * @return
+   */
   int bucketNum();
 
+  /**
+   * Bucket method. Default is HASH.
+   *
+   * @return
+   */
   BucketMethod bucketMethod();
 
   enum BucketMethod {
--- api/src/main/java/com/datastrato/graviton/rel/SortOrder.java
+++ api/src/main/java/com/datastrato/graviton/rel/SortOrder.java
@@ -7,10 +7,18 @@ package com.datastrato.graviton.rel;
 
 // This interface may not need it
 public interface SortOrder {
+
+  /**
+   * Sort transform. This is the transform that is used to sort the data. Say we have a table with 3
+   * columns: a, b, c. We want to sort on a. Then the sort transform is a. Generally, the sort
+   * transform is a field transform.
+   */
   Transform transform();
 
+  /** Sort direction. Default is ASC. */
   Direction direction();
 
+  /** Sort null order. Default is FIRST. */
   NullOrder nullOrder();
 
   enum Direction {
