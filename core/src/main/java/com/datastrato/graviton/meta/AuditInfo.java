7f3d4756245283bc1420c43bd333874b1bea1a30
Jerry Shao <jerryshao@datastrato.com>
2023-09-06 20:42:43 +0800

    Fix the style issue

--- core/src/main/java/com/datastrato/graviton/meta/AuditInfo.java
+++ core/src/main/java/com/datastrato/graviton/meta/AuditInfo.java
@@ -125,8 +125,8 @@ public final class AuditInfo implements Audit, Entity {
   /**
    * Merges the audit information with another audit information. If the {@code overwrite} flag is
    * set to {@code true} or the field is null, the values from the other audit information will
-   * overwrite the values of this audit information, otherwise the values of this audit
-   * information will be preserved.
+   * overwrite the values of this audit information, otherwise the values of this audit information
+   * will be preserved.
    *
    * @param other the other audit information.
    * @param overwrite the overwrite flag.
