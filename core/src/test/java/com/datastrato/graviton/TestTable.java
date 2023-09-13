70c00a3d620925b0595c786d642ccbaacb825775
yuqi <yuqi@datastrato.com>
2023-09-13 22:41:28 +0800

    Remove some unused code

--- core/src/test/java/com/datastrato/graviton/TestTable.java
+++ core/src/test/java/com/datastrato/graviton/TestTable.java
@@ -4,14 +4,9 @@
  */
 package com.datastrato.graviton;
 
-import com.datastrato.graviton.meta.AuditInfo;
 import com.datastrato.graviton.meta.rel.BaseTable;
-import com.datastrato.graviton.rel.Column;
 import com.datastrato.graviton.rel.Distribution;
 import com.datastrato.graviton.rel.SortOrder;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode(callSuper = true)
@@ -51,75 +46,5 @@ public class TestTable extends BaseTable {
       table.partitions = partitions;
       return table;
     }
-  }
-
-  public TestTable(
-      String name,
-      Namespace namespace,
-      String comment,
-      Map<String, String> properties,
-      AuditInfo auditInfo,
-      Column[] columns,
-      Distribution distribution,
-      SortOrder[] sortOrders) {
-    this.name = name;
-    this.namespace = namespace;
-    this.comment = comment;
-    this.properties = properties;
-    this.auditInfo = auditInfo;
-    this.columns = columns;
-    this.sortOrders = sortOrders;
-    this.distribution = distribution;
-
-    validate();
-  }
-
-  // For Jackson Deserialization only.
-  public TestTable() {}
-
-  @Override
-  public Map<Field, Object> fields() {
-    Map<Field, Object> fields = new HashMap<>();
-    fields.put(NAME, name);
-    fields.put(COMMENT, comment);
-    fields.put(PROPERTIES, properties);
-    fields.put(AUDIT_INFO, auditInfo);
-
-    return Collections.unmodifiableMap(fields);
-  }
-
-  @Override
-  public String name() {
-    return name;
-  }
-
-  @Override
-  public Namespace namespace() {
-    return namespace;
-  }
-
-  @Override
-  public AuditInfo auditInfo() {
-    return auditInfo;
-  }
-
-  @Override
-  public Column[] columns() {
-    return columns;
-  }
-
-  @Override
-  public String comment() {
-    return comment;
-  }
-
-  @Override
-  public Map<String, String> properties() {
-    return properties;
-  }
-
-  @Override
-  public EntityType type() {
-    return EntityType.TABLE;
   }
 }
