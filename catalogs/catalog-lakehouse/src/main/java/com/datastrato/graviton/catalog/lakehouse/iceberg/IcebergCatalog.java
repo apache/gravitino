e02c253883d787edd81f193b2b64bd3b75e42202
Clearvive <143773256+Clearvive@users.noreply.github.com>
2023-09-25 07:34:02 -0500

    [#357] feat(catalog-lakehouse): Add Iceberg catalog operations support in Gravi… (#359)
    ### What changes were proposed in this pull request?
    
    to support iceberg CatalogOperations api, we add interfaces:
    create&drop&get&list schema
    
    
    ### Why are the changes needed?
    Because we need to support the catalog implementation of iceberg in
    Graveton
    
    
    ### Does this PR introduce _any_ user-facing change?
    
    This PR did not introduce user oriented changes
    
    ### How was this patch tested?
    
    1. Tested through unit testing
    2. Tested by launching GravitonServer locally
    
    ---------
    
    Co-authored-by: kiki <kiki@kikis-MBP.lan>

---
+++ catalogs/catalog-lakehouse/src/main/java/com/datastrato/graviton/catalog/lakehouse/iceberg/IcebergCatalog.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2023 Datastrato.
+ * This software is licensed under the Apache License version 2.
+ */
+package com.datastrato.graviton.catalog.lakehouse.iceberg;
+
+import com.datastrato.graviton.catalog.BaseCatalog;
+import com.datastrato.graviton.catalog.CatalogOperations;
+import com.datastrato.graviton.rel.SupportsSchemas;
+import com.datastrato.graviton.rel.TableCatalog;
+import java.util.Map;
+
+/** Implementation of an Iceberg catalog in Graviton. */
+public class IcebergCatalog extends BaseCatalog<IcebergCatalog> {
+
+  /** @return The short name of the catalog. */
+  @Override
+  public String shortName() {
+    return "iceberg";
+  }
+
+  /**
+   * Creates a new instance of {@link IcebergCatalogOperations} with the provided configuration.
+   *
+   * @param config The configuration map for the Iceberg catalog operations.
+   * @return A new instance of {@link IcebergCatalogOperations}.
+   */
+  @Override
+  protected CatalogOperations newOps(Map<String, String> config) {
+    IcebergCatalogOperations ops = new IcebergCatalogOperations(entity());
+    ops.initialize(config);
+    return ops;
+  }
+
+  /** @return The Iceberg catalog operations as {@link IcebergCatalogOperations}. */
+  @Override
+  public SupportsSchemas asSchemas() {
+    return (IcebergCatalogOperations) ops();
+  }
+
+  /** @return The Iceberg catalog operations as {@link IcebergCatalogOperations}. */
+  @Override
+  public TableCatalog asTableCatalog() {
+    return (IcebergCatalogOperations) ops();
+  }
+}
---
+++ catalogs/catalog-lakehouse/src/test/java/com/datastrato/graviton/catalog/lakehouse/iceberg/IcebergCatalogTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2023 Datastrato.
+ * This software is licensed under the Apache License version 2.
+ */
+package com.datastrato.graviton.catalog.lakehouse.iceberg;
+
+import com.datastrato.graviton.Namespace;
+import com.datastrato.graviton.catalog.CatalogOperations;
+import com.datastrato.graviton.catalog.lakehouse.iceberg.ops.IcebergTableOps;
+import com.datastrato.graviton.meta.AuditInfo;
+import com.datastrato.graviton.meta.CatalogEntity;
+import com.google.common.collect.Maps;
+import java.time.Instant;
+import java.util.Map;
+import org.apache.iceberg.rest.responses.ListNamespacesResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class IcebergCatalogTest {
+  @Test
+  public void testListDatabases() {
+    AuditInfo auditInfo =
+        new AuditInfo.Builder().withCreator("creator").withCreateTime(Instant.now()).build();
+
+    CatalogEntity entity =
+        new CatalogEntity.Builder()
+            .withId(1L)
+            .withName("catalog")
+            .withNamespace(Namespace.of("metalake"))
+            .withType(IcebergCatalog.Type.RELATIONAL)
+            .withProvider("iceberg")
+            .withAuditInfo(auditInfo)
+            .build();
+
+    Map<String, String> conf = Maps.newHashMap();
+    IcebergCatalog icebergCatalog =
+        new IcebergCatalog().withCatalogConf(conf).withCatalogEntity(entity);
+    CatalogOperations catalogOperations = icebergCatalog.newOps(Maps.newHashMap());
+    Assertions.assertTrue(catalogOperations instanceof IcebergCatalogOperations);
+
+    IcebergTableOps icebergTableOps =
+        ((IcebergCatalogOperations) catalogOperations).icebergTableOps;
+    ListNamespacesResponse listNamespacesResponse =
+        icebergTableOps.listNamespace(org.apache.iceberg.catalog.Namespace.empty());
+    Assertions.assertTrue(listNamespacesResponse.namespaces().isEmpty());
+  }
+}
