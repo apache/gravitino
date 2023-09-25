7c9101de0d679beb2735a46c478cee36e64fffbf
fanng <xiaojing@datastrato.com>
2023-09-25 18:02:07 +0800

    change IcebergRESTConfig from 0.1.0 to 0.2.0

--- catalogs/catalog-lakehouse/src/main/java/com/datastrato/graviton/catalog/lakehouse/iceberg/IcebergRESTService.java
+++ catalogs/catalog-lakehouse/src/main/java/com/datastrato/graviton/catalog/lakehouse/iceberg/IcebergRESTService.java
@@ -34,7 +34,7 @@ import org.slf4j.LoggerFactory;
 
 public class IcebergRESTService implements GravitonAuxiliaryService {
 
-  private Logger LOG = LoggerFactory.getLogger(IcebergRESTService.class);
+  private static Logger LOG = LoggerFactory.getLogger(IcebergRESTService.class);
 
   private Server server;
 
