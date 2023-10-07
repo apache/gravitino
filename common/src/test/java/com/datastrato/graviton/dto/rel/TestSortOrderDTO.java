2d08dd744f7e2e695826204814136f6341959cbf
Jerry Shao <jerryshao@datastrato.com>
2023-10-07 16:36:39 +0800

    [#467]refactor(common): Move `Config` related code to common module (#469)
    ### What changes were proposed in this pull request?
    
    This PR proposes to move `Config` related code to common module.
    
    ### Why are the changes needed?
    
    Current `Config` related code is in core module, but potentially it will
    be leveraged by client, trino-connector modules, within core module make
    it hard to use by such modules, so here propose to move to common
    module.
    
    Fix: #467 
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Existing UTs.

--- common/src/test/java/com/datastrato/graviton/dto/rel/TestSortOrderDTO.java
+++ common/src/test/java/com/datastrato/graviton/dto/rel/TestSortOrderDTO.java
@@ -3,19 +3,17 @@
  * This software is licensed under the Apache License version 2.
  */
 
-package com.datastrato.graviton.rel;
+package com.datastrato.graviton.dto.rel;
 
 import com.datastrato.graviton.dto.rel.ExpressionPartitionDTO.Expression;
 import com.datastrato.graviton.dto.rel.ExpressionPartitionDTO.FieldExpression;
 import com.datastrato.graviton.dto.rel.ExpressionPartitionDTO.FunctionExpression;
 import com.datastrato.graviton.dto.rel.ExpressionPartitionDTO.LiteralExpression;
-import com.datastrato.graviton.dto.rel.SortOrderDTO;
 import com.datastrato.graviton.dto.rel.SortOrderDTO.Direction;
 import com.datastrato.graviton.dto.rel.SortOrderDTO.NullOrdering;
 import com.datastrato.graviton.json.JsonUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import io.substrait.type.StringTypeVisitor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -43,7 +41,7 @@ public class TestSortOrderDTO {
   }
 
   @Test
-  void test() throws JsonProcessingException {
+  void testJsonSerDe() throws JsonProcessingException {
     SortOrderDTO dto =
         new SortOrderDTO.Builder()
             .withDirection(Direction.ASC)
@@ -51,7 +49,6 @@ public class TestSortOrderDTO {
             .withExpression(
                 new FieldExpression.Builder().withFieldName(new String[] {"field1"}).build())
             .build();
-    JsonUtils.objectMapper().enable(SerializationFeature.INDENT_OUTPUT);
     String value = JsonUtils.objectMapper().writeValueAsString(dto);
     String expectedValue =
         "{\n"
@@ -66,8 +63,8 @@ public class TestSortOrderDTO {
             + "}";
 
     JsonNode expected = JsonUtils.objectMapper().readTree(expectedValue);
-    JsonNode acutal = JsonUtils.objectMapper().readTree(value);
-    Assertions.assertEquals(expected, acutal);
+    JsonNode actual = JsonUtils.objectMapper().readTree(value);
+    Assertions.assertEquals(expected, actual);
 
     SortOrderDTO dto2 = JsonUtils.objectMapper().readValue(value, SortOrderDTO.class);
     Assertions.assertEquals(dto, dto2);
