3c567c0ddca68bf9bbe84ea67ba0fe8e8a1211f6
yuqi <yuqi@datastrato.com>
2023-10-12 22:44:54 +0800

    Make HIVE_CATALOG_PROPERTY_ENTRIES as static final field

--- catalogs/catalog-hive/src/main/java/com/datastrato/graviton/catalog/hive/HiveCatalogPropertiesMeta.java
+++ catalogs/catalog-hive/src/main/java/com/datastrato/graviton/catalog/hive/HiveCatalogPropertiesMeta.java
@@ -18,7 +18,28 @@ public class HiveCatalogPropertiesMeta extends BaseCatalogPropertiesMetadata {
   public static final String CATALOG_CLIENT_POOL_MAXSIZE = "hive.client.pool.max-size";
   public static final int DEFAULT_CATALOG_CLIENT_POOL_MAXSIZE = 1;
 
-  private volatile Map<String, PropertyEntry<?>> hiveCatalogPropertyEntries;
+  private static final Map<String, PropertyEntry<?>> HIVE_CATALOG_PROPERTY_ENTRIES =
+      ImmutableMap.<String, PropertyEntry<?>>builder()
+          .put(
+              CATALOG_BYPASS_PREFIX + HiveConf.ConfVars.METASTOREURIS.varname,
+              PropertyEntry.stringPropertyEntry(
+                  CATALOG_BYPASS_PREFIX + HiveConf.ConfVars.METASTOREURIS.varname,
+                  "The Hive metastore URIs",
+                  true,
+                  true,
+                  null,
+                  false,
+                  false))
+          .put(
+              CATALOG_CLIENT_POOL_MAXSIZE,
+              PropertyEntry.integerOptionalPropertyEntry(
+                  CATALOG_CLIENT_POOL_MAXSIZE,
+                  "The maximum number of Hive clients in the pool for graviton",
+                  true,
+                  DEFAULT_CATALOG_CLIENT_POOL_MAXSIZE,
+                  false))
+          .putAll(BASIC_CATALOG_PROPERTY_ENTRIES)
+          .build();
 
   @Override
   protected Map<String, PropertyEntry<?>> specificPropertyEntries() {
@@ -26,35 +47,6 @@ public class HiveCatalogPropertiesMeta extends BaseCatalogPropertiesMetadata {
     // referring to the trino catalog.
     // TODO(yuqi), we can add more properties like username for metastore
     //  (kerberos authentication) when we finish refactor properties framework.
-    if (hiveCatalogPropertyEntries == null) {
-      synchronized (this) {
-        if (hiveCatalogPropertyEntries == null) {
-          hiveCatalogPropertyEntries =
-              ImmutableMap.<String, PropertyEntry<?>>builder()
-                  .put(
-                      CATALOG_BYPASS_PREFIX + HiveConf.ConfVars.METASTOREURIS.varname,
-                      PropertyEntry.stringPropertyEntry(
-                          CATALOG_BYPASS_PREFIX + HiveConf.ConfVars.METASTOREURIS.varname,
-                          "The Hive metastore URIs",
-                          true,
-                          true,
-                          null,
-                          false,
-                          false))
-                  .put(
-                      CATALOG_CLIENT_POOL_MAXSIZE,
-                      PropertyEntry.integerOptionalPropertyEntry(
-                          CATALOG_CLIENT_POOL_MAXSIZE,
-                          "The maximum number of Hive clients in the pool for graviton",
-                          true,
-                          DEFAULT_CATALOG_CLIENT_POOL_MAXSIZE,
-                          false))
-                  .putAll(BASIC_CATALOG_PROPERTY_ENTRIES)
-                  .build();
-        }
-      }
-    }
-
-    return hiveCatalogPropertyEntries;
+    return HIVE_CATALOG_PROPERTY_ENTRIES;
   }
 }
