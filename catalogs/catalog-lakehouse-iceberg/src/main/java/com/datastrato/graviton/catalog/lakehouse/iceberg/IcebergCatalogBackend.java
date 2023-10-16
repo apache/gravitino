3ce6cf49e67797510448ebdf850d916544b3cca4
Wayne <143773256+yunqing-wei@users.noreply.github.com>
2023-10-16 08:05:51 -0500

    [#446] feat(iceberg): Support detailed iceberg catalog property metadata (#486)
    ### What changes were proposed in this pull request?
    Implement catalogPropertiesMetadata、tablePropertiesMetadata for
    IcebergCatalogOperations
    
    ### Why are the changes needed?
    Currently catalogPropertiesMetadatatablePropertiesMetadata will return
    an empty map, and we need to implement it to return a real value for the
    iceberg property metadata.
    
    Fix: #446 
    
    ### Does this PR introduce any user-facing change?
    N/A
    
    ### How was this patch tested?
    Add test testCatalogProperty in TestIcebergCatalog
    Add test testTableProperty in TestIcebergTable

---
+++ catalogs/catalog-lakehouse-iceberg/src/main/java/com/datastrato/graviton/catalog/lakehouse/iceberg/IcebergCatalogBackend.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2023 Datastrato.
+ * This software is licensed under the Apache License version 2.
+ */
+package com.datastrato.graviton.catalog.lakehouse.iceberg;
+
+public enum IcebergCatalogBackend {
+  HIVE,
+  JDBC,
+  MEMORY
+}
