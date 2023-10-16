570294ccdae756b75104fb83219d341eb202491c
Qi Yu <yuqi@datastrato.com>
2023-10-16 21:11:44 +0800

    [#515] improvement(core): Add UT to test restart for `RocksDBBackend` (#515)
    ### What changes were proposed in this pull request?
    
    Add UT to test restart and reopen for `RocksDBBackend`
    
    ### Why are the changes needed?
    
    Fully test `RocksDBBackend` to verify if it works well.
    
    Fix: #514 
    
    ### Does this PR introduce _any_ user-facing change?
    
    N/A
    
    ### How was this patch tested?
    
    UT

--- core/src/main/java/com/datastrato/graviton/storage/RandomIdGenerator.java
+++ core/src/main/java/com/datastrato/graviton/storage/RandomIdGenerator.java
@@ -8,18 +8,18 @@ package com.datastrato.graviton.storage;
 import java.util.UUID;
 
 /**
- * Random id generator. This is used to generate random id for entities. Please see {@link
+ * Random id generator. This is used to generate random ids for entities. Please see {@link
  * com.datastrato.graviton.meta.BaseMetalake#ID} for more details.
  */
 public class RandomIdGenerator implements IdGenerator {
 
-  public static final RandomIdGenerator INSTACNE = new RandomIdGenerator();
+  public static final RandomIdGenerator INSTANCE = new RandomIdGenerator();
 
   public static final long MAX_ID = 0x7fffffffffffffffL;
 
   @Override
   public long nextId() {
-    // Make sure this is a postive number.
+    // Make sure this is a positive number.
     return UUID.randomUUID().getLeastSignificantBits() & MAX_ID;
   }
 }
