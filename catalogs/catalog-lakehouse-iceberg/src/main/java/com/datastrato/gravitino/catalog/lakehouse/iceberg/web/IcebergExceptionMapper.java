2ec153b5dd888e7ebd5712e37bf9cefec1b28a17
fanng <xiaojing@datastrato.com>
2023-11-10 15:34:05 +0800

    fix iceberg copyright

--- catalogs/catalog-lakehouse-iceberg/src/main/java/com/datastrato/gravitino/catalog/lakehouse/iceberg/web/IcebergExceptionMapper.java
+++ catalogs/catalog-lakehouse-iceberg/src/main/java/com/datastrato/gravitino/catalog/lakehouse/iceberg/web/IcebergExceptionMapper.java
@@ -1,6 +1,20 @@
 /*
- * Copyright 2023 Datastrato.
- * This software is licensed under the Apache License version 2.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package com.datastrato.gravitino.catalog.lakehouse.iceberg.web;
@@ -26,6 +40,8 @@ import org.apache.iceberg.exceptions.ValidationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// Referred from Apache Iceberg's EXCEPTION_ERROR_CODES implementation
+// core/src/test/java/org/apache/iceberg/rest/RESTCatalogAdapter
 @Provider
 public class IcebergExceptionMapper implements ExceptionMapper<Exception> {
 
