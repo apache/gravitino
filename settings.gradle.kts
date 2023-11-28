5f20f470d0d2f02dd20865514e78f266f398c66b
Jerry Shao <jerryshao@datastrato.com>
2023-11-28 23:07:20 +0800

    test

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,10 @@
  * Copyright 2023 Datastrato.
  * This software is licensed under the Apache License version 2.
  */
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention") version("0.7.0")
+}
+
 rootProject.name = "gravitino"
 
 include("api", "common", "core", "meta", "server", "integration-test", "server-common")
