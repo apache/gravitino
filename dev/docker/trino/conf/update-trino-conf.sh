71db364ba5fb03d5465e1581bb60a6440a26089e
xunliu <xun@datastrato.com>
2023-12-01 09:13:56 +0800

    fix

---
+++ dev/docker/trino/conf/update-trino-conf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Copyright 2023 Datastrato.
+# This software is licensed under the Apache License version 2.
+#
+# This script runs in the Trino docker container and updates the Trino configuration files
+set -ex
+trino_conf_dir="$(dirname "${BASH_SOURCE-$0}")"
+trino_conf_dir="$(cd "${trino_conf_dir}">/dev/null; pwd)"
+
+# Update `gravitino.uri = http://GRAVITINO_HOST_IP:GRAVITINO_HOST_PORT` in the `conf/catalog/gravitino.properties`
+sed "s/GRAVITINO_HOST_IP:GRAVITINO_HOST_PORT/${GRAVITINO_HOST_IP}:${GRAVITINO_HOST_PORT}/g" "${trino_conf_dir}/catalog/gravitino.properties.template" > "${trino_conf_dir}/catalog/gravitino.properties.1"
+# Update `gravitino.metalake = GRAVITINO_METALAKE_NAME` in the `conf/catalog/gravitino.properties`
+sed "s/GRAVITINO_METALAKE_NAME/${GRAVITINO_METALAKE_NAME}/g" "${trino_conf_dir}/catalog/gravitino.properties.1" > "${trino_conf_dir}/catalog/gravitino.properties"
+rm "${trino_conf_dir}/catalog/gravitino.properties.1"
--- dev/docker/trino/trino-dependency.sh
+++ dev/docker/trino/trino-dependency.sh
@@ -9,26 +9,13 @@ trino_dir="$(dirname "${BASH_SOURCE-$0}")"
 trino_dir="$(cd "${trino_dir}">/dev/null; pwd)"
 gravitino_home="$(cd "${trino_dir}/../../..">/dev/null; pwd)"
 
-if [[ ! -d "${trino_dir}/packages/trino" ]]; then
-  mkdir -p "${trino_dir}/packages/trino"
-fi
+# Clean packages
+rm -rf "${trino_dir}/packages"
+mkdir -p "${trino_dir}/packages"
 
 cd ${gravitino_home}
 ${gravitino_home}/gradlew clean assembleTrinoConnector -x test
-rm -rf "${trino_dir}/packages/gravitino-trino-connector"
 cp -r "${gravitino_home}/distribution/gravitino-trino-connector" "${trino_dir}/packages/gravitino-trino-connector"
 
-rm -rf "${trino_dir}/packages/trino/conf"
-cp -r -p "${gravitino_home}/dev/docker/trino/conf" "${trino_dir}/packages/trino/conf"
-
-# Set default gravitino metelake `gravitino.metalake = metalake_demo` in the `conf/catalog/gravitino.properties`
-# gravitino.uri = http://GRAVITINO_HOST_IP:GRAVITINO_HOST_PORT
-sed 's/GRAVITINO_HOST_IP:GRAVITINO_HOST_PORT/gravitino:8090/g' "${trino_dir}/packages/trino/conf/catalog/gravitino.properties.template" > "${trino_dir}/packages/trino/conf/catalog/gravitino.properties.1"
-# Set default hive docker container hostname and port `hive.metastore.uri = thrift://hive:9083` in the `trino/conf/catalog/hive.properties`
-# gravitino.metalake = GRAVITINO_METALAKE_NAME
-sed 's/GRAVITINO_METALAKE_NAME/metalake_demo/g' "${trino_dir}/packages/trino/conf/catalog/gravitino.properties.1" > "${trino_dir}/packages/trino/conf/catalog/gravitino.properties"
-rm "${trino_dir}/packages/trino/conf/catalog/gravitino.properties.1"
-
-# Set default hive docker container hostname and port `hive.metastore.uri = thrift://hive:9083` in the `trino/conf/catalog/hive.properties`
-# hive.metastore.uri = thrift://HIVE_HOST_IP:9083
-sed 's/HIVE_HOST_IP/hive/g' "${trino_dir}/packages/trino/conf/catalog/hive.properties.template" > "${trino_dir}/packages/trino/conf/catalog/hive.properties"
+mkdir -p "${trino_dir}/packages/trino"
+cp -r -p "${trino_dir}/conf" "${trino_dir}/packages/trino/conf"
