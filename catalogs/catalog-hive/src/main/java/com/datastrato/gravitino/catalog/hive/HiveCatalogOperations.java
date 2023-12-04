42c24ccb2dc09884fd560322fc59a000e4a00c6d
mchades <liminghuang@datastrato.com>
2023-12-04 17:17:59 +0800

    Unify parameter name of fieldName and fieldNames

--- catalogs/catalog-hive/src/main/java/com/datastrato/gravitino/catalog/hive/HiveCatalogOperations.java
+++ catalogs/catalog-hive/src/main/java/com/datastrato/gravitino/catalog/hive/HiveCatalogOperations.java
@@ -469,7 +469,7 @@ public class HiveCatalogOperations implements CatalogOperations, SupportsSchemas
         .filter(c -> c instanceof TableChange.ColumnChange)
         .forEach(
             c -> {
-              String fieldToAdd = String.join(".", ((TableChange.ColumnChange) c).fieldNames());
+              String fieldToAdd = String.join(".", ((TableChange.ColumnChange) c).fieldName());
               Preconditions.checkArgument(
                   c instanceof TableChange.UpdateColumnComment
                       || !partitionFields.contains(fieldToAdd),
@@ -658,7 +658,7 @@ public class HiveCatalogOperations implements CatalogOperations, SupportsSchemas
 
           if (change instanceof TableChange.AddColumn) {
             TableChange.AddColumn addColumn = (TableChange.AddColumn) change;
-            validateNullable(String.join(".", addColumn.fieldNames()), addColumn.isNullable());
+            validateNullable(String.join(".", addColumn.fieldName()), addColumn.isNullable());
             doAddColumn(cols, addColumn);
 
           } else if (change instanceof TableChange.DeleteColumn) {
@@ -787,27 +787,27 @@ public class HiveCatalogOperations implements CatalogOperations, SupportsSchemas
       targetPosition = cols.size();
       LOG.info(
           "Add position is null, add column {} to the end of non-partition columns",
-          change.fieldNames()[0]);
+          change.fieldName()[0]);
     } else {
       targetPosition = columnPosition(cols, change.getPosition());
     }
     cols.add(
         targetPosition,
         new FieldSchema(
-            change.fieldNames()[0],
+            change.fieldName()[0],
             ToHiveType.convert(change.getDataType()).getQualifiedName(),
             change.getComment()));
   }
 
   private void doDeleteColumn(List<FieldSchema> cols, TableChange.DeleteColumn change) {
-    String columnName = change.fieldNames()[0];
+    String columnName = change.fieldName()[0];
     if (!cols.removeIf(c -> c.getName().equals(columnName)) && !change.getIfExists()) {
       throw new IllegalArgumentException("DeleteColumn does not exist: " + columnName);
     }
   }
 
   private void doRenameColumn(List<FieldSchema> cols, TableChange.RenameColumn change) {
-    String columnName = change.fieldNames()[0];
+    String columnName = change.fieldName()[0];
     if (indexOfColumn(cols, columnName) == -1) {
       throw new IllegalArgumentException("RenameColumn does not exist: " + columnName);
     }
@@ -821,12 +821,12 @@ public class HiveCatalogOperations implements CatalogOperations, SupportsSchemas
 
   private void doUpdateColumnComment(
       List<FieldSchema> cols, TableChange.UpdateColumnComment change) {
-    cols.get(indexOfColumn(cols, change.fieldNames()[0])).setComment(change.getNewComment());
+    cols.get(indexOfColumn(cols, change.fieldName()[0])).setComment(change.getNewComment());
   }
 
   private void doUpdateColumnPosition(
       List<FieldSchema> cols, TableChange.UpdateColumnPosition change) {
-    String columnName = change.fieldNames()[0];
+    String columnName = change.fieldName()[0];
     int sourceIndex = indexOfColumn(cols, columnName);
     if (sourceIndex == -1) {
       throw new IllegalArgumentException("UpdateColumnPosition does not exist: " + columnName);
@@ -838,7 +838,7 @@ public class HiveCatalogOperations implements CatalogOperations, SupportsSchemas
   }
 
   private void doUpdateColumnType(List<FieldSchema> cols, TableChange.UpdateColumnType change) {
-    String columnName = change.fieldNames()[0];
+    String columnName = change.fieldName()[0];
     int indexOfColumn = indexOfColumn(cols, columnName);
     if (indexOfColumn == -1) {
       throw new IllegalArgumentException("UpdateColumnType does not exist: " + columnName);
