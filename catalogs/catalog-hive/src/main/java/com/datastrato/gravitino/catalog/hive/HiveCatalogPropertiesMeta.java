5f36f92fe36c4074f8edd08ed90e07d195549411
yuqi <yuqi@datastrato.com>
2023-12-05 23:01:13 +0800

    Add some test

--- catalogs/catalog-hive/src/main/java/com/datastrato/gravitino/catalog/hive/HiveCatalogPropertiesMeta.java
+++ catalogs/catalog-hive/src/main/java/com/datastrato/gravitino/catalog/hive/HiveCatalogPropertiesMeta.java
@@ -7,7 +7,6 @@ package com.datastrato.gravitino.catalog.hive;
 
 import com.datastrato.gravitino.catalog.BaseCatalogPropertiesMetadata;
 import com.datastrato.gravitino.catalog.PropertyEntry;
-import com.datastrato.gravitino.catalog.hive.HiveTablePropertiesMetadata.StorageFormat;
 import com.google.common.collect.ImmutableMap;
 import java.util.Map;
 
@@ -17,21 +16,6 @@ public class HiveCatalogPropertiesMeta extends BaseCatalogPropertiesMetadata {
   public static final int DEFAULT_CLIENT_POOL_SIZE = 1;
 
   public static final String METASTORE_URIS = "metastore.uris";
-
-  // Add these two general configurations for hive catalog.
-  public static final String STORAGE_FORMAT = "hive.storage-format";
-  public static final String COMPRESSION_CODEC = "hive.compression-codec";
-
-  enum CompressionCodeC {
-    NONE,
-    SNAPPY,
-    GZIP,
-    LZO,
-    LZ4,
-    ZSTD,
-    BZIP2,
-    ZLIB
-  }
 
   private static final Map<String, PropertyEntry<?>> HIVE_CATALOG_PROPERTY_ENTRIES =
       ImmutableMap.<String, PropertyEntry<?>>builder()
@@ -46,26 +30,6 @@ public class HiveCatalogPropertiesMeta extends BaseCatalogPropertiesMetadata {
                   "The maximum number of Hive metastore clients in the pool for Gravitino",
                   true,
                   DEFAULT_CLIENT_POOL_SIZE,
-                  false))
-          .put(
-              STORAGE_FORMAT,
-              PropertyEntry.enumImmutablePropertyEntry(
-                  STORAGE_FORMAT,
-                  "The default storage format for Hive tables",
-                  false,
-                  StorageFormat.class,
-                  StorageFormat.ORC,
-                  false,
-                  false))
-          .put(
-              COMPRESSION_CODEC,
-              PropertyEntry.enumImmutablePropertyEntry(
-                  COMPRESSION_CODEC,
-                  "The compression codec to use when writing files.",
-                  false,
-                  CompressionCodeC.class,
-                  CompressionCodeC.GZIP,
-                  false,
                   false))
           .putAll(BASIC_CATALOG_PROPERTY_ENTRIES)
           .build();
