7ebf0ca1fdeeac741c3cafb2416bb6d407f983f2
yuqi <yuqi@datastrato.com>
2023-12-18 11:40:51 +0800

    Fix

--- core/src/main/java/com/datastrato/gravitino/storage/kv/KvGarbageCollector.java
+++ core/src/main/java/com/datastrato/gravitino/storage/kv/KvGarbageCollector.java
@@ -188,7 +188,6 @@ public final class KvGarbageCollector implements Closeable {
 
       // All keys in this transaction have been deleted, we can remove the commit mark.
       if (keysDeletedCount == keysInTheTransaction.size()) {
-        LOG.info("Physically delete commit mark: {}", Bytes.wrap(kv.getKey()));
         kvBackend.delete(kv.getKey());
       }
     }
