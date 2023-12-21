dd6a9c3b32fa14c97c72caaed8724ca59dd61182
Heng Qin <qqtt@123.com>
2023-12-21 16:43:33 +0800

    fix

---
+++ common/src/main/java/com/datastrato/gravitino/config/ConfigConstants.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2023 Datastrato Pvt Ltd.
+ * This software is licensed under the Apache License version 2.
+ */
+
+package com.datastrato.gravitino.config;
+
+public interface ConfigConstants {
+  String NOT_BLANK_ERROR_MSG = "The value can't be the blank";
+}
