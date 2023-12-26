169f6a27140df752b87a64c53b55e6f7fb1ada7d
Clearvive <143773256+Clearvive@users.noreply.github.com>
2023-12-26 19:26:30 +0800

    [#1253] bugfix(jdbc): fix jdbc catalog store empty audit info issue (#1257)
    ### What changes were proposed in this pull request?
    
    Mysql&Postgres catalog store empty audit info fix.
    
    ### Why are the changes needed?
    Fix: #1253 
    
    ### Does this PR introduce _any_ user-facing change?
    Users will receive audit information
    
    ### How was this patch tested?
    IT
    
    ---------
    
    Co-authored-by: Clearvive <clearvive@datastrato.com>

--- catalogs/catalog-jdbc-common/src/main/java/com/datastrato/gravitino/catalog/jdbc/JdbcCatalogOperations.java
+++ catalogs/catalog-jdbc-common/src/main/java/com/datastrato/gravitino/catalog/jdbc/JdbcCatalogOperations.java
@@ -203,7 +203,7 @@ public class JdbcCatalogOperations implements CatalogOperations, SupportsSchemas
     }
     Map<String, String> properties =
         load.properties() == null ? Maps.newHashMap() : Maps.newHashMap(load.properties());
-    StringIdentifier.addToProperties(id, properties);
+    StringIdentifier.newPropertiesWithId(id, properties);
     return new JdbcSchema.Builder()
         .withAuditInfo(load.auditInfo())
         .withName(load.name())
@@ -276,7 +276,7 @@ public class JdbcCatalogOperations implements CatalogOperations, SupportsSchemas
     }
     Map<String, String> properties =
         load.properties() == null ? Maps.newHashMap() : Maps.newHashMap(load.properties());
-    StringIdentifier.addToProperties(id, properties);
+    properties = StringIdentifier.newPropertiesWithId(id, properties);
     return new JdbcTable.Builder()
         .withAuditInfo(load.auditInfo())
         .withName(tableName)
