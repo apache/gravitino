90751b681c1a68f01d5d9f1c285b7cb65833d47f
yuqi <yuqi@datastrato.com>
2024-01-04 10:26:09 +0800

    Fix format problem

--- catalogs/catalog-jdbc-mysql/src/main/java/com/datastrato/gravitino/catalog/mysql/operation/MysqlTableOperations.java
+++ catalogs/catalog-jdbc-mysql/src/main/java/com/datastrato/gravitino/catalog/mysql/operation/MysqlTableOperations.java
@@ -227,7 +227,12 @@ public class MysqlTableOperations extends JdbcTableOperations {
     // Add columns
     for (int i = 0; i < columns.length; i++) {
       JdbcColumn column = columns[i];
-      sqlBuilder.append(SPACE).append(SPACE).append(BACK_QUOTE).append(column.name()).append(BACK_QUOTE);
+      sqlBuilder
+          .append(SPACE)
+          .append(SPACE)
+          .append(BACK_QUOTE)
+          .append(column.name())
+          .append(BACK_QUOTE);
 
       appendColumnDefinition(column, sqlBuilder);
       // Add a comma for the next column, unless it's the last one
