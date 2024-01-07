fa2c98fc6966ed005a064117e29a07dc355b1177
724thomas <724thomas@gmail.com>
2024-01-07 17:50:53 +0900

    Refactor : Added Java doc comments in MetalakeChange.java.
    Added Java doc comments for get, equals, hashcode, tostring methods.

--- api/src/main/java/com/datastrato/gravitino/MetalakeChange.java
+++ api/src/main/java/com/datastrato/gravitino/MetalakeChange.java
@@ -58,13 +58,55 @@ public interface MetalakeChange {
   }
 
   /** A metalake change to rename the metalake. */
-  @Getter
-  @EqualsAndHashCode
   final class RenameMetalake implements MetalakeChange {
     private final String newName;
 
     private RenameMetalake(String newName) {
       this.newName = newName;
+    }
+
+    /**
+     * Retrieves the new name intended for the Metalake.
+     *
+     * @return The new name to be set for the Metalake.
+     */
+    public String getNewName() {
+      return newName;
+    }
+
+    /**
+     * Compares this RenameMetalake instance with another object for equality.
+     *
+     * @param o The object to compare with this instance.
+     * @return true if the given object represents an identical renaming operation; false otherwise.
+     */
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      RenameMetalake that = (RenameMetalake) o;
+      return Objects.equals(newName, that.newName);
+    }
+
+    /**
+     * Generates a hash code for this RenameMetalake instance.
+     *
+     * @return A hash code representing this renaming operation.
+     */
+    @Override
+    public int hashCode() {
+      return Objects.hash(newName);
+    }
+
+    /**
+     * Provides a string representation of the RenameMetalake instance.
+     * This string includes the class name and the new name set for the Metalake
+     *
+     * @return A string summary of this renaming operation.
+     */
+    @Override
+    public String toString() {
+      return "RENAMEMETALAKE " + newName;
     }
   }
 
@@ -76,10 +118,21 @@ public interface MetalakeChange {
       this.newComment = newComment;
     }
 
+    /**
+     * Retrieves the new comment intended for the Metalake.
+     *
+     * @return The new comment.
+     */
     public String getNewComment() {
       return newComment;
     }
 
+    /**
+     * Compares this UpdateMetalakeComment instance with another object for equality.
+     *
+     * @param o The object to compare with this instance.
+     * @return true if the given object is an UpdateMetalakeComment with the same new comment; false otherwise.
+     */
     @Override
     public boolean equals(Object o) {
       if (this == o) return true;
@@ -88,11 +141,22 @@ public interface MetalakeChange {
       return Objects.equals(newComment, that.newComment);
     }
 
+    /**
+     * Generates a hash code for this UpdateMetalakeComment instance.
+     *
+     * @return A hash code representing this updating operation.
+     */
     @Override
     public int hashCode() {
       return Objects.hash(newComment);
     }
 
+    /**
+     * Provides a string representation of the UpdateMetalakeComment instance.
+     * This string format includes the class name followed by the new comment.
+     *
+     * @return A string summary of the object.
+     */
     @Override
     public String toString() {
       return "UPDATEMETALAKECOMMENT " + newComment;
@@ -109,14 +173,31 @@ public interface MetalakeChange {
       this.value = value;
     }
 
+    /**
+     * Retrieves the name of the property.
+     *
+     * @return The name of the property set for the Metalake.
+     */
     public String getProperty() {
       return property;
     }
 
+    /**
+     * Retrieves the value assigned to the property.
+     *
+     * @return The value of the property set for the Metalake.
+     */
     public String getValue() {
       return value;
     }
 
+    /**
+     * Compares this SetProperty instance with another object for equality.
+     * Two instances are considered equal if they have the same property name and value.
+     *
+     * @param o The object to compare with this instance.
+     * @return true if the given object represents the same property setting; false otherwise.
+     */
     @Override
     public boolean equals(Object o) {
       if (this == o) return true;
@@ -126,11 +207,23 @@ public interface MetalakeChange {
               Objects.equals(value, that.value);
     }
 
+    /**
+     * Generates a hash code for this SetProperty instance.
+     * This hash code is based on both the property name and value.
+     *
+     * @return A hash code value for this property setting.
+     */
     @Override
     public int hashCode() {
       return Objects.hash(property, value);
     }
 
+    /**
+     * Provides a string representation of the SetProperty instance.
+     * This string format includes the class name followed by the property name and its value.
+     *
+     * @return A string summary of the property setting.
+     */
     @Override
     public String toString() {
       return "SETPROPERTY " + property + " " + value;
@@ -145,10 +238,22 @@ public interface MetalakeChange {
       this.property = property;
     }
 
+    /**
+     * Retrieves the name of the property to be removed.
+     *
+     * @return The name of the property for removal.
+     */
     public String getProperty() {
       return property;
     }
 
+    /**
+     * Compares this RemoveProperty instance with another object for equality.
+     * Two instances are considered equal if they target the same property for removal.
+     *
+     * @param o The object to compare with this instance.
+     * @return true if the given object represents the same property removal; false otherwise.
+     */
     @Override
     public boolean equals(Object o) {
       if (this == o) return true;
@@ -157,11 +262,23 @@ public interface MetalakeChange {
       return Objects.equals(property, that.property);
     }
 
+    /**
+     * Generates a hash code for this RemoveProperty instance.
+     * This hash code is based on the property name that is to be removed.
+     *
+     * @return A hash code value for this property removal operation.
+     */
     @Override
     public int hashCode() {
       return Objects.hash(property);
     }
 
+    /**
+     * Provides a string representation of the RemoveProperty instance.
+     * This string format includes the class name followed by the property name to be removed.
+     *
+     * @return A string summary of the property removal operation.
+     */
     @Override
     public String toString() {
       return "REMOVEPROPERTY " + property;
