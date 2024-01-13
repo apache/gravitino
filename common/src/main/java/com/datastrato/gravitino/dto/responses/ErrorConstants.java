193ae656eb6149a57298d8c6e5d25cd94879a5b0
lipeidian <lipeidian@qq.com>
2024-01-13 13:56:59 +0800

    fix error code

--- common/src/main/java/com/datastrato/gravitino/dto/responses/ErrorConstants.java
+++ common/src/main/java/com/datastrato/gravitino/dto/responses/ErrorConstants.java
@@ -12,6 +12,6 @@ public class ErrorConstants {
   public static final int NOT_FOUND_CODE = 1003;
   public static final int ALREADY_EXISTS_CODE = 1004;
   public static final int NON_EMPTY_CODE = 1005;
+  public static final int UNSUPPORTED_OPERATION_CODE = 1006;
   public static final int UNKNOWN_ERROR_CODE = 1100;
-  public static final int UNSUPPORTED_OPERATION_CODE = 1101;
 }
