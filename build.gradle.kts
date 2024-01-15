04c87b86c714ef3cdd33d909379b0e488989c297
Qi Yu <yuqi@datastrato.com>
2024-01-15 19:55:40 +0800

    [#1449] feat(catalogs): Introudce new module `bundled-catalog` for query engine. (#1454)
    ### What changes were proposed in this pull request?
    
    - Add a new module `bundled-catalog` to hold common information like
    property meta of all catalogs
    - Put `bundled-catalog` to `trino-connector` dependency and verify
    everything is OK.
    
    ### Why are the changes needed?
    
    We need a common module to hold the property meta of all catalogs so
    that the query engine depends on it can use it for query.
    
    Fix: #1449 
    
    ### Does this PR introduce _any_ user-facing change?
    
    N/A
    
    ### How was this patch tested?
    
    Add some UT in module `Trino-connector`

--- build.gradle.kts
+++ build.gradle.kts
@@ -453,7 +453,7 @@ tasks {
     subprojects.forEach() {
       if (!it.name.startsWith("catalog") &&
         !it.name.startsWith("client") && it.name != "trino-connector" &&
-        it.name != "integration-test"
+        it.name != "integration-test" && it.name != "bundled-catalog"
       ) {
         from(it.configurations.runtimeClasspath)
         into("distribution/package/libs")
@@ -466,7 +466,8 @@ tasks {
       if (!it.name.startsWith("catalog") &&
         !it.name.startsWith("client") &&
         it.name != "trino-connector" &&
-        it.name != "integration-test"
+        it.name != "integration-test" &&
+        it.name != "bundled-catalog"
       ) {
         dependsOn("${it.name}:build")
         from("${it.name}/build/libs")
--- trino-connector/src/main/java/com/datastrato/gravitino/trino/connector/catalog/hive/HiveTablePropertyConverter.java
+++ trino-connector/src/main/java/com/datastrato/gravitino/trino/connector/catalog/hive/HiveTablePropertyConverter.java
@@ -6,13 +6,15 @@ package com.datastrato.gravitino.trino.connector.catalog.hive;
 
 import com.datastrato.gravitino.shaded.org.apache.commons.collections4.bidimap.TreeBidiMap;
 import com.datastrato.gravitino.trino.connector.catalog.PropertyConverter;
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableMap;
 
 public class HiveTablePropertyConverter extends PropertyConverter {
 
   // Trino property key does not allow upper case character and '-', so we need to map it to
   // Gravitino
-  private static final TreeBidiMap<String, String> TRINO_KEY_TO_GRAVITINO_KEY =
+  @VisibleForTesting
+  static final TreeBidiMap<String, String> TRINO_KEY_TO_GRAVITINO_KEY =
       new TreeBidiMap<>(
           new ImmutableMap.Builder<String, String>()
               .put("format", "format")
