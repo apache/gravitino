1e0847e05fe92e4131384471f69aa0e73dd0f20e
CHEYNE <contact@ch3yne.com>
2024-01-17 15:17:39 +0800

    add display for object type

--- web/components/ColumnTypeChip.js
+++ web/components/ColumnTypeChip.js
@@ -7,15 +7,20 @@ import { Chip } from '@mui/material'
 import { ColumnTypeColorEnum } from '@/lib/enums/columnTypeEnum'
 import colors from '@/lib/theme/colors'
 import { alpha } from '@/lib/utils/color'
+import { isString } from 'lodash-es'
 
 const ColumnTypeChip = props => {
   const { type } = props
+
+  const label = isString(type) ? type : `${type.type}<${type.elementType ?? 'unknown'}>` ?? 'unknown'
 
   const columnTypeColor = ColumnTypeColorEnum[type] || 'secondary'
   const color = colors[columnTypeColor]?.main || '#8592A3'
   const bgColor = alpha(color, 0.1)
 
-  return <Chip size='small' label={type} sx={{ backgroundColor: bgColor }} color={columnTypeColor} variant='outlined' />
+  return (
+    <Chip size='small' label={label} sx={{ backgroundColor: bgColor }} color={columnTypeColor} variant='outlined' />
+  )
 }
 
 export default ColumnTypeChip
