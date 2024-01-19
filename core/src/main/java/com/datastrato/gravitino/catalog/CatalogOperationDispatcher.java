d6523c351cbaa46a5b3b4e28211130aedcaa614d
Jerry Shao <jerryshao@datastrato.com>
2024-01-19 16:22:45 +0800

    Fix style issue

--- core/src/main/java/com/datastrato/gravitino/catalog/CatalogOperationDispatcher.java
+++ core/src/main/java/com/datastrato/gravitino/catalog/CatalogOperationDispatcher.java
@@ -48,7 +48,6 @@ import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
