89ea97ce9484abc1940c620384eb0889d5587d6f
CHEYNE <contact@ch3yne.com>
2024-01-19 11:38:01 +0800

    remove unsed lines

--- web/next.config.js
+++ web/next.config.js
@@ -19,15 +19,6 @@ const nextConfig = {
     ? {}
     : {
         // ** Just for development
-        // async redirects() {
-        //   return [
-        //     {
-        //       source: '/',
-        //       destination: `/ui`,
-        //       permanent: true
-        //     }
-        //   ]
-        // },
         async rewrites() {
           return {
             fallback: [
