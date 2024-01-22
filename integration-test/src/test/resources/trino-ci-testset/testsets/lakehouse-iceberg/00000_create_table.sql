480870238153da4738e9e27c2e19477b23f6aa70
yuqi <yuqi@datastrato.com>
2024-01-22 21:21:49 +0800

    Fixed

--- integration-test/src/test/resources/trino-ci-testset/testsets/lakehouse-iceberg/00000_create_table.sql
+++ integration-test/src/test/resources/trino-ci-testset/testsets/lakehouse-iceberg/00000_create_table.sql
@@ -3,7 +3,12 @@ CREATE SCHEMA "test.gt_iceberg".gt_db2;
 CREATE TABLE "test.gt_iceberg".gt_db2.tb01 (
     name varchar,
     salary int
+) with (
+    partitioning = ARRAY['name'],
+    sorted_by = ARRAY['salary']
 );
+
+show create table "test.gt_iceberg".gt_db2.tb01;
 
 drop table "test.gt_iceberg".gt_db2.tb01;
 
