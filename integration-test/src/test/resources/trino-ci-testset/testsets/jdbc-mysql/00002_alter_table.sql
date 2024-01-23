c4b00c69ed0109a583aa0d424ff11c3e2de981c8
yuqi <yuqi@datastrato.com>
2024-01-23 10:47:48 +0800

    Optimize test cases

--- integration-test/src/test/resources/trino-ci-testset/testsets/jdbc-mysql/00002_alter_table.sql
+++ integration-test/src/test/resources/trino-ci-testset/testsets/jdbc-mysql/00002_alter_table.sql
@@ -18,10 +18,13 @@ show create table "test.gt_mysql".gt_db1.tb01;
 alter table "test.gt_mysql".gt_db1.tb01 alter column salary set data type bigint;
 show create table "test.gt_mysql".gt_db1.tb01;
 
-alter table "test.gt_mysql".gt_db1.tb01 add column city varchar(50) not null comment 'aaa';
+alter table "test.gt_mysql".gt_db1.tb01 add column city varchar(50) comment 'aaa';
 show create table "test.gt_mysql".gt_db1.tb01;
 
 alter table "test.gt_mysql".gt_db1.tb01 add column age int not null comment 'age of users';
+show create table "test.gt_mysql".gt_db1.tb01;
+
+alter table "test.gt_mysql".gt_db1.tb01 add column address varchar(200) not null comment 'address of users';
 show create table "test.gt_mysql".gt_db1.tb01;
 
 drop table "test.gt_mysql".gt_db1.tb01;
