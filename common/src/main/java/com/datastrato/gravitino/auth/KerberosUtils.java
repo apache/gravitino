0c7699fd59d90878ab4453558654e2c4754fd893
Heng Qin <qqtt@123.com>
2024-01-24 20:13:03 +0800

    fix

--- common/src/main/java/com/datastrato/gravitino/auth/KerberosUtils.java
+++ common/src/main/java/com/datastrato/gravitino/auth/KerberosUtils.java
@@ -12,7 +12,6 @@
  * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.datastrato.gravitino.auth;
 
 import java.security.Principal;
