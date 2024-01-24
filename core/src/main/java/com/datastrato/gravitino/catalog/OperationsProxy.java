23c55bb810d5abc7ef641f1392012b6ac73400e2
Heng Qin <qqtt@123.com>
2024-01-24 11:28:52 +0800

    fix

--- core/src/main/java/com/datastrato/gravitino/catalog/OperationsProxy.java
+++ core/src/main/java/com/datastrato/gravitino/catalog/OperationsProxy.java
@@ -4,14 +4,16 @@
  */
 package com.datastrato.gravitino.catalog;
 
+import com.datastrato.gravitino.catalog.rel.BaseTable;
 import com.datastrato.gravitino.rel.Table;
 import com.datastrato.gravitino.utils.PrincipalUtils;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Collections;
+import org.apache.commons.lang3.ArrayUtils;
 
-/** Proxy wrapper on a operation class to execute operations by impersonating given user */
+/** Proxy wrapper on an operation class to execute operations by impersonating given user */
 public class OperationsProxy<T> implements InvocationHandler {
 
   private final ProxyPlugin plugin;
@@ -23,21 +25,31 @@ public class OperationsProxy<T> implements InvocationHandler {
   }
 
   public static <T> T createProxy(T ops, ProxyPlugin plugin) {
+    if (!(ops instanceof CatalogOperations)) {
+      throw new IllegalArgumentException("Method only supports the type of CatalogOperations");
+    }
+    return createProxyInternal(ops, plugin, ops.getClass().getInterfaces());
+  }
+
+  private static <T> T createProxyInternal(T ops, ProxyPlugin plugin, Class<?>[] interfaces) {
     return (T)
         Proxy.newProxyInstance(
-            ops.getClass().getClassLoader(),
-            ops.getClass().getInterfaces(),
-            new OperationsProxy(plugin, ops));
+            ops.getClass().getClassLoader(), interfaces, new OperationsProxy(plugin, ops));
   }
 
   @Override
   public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-    Object object = plugin.doAs(
-        PrincipalUtils.getCurrentPrincipal(),
-        () -> method.invoke(ops, args),
-        Collections.emptyMap());
+    Object object =
+        plugin.doAs(
+            PrincipalUtils.getCurrentPrincipal(),
+            () -> method.invoke(ops, args),
+            Collections.emptyMap());
     if (object instanceof Table) {
-      return createProxy(object, plugin);
+
+      return createProxyInternal(
+          object,
+          plugin,
+          ArrayUtils.addAll(BaseTable.class.getInterfaces(), object.getClass().getInterfaces()));
     }
     return object;
   }
