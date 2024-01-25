2e5838092c3e8519bcab56ced872a8c5cf4674eb
yuqi <yuqi@datastrato.com>
2024-01-25 10:55:09 +0800

    change the method of clean thread local logic

--- core/src/main/java/com/datastrato/gravitino/lock/LockManager.java
+++ core/src/main/java/com/datastrato/gravitino/lock/LockManager.java
@@ -141,7 +141,7 @@ public class LockManager {
         treeLockPair.treeLockNode.unlock(treeLockPair.lockType);
       }
     } finally {
-      holdingLocks.remove();
+      holdingLocks.get().clear();
     }
   }
 }
