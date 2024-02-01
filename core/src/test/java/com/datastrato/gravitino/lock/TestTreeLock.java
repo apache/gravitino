513d59156450eb24d75310784e5bf1506d321ae0
Qi Yu <yuqi@datastrato.com>
2024-02-01 21:00:27 +0800

    [#778] core(feat):  Support Tree lock (#1264)
    ### What changes were proposed in this pull request?
    
    Add the tree-like locks to enhance performance. 
    
    ### Why are the changes needed?
    
    Add the tree-like locks to enhance performance. 
    
    Fix: #778 
    
    ### Does this PR introduce _any_ user-facing change?
    
    N/A
    
    ### How was this patch tested?
    
    UT, `TestLockManager`

---
+++ core/src/test/java/com/datastrato/gravitino/lock/TestTreeLock.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2024 Datastrato Pvt Ltd.
+ * This software is licensed under the Apache License version 2.
+ */
+
+package com.datastrato.gravitino.lock;
+
+import static com.datastrato.gravitino.lock.TestLockManager.getConfig;
+
+import org.junit.jupiter.api.Test;
+
+class TestTreeLock {
+
+  @Test
+  void testLock() {
+    LockManager lockManager = new LockManager(getConfig());
+
+    for (int i = 0; i < 1000; i++) {
+      TreeLock lock = lockManager.createTreeLock(TestLockManager.randomNameIdentifier());
+      lock.lock(i % 2 == 0 ? LockType.READ : LockType.WRITE);
+      try {
+        // Business logic here.
+      } finally {
+        lock.unlock();
+      }
+    }
+  }
+}
