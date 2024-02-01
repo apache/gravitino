7f892765b6d2921d87066753fa8619923448fb16
Heng Qin <qqtt@123.com>
2024-02-01 14:16:14 +0800

    fxi

--- catalogs/catalog-hive/src/main/java/com/datastrato/gravitino/catalog/hive/HiveProxyPlugin.java
+++ catalogs/catalog-hive/src/main/java/com/datastrato/gravitino/catalog/hive/HiveProxyPlugin.java
@@ -42,6 +42,10 @@ class HiveProxyPlugin implements ProxyPlugin {
 
       if (UserGroupInformation.isSecurityEnabled() && ops != null) {
 
+        // The Gravitino server may use multiple KDC servers.
+        // The http authentication use one KDC server, the Hive catalog may use another KDC server. 
+        // The KerberosAuthenticator will remove realm of principal.
+        // And then we add the realm of Hive catalog to the user.
         String proxyKerberosPrincipalName = principal.getName();
         if (!proxyKerberosPrincipalName.contains("@")) {
           proxyKerberosPrincipalName =
