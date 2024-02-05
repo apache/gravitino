abab416b12b921ff67691ad098d2e9acf88026d6
주빈 <orol116@naver.com>
2024-02-05 19:53:16 +0900

    [#1969] Improvement: In all gradle files group dependancies by their destination (#2055)
    ### What changes were proposed in this pull request?
    
    Group dependencies by their destination.
    
    ### Why are the changes needed?
    
    Change to group dependencies by their destination.
    
    Fix: #1969 
    
    ### Does this PR introduce _any_ user-facing change?
    
    no
    
    ### How was this patch tested?
    
    ---------
    
    Co-authored-by: Justin Mclean <justin@datastrato.com>

--- catalogs/catalog-hadoop/build.gradle.kts
+++ catalogs/catalog-hadoop/build.gradle.kts
@@ -22,8 +22,9 @@ dependencies {
 
   testImplementation(libs.junit.jupiter.api)
   testImplementation(libs.junit.jupiter.params)
-  testRuntimeOnly(libs.junit.jupiter.engine)
   testImplementation(libs.mockito.core)
+
+  testRuntimeOnly(libs.junit.jupiter.engine)
 }
 
 tasks {
