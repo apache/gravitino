8f08e34c1c8677a3b2bb2d9c02eba272bfa14893
Justin Mclean <justin@datastrato.com>
2024-02-16 11:43:10 +1100

    fix spotless issues

--- server/src/main/java/com/datastrato/gravitino/server/web/ConfigServlet.java
+++ server/src/main/java/com/datastrato/gravitino/server/web/ConfigServlet.java
@@ -19,7 +19,6 @@ import java.util.Map;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
