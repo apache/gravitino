0cffb2799b3998189d45b5ab4701bff42b36d932
lipeidian <lipeidian@qq.com>
2024-02-19 10:14:12 +0800

    fix spotless

--- clients/client-java/src/main/java/com/datastrato/gravitino/client/FilesetCatalog.java
+++ clients/client-java/src/main/java/com/datastrato/gravitino/client/FilesetCatalog.java
@@ -244,8 +244,7 @@ public class FilesetCatalog extends BaseSchemaCatalog
       Preconditions.checkArgument(restClient != null, "restClient must be set");
       Preconditions.checkArgument(StringUtils.isNotBlank(name), "name must not be blank");
       Preconditions.checkArgument(type != null, "type must not be null");
-      Preconditions.checkArgument(
-          StringUtils.isNotBlank(provider), "provider must not be null or empty");
+      Preconditions.checkArgument(StringUtils.isNotBlank(provider), "provider must not be blank");
       Preconditions.checkArgument(audit != null, "audit must not be null");
 
       return new FilesetCatalog(name, type, provider, comment, properties, audit, restClient);
