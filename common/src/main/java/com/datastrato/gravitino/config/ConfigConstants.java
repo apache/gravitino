1e3e2ff858a28735b515760e2e92db6266542993
ajay lele <ajay.lele83@gmail.com>
2024-02-22 09:25:36 +0100

    formatted according to standards

--- common/src/main/java/com/datastrato/gravitino/config/ConfigConstants.java
+++ common/src/main/java/com/datastrato/gravitino/config/ConfigConstants.java
@@ -11,7 +11,7 @@ public final class ConfigConstants {
   private ConfigConstants() {}
 
   /** The value of messages used to indicate that the configuration is not set. */
- public static final String NOT_BLANK_ERROR_MSG = "The value can't be blank";
+  public static final String NOT_BLANK_ERROR_MSG = "The value can't be blank";
 
   /** The value of messages used to indicate that the configuration should be a positive number. */
   public static final String POSITIVE_NUMBER_ERROR_MSG = "The value must be a positive number";
@@ -19,7 +19,8 @@ public final class ConfigConstants {
   /**
    * The value of messages used to indicate that the configuration should be a non-negative number.
    */
-  public static final String NON_NEGATIVE_NUMBER_ERROR_MSG = "The value must be a non-negative number";
+  public static final String NON_NEGATIVE_NUMBER_ERROR_MSG =
+      "The value must be a non-negative number";
 
   /** The version number for the 0.1.0 release. */
   public static final String VERSION_0_1_0 = "0.1.0";
