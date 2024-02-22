88e5260c3bed24d873a38275fd2c6b2a65ead15b
yuqi <yuqi@datastrato.com>
2024-02-22 18:08:57 +0800

    Optimize the code

--- integration-test/build.gradle.kts
+++ integration-test/build.gradle.kts
@@ -266,6 +266,26 @@ tasks.test {
   if (skipITs) {
     exclude("**/integration/test/**")
   } else {
+    // Get current project version
+    val version = project.version.toString()
+    println("Current project version: $version")
+    // Check whether this module has already built
+    val buildDir = project.buildDir
+    if (!buildDir.exists()) {
+      dependsOn(":trino-connector:jar")
+    } else {
+      // Check the version gravitino related jars in build equal to the current project version
+      val gravitinoJars = buildDir.resolve("libs").listFiles { _, name -> name.startsWith("gravitino") }.filter {
+        val jarVersion = name.substringAfterLast("-").substringBeforeLast(".")
+        jarVersion != version
+      }
+
+      if (gravitinoJars != null && gravitinoJars.isNotEmpty()) {
+        delete(project(":trino-connector").buildDir)
+        dependsOn(":trino-connector:jar")
+      }
+    }
+
     doFirst {
       printDockerCheckInfo()
 
