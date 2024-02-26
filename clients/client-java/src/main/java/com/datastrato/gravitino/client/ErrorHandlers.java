30e459af867dede14a6c02af495b4dd1ad1a255f
caican00 <imcaican@gmail.com>
2024-02-26 10:46:37 +0800

    [#1894] improvement(client-java)(api): Add default cases in switches

--- clients/client-java/src/main/java/com/datastrato/gravitino/client/ErrorHandlers.java
+++ clients/client-java/src/main/java/com/datastrato/gravitino/client/ErrorHandlers.java
@@ -182,9 +182,10 @@ public class ErrorHandlers {
 
         case ErrorConstants.UNSUPPORTED_OPERATION_CODE:
           throw new UnsupportedOperationException(errorMessage);
-      }
 
-      super.accept(errorResponse);
+        default:
+          super.accept(errorResponse);
+      }
     }
   }
 
@@ -217,9 +218,10 @@ public class ErrorHandlers {
           throw new RuntimeException(errorMessage);
         case ErrorConstants.UNSUPPORTED_OPERATION_CODE:
           throw new UnsupportedOperationException(errorMessage);
-      }
 
-      super.accept(errorResponse);
+        default:
+          super.accept(errorResponse);
+      }
     }
   }
 
@@ -253,9 +255,10 @@ public class ErrorHandlers {
 
         case ErrorConstants.INTERNAL_ERROR_CODE:
           throw new RuntimeException(errorMessage);
-      }
 
-      super.accept(errorResponse);
+        default:
+          super.accept(errorResponse);
+      }
     }
   }
 
@@ -286,9 +289,10 @@ public class ErrorHandlers {
 
         case ErrorConstants.INTERNAL_ERROR_CODE:
           throw new RuntimeException(errorMessage);
-      }
 
-      super.accept(errorResponse);
+        default:
+          super.accept(errorResponse);
+      }
     }
   }
 
@@ -313,9 +317,10 @@ public class ErrorHandlers {
 
         case ErrorConstants.INTERNAL_ERROR_CODE:
           throw new RuntimeException(errorMessage);
-      }
 
-      super.accept(errorResponse);
+        default:
+          super.accept(errorResponse);
+      }
     }
   }
 
@@ -350,21 +355,20 @@ public class ErrorHandlers {
      */
     @Override
     public void accept(ErrorResponse errorResponse) {
-      if (errorResponse.getType() != null) {
-        switch (errorResponse.getType()) {
-          case OAuth2ClientUtil.INVALID_CLIENT_ERROR:
-            throw new UnauthorizedException(
-                "Not authorized: %s: %s", errorResponse.getType(), errorResponse.getMessage());
-          case OAuth2ClientUtil.INVALID_REQUEST_ERROR:
-          case OAuth2ClientUtil.INVALID_GRANT_ERROR:
-          case OAuth2ClientUtil.UNAUTHORIZED_CLIENT_ERROR:
-          case OAuth2ClientUtil.UNSUPPORTED_GRANT_TYPE_ERROR:
-          case OAuth2ClientUtil.INVALID_SCOPE_ERROR:
-            throw new BadRequestException(
-                "Malformed request: %s: %s", errorResponse.getType(), errorResponse.getMessage());
-        }
+      switch (errorResponse.getType()) {
+        case OAuth2ClientUtil.INVALID_CLIENT_ERROR:
+          throw new UnauthorizedException(
+                  "Not authorized: %s: %s", errorResponse.getType(), errorResponse.getMessage());
+        case OAuth2ClientUtil.INVALID_REQUEST_ERROR:
+        case OAuth2ClientUtil.INVALID_GRANT_ERROR:
+        case OAuth2ClientUtil.UNAUTHORIZED_CLIENT_ERROR:
+        case OAuth2ClientUtil.UNSUPPORTED_GRANT_TYPE_ERROR:
+        case OAuth2ClientUtil.INVALID_SCOPE_ERROR:
+          throw new BadRequestException(
+                  "Malformed request: %s: %s", errorResponse.getType(), errorResponse.getMessage());
+        default:
+          super.accept(errorResponse);
       }
-      super.accept(errorResponse);
     }
   }
 
@@ -396,9 +400,10 @@ public class ErrorHandlers {
 
         case ErrorConstants.INTERNAL_ERROR_CODE:
           throw new RuntimeException(errorMessage);
-      }
 
-      super.accept(errorResponse);
+        default:
+          super.accept(errorResponse);
+      }
     }
   }
 
