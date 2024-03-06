2bece763fe7525a1a9363980e351e44484396c71
mchades <liminghuang@datastrato.com>
2024-03-06 17:12:21 +0800

    Add basic API and metadata module for stream catalog

---
+++ api/src/main/java/com/datastrato/gravitino/stream/DataLayout.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2024 Datastrato Pvt Ltd.
+ * This software is licensed under the Apache License version 2.
+ */
+package com.datastrato.gravitino.stream;
+
+/**
+ * The interface for message schema of a topic. Currently not implemented, only reserved as a
+ * placeholder interface.
+ */
+public interface DataLayout {}
