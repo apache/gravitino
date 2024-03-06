c2df3bf37f5d4e659bbb54c803051a0c9b699a78
yuqi <yuqi@datastrato.com>
2024-03-06 14:53:40 +0800

    Fix several bugs.

--- catalogs/catalog-common/build.gradle.kts
+++ catalogs/catalog-common/build.gradle.kts
@@ -1,14 +1,11 @@
 /*
- * Copyright 2023 Datastrato Pvt Ltd.
+ * Copyright 2024 Datastrato Pvt Ltd.
  * This software is licensed under the Apache License version 2.
  */
 
 plugins {
   id("java")
 }
-
-group = "org.example"
-version = "0.5.0-SNAPSHOT"
 
 repositories {
   mavenCentral()
