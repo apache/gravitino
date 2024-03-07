a9578443ec43bf95c701600f90c5cca22672afd4
fanng <xiaojing@datastrato.com>
2024-03-07 14:17:20 +0800

    xxx

---
+++ spark-connector/src/test/java/com/datastrato/gravitino/spark/connector/TestTransformTableChange.java
@@ -0,0 +1,17 @@
+/*
+ *  Copyright 2024 Datastrato Pvt Ltd.
+ *  This software is licensed under the Apache License version 2.
+ */
+
+package com.datastrato.gravitino.spark.connector;
+
+import org.junit.jupiter.api.Test;
+
+public class TestTransformTableChange {
+
+  @Test
+  void testTransformTableChange() {
+    // todo
+  }
+
+}
