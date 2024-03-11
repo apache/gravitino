7b2078a8fbab6076178372e1a3dd47e049cb725a
xiaojiebao <xiaojiebao@xiaomi.com>
2024-03-11 15:33:50 +0800

    fix the order of deps

--- clients/filesystem-hadoop/build.gradle.kts
+++ clients/filesystem-hadoop/build.gradle.kts
@@ -11,13 +11,13 @@ dependencies {
   compileOnly(libs.hadoop2.common)
   implementation(project(":clients:client-java-runtime", configuration = "shadow"))
 
+  testImplementation(libs.hadoop2.common)
   testImplementation(libs.junit.jupiter.api)
   testImplementation(libs.junit.jupiter.params)
   testImplementation(libs.mockito.core)
   testImplementation(libs.mockserver.netty) {
     exclude("com.google.guava", "guava")
   }
-  testImplementation(libs.hadoop2.common)
   testRuntimeOnly(libs.junit.jupiter.engine)
 }
 
