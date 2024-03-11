ec2cd58a1653b8f97a08dca41b6d6fcd515c9c80
Brandon Lu <brandonlu0924@gmail.com>
2024-03-11 22:54:07 +0800

    remove pydantic to built-in dataclass
    Co-authored-by: Yongjie Zhao <yongjiezhao@apache.org>

--- clients/client-python/gravitino_client/core/dto.py
+++ clients/client-python/gravitino_client/core/dto.py
@@ -2,9 +2,10 @@
 Copyright 2024 Datastrato Pvt Ltd.
 This software is licensed under the Apache License version 2.
 """
-from pydantic import BaseModel
+from dataclasses import dataclass
 
-class VersionDTO(BaseModel):
+@dataclass
+class VersionDTO:
     version: str
     compile_date: str
     git_commit: str
