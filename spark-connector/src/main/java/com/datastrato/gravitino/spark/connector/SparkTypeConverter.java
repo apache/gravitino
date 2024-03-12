dfc254941382a6ee18f59a2a027abf8c96c1e54e
caican00 <imcaican@gmail.com>
2024-03-12 10:44:45 +0800

    [#2497] feat(spark-connector): support other primitive types for spark-connector

--- spark-connector/src/main/java/com/datastrato/gravitino/spark/connector/SparkTypeConverter.java
+++ spark-connector/src/main/java/com/datastrato/gravitino/spark/connector/SparkTypeConverter.java
@@ -19,7 +19,6 @@ import org.apache.spark.sql.types.DoubleType;
 import org.apache.spark.sql.types.FloatType;
 import org.apache.spark.sql.types.IntegerType;
 import org.apache.spark.sql.types.LongType;
-import org.apache.spark.sql.types.NullType;
 import org.apache.spark.sql.types.ShortType;
 import org.apache.spark.sql.types.StringType;
 import org.apache.spark.sql.types.TimestampNTZType;
@@ -29,73 +28,71 @@ import org.apache.spark.sql.types.VarcharType;
 /** Transform DataTypes between Gravitino and Spark. */
 public class SparkTypeConverter {
   public static Type toGravitinoType(DataType sparkType) {
-    if (sparkType instanceof StringType) {
-      return Types.StringType.get();
-    } else if (sparkType instanceof BooleanType) {
-      return Types.BooleanType.get();
+    if (sparkType instanceof ByteType) {
+      return Types.ByteType.get();
+    } else if (sparkType instanceof ShortType) {
+      return Types.ShortType.get();
     } else if (sparkType instanceof IntegerType) {
       return Types.IntegerType.get();
+    } else if (sparkType instanceof LongType) {
+      return Types.LongType.get();
     } else if (sparkType instanceof FloatType) {
       return Types.FloatType.get();
     } else if (sparkType instanceof DoubleType) {
       return Types.DoubleType.get();
-    } else if (sparkType instanceof ByteType) {
-      return Types.ByteType.get();
-    } else if (sparkType instanceof ShortType) {
-      return Types.ShortType.get();
-    } else if (sparkType instanceof LongType) {
-      return Types.LongType.get();
     } else if (sparkType instanceof DecimalType) {
       DecimalType decimalType = (DecimalType) sparkType;
       return Types.DecimalType.of(decimalType.precision(), decimalType.scale());
-    } else if (sparkType instanceof BinaryType) {
-      return Types.BinaryType.get();
-    } else if (sparkType instanceof CharType) {
-      CharType charType = (CharType) sparkType;
-      return Types.FixedCharType.of(charType.length());
+    } else if (sparkType instanceof StringType) {
+      return Types.StringType.get();
     } else if (sparkType instanceof VarcharType) {
       VarcharType varcharType = (VarcharType) sparkType;
       return Types.VarCharType.of(varcharType.length());
+    } else if (sparkType instanceof CharType) {
+      CharType charType = (CharType) sparkType;
+      return Types.FixedCharType.of(charType.length());
+    } else if (sparkType instanceof BinaryType) {
+      return Types.BinaryType.get();
+    } else if (sparkType instanceof BooleanType) {
+      return Types.BooleanType.get();
     } else if (sparkType instanceof DateType) {
       return Types.DateType.get();
     } else if (sparkType instanceof TimestampType) {
       return Types.TimestampType.withTimeZone();
     } else if (sparkType instanceof TimestampNTZType) {
       return Types.TimestampType.withoutTimeZone();
-    } else if (sparkType instanceof NullType) {
-      return Types.NullType.get();
     }
     throw new UnsupportedOperationException("Not support " + sparkType.toString());
   }
 
   public static DataType toSparkType(Type gravitinoType) {
-    if (gravitinoType instanceof Types.StringType) {
-      return DataTypes.StringType;
-    } else if (gravitinoType instanceof Types.BooleanType) {
-      return DataTypes.BooleanType;
+    if (gravitinoType instanceof Types.ByteType) {
+      return DataTypes.ByteType;
+    } else if (gravitinoType instanceof Types.ShortType) {
+      return DataTypes.ShortType;
     } else if (gravitinoType instanceof Types.IntegerType) {
       return DataTypes.IntegerType;
+    } else if (gravitinoType instanceof Types.LongType) {
+      return DataTypes.LongType;
     } else if (gravitinoType instanceof Types.FloatType) {
       return DataTypes.FloatType;
     } else if (gravitinoType instanceof Types.DoubleType) {
       return DataTypes.DoubleType;
-    } else if (gravitinoType instanceof Types.ByteType) {
-      return DataTypes.ByteType;
-    } else if (gravitinoType instanceof Types.ShortType) {
-      return DataTypes.ShortType;
-    } else if (gravitinoType instanceof Types.LongType) {
-      return DataTypes.LongType;
     } else if (gravitinoType instanceof Types.DecimalType) {
       Types.DecimalType decimalType = (Types.DecimalType) gravitinoType;
       return DataTypes.createDecimalType(decimalType.precision(), decimalType.scale());
-    } else if (gravitinoType instanceof Types.BinaryType) {
-      return DataTypes.BinaryType;
-    } else if (gravitinoType instanceof Types.FixedCharType) {
-      Types.FixedCharType charType = (Types.FixedCharType) gravitinoType;
-      return CharType.apply((charType.length()));
+    } else if (gravitinoType instanceof Types.StringType) {
+      return DataTypes.StringType;
     } else if (gravitinoType instanceof Types.VarCharType) {
       Types.VarCharType varCharType = (Types.VarCharType) gravitinoType;
       return VarcharType.apply(varCharType.length());
+    } else if (gravitinoType instanceof Types.FixedCharType) {
+      Types.FixedCharType charType = (Types.FixedCharType) gravitinoType;
+      return CharType.apply((charType.length()));
+    } else if (gravitinoType instanceof Types.BinaryType) {
+      return DataTypes.BinaryType;
+    } else if (gravitinoType instanceof Types.BooleanType) {
+      return DataTypes.BooleanType;
     } else if (gravitinoType instanceof Types.DateType) {
       return DataTypes.DateType;
     } else if (gravitinoType instanceof Types.TimestampType
@@ -104,8 +101,6 @@ public class SparkTypeConverter {
     } else if (gravitinoType instanceof Types.TimestampType
         && !((Types.TimestampType) gravitinoType).hasTimeZone()) {
       return DataTypes.TimestampNTZType;
-    } else if (gravitinoType instanceof Types.NullType) {
-      return DataTypes.NullType;
     }
     throw new UnsupportedOperationException("Not support " + gravitinoType.toString());
   }
--- spark-connector/src/test/java/com/datastrato/gravitino/spark/connector/TestSparkTypeConverter.java
+++ spark-connector/src/test/java/com/datastrato/gravitino/spark/connector/TestSparkTypeConverter.java
@@ -7,18 +7,26 @@ package com.datastrato.gravitino.spark.connector;
 
 import com.datastrato.gravitino.rel.types.Type;
 import com.datastrato.gravitino.rel.types.Types;
+import com.datastrato.gravitino.rel.types.Types.BinaryType;
 import com.datastrato.gravitino.rel.types.Types.BooleanType;
+import com.datastrato.gravitino.rel.types.Types.ByteType;
+import com.datastrato.gravitino.rel.types.Types.DateType;
+import com.datastrato.gravitino.rel.types.Types.DecimalType;
 import com.datastrato.gravitino.rel.types.Types.DoubleType;
+import com.datastrato.gravitino.rel.types.Types.FixedCharType;
 import com.datastrato.gravitino.rel.types.Types.FloatType;
 import com.datastrato.gravitino.rel.types.Types.IntegerType;
+import com.datastrato.gravitino.rel.types.Types.LongType;
+import com.datastrato.gravitino.rel.types.Types.ShortType;
 import com.datastrato.gravitino.rel.types.Types.StringType;
+import com.datastrato.gravitino.rel.types.Types.TimestampType;
+import com.datastrato.gravitino.rel.types.Types.VarCharType;
 import com.google.common.collect.ImmutableSet;
 import java.util.HashMap;
 import java.util.Set;
 import org.apache.spark.sql.types.CharType;
 import org.apache.spark.sql.types.DataType;
 import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.types.DecimalType;
 import org.apache.spark.sql.types.VarcharType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -30,7 +38,7 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 public class TestSparkTypeConverter {
 
   private HashMap<Type, DataType> gravitinoToSparkTypeMapper = new HashMap<>();
-  private Set<Type> notSupportGravitinoTypes = ImmutableSet.of();
+  private Set<Type> notSupportGravitinoTypes = ImmutableSet.of(Types.NullType.get());
 
   private Set<DataType> notSupportSparkTypes = ImmutableSet.of();
 
@@ -41,18 +49,17 @@ public class TestSparkTypeConverter {
     gravitinoToSparkTypeMapper.put(StringType.get(), DataTypes.StringType);
     gravitinoToSparkTypeMapper.put(FloatType.get(), DataTypes.FloatType);
     gravitinoToSparkTypeMapper.put(DoubleType.get(), DataTypes.DoubleType);
-    gravitinoToSparkTypeMapper.put(Types.ByteType.get(), DataTypes.ByteType);
-    gravitinoToSparkTypeMapper.put(Types.ShortType.get(), DataTypes.ShortType);
-    gravitinoToSparkTypeMapper.put(Types.LongType.get(), DataTypes.LongType);
-    gravitinoToSparkTypeMapper.put(Types.DecimalType.of(10, 2), DecimalType.apply(10, 2));
-    gravitinoToSparkTypeMapper.put(Types.BinaryType.get(), DataTypes.BinaryType);
-    gravitinoToSparkTypeMapper.put(Types.FixedCharType.of(2), CharType.apply(2));
-    gravitinoToSparkTypeMapper.put(Types.VarCharType.of(2), VarcharType.apply(2));
-    gravitinoToSparkTypeMapper.put(Types.DateType.get(), DataTypes.DateType);
-    gravitinoToSparkTypeMapper.put(Types.TimestampType.withTimeZone(), DataTypes.TimestampType);
+    gravitinoToSparkTypeMapper.put(ByteType.get(), DataTypes.ByteType);
+    gravitinoToSparkTypeMapper.put(ShortType.get(), DataTypes.ShortType);
+    gravitinoToSparkTypeMapper.put(LongType.get(), DataTypes.LongType);
+    gravitinoToSparkTypeMapper.put(DecimalType.of(10, 2), DataTypes.createDecimalType(10, 2));
+    gravitinoToSparkTypeMapper.put(BinaryType.get(), DataTypes.BinaryType);
+    gravitinoToSparkTypeMapper.put(FixedCharType.of(2), CharType.apply(2));
+    gravitinoToSparkTypeMapper.put(VarCharType.of(2), VarcharType.apply(2));
+    gravitinoToSparkTypeMapper.put(DateType.get(), DataTypes.DateType);
+    gravitinoToSparkTypeMapper.put(TimestampType.withTimeZone(), DataTypes.TimestampType);
     gravitinoToSparkTypeMapper.put(
         Types.TimestampType.withoutTimeZone(), DataTypes.TimestampNTZType);
-    gravitinoToSparkTypeMapper.put(Types.NullType.get(), DataTypes.NullType);
   }
 
   @Test
