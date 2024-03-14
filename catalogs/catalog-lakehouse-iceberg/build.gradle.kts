8bb9cc73200efedcea8d88b11bc4d64a90c63a1d
charliecheng630 <charlie.cheng630@gmail.com>
2024-03-14 19:13:06 +0800

    Update gravitino-ci-hive version to 0.1.9 in build.gradle.kts

--- catalogs/catalog-lakehouse-iceberg/build.gradle.kts
+++ catalogs/catalog-lakehouse-iceberg/build.gradle.kts
@@ -161,7 +161,7 @@ tasks.test {
     dependsOn(tasks.jar)
 
     doFirst {
-      environment("GRAVITINO_CI_HIVE_DOCKER_IMAGE", "datastrato/gravitino-ci-hive:0.1.8")
+      environment("GRAVITINO_CI_HIVE_DOCKER_IMAGE", "datastrato/gravitino-ci-hive:0.1.9")
     }
 
     val init = project.extra.get("initIntegrationTest") as (Test) -> Unit
