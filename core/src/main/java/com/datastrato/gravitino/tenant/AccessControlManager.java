8f1946ae232234702c059e87f3be4ee14e8f1b62
Heng Qin <qqtt@123.com>
2024-03-14 17:15:55 +0800

    fix

--- core/src/main/java/com/datastrato/gravitino/tenant/AccessControlManager.java
+++ core/src/main/java/com/datastrato/gravitino/tenant/AccessControlManager.java
@@ -4,13 +4,8 @@
  */
 package com.datastrato.gravitino.tenant;
 
-import com.datastrato.gravitino.Entity;
-import com.datastrato.gravitino.EntityAlreadyExistsException;
-import com.datastrato.gravitino.EntityStore;
-import com.datastrato.gravitino.NameIdentifier;
-import com.datastrato.gravitino.exceptions.NoSuchEntityException;
-import com.datastrato.gravitino.exceptions.NoSuchUserException;
-import com.datastrato.gravitino.exceptions.UserAlreadyExistsException;
+import com.datastrato.gravitino.*;
+import com.datastrato.gravitino.exceptions.*;
 import com.datastrato.gravitino.meta.AuditInfo;
 import com.datastrato.gravitino.meta.MetalakeUser;
 import com.datastrato.gravitino.storage.IdGenerator;
@@ -32,14 +27,30 @@ public class AccessControlManager implements SupportsUserManagement {
   private final EntityStore store;
   private final IdGenerator idGenerator;
 
+  /**
+   * Constructs a AccessControlManager instance.
+   *
+   * @param store The EntityStore to use for managing access control.
+   * @param idGenerator The IdGenerator to use for generating identifiers.
+   */
   public AccessControlManager(EntityStore store, IdGenerator idGenerator) {
     this.store = store;
     this.idGenerator = idGenerator;
   }
 
+  /**
+   * Creates a new User.
+   *
+   * @param metalake The Metalake of the User.
+   * @param userName THe name of the User.
+   * @param properties Additional properties for the Metalake.
+   * @return The created User instance.
+   * @throws UserAlreadyExistsException If a User with the same identifier already exists.
+   * @throws RuntimeException If creating the User encounters storage issues.
+   */
   @Override
-  public com.datastrato.gravitino.User createUser(
-      String metalake, String userName, Map<String, String> properties) {
+  public User createUser(
+      String metalake, String userName, Map<String, String> properties) throws UserAlreadyExistsException {
     MetalakeUser metalakeUser =
         MetalakeUser.builder()
             .withId(idGenerator.nextId())
@@ -69,6 +80,16 @@ public class AccessControlManager implements SupportsUserManagement {
     }
   }
 
+
+  /**
+   * Deletes a User.
+   *
+   * @param metalake The Metalake of the User.
+   * @param userName THe name of the User.
+   *
+   * @return `true` if the User was successfully deleted, `false` otherwise.
+   * @throws RuntimeException If deleting the User encounters storage issues.
+   */
   @Override
   public boolean dropUser(String metalake, String userName) {
 
@@ -84,8 +105,17 @@ public class AccessControlManager implements SupportsUserManagement {
     }
   }
 
+  /**
+   * Loads a User.
+   *
+   * @param metalake The Metalake of the User.
+   * @param userName THe name of the User.
+   * @return The loaded User instance.
+   * @throws NoSuchUserException If the User with the given identifier does not exist.
+   * @throws RuntimeException If loading the User encounters storage issues.
+   */
   @Override
-  public com.datastrato.gravitino.User loadUser(String metalake, String userName) {
+  public User loadUser(String metalake, String userName) throws NoSuchUserException {
     try {
       return store.get(
           NameIdentifier.of(metalake, userName), Entity.EntityType.USER, MetalakeUser.class);
