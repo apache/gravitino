6bfc00673815e2d47a4390761969ff6eb5a7eaaf
Heng Qin <qqtt@123.com>
2024-03-14 14:14:40 +0800

    Add the ut

--- common/src/main/java/com/datastrato/gravitino/dto/requests/UserCreateRequest.java
+++ common/src/main/java/com/datastrato/gravitino/dto/requests/UserCreateRequest.java
@@ -2,11 +2,13 @@ package com.datastrato.gravitino.dto.requests;
 
 import com.datastrato.gravitino.rest.RESTRequest;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Preconditions;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 import lombok.extern.jackson.Jacksonized;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.annotation.Nullable;
 import java.util.Map;
@@ -25,6 +27,17 @@ public class UserCreateRequest implements RESTRequest {
     @JsonProperty("properties")
     private final Map<String, String> properties;
 
+    /** Default constructor for MetalakeCreateRequest. (Used for Jackson deserialization.) */
+    public UserCreateRequest() {
+        this(null, null);
+    }
+
+    public UserCreateRequest(String name, Map<String, String> properties) {
+        super();
+        this.name = name;
+        this.properties = properties;
+    }
+
     /**
      * Validates the {@link UserCreateRequest} request.
      *
@@ -32,6 +45,8 @@ public class UserCreateRequest implements RESTRequest {
      */
     @Override
     public void validate() throws IllegalArgumentException {
+        Preconditions.checkArgument(
+                StringUtils.isNotBlank(name), "\"name\" field is required and cannot be empty");
 
     }
 }
