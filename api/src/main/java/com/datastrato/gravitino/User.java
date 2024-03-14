7e5c0d81e0213dfc6efe1df17555e93a315c7361
Heng Qin <qqtt@123.com>
2024-03-14 19:31:03 +0800

    Add comment

--- api/src/main/java/com/datastrato/gravitino/User.java
+++ api/src/main/java/com/datastrato/gravitino/User.java
@@ -8,7 +8,18 @@ import java.util.Map;
 
 public interface User extends Auditable {
 
+  /**
+   * The name of the user.
+   *
+   * @return The name of the user.
+   */
   String name();
 
+  /**
+   * The properties of the user. Note, this method will return null if the properties are not
+   * set.
+   *
+   * @return The properties of the user.
+   */
   Map<String, String> properties();
 }
