cfec636146d052b9ff186446825a80524f84b07d
Heng Qin <qqtt@123.com>
2024-03-14 19:31:03 +0800

    Add comment

--- api/src/main/java/com/datastrato/gravitino/exceptions/NoSuchUserException.java
+++ api/src/main/java/com/datastrato/gravitino/exceptions/NoSuchUserException.java
@@ -7,13 +7,27 @@ package com.datastrato.gravitino.exceptions;
 import com.google.errorprone.annotations.FormatMethod;
 import com.google.errorprone.annotations.FormatString;
 
+/** An exception thrown when a user is not found. */
 public class NoSuchUserException extends NotFoundException {
 
+  /**
+   * Constructs a new exception with the specified detail message.
+   *
+   * @param message the detail message.
+   * @param args the arguments to the message.
+   */
   @FormatMethod
   public NoSuchUserException(@FormatString String message, Object... args) {
     super(message, args);
   }
 
+  /**
+   * Constructs a new exception with the specified detail message and cause.
+   *
+   * @param cause the cause.
+   * @param message the detail message.
+   * @param args the arguments to the message.
+   */
   @FormatMethod
   public NoSuchUserException(Throwable cause, String message, Object... args) {
     super(cause, message, args);
--- api/src/main/java/com/datastrato/gravitino/exceptions/UserAlreadyExistsException.java
+++ api/src/main/java/com/datastrato/gravitino/exceptions/UserAlreadyExistsException.java
@@ -7,13 +7,27 @@ package com.datastrato.gravitino.exceptions;
 import com.google.errorprone.annotations.FormatMethod;
 import com.google.errorprone.annotations.FormatString;
 
+/** An exception thrown when a resource already exists. */
 public class UserAlreadyExistsException extends AlreadyExistsException {
 
+  /**
+   * Constructs a new exception with the specified detail message.
+   *
+   * @param message the detail message.
+   * @param args the arguments to the message.
+   */
   @FormatMethod
   public UserAlreadyExistsException(@FormatString String message, Object... args) {
     super(message, args);
   }
 
+  /**
+   * Constructs a new exception with the specified detail message and cause.
+   *
+   * @param cause the cause.
+   * @param message the detail message.
+   * @param args the arguments to the message.
+   */
   @FormatMethod
   public UserAlreadyExistsException(Throwable cause, String message, Object... args) {
     super(cause, message, args);
