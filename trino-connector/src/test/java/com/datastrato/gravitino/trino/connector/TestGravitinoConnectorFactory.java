bb9c16805c8013d210faa57bb362dbbb0fca3ee9
lipeidian <lipeidian@qq.com>
2024-03-15 09:18:34 +0800

    add licence

--- trino-connector/src/test/java/com/datastrato/gravitino/trino/connector/TestGravitinoConnectorFactory.java
+++ trino-connector/src/test/java/com/datastrato/gravitino/trino/connector/TestGravitinoConnectorFactory.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2024 Datastrato Pvt Ltd.
+ * This software is licensed under the Apache License version 2.
+ */
 package com.datastrato.gravitino.trino.connector;
 
 import com.datastrato.gravitino.client.GravitinoClient;
--- trino-connector/src/test/java/com/datastrato/gravitino/trino/connector/TestGravitinoPlugin.java
+++ trino-connector/src/test/java/com/datastrato/gravitino/trino/connector/TestGravitinoPlugin.java
@@ -2,7 +2,6 @@
  * Copyright 2024 Datastrato Pvt Ltd.
  * This software is licensed under the Apache License version 2.
  */
-
 package com.datastrato.gravitino.trino.connector;
 
 import com.datastrato.gravitino.client.GravitinoClient;
