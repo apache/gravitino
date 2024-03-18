4b10eed5564e734c2738df144a9604bbdf8e4ba8
yuqi <yuqi@datastrato.com>
2024-03-18 20:47:10 +0800

    Optimize char/varchar mapping for Hive catalog between Gravitino and Trino

--- trino-connector/src/main/java/com/datastrato/gravitino/trino/connector/catalog/hive/HiveDataTypeTransformer.java
+++ trino-connector/src/main/java/com/datastrato/gravitino/trino/connector/catalog/hive/HiveDataTypeTransformer.java
@@ -6,28 +6,52 @@
 package com.datastrato.gravitino.trino.connector.catalog.hive;
 
 import com.datastrato.gravitino.rel.types.Type;
-import com.datastrato.gravitino.rel.types.Type.Name;
 import com.datastrato.gravitino.rel.types.Types;
+import com.datastrato.gravitino.trino.connector.GravitinoErrorCode;
 import com.datastrato.gravitino.trino.connector.util.GeneralDataTypeTransformer;
+import io.trino.spi.TrinoException;
+import io.trino.spi.type.VarcharType;
 
 /** Type transformer between Hive and Trino */
 public class HiveDataTypeTransformer extends GeneralDataTypeTransformer {
   // Max length of Hive varchar is 65535
   private static final int HIVE_VARCHAR_MAX_LENGTH = 65535;
+  private static final int HIVE_CHAR_MAX_LENGTH = 255;
 
   @Override
   public Type getGravitinoType(io.trino.spi.type.Type type) {
-    Type gravitinoType = super.getGravitinoType(type);
-    if (gravitinoType.name() == Name.VARCHAR
-        && ((Types.VarCharType) gravitinoType).length() > HIVE_VARCHAR_MAX_LENGTH) {
-      return Types.VarCharType.of(HIVE_VARCHAR_MAX_LENGTH);
+    Class<? extends io.trino.spi.type.Type> typeClass = type.getClass();
+    if (typeClass == VarcharType.class) {
+      VarcharType varcharType = (VarcharType) type;
+      if (varcharType.getLength().isEmpty()) {
+        // It's was creating a table with column type 'varchar' NOT 'varchar(n)', We not support
+        // this case
+        throw new TrinoException(
+            GravitinoErrorCode.GRAVITINO_ILLEGAL_ARGUMENT,
+            "Hive does not support the datatype VARCHAR without length");
+      }
+
+      int length = varcharType.getLength().get();
+      if (length > HIVE_VARCHAR_MAX_LENGTH) {
+        throw new TrinoException(
+            GravitinoErrorCode.GRAVITINO_ILLEGAL_ARGUMENT,
+            "Hive does not support the datatype VARCHAR with the length greater than "
+                + HIVE_VARCHAR_MAX_LENGTH);
+      }
+
+      return Types.VarCharType.of(length);
+    } else if (typeClass == io.trino.spi.type.CharType.class) {
+      io.trino.spi.type.CharType charType = (io.trino.spi.type.CharType) type;
+      if (charType.getLength() > HIVE_CHAR_MAX_LENGTH) {
+        throw new TrinoException(
+            GravitinoErrorCode.GRAVITINO_ILLEGAL_ARGUMENT,
+            "Hive does not support the datatype CHAR with the length greater than "
+                + HIVE_CHAR_MAX_LENGTH);
+      }
+
+      return Types.FixedCharType.of(charType.getLength());
     }
 
-    if (gravitinoType.name() == Name.FIXEDCHAR
-        && ((Types.FixedCharType) gravitinoType).length() > HIVE_VARCHAR_MAX_LENGTH) {
-      return Types.FixedCharType.of(HIVE_VARCHAR_MAX_LENGTH);
-    }
-
-    return gravitinoType;
+    return super.getGravitinoType(type);
   }
 }
