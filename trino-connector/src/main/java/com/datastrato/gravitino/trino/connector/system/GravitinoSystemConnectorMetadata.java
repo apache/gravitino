7e4768c72b28cfcfadd273c5aad8b9e3eea2cafa
Yuhui <hui@datastrato.com>
2024-03-18 14:15:53 +0800

    [#2416] feat(trino-connector): Support system table catalog (#2522)
    ### What changes were proposed in this pull request?
    
    Support the system table catalog to display all loaded catalog
    information in the Trino connector.
    
    ### Why are the changes needed?
    
    Fix: #2416 
    
    ### Does this PR introduce _any_ user-facing change?
    
    NO
    
    ### How was this patch tested?
    
    Add new UT

---
+++ trino-connector/src/main/java/com/datastrato/gravitino/trino/connector/system/GravitinoSystemConnectorMetadata.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2024 Datastrato Pvt Ltd.
+ * This software is licensed under the Apache License version 2.
+ */
+package com.datastrato.gravitino.trino.connector.system;
+
+import static com.datastrato.gravitino.trino.connector.system.table.GravitinoSystemTable.SYSTEM_TABLE_SCHEMA_NAME;
+
+import com.datastrato.gravitino.trino.connector.system.table.GravitinoSystemTableFactory;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.trino.spi.connector.ColumnHandle;
+import io.trino.spi.connector.ColumnMetadata;
+import io.trino.spi.connector.ConnectorMetadata;
+import io.trino.spi.connector.ConnectorSession;
+import io.trino.spi.connector.ConnectorTableHandle;
+import io.trino.spi.connector.ConnectorTableMetadata;
+import io.trino.spi.connector.ConnectorTableVersion;
+import io.trino.spi.connector.SchemaTableName;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+/** An implementation of Gravitino System connector Metadata */
+public class GravitinoSystemConnectorMetadata implements ConnectorMetadata {
+
+  @Override
+  public List<String> listSchemaNames(ConnectorSession session) {
+    return List.of(SYSTEM_TABLE_SCHEMA_NAME);
+  }
+
+  @Override
+  public List<SchemaTableName> listTables(ConnectorSession session, Optional<String> schemaName) {
+    return GravitinoSystemTableFactory.SYSTEM_TABLES.keySet().stream().toList();
+  }
+
+  @Override
+  public ConnectorTableHandle getTableHandle(
+      ConnectorSession session,
+      SchemaTableName tableName,
+      Optional<ConnectorTableVersion> startVersion,
+      Optional<ConnectorTableVersion> endVersion) {
+    return GravitinoSystemTableFactory.SYSTEM_TABLES.get(tableName) != null
+        ? new SystemTableHandle(tableName)
+        : null;
+  }
+
+  @Override
+  public ConnectorTableMetadata getTableMetadata(
+      ConnectorSession session, ConnectorTableHandle table) {
+    SchemaTableName tableName = ((SystemTableHandle) table).name;
+    return GravitinoSystemTableFactory.getTableMetaData(tableName);
+  }
+
+  @Override
+  public Map<String, ColumnHandle> getColumnHandles(
+      ConnectorSession session, ConnectorTableHandle tableHandle) {
+    SchemaTableName tableName = ((SystemTableHandle) tableHandle).name;
+    Map<String, ColumnHandle> columnHandles = new HashMap<>();
+    List<ColumnMetadata> columns =
+        GravitinoSystemTableFactory.getTableMetaData(tableName).getColumns();
+    for (int i = 0; i < columns.size(); i++) {
+      columnHandles.put(columns.get(i).getName(), new SystemColumnHandle(i));
+    }
+    return columnHandles;
+  }
+
+  @Override
+  public ColumnMetadata getColumnMetadata(
+      ConnectorSession session, ConnectorTableHandle tableHandle, ColumnHandle columnHandle) {
+    SchemaTableName tableName = ((SystemTableHandle) tableHandle).name;
+    return GravitinoSystemTableFactory.getTableMetaData(tableName)
+        .getColumns()
+        .get(((SystemColumnHandle) columnHandle).index);
+  }
+
+  public static class SystemTableHandle implements ConnectorTableHandle {
+    private final SchemaTableName name;
+
+    @JsonCreator
+    public SystemTableHandle(@JsonProperty("name") SchemaTableName name) {
+      this.name = name;
+    }
+
+    @JsonProperty
+    public SchemaTableName getName() {
+      return name;
+    }
+  }
+
+  public static class SystemColumnHandle implements ColumnHandle {
+    private final int index;
+
+    @JsonCreator
+    public SystemColumnHandle(@JsonProperty("index") int index) {
+      this.index = index;
+    }
+
+    @JsonProperty
+    public int getIndex() {
+      return index;
+    }
+  }
+}
--- trino-connector/src/test/java/com/datastrato/gravitino/trino/connector/TestGravitinoConnector.java
+++ trino-connector/src/test/java/com/datastrato/gravitino/trino/connector/TestGravitinoConnector.java
@@ -20,6 +20,7 @@ import io.trino.testing.QueryRunner;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.slf4j.Logger;
@@ -260,6 +261,17 @@ public class TestGravitinoConnector extends AbstractTestQueryFramework {
     assertThat(computeActual("show catalogs").getOnlyColumnAsSet()).contains("test1.memory1");
     assertUpdate("call test1.system.drop_catalog('memory1')");
     assertThat(computeActual("show catalogs").getOnlyColumnAsSet()).doesNotContain("test1.memory1");
+  }
+
+  @Test
+  public void testSystemTable() throws Exception {
+    MaterializedResult expectedResult = computeActual("select * from gravitino.system.catalog");
+    assertEquals(expectedResult.getRowCount(), 1);
+    List<MaterializedRow> expectedRows = expectedResult.getMaterializedRows();
+    MaterializedRow row = expectedRows.get(0);
+    assertEquals(row.getField(0), "test.memory");
+    assertEquals(row.getField(1), "memory");
+    assertEquals(row.getField(2), Map.of("max_ttl", "10"));
   }
 
   private TableName createTestTable(String fullTableName) throws Exception {
