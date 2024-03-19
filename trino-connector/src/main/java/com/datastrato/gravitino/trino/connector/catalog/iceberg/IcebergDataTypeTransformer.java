b53ff6bf2598e26dfd365c859e99da489b866e1d
Qi Yu <yuqi@datastrato.com>
2024-03-19 22:06:00 +0800

    [#2569] improvement(trino): Optimize char/varchar mapping for Iceberg catalog between Gravitino and Trino (#2579)
    ### What changes were proposed in this pull request?
    
    Changing the char/varchar conversion for Iceberg catalog between
    Gravitino and Trino.
    
    ### Why are the changes needed?
    
    We need to make type conversion more exactly.
    
    Fix: #2569 
    
    ### Does this PR introduce _any_ user-facing change?
    
    N/A.
    
    ### How was this patch tested?
    
    UT and ITs

--- trino-connector/src/main/java/com/datastrato/gravitino/trino/connector/catalog/iceberg/IcebergDataTypeTransformer.java
+++ trino-connector/src/main/java/com/datastrato/gravitino/trino/connector/catalog/iceberg/IcebergDataTypeTransformer.java
@@ -8,19 +8,34 @@ package com.datastrato.gravitino.trino.connector.catalog.iceberg;
 import com.datastrato.gravitino.rel.types.Type;
 import com.datastrato.gravitino.rel.types.Type.Name;
 import com.datastrato.gravitino.rel.types.Types;
+import com.datastrato.gravitino.trino.connector.GravitinoErrorCode;
 import com.datastrato.gravitino.trino.connector.util.GeneralDataTypeTransformer;
+import io.trino.spi.TrinoException;
 import io.trino.spi.type.VarbinaryType;
+import io.trino.spi.type.VarcharType;
 
 /** Type transformer between Iceberg and Trino */
 public class IcebergDataTypeTransformer extends GeneralDataTypeTransformer {
 
   @Override
   public Type getGravitinoType(io.trino.spi.type.Type type) {
-    Type gravitinoType = super.getGravitinoType(type);
-    if (gravitinoType.name() == Name.VARCHAR || gravitinoType.name() == Name.FIXEDCHAR) {
+    Class<? extends io.trino.spi.type.Type> typeClass = type.getClass();
+    if (typeClass == io.trino.spi.type.CharType.class) {
+      throw new TrinoException(
+          GravitinoErrorCode.GRAVITINO_ILLEGAL_ARGUMENT,
+          "Iceberg does not support the datatype CHAR");
+    } else if (typeClass == io.trino.spi.type.VarcharType.class) {
+      VarcharType varCharType = (VarcharType) type;
+      if (varCharType.getLength().isPresent()) {
+        throw new TrinoException(
+            GravitinoErrorCode.GRAVITINO_ILLEGAL_ARGUMENT,
+            "Iceberg does not support the datatype VARCHAR with length");
+      }
+
       return Types.StringType.get();
     }
-    return gravitinoType;
+
+    return super.getGravitinoType(type);
   }
 
   @Override
