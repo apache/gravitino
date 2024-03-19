8254a2bde7eff41fa1b13a7079af53ffbaa175c8
lipeidian <lipeidian@qq.com>
2024-03-19 17:35:14 +0800

    remove useless code

--- clients/client-python/gravitino_client/core/dto.py
+++ clients/client-python/gravitino_client/core/dto.py
@@ -12,22 +12,3 @@ class VersionDTO:
     compile_date: str
     git_commit: str
 
-
-class AuditDTO:
-    creator: str
-    create_time: datetime
-    last_modifier: str
-    last_modified_time: datetime
-
-class Type(Enum):
-     MANAGED = 'managed'
-     EXTERNAL = 'external'
-
-class FilesetDTO:
-    name: str
-    comment: str
-    type: Type
-    storage_location: str
-    properties: dict
-    audit: AuditDTO
-
