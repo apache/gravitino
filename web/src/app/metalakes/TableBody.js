e4716980d354567fe47e9fa0e66bdb469bb0416d
CHEYNE <contact@ch3yne.com>
2024-03-19 17:04:57 +0800

    fix missing data-refer from merged PR#2574

--- web/src/app/metalakes/TableBody.js
+++ web/src/app/metalakes/TableBody.js
@@ -232,6 +232,7 @@ const TableBody = props => {
           },
           maxHeight: 'calc(100vh - 23.2rem)'
         }}
+        data-refer='metalake-table-grid'
         getRowId={row => row?.name}
         rows={store.filteredMetalakes}
         columns={columns}
