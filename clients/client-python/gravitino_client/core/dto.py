ddb44b1c69e0dc976d8c75b4a4312e19f5f6f25b
lipeidian <lipeidian@qq.com>
2024-03-19 17:38:00 +0800

    remove useless code

--- clients/client-python/gravitino_client/core/dto.py
+++ clients/client-python/gravitino_client/core/dto.py
@@ -3,12 +3,9 @@ Copyright 2024 Datastrato Pvt Ltd.
 This software is licensed under the Apache License version 2.
 """
 from dataclasses import dataclass
-from datetime import datetime
-from enum import Enum
 
 @dataclass
 class VersionDTO:
     version: str
     compile_date: str
     git_commit: str
-
