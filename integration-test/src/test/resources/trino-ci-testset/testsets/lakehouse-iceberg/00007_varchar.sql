389fd2ad208d184071e4df7bae2f9342ba3be747
yuqi <yuqi@datastrato.com>
2024-03-19 11:35:11 +0800

    Fix

--- integration-test/src/test/resources/trino-ci-testset/testsets/lakehouse-iceberg/00007_varchar.sql
+++ integration-test/src/test/resources/trino-ci-testset/testsets/lakehouse-iceberg/00007_varchar.sql
@@ -6,7 +6,7 @@ CREATE TABLE tb01 (id int, name char(20));
 
 CREATE TABLE tb02 (id int, name char);
 
-CREATE TABLE tb03 (id int, name char(233));
+CREATE TABLE tb03 (id int, name varchar(233));
 
 CREATE TABLE tb04 (id int, name varchar);
 
