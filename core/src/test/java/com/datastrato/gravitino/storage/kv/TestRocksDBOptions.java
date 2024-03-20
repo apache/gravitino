00436e126e0175fa91c6a76bfc623a0eca844ea1
unknowntpo <e850506@gmail.com>
2024-03-20 12:53:00 +0800

    fix(doc): add licenses

--- core/src/test/java/com/datastrato/gravitino/storage/kv/TestRocksDBOptions.java
+++ core/src/test/java/com/datastrato/gravitino/storage/kv/TestRocksDBOptions.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2024 Datastrato Pvt Ltd.
+ * This software is licensed under the Apache License version 2.
+ */
+
 package com.datastrato.gravitino.storage.kv;
 
 import com.datastrato.gravitino.Config;
