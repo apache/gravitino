49181d27a28dfcbc73e5e3a537aeae2100f69964
caican00 <imcaican@gmail.com>
2024-03-22 15:06:56 +0800

    fix

--- integration-test/src/test/java/com/datastrato/gravitino/integration/test/spark/iceberg/SparkIcebergCatalogIT.java
+++ integration-test/src/test/java/com/datastrato/gravitino/integration/test/spark/iceberg/SparkIcebergCatalogIT.java
@@ -5,7 +5,11 @@
 package com.datastrato.gravitino.integration.test.spark.iceberg;
 
 import com.datastrato.gravitino.integration.test.spark.SparkCommonIT;
+import java.util.Map;
+import org.apache.spark.sql.catalyst.analysis.NoSuchNamespaceException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @Tag("gravitino-docker-it")
@@ -25,5 +29,53 @@ public class SparkIcebergCatalogIT extends SparkCommonIT {
   @Override
   protected String getUsingClause() {
     return "USING ICEBERG";
+  }
+
+  @Test
+  void testCreateAndLoadSchema() {
+    String testDatabaseName = "t_create1";
+    dropDatabaseIfExists(testDatabaseName);
+    sql("CREATE DATABASE " + testDatabaseName);
+    Map<String, String> databaseMeta = getDatabaseMetadata(testDatabaseName);
+    Assertions.assertFalse(databaseMeta.containsKey("Comment"));
+    Assertions.assertTrue(databaseMeta.containsKey("Location"));
+    Assertions.assertEquals("datastrato", databaseMeta.get("Owner"));
+    String properties = databaseMeta.get("Properties");
+    Assertions.assertEquals("((owner,datastrato))", properties);
+
+    testDatabaseName = "t_create2";
+    dropDatabaseIfExists(testDatabaseName);
+    String testDatabaseLocation = "/tmp/" + testDatabaseName;
+    sql(
+        String.format(
+            "CREATE DATABASE %s COMMENT 'comment' LOCATION '%s'\n" + " WITH DBPROPERTIES (ID=001);",
+            testDatabaseName, testDatabaseLocation));
+    databaseMeta = getDatabaseMetadata(testDatabaseName);
+    String comment = databaseMeta.get("Comment");
+    Assertions.assertEquals("comment", comment);
+    Assertions.assertEquals("datastrato", databaseMeta.get("Owner"));
+    // underlying catalog may change /tmp/t_create2 to file:/tmp/t_create2
+    Assertions.assertTrue(databaseMeta.get("Location").contains(testDatabaseLocation));
+    properties = databaseMeta.get("Properties");
+    Assertions.assertEquals("((owner,datastrato),(ID,001))", properties);
+  }
+
+  @Test
+  void testAlterSchema() {
+    String testDatabaseName = "t_alter";
+    sql("CREATE DATABASE " + testDatabaseName);
+    Assertions.assertEquals(
+        "((owner,datastrato))", getDatabaseMetadata(testDatabaseName).get("Properties"));
+
+    sql(String.format("ALTER DATABASE %s SET DBPROPERTIES ('ID'='001')", testDatabaseName));
+    Assertions.assertEquals(
+        "((owner,datastrato),(ID,001))", getDatabaseMetadata(testDatabaseName).get("Properties"));
+
+    // Hive metastore doesn't support alter database location, therefore this test method
+    // doesn't verify ALTER DATABASE database_name SET LOCATION 'new_location'.
+
+    Assertions.assertThrowsExactly(
+        NoSuchNamespaceException.class,
+        () -> sql("ALTER DATABASE notExists SET DBPROPERTIES ('ID'='001')"));
   }
 }
