246a543541050be01d843c536175e5ef5f95c290
mchades <liminghuang@datastrato.com>
2024-03-26 08:38:41 +0800

    use catalgo info

--- core/src/main/java/com/datastrato/gravitino/catalog/CatalogManager.java
+++ core/src/main/java/com/datastrato/gravitino/catalog/CatalogManager.java
@@ -231,7 +231,7 @@ public class CatalogManager implements SupportsCatalogs, Closeable {
 
     try {
       return store.list(namespace, CatalogEntity.class, EntityType.CATALOG).stream()
-          .map(CatalogEntity::toBasicCatalog)
+          .map(CatalogEntity::toCatalogInfo)
           .toArray(Catalog[]::new);
     } catch (IOException ioe) {
       LOG.error("Failed to list catalogs in metalake {}", metalakeIdent, ioe);
