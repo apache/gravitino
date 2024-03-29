2c6c9233827efa37d7b8690cf18cf81be4784c19
Heng Qin <qqtt@123.com>
2024-03-29 17:21:37 +0800

    [#2235] feat(core): Add the support of Group entity

--- core/src/main/java/com/datastrato/gravitino/authorization/AccessControlManager.java
+++ core/src/main/java/com/datastrato/gravitino/authorization/AccessControlManager.java
@@ -5,6 +5,8 @@
 package com.datastrato.gravitino.authorization;
 
 import com.datastrato.gravitino.EntityStore;
+import com.datastrato.gravitino.exceptions.GroupAlreadyExistsException;
+import com.datastrato.gravitino.exceptions.NoSuchGroupException;
 import com.datastrato.gravitino.exceptions.NoSuchUserException;
 import com.datastrato.gravitino.exceptions.UserAlreadyExistsException;
 import com.datastrato.gravitino.storage.IdGenerator;
@@ -16,9 +18,11 @@ import com.datastrato.gravitino.storage.IdGenerator;
 public class AccessControlManager {
 
   private final UserManager userManager;
+  private final GroupManager groupManager;
 
   public AccessControlManager(EntityStore store, IdGenerator idGenerator) {
     this.userManager = new UserManager(store, idGenerator);
+    this.groupManager = new GroupManager(store, idGenerator);
   }
 
   /**
@@ -57,5 +61,43 @@ public class AccessControlManager {
    */
   public User getUser(String metalake, String user) throws NoSuchUserException {
     return userManager.getUser(metalake, user);
+  }
+
+  /**
+   * Adds a new Group.
+   *
+   * @param metalake The Metalake of the Group.
+   * @param group The name of the Group.
+   * @return The Added Group instance.
+   * @throws GroupAlreadyExistsException If a Group with the same identifier already exists.
+   * @throws RuntimeException If creating the Group encounters storage issues.
+   */
+  public Group addGroup(String metalake, String group) throws GroupAlreadyExistsException {
+    return groupManager.addGroup(metalake, group);
+  }
+
+  /**
+   * Removes a Group.
+   *
+   * @param metalake The Metalake of the Group.
+   * @param group THe name of the Group.
+   * @return `true` if the Group was successfully deleted, `false` otherwise.
+   * @throws RuntimeException If deleting the Group encounters storage issues.
+   */
+  public boolean removeGroup(String metalake, String group) {
+    return groupManager.removeGroup(metalake, group);
+  }
+
+  /**
+   * Gets a Group.
+   *
+   * @param metalake The Metalake of the Group.
+   * @param group THe name of the Group.
+   * @return The getting Group instance.
+   * @throws NoSuchGroupException If the Group with the given identifier does not exist.
+   * @throws RuntimeException If loading the Group encounters storage issues.
+   */
+  public Group getGroup(String metalake, String group) throws NoSuchGroupException {
+    return groupManager.getGroup(metalake, group);
   }
 }
--- core/src/main/java/com/datastrato/gravitino/storage/kv/BinaryEntityKeyEncoder.java
+++ core/src/main/java/com/datastrato/gravitino/storage/kv/BinaryEntityKeyEncoder.java
@@ -6,6 +6,7 @@ package com.datastrato.gravitino.storage.kv;
 
 import static com.datastrato.gravitino.Entity.EntityType.CATALOG;
 import static com.datastrato.gravitino.Entity.EntityType.FILESET;
+import static com.datastrato.gravitino.Entity.EntityType.GROUP;
 import static com.datastrato.gravitino.Entity.EntityType.METALAKE;
 import static com.datastrato.gravitino.Entity.EntityType.SCHEMA;
 import static com.datastrato.gravitino.Entity.EntityType.TABLE;
@@ -78,6 +79,8 @@ public class BinaryEntityKeyEncoder implements EntityKeyEncoder<byte[]> {
           FILESET,
           new String[] {FILESET.getShortName() + "/", "/", "/", "/"},
           USER,
+          new String[] {USER.getShortName() + "/", "/", "/", "/"},
+          GROUP,
           new String[] {USER.getShortName() + "/", "/", "/", "/"});
 
   @VisibleForTesting final NameMappingService nameMappingService;
