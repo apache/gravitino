db14ff317c994cdcb1d53e617ce1e5bc82670aee
YxAc <yongxing@YxAcdeMacBook-Air.local>
2024-03-29 14:30:58 +0800

    update

--- core/src/test/java/com/datastrato/gravitino/storage/kv/TestKvEntityStorage.java
+++ core/src/test/java/com/datastrato/gravitino/storage/kv/TestKvEntityStorage.java
@@ -5,7 +5,12 @@
 
 package com.datastrato.gravitino.storage.kv;
 
+import static com.datastrato.gravitino.Configs.DEFAULT_ENTITY_KV_STORE;
+import static com.datastrato.gravitino.Configs.ENTITY_KV_STORE;
+import static com.datastrato.gravitino.Configs.ENTITY_STORE;
 import static com.datastrato.gravitino.Configs.ENTRY_KV_ROCKSDB_BACKEND_PATH;
+import static com.datastrato.gravitino.Configs.STORE_DELETE_AFTER_TIME;
+import static com.datastrato.gravitino.Configs.STORE_TRANSACTION_MAX_SKEW_TIME;
 
 import com.datastrato.gravitino.Config;
 import com.datastrato.gravitino.Configs;
@@ -55,11 +60,20 @@ public class TestKvEntityStorage extends TestEntityStorage {
     }
   }
 
+  public Config getConfig() throws IOException {
+    Config config = Mockito.mock(Config.class);
+    Mockito.when(config.get(ENTITY_STORE)).thenReturn("kv");
+    Mockito.when(config.get(ENTITY_KV_STORE)).thenReturn(DEFAULT_ENTITY_KV_STORE);
+    Mockito.when(config.get(Configs.ENTITY_SERDE)).thenReturn("proto");
+    Mockito.when(config.get(ENTRY_KV_ROCKSDB_BACKEND_PATH)).thenReturn("/tmp/gravitino");
+    Mockito.when(config.get(STORE_TRANSACTION_MAX_SKEW_TIME)).thenReturn(1000L);
+    Mockito.when(config.get(STORE_DELETE_AFTER_TIME)).thenReturn(20 * 60 * 1000L);
+    return config;
+  }
+
   @Test
   void testCreateKvEntityStore() throws IOException {
     Config config = getConfig();
-    Mockito.when(config.get(ENTRY_KV_ROCKSDB_BACKEND_PATH)).thenReturn("/tmp/gravitino");
-
     FileUtils.deleteDirectory(FileUtils.getFile("/tmp/gravitino"));
 
     try (EntityStore store = EntityStoreFactory.createEntityStore(config)) {
