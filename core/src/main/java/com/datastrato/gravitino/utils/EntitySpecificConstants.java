e9348dc3055e82f0b4f7a8a4effb230484083756
Heng Qin <qqtt@123.com>
2024-04-02 21:04:11 +0800

    fix

--- core/src/main/java/com/datastrato/gravitino/utils/EntitySpecificConstants.java
+++ core/src/main/java/com/datastrato/gravitino/utils/EntitySpecificConstants.java
@@ -5,12 +5,12 @@
 package com.datastrato.gravitino.utils;
 
 public class EntitySpecificConstants {
-  public static final String AUTHORIZATION_CATALOG_NAME = "authorization";
 
   private EntitySpecificConstants() {}
 
   public static final String SYSTEM_METALAKE_RESERVED_NAME = "system";
   public static final String SYSTEM_CATALOG_RESERVED_NAME = "system";
+  public static final String AUTHORIZATION_CATALOG_NAME = "authorization";
   public static final String USER_SCHEMA_NAME = "user";
   public static final String GROUP_SCHEMA_NAME = "group";
   public static final String ADMIN_SCHEMA_NAME = "admin";
