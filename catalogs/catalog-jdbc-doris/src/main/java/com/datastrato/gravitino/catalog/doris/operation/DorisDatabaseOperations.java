f41bbb269292a21b1304f264ed716eceb39e9887
Zhou Kang <zhoukangcn@gmail.com>
2024-04-02 18:32:30 +0800

    replace use system table

--- catalogs/catalog-jdbc-doris/src/main/java/com/datastrato/gravitino/catalog/doris/operation/DorisDatabaseOperations.java
+++ catalogs/catalog-jdbc-doris/src/main/java/com/datastrato/gravitino/catalog/doris/operation/DorisDatabaseOperations.java
@@ -16,6 +16,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
 
@@ -51,16 +52,16 @@ public class DorisDatabaseOperations extends JdbcDatabaseOperations {
       return sqlBuilder.toString();
     }
 
-    try (final Connection connection = this.dataSource.getConnection()) {
-      String query = String.format("SHOW TABLES IN `%s`", databaseName);
-      try (Statement statement = connection.createStatement();
-          ResultSet resultSet = statement.executeQuery(query)) {
-        // Execute the query and check if there exists any tables in the database
-        if (resultSet.next()) {
-          throw new IllegalStateException(
-              String.format(
-                  "Database %s is not empty, the value of cascade should be true.", databaseName));
-        }
+    // Execute the query and check if there exists any tables in the database
+    String query = String.format("SHOW TABLES IN `%s`", databaseName);
+
+    try (final Connection connection = this.dataSource.getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(query)) {
+      if (resultSet.next()) {
+        throw new IllegalStateException(
+            String.format(
+                "Database %s is not empty, the value of cascade should be true.", databaseName));
       }
     } catch (SQLException sqlException) {
       throw this.exceptionMapper.toGravitinoException(sqlException);
@@ -71,50 +72,41 @@ public class DorisDatabaseOperations extends JdbcDatabaseOperations {
 
   @Override
   public JdbcSchema load(String databaseName) throws NoSuchSchemaException {
-    String query = "SELECT * FROM information_schema.SCHEMATA WHERE SCHEMA_NAME = ?";
+    List<String> allDatabases = listDatabases();
+    String dbName =
+        allDatabases.stream()
+            .filter(db -> db.equals(databaseName))
+            .findFirst()
+            .orElseThrow(
+                () -> new NoSuchSchemaException("Database %s could not be found", databaseName));
 
-    try (final Connection connection = this.dataSource.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-      preparedStatement.setString(1, databaseName);
+    Map<String, String> properties = getDatabaseProperties(databaseName);
 
-      // Execute the query
-      try (ResultSet resultSet = preparedStatement.executeQuery()) {
-        if (!resultSet.next()) {
-          throw new NoSuchSchemaException(
-              "Database %s could not be found in information_schema.SCHEMATA", databaseName);
-        }
-        String schemaName = resultSet.getString("SCHEMA_NAME");
-        Map<String, String> properties = getDatabaseProperties(connection, databaseName);
-        return JdbcSchema.builder()
-            .withName(schemaName)
-            .withProperties(properties)
-            .withAuditInfo(AuditInfo.EMPTY)
-            .build();
-      }
-    } catch (final SQLException se) {
-      throw this.exceptionMapper.toGravitinoException(se);
-    }
+    return JdbcSchema.builder()
+        .withName(dbName)
+        .withProperties(properties)
+        .withAuditInfo(AuditInfo.EMPTY)
+        .build();
   }
 
-  protected Map<String, String> getDatabaseProperties(Connection connection, String databaseName)
-      throws SQLException {
+  protected Map<String, String> getDatabaseProperties(String databaseName) {
 
     String showCreateDatabaseSql = String.format("SHOW CREATE DATABASE `%s`", databaseName);
-
     StringBuilder createDatabaseSb = new StringBuilder();
-    try (PreparedStatement statement = connection.prepareStatement(showCreateDatabaseSql)) {
-      try (ResultSet resultSet = statement.executeQuery()) {
-        while (resultSet.next()) {
-          createDatabaseSb.append(resultSet.getString("Create Database"));
-        }
+    try (final Connection connection = this.dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(showCreateDatabaseSql);
+        ResultSet resultSet = statement.executeQuery()) {
+      while (resultSet.next()) {
+        createDatabaseSb.append(resultSet.getString("Create Database"));
       }
+    } catch (SQLException sqlException) {
+      throw this.exceptionMapper.toGravitinoException(sqlException);
     }
 
     String createDatabaseSql = createDatabaseSb.toString();
 
     if (StringUtils.isEmpty(createDatabaseSql)) {
-      throw new NoSuchTableException(
-          "Database %s does not exist in %s.", databaseName, connection.getCatalog());
+      throw new NoSuchTableException("Database %s does not exist.", databaseName);
     }
 
     return Collections.unmodifiableMap(DorisUtils.extractPropertiesFromSql(createDatabaseSql));
