e4b31833e8f332867c192829377677309915c6c6
Heng Qin <qqtt@123.com>
2024-04-03 19:23:58 +0800

    address comments

---
+++ server/src/main/java/com/datastrato/gravitino/server/web/AccessControlNotAllowedFilter.java
@@ -0,0 +1,23 @@
+package com.datastrato.gravitino.server.web;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class AccessControlAllowFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
