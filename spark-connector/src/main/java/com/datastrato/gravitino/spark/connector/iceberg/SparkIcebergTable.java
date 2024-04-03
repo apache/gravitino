e3fcf4218e30b3ff2bf551fb1f6aed7dc1f46d8b
caican00 <imcaican@gmail.com>
2024-04-03 11:20:11 +0800

    update

--- spark-connector/src/main/java/com/datastrato/gravitino/spark/connector/iceberg/SparkIcebergTable.java
+++ spark-connector/src/main/java/com/datastrato/gravitino/spark/connector/iceberg/SparkIcebergTable.java
@@ -9,7 +9,9 @@ import com.datastrato.gravitino.rel.Table;
 import com.datastrato.gravitino.spark.connector.PropertiesConverter;
 import com.datastrato.gravitino.spark.connector.table.SparkBaseTable;
 import org.apache.spark.sql.connector.catalog.Identifier;
+import org.apache.spark.sql.connector.catalog.MetadataColumn;
 import org.apache.spark.sql.connector.catalog.SupportsDelete;
+import org.apache.spark.sql.connector.catalog.SupportsMetadataColumns;
 import org.apache.spark.sql.connector.catalog.SupportsRowLevelOperations;
 import org.apache.spark.sql.connector.catalog.TableCatalog;
 import org.apache.spark.sql.connector.write.RowLevelOperationBuilder;
@@ -17,7 +19,7 @@ import org.apache.spark.sql.connector.write.RowLevelOperationInfo;
 import org.apache.spark.sql.sources.Filter;
 
 public class SparkIcebergTable extends SparkBaseTable
-    implements SupportsDelete, SupportsRowLevelOperations {
+    implements SupportsDelete, SupportsRowLevelOperations, SupportsMetadataColumns {
 
   public SparkIcebergTable(
       Identifier identifier,
@@ -40,5 +42,10 @@ public class SparkIcebergTable extends SparkBaseTable
   @Override
   public RowLevelOperationBuilder newRowLevelOperationBuilder(RowLevelOperationInfo info) {
     return ((SupportsRowLevelOperations) getSparkTable()).newRowLevelOperationBuilder(info);
+  }
+
+  @Override
+  public MetadataColumn[] metadataColumns() {
+    return ((SupportsMetadataColumns) getSparkTable()).metadataColumns();
   }
 }
