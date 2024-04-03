297e2fb5caba3238f0013d0af8a8d63e52e8ea02
caican00 <imcaican@gmail.com>
2024-04-03 15:07:18 +0800

    update

--- integration-test/src/test/java/com/datastrato/gravitino/integration/test/spark/iceberg/SparkIcebergCatalogIT.java
+++ integration-test/src/test/java/com/datastrato/gravitino/integration/test/spark/iceberg/SparkIcebergCatalogIT.java
@@ -5,8 +5,15 @@
 package com.datastrato.gravitino.integration.test.spark.iceberg;
 
 import com.datastrato.gravitino.integration.test.spark.SparkCommonIT;
+import org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions;
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.internal.StaticSQLConf;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.platform.commons.util.StringUtils;
 
 @Tag("gravitino-docker-it")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -35,5 +42,15 @@ public class SparkIcebergCatalogIT extends SparkCommonIT {
   @Override
   protected boolean supportsDelete() {
     return true;
+  }
+
+  @Test
+  void testInjectSparkExtensions() {
+    SparkSession sparkSession = getSparkSession();
+    SparkConf conf = sparkSession.sparkContext().getConf();
+    Assertions.assertTrue(conf.contains(StaticSQLConf.SPARK_SESSION_EXTENSIONS().key()));
+    String extensions = conf.get(StaticSQLConf.SPARK_SESSION_EXTENSIONS().key());
+    Assertions.assertTrue(StringUtils.isNotBlank(extensions));
+    Assertions.assertEquals(IcebergSparkSessionExtensions.class.getName(), extensions);
   }
 }
