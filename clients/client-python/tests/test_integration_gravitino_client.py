a4e2bd1b7ac9314449ba144b05b3af6b0059bff5
Xun Liu <xun@datastrato.com>
2024-04-07 18:56:21 +0800

    [#2756] feat(PyClient): Integration test with Gravitino Python client (#2797)
    ### What changes were proposed in this pull request?
    
    Add two Gradle command in the client-python module
    1. `./gradlew :clients:client-python:test`
    2. `./gradlew :clients:client-python: integrationTest`
    
    ### Why are the changes needed?
    
    Proved a real test environment for Gravitino server. 
    
    Fix: #2756
    
    ### Does this PR introduce _any_ user-facing change?
    
    N/A
    
    ### How was this patch tested?
    
    CI Passed

---
+++ clients/client-python/tests/test_integration_gravitino_client.py
@@ -0,0 +1,23 @@
+"""
+Copyright 2024 Datastrato Pvt Ltd.
+This software is licensed under the Apache License version 2.
+"""
+
+import os
+import unittest
+from tests.integration_test_env import IntegrationTestEnv
+
+
+@unittest.skipIf(IntegrationTestEnv.not_in_ITE(),
+                 "Currently, Python Client integration test only runs in the Gradle build environment")
+class IntegrationTestGravitinoClient(IntegrationTestEnv):
+    def test_version(self):
+        versionDTO = self.client.version
+        assert versionDTO['version'] is not None
+
+        # Get project version from environment (Setting by Gradle build script `build.gradle.kts`),
+        # But if you directly execute this test in IDEA, maybe can not get it.
+        projectVersion = os.environ.get('PROJECT_VERSION', '')
+
+        if projectVersion != '':
+            assert versionDTO['version'] == projectVersion
