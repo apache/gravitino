db9243a5d1cd97195ca9a69b717b3164b2fcc7d4
yuqi <yuqi@datastrato.com>
2024-04-08 19:17:36 +0800

    fix

--- dev/ci/check_commands.sh
+++ dev/ci/check_commands.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+#
+# Copyright 2024 Datastrato Pvt Ltd.
+# This software is licensed under the Apache License version 2.
+#
 
-if ! command -v docker &> /dev/null; then
-    echo "docker could not be found, exiting."
+if ! command -v docker &> /dev/null || file $(which docker) | grep -q 'executable';  then
+    echo "docker could not be found or is not a real executable file, exiting."
     exit 1
 fi
 
-if ! command -v docker-proxy &> /dev/null; then
-    echo "docker-proxy could not be found, exiting."
+if ! command -v docker-proxy &> /dev/null || ! file $(which docker-proxy) | grep -q 'executable'; then
+    echo "docker-proxy could not be found or is not a real executable file, exiting."
     exit 1
 fi
 
