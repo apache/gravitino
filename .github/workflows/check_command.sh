d6734f7a7f32edcf75839584519baf000117c0d8
yuqi <yuqi@datastrato.com>
2024-04-08 17:50:35 +0800

    Add check_command.sh in the pipeline

---
+++ .github/workflows/check_command.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if ! command -v docker &> /dev/null; then
+    echo "docker could not be found, exiting."
+    exit 1
+fi
+
+if ! command -v docker-proxy &> /dev/null; then
+    echo "wget could not be found, exiting."
+    exit 1
+fi
+
+# More commands can be added here
+echo "All required commands are installed."
