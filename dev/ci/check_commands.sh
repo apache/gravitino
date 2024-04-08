4c6b9608887b8f62ef62165b990ce21e9433e77f
yuqi <yuqi@datastrato.com>
2024-04-08 19:24:49 +0800

    fix

--- dev/ci/check_commands.sh
+++ dev/ci/check_commands.sh
@@ -4,7 +4,7 @@
 # This software is licensed under the Apache License version 2.
 #
 
-if ! command -v docker &> /dev/null || file $(which docker) | grep -q 'executable';  then
+if ! command -v docker &> /dev/null || ! file $(which docker) | grep -q 'executable';  then
     echo "docker could not be found or is not a real executable file, exiting."
     exit 1
 fi
