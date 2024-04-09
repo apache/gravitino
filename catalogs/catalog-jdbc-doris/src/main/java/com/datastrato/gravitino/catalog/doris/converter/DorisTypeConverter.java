4fa92221e7911e338421aa6384cb4d49652b22e2
Zhou Kang <zhoukangcn@gmail.com>
2024-04-10 00:07:46 +0800

    add Doris table operation

--- catalogs/catalog-jdbc-doris/src/main/java/com/datastrato/gravitino/catalog/doris/converter/DorisTypeConverter.java
+++ catalogs/catalog-jdbc-doris/src/main/java/com/datastrato/gravitino/catalog/doris/converter/DorisTypeConverter.java
@@ -10,15 +10,96 @@ import com.datastrato.gravitino.rel.types.Types;
 
 /** Type converter for Doris. */
 public class DorisTypeConverter extends JdbcTypeConverter<String> {
-  // TODO: add implementation for doris catalog
+  static final String BOOLEAN = "boolean";
+  static final String TINYINT = "tinyint";
+  static final String SMALLINT = "smallint";
+  static final String INT = "int";
+  static final String BIGINT = "bigint";
+  static final String FLOAT = "float";
+  static final String DOUBLE = "double";
+  static final String DECIMAL = "decimal";
+  static final String DATETIME = "datetime";
+  static final String CHAR = "char";
+  static final String STRING = "string";
 
   @Override
   public Type toGravitinoType(JdbcTypeBean typeBean) {
-    return Types.UnparsedType.of(typeBean.getTypeName());
+    switch (typeBean.getTypeName().toLowerCase()) {
+      case BOOLEAN:
+        return Types.BooleanType.get();
+      case TINYINT:
+        return Types.ByteType.get();
+      case SMALLINT:
+        return Types.ShortType.get();
+      case INT:
+        return Types.IntegerType.get();
+      case BIGINT:
+        return Types.LongType.get();
+      case FLOAT:
+        return Types.FloatType.get();
+      case DOUBLE:
+        return Types.DoubleType.get();
+      case DECIMAL:
+        return Types.DecimalType.of(
+            Integer.parseInt(typeBean.getColumnSize()), Integer.parseInt(typeBean.getScale()));
+      case DATE:
+        return Types.DateType.get();
+      case DATETIME:
+        return Types.TimestampType.withTimeZone();
+      case CHAR:
+        return Types.FixedCharType.of(Integer.parseInt(typeBean.getColumnSize()));
+      case VARCHAR:
+        return Types.VarCharType.of(Integer.parseInt(typeBean.getColumnSize()));
+      case STRING:
+      case TEXT:
+        return Types.StringType.get();
+      default:
+        throw new IllegalArgumentException("Not a supported type: " + typeBean);
+    }
   }
 
   @Override
   public String fromGravitinoType(Type type) {
-    throw new IllegalArgumentException("unsupported type: " + type.simpleString());
+    if (type instanceof Types.BooleanType) {
+      return BOOLEAN;
+    } else if (type instanceof Types.ByteType) {
+      return TINYINT;
+    } else if (type instanceof Types.ShortType) {
+      return SMALLINT;
+    } else if (type instanceof Types.IntegerType) {
+      return INT;
+    } else if (type instanceof Types.LongType) {
+      return BIGINT;
+    } else if (type instanceof Types.FloatType) {
+      return FLOAT;
+    } else if (type instanceof Types.DoubleType) {
+      return DOUBLE;
+    } else if (type instanceof Types.DecimalType) {
+      return DECIMAL
+          + "("
+          + ((Types.DecimalType) type).precision()
+          + ","
+          + ((Types.DecimalType) type).scale()
+          + ")";
+    } else if (type instanceof Types.DateType) {
+      return DATE;
+    } else if (type instanceof Types.TimestampType) {
+      return DATETIME;
+    } else if (type instanceof Types.VarCharType) {
+      return VARCHAR + "(" + ((Types.VarCharType) type).length() + ")";
+    } else if (type instanceof Types.FixedCharType) {
+      int length = ((Types.FixedCharType) type).length();
+      if (length < 1 || length > 255) {
+        throw new IllegalArgumentException(
+            String.format(
+                "Type %s is invalid, length should be between 1 and 255", type.toString()));
+      }
+
+      return CHAR + "(" + ((Types.FixedCharType) type).length() + ")";
+    } else if (type instanceof Types.StringType) {
+      return STRING;
+    }
+    throw new IllegalArgumentException(
+        String.format("Couldn't convert Gravitino type %s to Doris type", type.toString()));
   }
 }
---
+++ catalogs/catalog-jdbc-doris/src/test/java/com/datastrato/gravitino/catalog/doris/integration/test/DorisTableOperationsIT.java
@@ -0,0 +1,246 @@
+/*
+ * Copyright 2023 Datastrato Pvt Ltd.
+ * This software is licensed under the Apache License version 2.
+ */
+package com.datastrato.gravitino.catalog.doris.integration.test;
+
+import com.datastrato.gravitino.catalog.jdbc.JdbcColumn;
+import com.datastrato.gravitino.catalog.jdbc.JdbcTable;
+import com.datastrato.gravitino.integration.test.util.GravitinoITUtils;
+import com.datastrato.gravitino.rel.TableChange;
+import com.datastrato.gravitino.rel.expressions.NamedReference;
+import com.datastrato.gravitino.rel.expressions.distributions.Distribution;
+import com.datastrato.gravitino.rel.expressions.distributions.Distributions;
+import com.datastrato.gravitino.rel.indexes.Index;
+import com.datastrato.gravitino.rel.types.Type;
+import com.datastrato.gravitino.rel.types.Types;
+import com.google.common.collect.Maps;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("gravitino-docker-it")
+public class DorisTableOperationsIT extends TestDorisAbstractIT {
+  private static final Type VARCHAR_255 = Types.VarCharType.of(255);
+  private static final Type VARCHAR_1024 = Types.VarCharType.of(1024);
+
+  private static Type INT = Types.IntegerType.get();
+
+  private static final String databaseName = GravitinoITUtils.genRandomName("doris_test_db");
+
+  @BeforeAll
+  public static void startup() {
+    TestDorisAbstractIT.startup();
+    createDatabase();
+  }
+
+  private static void createDatabase() {
+    DATABASE_OPERATIONS.create(databaseName, "test_comment", new HashMap<>());
+  }
+
+  private static Map<String, String> createProperties() {
+    Map<String, String> properties = Maps.newHashMap();
+    properties.put("replication_allocation", "tag.location.default: 1");
+    return properties;
+  }
+
+  private static void waitForDorisOperation() {
+    // TODO: use a better way to wait for the operation to complete
+    // see: https://doris.apache.org/docs/1.2/advanced/alter-table/schema-change/
+    try {
+      Thread.sleep(2000);
+    } catch (InterruptedException e) {
+      // do nothing
+    }
+  }
+
+  @Test
+  public void testBasicTableOperation() {
+    String tableName = GravitinoITUtils.genRandomName("doris_basic_test_table");
+    String tableComment = "test_comment";
+    List<JdbcColumn> columns = new ArrayList<>();
+    JdbcColumn col_1 =
+        JdbcColumn.builder().withName("col_1").withType(INT).withComment("id").build();
+    columns.add(col_1);
+    JdbcColumn col_2 =
+        JdbcColumn.builder().withName("col_2").withType(VARCHAR_255).withComment("col_2").build();
+    columns.add(col_2);
+    JdbcColumn col_3 =
+        JdbcColumn.builder().withName("col_3").withType(VARCHAR_255).withComment("col_3").build();
+    columns.add(col_3);
+    Map<String, String> properties = new HashMap<>();
+
+    Distribution distribution = Distributions.hash(32, NamedReference.field("col_1"));
+    Index[] indexes = new Index[] {};
+
+    // create table
+    TABLE_OPERATIONS.create(
+        databaseName,
+        tableName,
+        columns.toArray(new JdbcColumn[0]),
+        tableComment,
+        createProperties(),
+        null,
+        distribution,
+        indexes);
+    List<String> listTables = TABLE_OPERATIONS.listTables(databaseName);
+    Assertions.assertTrue(listTables.contains(tableName));
+    JdbcTable load = TABLE_OPERATIONS.load(databaseName, tableName);
+    assertionsTableInfo(tableName, tableComment, columns, properties, indexes, load);
+  }
+
+  @Test
+  public void testAlterTable() {
+    String tableName = GravitinoITUtils.genRandomName("doris_alter_test_table");
+
+    String tableComment = "test_comment";
+    List<JdbcColumn> columns = new ArrayList<>();
+    JdbcColumn col_1 =
+        JdbcColumn.builder().withName("col_1").withType(INT).withComment("id").build();
+    columns.add(col_1);
+    JdbcColumn col_2 =
+        JdbcColumn.builder().withName("col_2").withType(VARCHAR_255).withComment("col_2").build();
+    columns.add(col_2);
+    JdbcColumn col_3 =
+        JdbcColumn.builder().withName("col_3").withType(VARCHAR_255).withComment("col_3").build();
+    columns.add(col_3);
+    Map<String, String> properties = new HashMap<>();
+
+    Distribution distribution = Distributions.hash(32, NamedReference.field("col_1"));
+    Index[] indexes = new Index[] {};
+
+    // create table
+    TABLE_OPERATIONS.create(
+        databaseName,
+        tableName,
+        columns.toArray(new JdbcColumn[0]),
+        tableComment,
+        createProperties(),
+        null,
+        distribution,
+        indexes);
+    JdbcTable load = TABLE_OPERATIONS.load(databaseName, tableName);
+    assertionsTableInfo(tableName, tableComment, columns, properties, indexes, load);
+
+    TABLE_OPERATIONS.alterTable(
+        databaseName,
+        tableName,
+        TableChange.updateColumnType(new String[] {col_3.name()}, VARCHAR_1024));
+
+    waitForDorisOperation();
+
+    load = TABLE_OPERATIONS.load(databaseName, tableName);
+
+    // After modifying the type, check it
+    columns.clear();
+    col_3 =
+        JdbcColumn.builder()
+            .withName(col_3.name())
+            .withType(VARCHAR_1024)
+            .withComment(col_3.comment())
+            .build();
+    columns.add(col_1);
+    columns.add(col_2);
+    columns.add(col_3);
+    assertionsTableInfo(tableName, tableComment, columns, properties, indexes, load);
+
+    String colNewComment = "new_comment";
+    // update column comment
+
+    TABLE_OPERATIONS.alterTable(
+        databaseName,
+        tableName,
+        TableChange.updateColumnComment(new String[] {col_2.name()}, colNewComment));
+    load = TABLE_OPERATIONS.load(databaseName, tableName);
+
+    columns.clear();
+    col_2 =
+        JdbcColumn.builder()
+            .withName(col_2.name())
+            .withType(col_2.dataType())
+            .withComment(colNewComment)
+            .build();
+    columns.add(col_1);
+    columns.add(col_2);
+    columns.add(col_3);
+    assertionsTableInfo(tableName, tableComment, columns, properties, indexes, load);
+
+    // add new column
+    TABLE_OPERATIONS.alterTable(
+        databaseName,
+        tableName,
+        TableChange.addColumn(new String[] {"col_4"}, VARCHAR_255, "txt4", true));
+
+    waitForDorisOperation();
+    load = TABLE_OPERATIONS.load(databaseName, tableName);
+
+    columns.clear();
+    JdbcColumn col_4 =
+        JdbcColumn.builder().withName("col_4").withType(VARCHAR_255).withComment("txt4").build();
+    columns.add(col_1);
+    columns.add(col_2);
+    columns.add(col_3);
+    columns.add(col_4);
+    assertionsTableInfo(tableName, tableComment, columns, properties, indexes, load);
+
+    // change column position
+    TABLE_OPERATIONS.alterTable(
+        databaseName,
+        tableName,
+        TableChange.updateColumnPosition(
+            new String[] {"col_3"}, TableChange.ColumnPosition.after("col_4")));
+    waitForDorisOperation();
+    load = TABLE_OPERATIONS.load(databaseName, tableName);
+
+    columns.clear();
+    columns.add(col_1);
+    columns.add(col_2);
+    columns.add(col_4);
+    columns.add(col_3);
+    assertionsTableInfo(tableName, tableComment, columns, properties, indexes, load);
+  }
+
+  @Test
+  public void testCreateAllTypeTable() {
+    String tableName = GravitinoITUtils.genRandomName("type_table");
+    String tableComment = "test_comment";
+    List<JdbcColumn> columns = new ArrayList<>();
+    columns.add(JdbcColumn.builder().withName("col_1").withType(Types.IntegerType.get()).build());
+    columns.add(JdbcColumn.builder().withName("col_2").withType(Types.BooleanType.get()).build());
+    columns.add(JdbcColumn.builder().withName("col_3").withType(Types.ByteType.get()).build());
+    columns.add(JdbcColumn.builder().withName("col_4").withType(Types.ShortType.get()).build());
+    columns.add(JdbcColumn.builder().withName("col_5").withType(Types.IntegerType.get()).build());
+    columns.add(JdbcColumn.builder().withName("col_6").withType(Types.LongType.get()).build());
+    columns.add(JdbcColumn.builder().withName("col_7").withType(Types.FloatType.get()).build());
+    columns.add(JdbcColumn.builder().withName("col_8").withType(Types.DoubleType.get()).build());
+    columns.add(
+        JdbcColumn.builder().withName("col_9").withType(Types.DecimalType.of(10, 2)).build());
+    columns.add(JdbcColumn.builder().withName("col_10").withType(Types.DateType.get()).build());
+    columns.add(
+        JdbcColumn.builder().withName("col_11").withType(Types.FixedCharType.of(10)).build());
+    columns.add(JdbcColumn.builder().withName("col_12").withType(Types.VarCharType.of(10)).build());
+    columns.add(JdbcColumn.builder().withName("col_13").withType(Types.StringType.get()).build());
+
+    Distribution distribution = Distributions.hash(32, NamedReference.field("col_1"));
+    Index[] indexes = new Index[] {};
+    // create table
+    TABLE_OPERATIONS.create(
+        databaseName,
+        tableName,
+        columns.toArray(new JdbcColumn[0]),
+        tableComment,
+        createProperties(),
+        null,
+        distribution,
+        indexes);
+
+    JdbcTable load = TABLE_OPERATIONS.load(databaseName, tableName);
+    assertionsTableInfo(tableName, tableComment, columns, Collections.emptyMap(), null, load);
+  }
+}
