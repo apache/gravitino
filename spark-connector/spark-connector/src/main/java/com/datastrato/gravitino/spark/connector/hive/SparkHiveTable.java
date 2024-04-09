d56cc482b60474339249f70c8a1ec2e8c7ddc404
caican00 <imcaican@gmail.com>
2024-04-09 21:32:43 +0800

    [#2845] Improvement[spark-connector]: Refactor the table implementation in spark-connector

--- spark-connector/spark-connector/src/main/java/com/datastrato/gravitino/spark/connector/hive/SparkHiveTable.java
+++ spark-connector/spark-connector/src/main/java/com/datastrato/gravitino/spark/connector/hive/SparkHiveTable.java
@@ -8,13 +8,23 @@ package com.datastrato.gravitino.spark.connector.hive;
 import com.datastrato.gravitino.rel.Table;
 import com.datastrato.gravitino.spark.connector.PropertiesConverter;
 import com.datastrato.gravitino.spark.connector.table.SparkBaseTable;
+import java.util.Map;
+import java.util.Set;
 import lombok.Getter;
+import org.apache.kyuubi.spark.connector.hive.HiveTable;
 import org.apache.spark.sql.connector.catalog.Identifier;
+import org.apache.spark.sql.connector.catalog.TableCapability;
 import org.apache.spark.sql.connector.catalog.TableCatalog;
+import org.apache.spark.sql.connector.expressions.Transform;
+import org.apache.spark.sql.connector.read.ScanBuilder;
+import org.apache.spark.sql.connector.write.LogicalWriteInfo;
+import org.apache.spark.sql.connector.write.WriteBuilder;
+import org.apache.spark.sql.types.StructType;
+import org.apache.spark.sql.util.CaseInsensitiveStringMap;
 
 /** May support more capabilities like partition management. */
 @Getter
-public class SparkHiveTable implements SparkBaseTable {
+public class SparkHiveTable extends HiveTable implements SparkBaseTable {
 
   private final Identifier identifier;
   private final Table gravitinoTable;
@@ -28,6 +38,7 @@ public class SparkHiveTable implements SparkBaseTable {
       TableCatalog sparkHiveCatalog,
       org.apache.spark.sql.connector.catalog.Table sparkHiveTable,
       PropertiesConverter propertiesConverter) {
+    super();
     this.identifier = identifier;
     this.gravitinoTable = gravitinoTable;
     this.sparkCatalog = sparkHiveCatalog;
@@ -36,7 +47,58 @@ public class SparkHiveTable implements SparkBaseTable {
   }
 
   @Override
+  public String name() {
+    return SparkBaseTable.super.name();
+  }
+
+  @Override
+  @SuppressWarnings("deprecation")
+  public StructType schema() {
+    return SparkBaseTable.super.schema();
+  }
+
+  @Override
+  public Map<String, String> properties() {
+    return SparkBaseTable.super.properties();
+  }
+
+  @Override
+  public Set<TableCapability> capabilities() {
+    return SparkBaseTable.super.capabilities();
+  }
+
+  @Override
+  public ScanBuilder newScanBuilder(CaseInsensitiveStringMap options) {
+    return SparkBaseTable.super.newScanBuilder(options);
+  }
+
+  @Override
+  public WriteBuilder newWriteBuilder(LogicalWriteInfo info) {
+    return SparkBaseTable.super.newWriteBuilder(info);
+  }
+
+  @Override
+  public Transform[] partitioning() {
+    return SparkBaseTable.super.partitioning();
+  }
+
+  @Override
   public boolean isCaseSensitive() {
     return false;
+  }
+
+  @Override
+  public boolean canEqual(Object that) {
+    throw new UnsupportedOperationException("Unsupported operation");
+  }
+
+  @Override
+  public Object productElement(int n) {
+    throw new UnsupportedOperationException("Unsupported operation");
+  }
+
+  @Override
+  public int productArity() {
+    throw new UnsupportedOperationException("Unsupported operation");
   }
 }
