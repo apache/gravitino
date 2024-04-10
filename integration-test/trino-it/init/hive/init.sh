ebd3a16e7d3c3867d769991f19b7722d75b5bf00
yuhui <hui@datastrato.com>
2024-04-10 17:51:25 +0800

    Update range of random port allocation

--- integration-test/trino-it/init/hive/init.sh
+++ integration-test/trino-it/init/hive/init.sh
@@ -5,5 +5,6 @@
 
 IP=$(hostname -I | awk '{print $1}')
 sed -i "s|<value>hdfs://localhost:9000|<value>hdfs://${IP}:9000|g" /usr/local/hive/conf/hive-site.xml
+echo 10000 50000 > /proc/sys/net/ipv4/ip_local_port_range
 
 /bin/bash /usr/local/sbin/start.sh
