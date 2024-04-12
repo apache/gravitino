33b5953d3f7c41b4eec0e0e60d97e682396b2d7d
Heng Qin <qqtt@123.com>
2024-04-12 19:51:57 +0800

    polish

--- api/src/main/java/com/datastrato/gravitino/authorization/Resource.java
+++ api/src/main/java/com/datastrato/gravitino/authorization/Resource.java
@@ -7,7 +7,7 @@ package com.datastrato.gravitino.authorization;
 import com.datastrato.gravitino.annotation.Evolving;
 
 /**
- * The resource is the unit of the authorization. Our system organizes the resources using tree
+ * The resource is the entity of the authorization. Gravitino organizes the resources using tree
  * structure. The resource may be a catalog, a table or a schema, etc. For example,
  * `catalog1.schema1.table1` represents a table named `table1`. It's in the schema named `schema1`.
  * The schema is in the catalog named `catalog1`.
