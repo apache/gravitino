520f8eede2a2a99d414bb189cc88541f7d2c117f
Jiebao Xiao <xiaojiebao@xiaomi.com>
2024-04-14 11:42:02 +0800

    fix

--- core/src/main/java/com/datastrato/gravitino/storage/relational/converters/SQLExceptionConverter.java
+++ core/src/main/java/com/datastrato/gravitino/storage/relational/converters/SQLExceptionConverter.java
@@ -16,8 +16,8 @@ public interface SQLExceptionConverter {
    * @param sqlException The sql exception to map
    * @param type The type of the entity
    * @param name The name of the entity
-   * @return A best attempt at a corresponding connector exception or generic with the SQLException
-   *     as the cause
+   * @return A best attempt at a corresponding jdbc connector exception or generic with the
+   *     SQLException as the cause
    */
   GravitinoRuntimeException toGravitinoException(
       SQLException sqlException, Entity.EntityType type, String name);
