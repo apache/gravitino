2edd254535cc83ab429dd77c64ad3793a5288ca5
caican00 <imcaican@gmail.com>
2024-04-16 21:21:10 +0800

    [#2979] feat(spark-connector): Implement FunctionCatalog in GravitinoIcebergCatalog

--- integration-test/src/test/java/com/datastrato/gravitino/integration/test/spark/iceberg/SparkIcebergCatalogIT.java
+++ integration-test/src/test/java/com/datastrato/gravitino/integration/test/spark/iceberg/SparkIcebergCatalogIT.java
@@ -6,7 +6,15 @@ package com.datastrato.gravitino.integration.test.spark.iceberg;
 
 import com.datastrato.gravitino.integration.test.spark.SparkCommonIT;
 import com.datastrato.gravitino.integration.test.util.spark.SparkTableInfo;
+import com.google.common.collect.ImmutableList;
+import java.util.Arrays;
 import java.util.List;
+import org.apache.spark.sql.catalyst.analysis.NoSuchFunctionException;
+import org.apache.spark.sql.catalyst.analysis.NoSuchNamespaceException;
+import org.apache.spark.sql.connector.catalog.CatalogPlugin;
+import org.apache.spark.sql.connector.catalog.FunctionCatalog;
+import org.apache.spark.sql.connector.catalog.Identifier;
+import org.apache.spark.sql.connector.catalog.functions.UnboundFunction;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -54,5 +62,69 @@ public class SparkIcebergCatalogIT extends SparkCommonIT {
     sql(getDeleteSql(tableName, "id < 10"));
     List<String> queryResult2 = getTableData(tableName);
     Assertions.assertEquals(0, queryResult2.size());
+  }
+
+  @Test
+  void testFunctionsViaGravitinoIcebergCatalog()
+      throws NoSuchNamespaceException, NoSuchFunctionException {
+    String[] empty_namespace = new String[] {};
+    String[] system_namespace = new String[] {"system"};
+    String[] default_namespace = new String[] {getDefaultDatabase()};
+    String[] non_exists_namespace = new String[] {"non_existent"};
+    List<String> functions =
+        Arrays.asList("iceberg_version", "years", "months", "days", "hours", "bucket", "truncate");
+
+    CatalogPlugin catalogPlugin =
+        getSparkSession().sessionState().catalogManager().catalog(getCatalogName());
+    Assertions.assertInstanceOf(FunctionCatalog.class, catalogPlugin);
+    FunctionCatalog functionCatalog = (FunctionCatalog) catalogPlugin;
+
+    for (String[] namespace : ImmutableList.of(empty_namespace, system_namespace)) {
+      Arrays.stream(functionCatalog.listFunctions(namespace))
+          .map(Identifier::name)
+          .forEach(function -> Assertions.assertTrue(functions.contains(function)));
+    }
+    Arrays.stream(functionCatalog.listFunctions(default_namespace))
+        .map(Identifier::name)
+        .forEach(function -> Assertions.assertFalse(functions.contains(function)));
+    Assertions.assertThrows(
+        NoSuchNamespaceException.class, () -> functionCatalog.listFunctions(non_exists_namespace));
+
+    for (String[] namespace : ImmutableList.of(empty_namespace, system_namespace)) {
+      for (String function : functions) {
+        Identifier identifier = Identifier.of(namespace, function);
+        UnboundFunction func = functionCatalog.loadFunction(identifier);
+        Assertions.assertEquals(function, func.name());
+      }
+    }
+    functions.forEach(
+        function -> {
+          Identifier identifier = Identifier.of(new String[] {getDefaultDatabase()}, function);
+          Assertions.assertThrows(
+              NoSuchFunctionException.class, () -> functionCatalog.loadFunction(identifier));
+        });
+  }
+
+  @Test
+  void testCallingFunctionInSQLEndToEnd() {
+    String[] catalogAndNamespaces = new String[] {getCatalogName() + ".system", getCatalogName()};
+    Arrays.stream(catalogAndNamespaces)
+        .forEach(
+            catalogAndNamespace -> {
+              List<String> bucket =
+                  getQueryData(String.format("SELECT %s.bucket(2, 100)", catalogAndNamespace));
+              Assertions.assertEquals(1, bucket.size());
+              Assertions.assertEquals("0", bucket.get(0));
+            });
+
+    Arrays.stream(catalogAndNamespaces)
+        .forEach(
+            catalogAndNamespace -> {
+              List<String> bucket =
+                  getQueryData(
+                      String.format("SELECT %s.truncate(2, 'abcdef')", catalogAndNamespace));
+              Assertions.assertEquals(1, bucket.size());
+              Assertions.assertEquals("ab", bucket.get(0));
+            });
   }
 }
