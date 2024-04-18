b58f1a1c6dd73eb490d8e65e5fcd4ae88978ba78
YxAc <yongxing@YxAcdeMacBook-Air.local>
2024-04-18 19:52:40 +0800

    update ut

--- core/src/test/java/com/datastrato/gravitino/storage/TestEntityStorage.java
+++ core/src/test/java/com/datastrato/gravitino/storage/TestEntityStorage.java
@@ -114,6 +114,7 @@ public class TestEntityStorage {
       Mockito.when(config.get(ENTITY_RELATIONAL_JDBC_BACKEND_USER)).thenReturn("root");
       Mockito.when(config.get(ENTITY_RELATIONAL_JDBC_BACKEND_PASSWORD)).thenReturn("123");
       Mockito.when(config.get(ENTITY_RELATIONAL_JDBC_BACKEND_DRIVER)).thenReturn("org.h2.Driver");
+      Mockito.when(config.get(STORE_DELETE_AFTER_TIME)).thenReturn(20 * 60 * 1000L);
     } else {
       throw new UnsupportedOperationException("Unsupported entity store type: " + type);
     }
