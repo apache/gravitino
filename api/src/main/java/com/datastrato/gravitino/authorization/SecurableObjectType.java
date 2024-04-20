759270c33c23fab19c99e74a80fc3cf11d1336b0
Heng Qin <qqtt@123.com>
2024-04-20 14:32:14 +0800

    Add comment

--- api/src/main/java/com/datastrato/gravitino/authorization/SecurableObjectType.java
+++ api/src/main/java/com/datastrato/gravitino/authorization/SecurableObjectType.java
@@ -9,16 +9,20 @@ package com.datastrato.gravitino.authorization;
  * of the underlying system.
  */
 public enum SecurableObjectType {
-  /* A catalog is a collection of metadata from a specific metadata source,
-  like Apache Hive catalog, Apache Iceberg catalog, JDBC catalog, etc.*/
+  /**
+   * A catalog is a collection of metadata from a specific metadata source, like Apache Hive
+   * catalog, Apache Iceberg catalog, JDBC catalog, etc.
+   */
   CATALOG,
-  /* A schema is a sub collection of the catalog.
-  The schema can contain filesets, tables, topics, etc.*/
+  /**
+   * A schema is a sub collection of the catalog. The schema can contain filesets, tables, topics,
+   * etc.
+   */
   SCHEMA,
-  /* A fileset is mapped to a directory on a file system like HDFS, S3, ADLS, GCS, etc. */
+  /** A fileset is mapped to a directory on a file system like HDFS, S3, ADLS, GCS, etc. */
   FILESET,
-  /* A table is mapped the table of relational data sources like Apache Hive, MySQL, etc*/
+  /** A table is mapped the table of relational data sources like Apache Hive, MySQL, etc */
   TABLE,
-  /* A topic is mapped the topic of messaging data sources like Apache Kafka, Apache Pulsar, etc*/
+  /** A topic is mapped the topic of messaging data sources like Apache Kafka, Apache Pulsar, etc */
   TOPIC
 }
