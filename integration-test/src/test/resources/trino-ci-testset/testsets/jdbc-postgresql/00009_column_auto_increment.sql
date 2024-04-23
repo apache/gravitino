cc15373e126aedb0ead3df21e9f5232662bf1cff
yuhui <hui@datastrato.com>
2024-04-23 11:51:32 +0800

    pg support auto increment

--- integration-test/src/test/resources/trino-ci-testset/testsets/jdbc-postgresql/00009_column_auto_increment.sql
+++ integration-test/src/test/resources/trino-ci-testset/testsets/jdbc-postgresql/00009_column_auto_increment.sql
@@ -2,9 +2,14 @@ CREATE SCHEMA gt_postgresql.gt_db1;
 
 CREATE TABLE gt_postgresql.gt_db1.tb01 (
     name varchar,
-    id int with ("auto_increment" = true)
+    id int not null with ("auto_increment" = true)
 );
 
+show create table gt_postgresql.gt_db1.tb01;
+
+-- trino does not support insert into table with auto_increment column
+-- insert into gt_postgresql.gt_db1.tb01 (name) values ('name01');  // failed: NULL value not allowed for NOT NULL column: id
+-- insert into gt_postgresql.gt_db1.tb01 (name, id) values ('name01', 10); // success: but id is set by insert
 
 drop table gt_postgresql.gt_db1.tb01;
 
