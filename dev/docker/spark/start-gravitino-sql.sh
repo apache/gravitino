6191e177193ce6e151005cb978a6c307c659d76f
fanng <xiaojing@datastrato.com>
2024-04-23 15:37:36 +0800

    polish

--- dev/docker/spark/start-gravitino-sql.sh
+++ dev/docker/spark/start-gravitino-sql.sh
@@ -7,6 +7,5 @@
 --conf spark.plugins="com.datastrato.gravitino.spark.connector.plugin.GravitinoSparkPlugin" \
 --conf spark.sql.gravitino.uri=http://gravitino:8090 \
 --conf spark.sql.gravitino.metalake=metalake_demo \
---conf hive.exec.dynamic.partition=true \
 --conf spark.hadoop.hive.exec.dynamic.partition.mode=nonstrict \
 --conf spark.sql.session.timeZone=UTC 
