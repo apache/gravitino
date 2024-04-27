8afd61165007f408ffeaa805cc8d6288d4a161cb
unknowntpo <e850506@gmail.com>
2024-04-27 18:38:55 +0800

    chore: add missing liscence

--- integration-test-common/src/test/java/com/datastrato/gravitino/integration/test/container/PostgreSQLContainer.java
+++ integration-test-common/src/test/java/com/datastrato/gravitino/integration/test/container/PostgreSQLContainer.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2024 Datastrato Pvt Ltd.
+ * This software is licensed under the Apache License version 2.
+ */
 package com.datastrato.gravitino.integration.test.container;
 
 import static java.lang.String.format;
