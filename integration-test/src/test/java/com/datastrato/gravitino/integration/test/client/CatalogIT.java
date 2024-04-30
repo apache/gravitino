52e462c29b22fbd94948b74797930816da460407
Jerry Shao <jerryshao@datastrato.com>
2024-04-30 21:56:17 +0800

    [#3165] fix(client-java): Fix the http client `toJson` encode without UTF-8 (#3240)
    ### What changes were proposed in this pull request?
    
    Currently in the java client, the json result is not encoded with
    `UTF-8` when requesting the server, which will cause some Chinese
    characters to be garbled.It will use `ISO_8859_1` as default. This PR
    fixed this.
    
    ![image](https://github.com/datastrato/gravitino/assets/26177232/9342dd32-1ded-4670-a3c7-37d9a5673955)
    
    ### Why are the changes needed?
    
    Fix: #3165 
    
    ### How was this patch tested?
    
    Add some ITs.
    
    Co-authored-by: xloya <982052490@qq.com>
    Co-authored-by: xiaojiebao <xiaojiebao@xiaomi.com>

--- integration-test/src/test/java/com/datastrato/gravitino/integration/test/client/CatalogIT.java
+++ integration-test/src/test/java/com/datastrato/gravitino/integration/test/client/CatalogIT.java
@@ -113,6 +113,27 @@ public class CatalogIT extends AbstractIT {
   }
 
   @Test
+  public void testCreateCatalogWithChinese() {
+    String catalogName = GravitinoITUtils.genRandomName("catalogz");
+    NameIdentifier catalogIdent = NameIdentifier.of(metalakeName, catalogName);
+    Assertions.assertFalse(metalake.catalogExists(catalogIdent));
+
+    Map<String, String> properties = Maps.newHashMap();
+    properties.put("metastore.uris", hmsUri);
+    metalake.createCatalog(
+        catalogIdent, Catalog.Type.RELATIONAL, "hive", "这是中文comment", properties);
+    Assertions.assertTrue(metalake.catalogExists(catalogIdent));
+    Catalog catalog = metalake.loadCatalog(catalogIdent);
+    Assertions.assertEquals(catalogName, catalog.name());
+    Assertions.assertEquals(Catalog.Type.RELATIONAL, catalog.type());
+    Assertions.assertEquals("hive", catalog.provider());
+    Assertions.assertEquals("这是中文comment", catalog.comment());
+    Assertions.assertTrue(catalog.properties().containsKey("metastore.uris"));
+
+    metalake.dropCatalog(catalogIdent);
+  }
+
+  @Test
   public void testListCatalogsInfo() {
     String relCatalogName = GravitinoITUtils.genRandomName("rel_catalog_");
     NameIdentifier relCatalogIdent = NameIdentifier.of(metalakeName, relCatalogName);
