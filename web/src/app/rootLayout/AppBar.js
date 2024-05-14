a5beed63add1be77c02db51f815ed100af76cbbf
Qian Xia <lauraxiaqian@gmail.com>
2024-05-14 16:15:23 +0800

    minor, format code

--- web/src/app/rootLayout/AppBar.js
+++ web/src/app/rootLayout/AppBar.js
@@ -90,7 +90,7 @@ const AppBar = () => {
                       data-refer='select-metalake'
                       value={metalake}
                       label='Metalake'
-                      disabled={store.metalakes.length === 1} 
+                      disabled={store.metalakes.length === 1}
                       sx={{ width: '100%' }}
                     >
                       {metalakes.map(item => {
