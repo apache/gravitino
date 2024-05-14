315ffc4cfbfebb5ed257caef642b26db0b49d919
cai can <94670132+caican00@users.noreply.github.com>
2024-05-14 17:34:32 +0800

    [#2543] feat(spark-connector): support row-level operations to iceberg Table (#3382)
    ### What changes were proposed in this pull request?
    
    - refactor table implementation, make `SparkIcebergTable` extend Iceberg
    `SparkTable`, and `SparkHiveTable` extend Kyuubi `HiveTable`.
    
    - support row-level operations to iceberg Table
    
    ```
    1. update tableName set c1=v1, c2=v2, ...
    
    2. merge into targetTable t
       using sourceTable s
       on s.key=t.key
       when matched then ...
       when not matched then ...
    
    3. delete from table where xxx
    ```
    
    ### Why are the changes needed?
    
    1. For spark-connector in Iceberg, it explicitly uses `SparkTable` to
    identify whether it is an Iceberg table, so the `SparkIcebergTable` must
    extend `SparkTable`.
    
    2. support row-level operations to iceberg Table.
    
    Fix: https://github.com/datastrato/gravitino/issues/2543
    
    ### Does this PR introduce any user-facing change? 
    Yes, support update ... , merge into ..., delete from ...
    
    ### How was this patch tested?
    New ITs.

--- spark-connector/spark-connector/src/main/java/com/datastrato/gravitino/spark/connector/hive/GravitinoHiveCatalog.java
+++ spark-connector/spark-connector/src/main/java/com/datastrato/gravitino/spark/connector/hive/GravitinoHiveCatalog.java
@@ -9,9 +9,10 @@ import com.datastrato.gravitino.rel.Table;
 import com.datastrato.gravitino.spark.connector.PropertiesConverter;
 import com.datastrato.gravitino.spark.connector.SparkTransformConverter;
 import com.datastrato.gravitino.spark.connector.catalog.BaseCatalog;
-import com.datastrato.gravitino.spark.connector.table.SparkBaseTable;
 import java.util.Map;
+import org.apache.kyuubi.spark.connector.hive.HiveTable;
 import org.apache.kyuubi.spark.connector.hive.HiveTableCatalog;
+import org.apache.spark.sql.catalyst.analysis.NoSuchTableException;
 import org.apache.spark.sql.connector.catalog.Identifier;
 import org.apache.spark.sql.connector.catalog.TableCatalog;
 import org.apache.spark.sql.util.CaseInsensitiveStringMap;
@@ -30,14 +31,29 @@ public class GravitinoHiveCatalog extends BaseCatalog {
   }
 
   @Override
-  protected SparkBaseTable createSparkTable(
+  protected org.apache.spark.sql.connector.catalog.Table createSparkTable(
       Identifier identifier,
       Table gravitinoTable,
-      TableCatalog sparkCatalog,
+      TableCatalog sparkHiveCatalog,
       PropertiesConverter propertiesConverter,
       SparkTransformConverter sparkTransformConverter) {
+    org.apache.spark.sql.connector.catalog.Table sparkTable;
+    try {
+      sparkTable = sparkHiveCatalog.loadTable(identifier);
+    } catch (NoSuchTableException e) {
+      throw new RuntimeException(
+          String.format(
+              "Failed to load the real sparkTable: %s",
+              String.join(".", getDatabase(identifier), identifier.name())),
+          e);
+    }
     return new SparkHiveTable(
-        identifier, gravitinoTable, sparkCatalog, propertiesConverter, sparkTransformConverter);
+        identifier,
+        gravitinoTable,
+        (HiveTable) sparkTable,
+        (HiveTableCatalog) sparkHiveCatalog,
+        propertiesConverter,
+        sparkTransformConverter);
   }
 
   @Override
