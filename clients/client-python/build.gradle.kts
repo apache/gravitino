12c32c70f0e220b22d0ea76019c11b395eac8592
SophieTech88 <141538510+SophieTech88@users.noreply.github.com>
2024-05-14 09:19:41 -0500

    [#2230] feat(client-python): Add doc and support publishing Gravitino Python client to Pypi (#2932)
    ### What changes were proposed in this pull request?
    We published the first version of python-client package of Gravitino to
    Pypi.
    1. Add how to use Python Client document
    2. add some tools to requirements.txt
    3. `./gradlew :clients:client-python:distribution`
    4. `./gradlew :clients:client-python:deploy`
    
    ### Why are the changes needed?
    We need the python client package for user to use Gravitino in Python
    project.
    
    #2230
    
    ### Does this PR introduce _any_ user-facing change?
    Users can use `pip install gravitino` to install the Gravitino
    python-client package directly.
    
    ### How was this patch tested?
    CI Passed
    
    ---------
    
    Co-authored-by: xunliu <xun@datastrato.com>
    Co-authored-by: Jerry Shao <jerryshao@datastrato.com>

--- clients/client-python/build.gradle.kts
+++ clients/client-python/build.gradle.kts
@@ -32,6 +32,62 @@ fun gravitinoServer(operation: String) {
     }
 }
 
+fun generatePypiProjectHomePage() {
+  try {
+    val inputFile = file("${project.rootDir}/docs/how-to-use-python-client.md")
+    val outputFile = file("README.md")
+
+    // Copy the contents of the how-to-use-python-client.md file to the README.md file for PyPi
+    // release, remove the front matter as PyPi doesn't support README file contains front
+    // matter.
+    val lines = inputFile.readLines()
+    var skipFrontMatterHead = false
+    var skipFrontMatterEnd = false
+    for (line in lines) {
+      if (line.trim() == "---") {
+        if (!skipFrontMatterHead) {
+          skipFrontMatterHead = true
+          continue
+        } else if (!skipFrontMatterEnd) {
+          skipFrontMatterEnd = true
+          continue
+        }
+      }
+      if (skipFrontMatterHead && skipFrontMatterEnd) {
+        outputFile.appendText(line + "\n")
+      }
+    }
+
+    // Because the README.md file is generated from the how-to-use-python-client.md file, the
+    // relative path of the images in the how-to-use-python-client.md file is incorrect. We need
+    // to fix the relative path of the images/markdown to the absolute path.
+    val content = outputFile.readText()
+    val docsUrl = "https://datastrato.ai/docs/latest"
+
+    // Use regular expression to match the `[](./a/b/c.md#arg1)` link in the content
+    val patternDocs = "(?<!!)\\[([^]]+)]\\(\\.\\/([^)]*\\.md(?:#[^)]*)?)\\)".toRegex()
+    val contentUpdateDocs = patternDocs.replace(content) { matchResult ->
+      val linkText = matchResult.groupValues[1]
+      val relativePath = matchResult.groupValues[2].replace(".md", "/")
+      "[$linkText]($docsUrl/$relativePath)"
+    }
+
+    // Use regular expression to match the `![](./a/b/c.png)` link in the content
+    val assertUrl = "https://raw.githubusercontent.com/datastrato/gravitino/main/docs"
+    val patternImage = """!\[([^\]]+)]\(\./assets/([^)]+)\)""".toRegex()
+    val contentUpdateImage = patternImage.replace(contentUpdateDocs) { matchResult ->
+      val altText = matchResult.groupValues[1]
+      val fileName = matchResult.groupValues[2]
+      "![${altText}]($assertUrl/assets/$fileName)"
+    }
+
+    val readmeFile = file("README.md")
+    readmeFile.writeText(contentUpdateImage)
+  } catch (e: Exception) {
+    throw GradleException("client-python README.md file not generated!")
+  }
+}
+
 tasks {
   val pipInstall by registering(VenvTask::class) {
     venvExec = "pip"
@@ -89,8 +145,31 @@ tasks {
   val build by registering(VenvTask::class) {
   }
 
+  val distribution by registering(VenvTask::class) {
+    doFirst {
+      generatePypiProjectHomePage()
+      delete("dist")
+    }
+
+    venvExec = "Python3"
+    args = listOf("setup.py", "sdist")
+
+    doLast {
+      delete("README.md")
+    }
+  }
+
+  // Deploy to https://pypi.org/project/gravitino/
+  val deploy by registering(VenvTask::class) {
+    dependsOn(distribution)
+    val twine_password = System.getenv("TWINE_PASSWORD")
+    venvExec = "twine"
+    args = listOf("upload", "dist/*", "-p${twine_password}")
+  }
+
   val clean by registering(Delete::class) {
     delete("build")
+    delete("dist")
     delete("gravitino.egg-info")
 
     doLast {
