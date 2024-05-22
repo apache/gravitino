acf79aac27ca776a8d4ecb894ad5f9507d738f93
unknowntpo <e850506@gmail.com>
2024-05-22 12:33:43 +0800

    change hive image back to gravitino-ci-hive, update changelog

--- integration-test-common/src/test/java/com/datastrato/gravitino/integration/test/container/ContainerSuite.java
+++ integration-test-common/src/test/java/com/datastrato/gravitino/integration/test/container/ContainerSuite.java
@@ -88,7 +88,6 @@ public class ContainerSuite implements Closeable {
           HiveContainer.Builder hiveBuilder =
               HiveContainer.builder()
                   .withHostName("gravitino-ci-hive")
-                  .withImage("unknowntpo/gravitino-ci-hive:latest")
                   .withEnvVars(
                       ImmutableMap.<String, String>builder()
                           .put("HADOOP_USER_NAME", "datastrato")
