a15782fd61370e1f4003ad65ed8dd137a4bc0e45
caican00 <imcaican@gmail.com>
2024-05-23 19:40:33 +0800

    update

--- spark-connector/spark-connector/src/test/java/com/datastrato/gravitino/spark/connector/integration/test/iceberg/SparkIcebergCatalogIT.java
+++ spark-connector/spark-connector/src/test/java/com/datastrato/gravitino/spark/connector/integration/test/iceberg/SparkIcebergCatalogIT.java
@@ -4,12 +4,12 @@
  */
 package com.datastrato.gravitino.spark.connector.integration.test.iceberg;
 
+import com.datastrato.gravitino.spark.connector.iceberg.IcebergPropertiesConstants;
 import com.datastrato.gravitino.spark.connector.iceberg.SparkIcebergTable;
 import com.datastrato.gravitino.spark.connector.integration.test.SparkCommonIT;
 import com.datastrato.gravitino.spark.connector.integration.test.util.SparkMetadataColumnInfo;
 import com.datastrato.gravitino.spark.connector.integration.test.util.SparkTableInfo;
 import com.datastrato.gravitino.spark.connector.integration.test.util.SparkTableInfoChecker;
-import com.datastrato.gravitino.spark.connector.iceberg.IcebergPropertiesConstants;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import java.io.File;
