31f1d359a12e0dcef2943d2a3bfcc411ea7323f2
yangliwei <yangliwei@xiaomi.com>
2024-05-23 21:34:41 +0800

    fix: upgrade sql

--- scripts/mysql/upgrade-0.5.0-to-0.6.0-mysql.sql
+++ scripts/mysql/upgrade-0.5.0-to-0.6.0-mysql.sql
@@ -19,4 +19,13 @@ ALTER TABLE `role_meta`
 ALTER TABLE `role_meta`
     ADD COLUMN `schema_id` BIGINT(20) UNSIGNED NOT NULL COMMENT 'schema id' AFTER `catalog_id`;
 
--- todo: insert system catalog, user, group and role schema
+-- todo: initialize the system catalog and schema, and update user, group and role meta table with the catalog and schema id
+-- SELECT metalake_id INTO @metalake_id FROM metalake_meta;
+-- WHILE @metalake_id IS NOT NULL DO
+-- insert into catalog_meta (catalog_id, metalake_id, catalog_name...) values (@random_id, @metalake_id, 'system', ...)
+-- insert into schema_meta (schema_id, metalake_id, catalog_id, schema_name...) values (@random_id, @metalake_id, @catalog_id, 'user' ...)
+-- insert into schema_meta (schema_id, metalake_id, catalog_id, schema_name...) values (@random_id, @metalake_id, @catalog_id, 'group' ...)
+-- insert into schema_meta (schema_id, metalake_id, catalog_id, schema_name...) values (@random_id, @metalake_id, @catalog_id, 'role' ...)
+-- update user_meta set catalog_id = @catalog_id, schema_id = @schema_id_user where metalake_id = @metalake_id;
+-- update group_meta set catalog_id = @catalog_id, schema_id = @schema_id_group where metalake_id = @metalake_id;
+-- update role_meta set catalog_id = @catalog_id, schema_id = @schema_id_role where metalake_id = @metalake_id;
