7a7e4e6e6de9ff5e818adae15d581be98453d180
fanng <xiaojing@datastrato.com>
2024-05-24 12:22:54 +0800

    enableIcebergSupport

--- spark-connector/spark-connector/src/main/java/com/datastrato/gravitino/spark/connector/GravitinoSparkConfig.java
+++ spark-connector/spark-connector/src/main/java/com/datastrato/gravitino/spark/connector/GravitinoSparkConfig.java
@@ -10,7 +10,8 @@ public class GravitinoSparkConfig {
   private static final String GRAVITINO_PREFIX = "spark.sql.gravitino.";
   public static final String GRAVITINO_URI = GRAVITINO_PREFIX + "uri";
   public static final String GRAVITINO_METALAKE = GRAVITINO_PREFIX + "metalake";
-  public static final String GRAVITINO_ENABLE_ICEBERG_SUPPORT = GRAVITINO_PREFIX + "enableIcebergSupport";
+  public static final String GRAVITINO_ENABLE_ICEBERG_SUPPORT =
+      GRAVITINO_PREFIX + "enableIcebergSupport";
   public static final String GRAVITINO_HIVE_METASTORE_URI = "metastore.uris";
   public static final String SPARK_HIVE_METASTORE_URI = "hive.metastore.uris";
 
