c4efab6b44ed4d09b5d1f464b1806aa480807652
yangliwei <yangliwei@xiaomi.com>
2024-05-24 10:50:25 +0800

    fix: upgrade sql

--- scripts/mysql/upgrade-0.5.0-to-0.6.0-mysql.sql
+++ scripts/mysql/upgrade-0.5.0-to-0.6.0-mysql.sql
@@ -19,13 +19,74 @@ ALTER TABLE `role_meta`
 ALTER TABLE `role_meta`
     ADD COLUMN `schema_id` BIGINT(20) UNSIGNED NOT NULL COMMENT 'schema id' AFTER `catalog_id`;
 
--- todo: initialize the system catalog and schema, and update user, group and role meta table with the catalog and schema id
--- SELECT metalake_id INTO @metalake_id FROM metalake_meta;
--- WHILE @metalake_id IS NOT NULL DO
--- insert into catalog_meta (catalog_id, metalake_id, catalog_name...) values (@random_id, @metalake_id, 'system', ...)
--- insert into schema_meta (schema_id, metalake_id, catalog_id, schema_name...) values (@random_id, @metalake_id, @catalog_id, 'user' ...)
--- insert into schema_meta (schema_id, metalake_id, catalog_id, schema_name...) values (@random_id, @metalake_id, @catalog_id, 'group' ...)
--- insert into schema_meta (schema_id, metalake_id, catalog_id, schema_name...) values (@random_id, @metalake_id, @catalog_id, 'role' ...)
--- update user_meta set catalog_id = @catalog_id, schema_id = @schema_id_user where metalake_id = @metalake_id;
--- update group_meta set catalog_id = @catalog_id, schema_id = @schema_id_group where metalake_id = @metalake_id;
--- update role_meta set catalog_id = @catalog_id, schema_id = @schema_id_role where metalake_id = @metalake_id;
+-- This stored procedure is designed to initialize system catalog and schema for each metalake
+-- and it updates the `catalog_id` and `schema_id` in the `user_meta`, `group_meta`, and `role_meta` tables to the newly generated `catalog_id` and `schema_id`.
+DELIMITER $$
+
+CREATE PROCEDURE InitializeSystemCatalogAndSchema()
+BEGIN
+    DECLARE v_metalake_id BIGINT(20) UNSIGNED;
+    DECLARE v_catalog_id BIGINT(20) UNSIGNED;
+    DECLARE v_schema_id_user BIGINT(20) UNSIGNED;
+    DECLARE v_schema_id_group BIGINT(20) UNSIGNED;
+    DECLARE v_schema_id_role BIGINT(20) UNSIGNED;
+    DECLARE v_now VARCHAR(255);
+    DECLARE done INT DEFAULT FALSE;
+
+    DECLARE cur CURSOR FOR SELECT metalake_id FROM metalake_meta WHERE deleted_at = 0;
+    -- Declare a continue handler for when no more rows are found in the cursor
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    -- Declare an exit handler for any SQL exception that will rollback the transaction
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        SELECT 'An error occurred, rolling back transaction' AS message;
+        ROLLBACK;
+    END;
+
+    SET v_now = DATE_FORMAT(NOW(), '%Y-%m-%dT%H:%i:%s.000Z');
+
+    START TRANSACTION;
+
+    OPEN cur;
+
+    read_loop: LOOP
+        FETCH cur INTO v_metalake_id;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+
+        -- 9223372036854775807 is the maximum number that RandomIdGenerator can generate
+        SET v_catalog_id = FLOOR(RAND() * 9223372036854775807);
+        SET @audit_info = CONCAT('{"creator":"system","createTime":"', v_now, '","lastModifier":null,"lastModifiedTime":null}');
+
+        INSERT INTO catalog_meta (catalog_id, metalake_id, catalog_name, type, provider, catalog_comment, properties, audit_info, current_version, last_version, deleted_at)
+        VALUES (v_catalog_id, v_metalake_id, 'system', 'system', 'system', 'reserved system catalog', NULL, @audit_info, 1, 1, 0);
+
+        SET v_schema_id_user = FLOOR(RAND() * 9223372036854775807);
+        INSERT INTO schema_meta (schema_id, metalake_id, catalog_id, schema_name, schema_comment, properties, audit_info, current_version, last_version, deleted_at)
+        VALUES (v_schema_id_user, v_metalake_id, v_catalog_id, 'user', 'reserved user schema', NULL, @audit_info, 1, 1, 0);
+
+        SET v_schema_id_group = FLOOR(RAND() * 9223372036854775807);
+        INSERT INTO schema_meta (schema_id, metalake_id, catalog_id, schema_name, schema_comment, properties, audit_info, current_version, last_version, deleted_at)
+        VALUES (v_schema_id_group, v_metalake_id, v_catalog_id, 'group', 'reserved group schema', NULL, @audit_info, 1, 1, 0);
+
+        SET v_schema_id_role = FLOOR(RAND() * 9223372036854775807);
+        INSERT INTO schema_meta (schema_id, metalake_id, catalog_id, schema_name, schema_comment, properties, audit_info, current_version, last_version, deleted_at)
+        VALUES (v_schema_id_role, v_metalake_id, v_catalog_id, 'role', 'reserved role schema', NULL, @audit_info, 1, 1, 0);
+
+        UPDATE user_meta SET catalog_id = v_catalog_id, schema_id = v_schema_id_user WHERE metalake_id = v_metalake_id;
+        UPDATE group_meta SET catalog_id = v_catalog_id, schema_id = v_schema_id_group WHERE metalake_id = v_metalake_id;
+        UPDATE role_meta SET catalog_id = v_catalog_id, schema_id = v_schema_id_role WHERE metalake_id = v_metalake_id;
+
+    END LOOP;
+
+    -- Close the cursor
+    CLOSE cur;
+
+    -- Commit the transaction
+    COMMIT;
+END$$
+
+DELIMITER ;
+
+call InitializeSystemCatalogAndSchema();
