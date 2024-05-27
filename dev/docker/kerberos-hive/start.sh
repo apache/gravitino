cd25ea259c6036ba87885d28a331e96fbb24740e
yuqi <yuqi@datastrato.com>
2024-05-27 18:15:53 +0800

    Fix

--- dev/docker/kerberos-hive/start.sh
+++ dev/docker/kerberos-hive/start.sh
@@ -7,7 +7,7 @@
 # start ssh
 HOSTNAME=`hostname`
 service ssh start
-ssh-keyscan localhost >> /root/.ssh/known_hosts
+ssh-keyscan ${HOSTNAME} >> /root/.ssh/known_hosts
 ssh-keyscan 0.0.0.0 >> /root/.ssh/known_hosts
 ssh-keyscan 127.0.0.1 >> /root/.ssh/known_hosts
 
