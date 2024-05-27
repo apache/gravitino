0fa04010ab210eda8bc436c079a165ef15734723
Jerry Shao <jerryshao@datastrato.com>
2024-05-27 10:43:03 +0800

    [#3561] improvement(client-python): Fix redefined-builtin Pylint rule for client-python (#3568)
    <!--
    1. Title: [#<issue>] <type>(<scope>): <subject>
       Examples:
         - "[#123] feat(operator): support xxx"
         - "[#233] fix: check null before access result in xxx"
         - "[MINOR] refactor: fix typo in variable name"
         - "[MINOR] docs: fix typo in README"
         - "[#255] test: fix flaky test NameOfTheTest"
       Reference: https://www.conventionalcommits.org/en/v1.0.0/
    2. If the PR is unfinished, please mark this PR as draft.
    -->
    
    ### What changes were proposed in this pull request?
    
    * Replace varaible names `type` and `property`, which is Python
    functions and keywords, with more readable and custom variable names
    
    ### Why are the changes needed?
    
    Fix: #3561 
    
    ### Does this PR introduce _any_ user-facing change?
    
    No
    
    ### How was this patch tested?
    
    `./gradlew clients:client-python:test`
    
    Co-authored-by: noidname01 <55401762+noidname01@users.noreply.github.com>
    Co-authored-by: TimWang <tim.wang@pranaq.com>

--- clients/client-python/gravitino/dto/requests/schema_update_request.py
+++ clients/client-python/gravitino/dto/requests/schema_update_request.py
@@ -16,8 +16,8 @@ from gravitino.rest.rest_message import RESTRequest
 class SchemaUpdateRequestBase(RESTRequest):
     _type: str = field(metadata=config(field_name="@type"))
 
-    def __init__(self, type: str):
-        self._type = type
+    def __init__(self, action_type: str):
+        self._type = action_type
 
     @abstractmethod
     def schema_change(self):
@@ -38,19 +38,21 @@ class SchemaUpdateRequest:
         _value: str = field(metadata=config(field_name="value"))
         """The value of the property."""
 
-        def __init__(self, property: str, value: str):
+        def __init__(self, schema_property: str, value: str):
             super().__init__("setProperty")
-            self._property = property
+            self._property = schema_property
             self._value = value
 
         def validate(self):
             """Validates the fields of the request.
 
             Raises:
-                 IllegalArgumentException if property or value are not set.
+                 IllegalArgumentException if schema_property or value are not set.
             """
             if not self._property:
-                raise ValueError('"property" field is required and cannot be empty')
+                raise ValueError(
+                    '"schema_property" field is required and cannot be empty'
+                )
             if not self._value:
                 raise ValueError('"value" field is required and cannot be empty')
 
@@ -64,18 +66,20 @@ class SchemaUpdateRequest:
         _property: str = field(metadata=config(field_name="property"))
         """The property to remove."""
 
-        def __init__(self, property: str):
+        def __init__(self, schema_property: str):
             super().__init__("removeProperty")
-            self._property = property
+            self._property = schema_property
 
         def validate(self):
             """Validates the fields of the request.
 
             Raises:
-                 IllegalArgumentException if property is not set.
+                 IllegalArgumentException if schema_property is not set.
             """
             if not self._property:
-                raise ValueError('"property" field is required and cannot be empty')
+                raise ValueError(
+                    '"schema_property" field is required and cannot be empty'
+                )
 
         def schema_change(self):
             return SchemaChange.remove_property(self._property)
