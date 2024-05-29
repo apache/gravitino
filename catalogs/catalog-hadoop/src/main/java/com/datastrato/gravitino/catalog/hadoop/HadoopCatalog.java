783839f82d36c468a691f5c88bb9385d72248780
Qi Yu <yuqi@datastrato.com>
2024-05-29 21:39:40 +0800

    [#3337] improvement(hadoop-catalog): Support user impersonation for Hadoop catalog. (#3352)
    ### What changes were proposed in this pull request?
    
    Add user impersonation for the Hadoop catalog.
    
    ### Why are the changes needed?
    
    We need authentication for the encrypted HDFS cluster.
    
    Fix: #3337
    
    ### Does this PR introduce _any_ user-facing change?
    
    N/A. 
    
    ### How was this patch tested?
    
    UT(TO add).

--- catalogs/catalog-hadoop/src/main/java/com/datastrato/gravitino/catalog/hadoop/HadoopCatalog.java
+++ catalogs/catalog-hadoop/src/main/java/com/datastrato/gravitino/catalog/hadoop/HadoopCatalog.java
@@ -4,10 +4,13 @@
  */
 package com.datastrato.gravitino.catalog.hadoop;
 
+import com.datastrato.gravitino.catalog.hadoop.kerberos.KerberosConfig;
 import com.datastrato.gravitino.connector.BaseCatalog;
 import com.datastrato.gravitino.connector.CatalogOperations;
+import com.datastrato.gravitino.connector.ProxyPlugin;
 import com.datastrato.gravitino.connector.capability.Capability;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Hadoop catalog is a fileset catalog that can manage filesets on the Hadoop Compatible File
@@ -30,5 +33,14 @@ public class HadoopCatalog extends BaseCatalog<HadoopCatalog> {
   @Override
   protected Capability newCapability() {
     return new HadoopCatalogCapability();
+  }
+
+  @Override
+  protected Optional<ProxyPlugin> newProxyPlugin(Map<String, String> config) {
+    boolean impersonationEnabled = new KerberosConfig(config).isImpersonationEnabled();
+    if (!impersonationEnabled) {
+      return Optional.empty();
+    }
+    return Optional.of(new HadoopProxyPlugin());
   }
 }
