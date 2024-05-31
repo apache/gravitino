84731c40a68753929947f04b9d3da6740e4ff40f
liujinhui1994 <965147871@qq.com>
2024-05-31 14:23:05 +0800

    fix checkstyle

--- catalogs/catalog-hive/src/main/java/com/datastrato/gravitino/catalog/hive/dyn/DynFields.java
+++ catalogs/catalog-hive/src/main/java/com/datastrato/gravitino/catalog/hive/dyn/DynFields.java
@@ -21,7 +21,6 @@ package com.datastrato.gravitino.catalog.hive.dyn;
 import com.google.common.base.Joiner;
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Preconditions;
-import com.google.common.base.Throwables;
 import com.google.common.collect.Sets;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
