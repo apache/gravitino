8b18e5b70723e6217a531b640b0cea7dfe6726f1
unknowntpo <e850506@gmail.com>
2024-06-17 13:46:51 +0800

    remove log4jdbc and switch back RANGER_DOWNLOAD_URL to datastrato release

--- dev/docker/ranger/ranger-dependency.sh
+++ dev/docker/ranger/ranger-dependency.sh
@@ -10,15 +10,11 @@ ranger_dir="$(cd "${ranger_dir}">/dev/null; pwd)"
 # Environment variables definition
 RANGER_VERSION=2.4.0
 RANGER_PACKAGE_NAME="ranger-${RANGER_VERSION}-admin.tar.gz" # Must export this variable for Dockerfile
-RANGER_DOWNLOAD_URL=https://github.com/unknowntpo/apache-ranger/releases/download/release-ranger-${RANGER_VERSION}/ranger-${RANGER_VERSION}-admin.tar.gz
+RANGER_DOWNLOAD_URL=https://github.com/datastrato/apache-ranger/releases/download/release-ranger-${RANGER_VERSION}/ranger-${RANGER_VERSION}-admin.tar.gz
 
 MYSQL_CONNECTOR_VERSION=8.0.28
 MYSQL_CONNECTOR_PACKAGE_NAME="mysql-connector-java-${MYSQL_CONNECTOR_VERSION}.jar"
 MYSQL_CONNECTOR_DOWNLOAD_URL=https://search.maven.org/remotecontent?filepath=mysql/mysql-connector-java/${MYSQL_CONNECTOR_VERSION}/mysql-connector-java-${MYSQL_CONNECTOR_VERSION}.jar
-
-LOG4JDBC_VERSION=8.0.28
-LOG4JDBC_PACKAGE_NAME="log4jdbc-${LOG4JDBC_VERSION}.jar"
-LOG4JDBC_DOWNLOAD_URL=https://repo1.maven.org/maven2/com/googlecode/log4jdbc/log4jdbc/${LOG4JDBC_VERSION}/log4jdbc-${LOG4JDBC_VERSION}.jar
 
 # Prepare download packages
 if [[ ! -d "${ranger_dir}/packages" ]]; then
@@ -31,8 +27,4 @@ fi
 
 if [ ! -f "${ranger_dir}/packages/${MYSQL_CONNECTOR_PACKAGE_NAME}" ]; then
   curl -L -s -o "${ranger_dir}/packages/${MYSQL_CONNECTOR_PACKAGE_NAME}" ${MYSQL_CONNECTOR_DOWNLOAD_URL}
-fi
-
-if [ ! -f "${ranger_dir}/packages/${LOG4JDBC_PACKAGE_NAME}" ]; then
-  curl -L -s -o "${ranger_dir}/packages/${LOG4JDBC_PACKAGE_NAME}" ${LOG4JDBC_DOWNLOAD_URL}
 fi
