69902ba8d6c06ffbd63978636b762718793af0c3
caican <caican@xiaomi.com>
2024-06-18 17:07:36 +0800

    [#3891] feat(catalog-lakehouse-paimon): Support schema operations for Paimon Catalog

---
+++ catalogs/catalog-lakehouse-paimon/src/main/java/com/datastrato/gravitino/catalog/lakehouse/paimon/PaimonCatalogPropertiesMetadata.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2024 Datastrato Pvt Ltd.
+ * This software is licensed under the Apache License version 2.
+ */
+package com.datastrato.gravitino.catalog.lakehouse.paimon;
+
+import static com.datastrato.gravitino.connector.PropertyEntry.enumImmutablePropertyEntry;
+import static com.datastrato.gravitino.connector.PropertyEntry.stringOptionalPropertyEntry;
+import static com.datastrato.gravitino.connector.PropertyEntry.stringRequiredPropertyEntry;
+
+import com.datastrato.gravitino.connector.BaseCatalogPropertiesMetadata;
+import com.datastrato.gravitino.connector.PropertiesMetadata;
+import com.datastrato.gravitino.connector.PropertyEntry;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Implementation of {@link PropertiesMetadata} that represents Paimon catalog properties metadata.
+ */
+public class PaimonCatalogPropertiesMetadata extends BaseCatalogPropertiesMetadata {
+
+  public static final String GRAVITINO_CATALOG_BACKEND = "catalog-backend";
+  public static final String PAIMON_METASTORE = "metastore";
+  public static final String WAREHOUSE = "warehouse";
+  public static final String URI = "uri";
+
+  private static final Map<String, PropertyEntry<?>> PROPERTIES_METADATA;
+
+  public static final Map<String, String> GRAVITINO_CONFIG_TO_PAIMON =
+      ImmutableMap.of(GRAVITINO_CATALOG_BACKEND, PAIMON_METASTORE, WAREHOUSE, WAREHOUSE, URI, URI);
+
+  static {
+    List<PropertyEntry<?>> propertyEntries =
+        ImmutableList.of(
+            enumImmutablePropertyEntry(
+                GRAVITINO_CATALOG_BACKEND,
+                "Paimon catalog backend type",
+                true,
+                PaimonCatalogBackend.class,
+                null,
+                false,
+                false),
+            stringRequiredPropertyEntry(WAREHOUSE, "Paimon catalog warehouse config", false, false),
+            stringOptionalPropertyEntry(URI, "Paimon catalog uri config", false, null, false));
+    HashMap<String, PropertyEntry<?>> result = Maps.newHashMap(BASIC_CATALOG_PROPERTY_ENTRIES);
+    result.putAll(Maps.uniqueIndex(propertyEntries, PropertyEntry::getName));
+    PROPERTIES_METADATA = ImmutableMap.copyOf(result);
+  }
+
+  @Override
+  protected Map<String, PropertyEntry<?>> specificPropertyEntries() {
+    return PROPERTIES_METADATA;
+  }
+
+  public Map<String, String> transformProperties(Map<String, String> properties) {
+    Map<String, String> gravitinoConfig = Maps.newHashMap();
+    properties.forEach(
+        (key, value) -> {
+          if (GRAVITINO_CONFIG_TO_PAIMON.containsKey(key)) {
+            gravitinoConfig.put(GRAVITINO_CONFIG_TO_PAIMON.get(key), value);
+          }
+        });
+    return gravitinoConfig;
+  }
+}
---
+++ catalogs/catalog-lakehouse-paimon/src/main/java/com/datastrato/gravitino/catalog/lakehouse/paimon/ops/PaimonTableOps.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2024 Datastrato Pvt Ltd.
+ * This software is licensed under the Apache License version 2.
+ */
+package com.datastrato.gravitino.catalog.lakehouse.paimon.ops;
+
+import static com.datastrato.gravitino.catalog.lakehouse.paimon.utils.CatalogUtils.loadCatalogBackend;
+
+import com.datastrato.gravitino.catalog.lakehouse.paimon.PaimonConfig;
+import java.util.List;
+import java.util.Map;
+import org.apache.paimon.catalog.Catalog;
+import org.apache.paimon.catalog.Catalog.DatabaseAlreadyExistException;
+import org.apache.paimon.catalog.Catalog.DatabaseNotEmptyException;
+import org.apache.paimon.catalog.Catalog.DatabaseNotExistException;
+import org.apache.paimon.utils.Pair;
+
+/** Table operation proxy that handles table operations of an underlying Paimon catalog. */
+public class PaimonTableOps implements AutoCloseable {
+
+  protected Catalog catalog;
+
+  public PaimonTableOps(PaimonConfig paimonConfig) {
+    catalog = loadCatalogBackend(paimonConfig);
+  }
+
+  @Override
+  public void close() throws Exception {
+    if (catalog != null) {
+      catalog.close();
+    }
+  }
+
+  public List<String> listDatabases() {
+    return catalog.listDatabases();
+  }
+
+  public Map<String, String> loadDatabase(String databaseName) throws DatabaseNotExistException {
+    return catalog.loadDatabaseProperties(databaseName);
+  }
+
+  public void createDatabase(Pair<String, Map<String, String>> database)
+      throws DatabaseAlreadyExistException {
+    catalog.createDatabase(database.getKey(), false, database.getRight());
+  }
+
+  public void dropDatabase(String databaseName, boolean cascade)
+      throws DatabaseNotExistException, DatabaseNotEmptyException {
+    catalog.dropDatabase(databaseName, false, cascade);
+  }
+}
