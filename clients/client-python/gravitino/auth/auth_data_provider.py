8778d2a9793fa206a92559ce226ed382a44a741b
xiaojiebao <xiaojiebao@xiaomi.com>
2024-06-24 16:14:03 +0800

    add simple auth provider

---
+++ clients/client-python/gravitino/auth/auth_data_provider.py
@@ -0,0 +1,31 @@
+"""
+Copyright 2024 Datastrato Pvt Ltd.
+This software is licensed under the Apache License version 2.
+"""
+
+from abc import ABC, abstractmethod
+
+
+class AuthDataProvider(ABC):
+    """
+    The provider of authentication data
+    """
+
+    @abstractmethod
+    def has_token_data(self) -> bool:
+        """Judge whether AuthDataProvider can provide token data.
+
+        Returns:
+            true if the AuthDataProvider can provide token data otherwise false.
+        """
+        pass
+
+    @abstractmethod
+    def get_token_data(self) -> bytes:
+        """Acquire the data of token for authentication. The client will set the token data as HTTP header
+        Authorization directly. So the return value should ensure token data contain the token header
+        (eg: Bearer, Basic) if necessary.
+
+        Returns:
+            the token data is used for authentication.
+        """
---
+++ clients/client-python/gravitino/auth/simple_auth_provider.py
@@ -0,0 +1,39 @@
+"""
+Copyright 2024 Datastrato Pvt Ltd.
+This software is licensed under the Apache License version 2.
+"""
+
+import base64
+import os
+
+from .auth_constants import AuthConstants
+from .auth_data_provider import AuthDataProvider
+
+
+class SimpleAuthProvider(AuthDataProvider):
+    """SimpleAuthProvider will use the environment variable `GRAVITINO_USER` or
+    the user of the system to generate a basic token for every request.
+
+    """
+
+    _token: bytes
+
+    def __init__(self):
+        gravitino_user = os.environ.get("GRAVITINO_USER")
+        if gravitino_user is None or len(gravitino_user) == 0:
+            gravitino_user = os.environ.get("user.name")
+
+        if gravitino_user is None or len(gravitino_user) == 0:
+            gravitino_user = "anonymous"
+
+        user_information = f"{gravitino_user}:dummy"
+        self._token = (
+            AuthConstants.AUTHORIZATION_BASIC_HEADER
+            + base64.b64encode(user_information.encode("utf-8")).decode("utf-8")
+        ).encode("utf-8")
+
+    def has_token_data(self) -> bool:
+        return True
+
+    def get_token_data(self) -> bytes:
+        return self._token
