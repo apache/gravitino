709759cb0ad77cb3f1698b31005647bd793eec14
lipeidian <lipeidian@qq.com>
2024-07-24 09:40:07 +0800

    [#3982]feat(flink-connector):Support more column data type for hive table

--- flink-connector/src/test/java/org/apache/gravitino/flink/connector/integration/test/hive/FlinkHiveCatalogIT.java
+++ flink-connector/src/test/java/org/apache/gravitino/flink/connector/integration/test/hive/FlinkHiveCatalogIT.java
@@ -19,6 +19,8 @@
 package org.apache.gravitino.flink.connector.integration.test.hive;
 
 import static org.apache.gravitino.flink.connector.integration.test.utils.TestUtils.assertColumns;
+import static org.apache.gravitino.flink.connector.integration.test.utils.TestUtils.toFlinkPhysicalColumn;
+import static org.apache.gravitino.rel.expressions.transforms.Transforms.EMPTY_TRANSFORM;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
@@ -29,15 +31,18 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.table.api.DataTypes;
 import org.apache.flink.table.api.ResultKind;
 import org.apache.flink.table.api.TableResult;
 import org.apache.flink.table.api.ValidationException;
 import org.apache.flink.table.catalog.Catalog;
 import org.apache.flink.table.catalog.CatalogBaseTable;
 import org.apache.flink.table.catalog.CatalogDescriptor;
+import org.apache.flink.table.catalog.CatalogTable;
 import org.apache.flink.table.catalog.CommonCatalogOptions;
 import org.apache.flink.table.catalog.DefaultCatalogTable;
 import org.apache.flink.table.catalog.ObjectPath;
+import org.apache.flink.table.catalog.exceptions.TableNotExistException;
 import org.apache.flink.table.catalog.hive.factories.HiveCatalogFactoryOptions;
 import org.apache.flink.types.Row;
 import org.apache.gravitino.NameIdentifier;
@@ -65,17 +70,21 @@ import org.junit.jupiter.api.TestInstance;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class FlinkHiveCatalogIT extends FlinkCommonIT {
   private static final String DEFAULT_HIVE_CATALOG = "test_flink_hive_schema_catalog";
+  private static final String DEFAULT_HIVE_SCHEMA = "test_flink_hive_default_schema";
 
   private static org.apache.gravitino.Catalog hiveCatalog;
 
   @BeforeAll
   static void hiveStartUp() {
     initDefaultHiveCatalog();
+    initDefaultHiveSchema();
   }
 
   @AfterAll
   static void hiveStop() {
     Preconditions.checkNotNull(metalake);
+    Preconditions.checkNotNull(hiveCatalog);
+    hiveCatalog.asSchemas().dropSchema(DEFAULT_HIVE_SCHEMA, true);
     metalake.dropCatalog(DEFAULT_HIVE_CATALOG);
   }
 
@@ -88,6 +97,11 @@ public class FlinkHiveCatalogIT extends FlinkCommonIT {
             "hive",
             null,
             ImmutableMap.of("metastore.uris", hiveMetastoreUri));
+  }
+
+  private static void initDefaultHiveSchema() {
+    Preconditions.checkNotNull(hiveCatalog);
+    hiveCatalog.asSchemas().createSchema(DEFAULT_HIVE_SCHEMA, null, ImmutableMap.of());
   }
 
   @Test
@@ -375,6 +389,203 @@ public class FlinkHiveCatalogIT extends FlinkCommonIT {
           }
         },
         true);
+  }
+
+  @Test
+  public void testCreateHiveTable() {
+    String tableName, comment;
+    tableName = "test_create_no_partition_table";
+    comment = "test comment";
+    String key = "test key";
+    String value = "test value";
+
+    // 1. The NOT NULL constraint for column is only supported since Hive 3.0,
+    // but the current Gravitino Hive catalog only supports Hive 2.x.
+    // 2. Hive doesn't support Time and Timestamp with timezone type.
+    // 3. Flink SQL only support to create Interval Month and Second(3).
+    doWithSchema(
+        metalake.loadCatalog(DEFAULT_HIVE_CATALOG),
+        DEFAULT_HIVE_SCHEMA,
+        catalog -> {
+          TableResult result =
+              sql(
+                  "CREATE TABLE %s "
+                      + "(string_type STRING COMMENT 'string_type', "
+                      + " double_type DOUBLE COMMENT 'double_type',"
+                      + " int_type INT COMMENT 'int_type',"
+                      + " varchar_type VARCHAR COMMENT 'varchar_type',"
+                      + " char_type CHAR COMMENT 'char_type',"
+                      + " boolean_type BOOLEAN COMMENT 'boolean_type',"
+                      + " byte_type BINARY COMMENT 'byte_type',"
+                      + " binary_type BINARY(10) COMMENT 'binary_type',"
+                      + " decimal_type DECIMAL(10, 2) COMMENT 'decimal_type',"
+                      + " bigint_type BIGINT COMMENT 'bigint_type',"
+                      + " float_type FLOAT COMMENT 'float_type',"
+                      + " date_type DATE COMMENT 'date_type',"
+                      + " timestamp_type TIMESTAMP COMMENT 'timestamp_type',"
+                      + " smallint_type SMALLINT COMMENT 'smallint_type',"
+                      + " array_type ARRAY<INT> COMMENT 'array_type',"
+                      + " map_type MAP<INT, STRING> COMMENT 'map_type',"
+                      + " struct_type ROW<k1 INT, k2 String>)"
+                      + " COMMENT '%s' WITH ("
+                      + "'%s' = '%s')",
+                  tableName, comment, key, value);
+          TestUtils.assertTableResult(result, ResultKind.SUCCESS);
+
+          Table table =
+              catalog.asTableCatalog().loadTable(NameIdentifier.of(DEFAULT_HIVE_SCHEMA, tableName));
+          Assertions.assertNotNull(table);
+          Assertions.assertEquals(comment, table.comment());
+          Assertions.assertEquals(value, table.properties().get(key));
+          Column[] columns =
+              new Column[] {
+                Column.of("string_type", Types.StringType.get(), "string_type", true, false, null),
+                Column.of("double_type", Types.DoubleType.get(), "double_type"),
+                Column.of("int_type", Types.IntegerType.get(), "int_type"),
+                Column.of("varchar_type", Types.StringType.get(), "varchar_type"),
+                Column.of("char_type", Types.FixedCharType.of(1), "char_type"),
+                Column.of("boolean_type", Types.BooleanType.get(), "boolean_type"),
+                Column.of("byte_type", Types.ByteType.get(), "byte_type"),
+                Column.of("binary_type", Types.BinaryType.get(), "binary_type"),
+                Column.of("decimal_type", Types.DecimalType.of(10, 2), "decimal_type"),
+                Column.of("bigint_type", Types.LongType.get(), "bigint_type"),
+                Column.of("float_type", Types.FloatType.get(), "float_type"),
+                Column.of("date_type", Types.DateType.get(), "date_type"),
+                Column.of(
+                    "timestamp_type", Types.TimestampType.withoutTimeZone(), "timestamp_type"),
+                Column.of("smallint_type", Types.ShortType.get(), "smallint_type"),
+                Column.of(
+                    "array_type", Types.ListType.of(Types.IntegerType.get(), true), "array_type"),
+                Column.of(
+                    "map_type",
+                    Types.MapType.of(Types.IntegerType.get(), Types.StringType.get(), true),
+                    "map_type"),
+                Column.of(
+                    "struct_type",
+                    Types.StructType.of(
+                        Types.StructType.Field.nullableField("k1", Types.IntegerType.get()),
+                        Types.StructType.Field.nullableField("k2", Types.StringType.get())),
+                    null)
+              };
+          assertColumns(columns, table.columns());
+          Assertions.assertArrayEquals(EMPTY_TRANSFORM, table.partitioning());
+        },
+        false);
+  }
+
+  @Test
+  public void testGetHiveTable() {
+    Column[] columns =
+        new Column[] {
+          Column.of("string_type", Types.StringType.get(), "string_type", true, false, null),
+          Column.of("double_type", Types.DoubleType.get(), "double_type"),
+          Column.of("int_type", Types.IntegerType.get(), "int_type"),
+          Column.of("varchar_type", Types.StringType.get(), "varchar_type"),
+          Column.of("char_type", Types.FixedCharType.of(1), "char_type"),
+          Column.of("boolean_type", Types.BooleanType.get(), "boolean_type"),
+          Column.of("byte_type", Types.ByteType.get(), "byte_type"),
+          Column.of("binary_type", Types.BinaryType.get(), "binary_type"),
+          Column.of("decimal_type", Types.DecimalType.of(10, 2), "decimal_type"),
+          Column.of("bigint_type", Types.LongType.get(), "bigint_type"),
+          Column.of("float_type", Types.FloatType.get(), "float_type"),
+          Column.of("date_type", Types.DateType.get(), "date_type"),
+          Column.of("timestamp_type", Types.TimestampType.withoutTimeZone(), "timestamp_type"),
+          Column.of("smallint_type", Types.ShortType.get(), "smallint_type"),
+          Column.of("array_type", Types.ListType.of(Types.IntegerType.get(), true), "array_type"),
+          Column.of(
+              "map_type",
+              Types.MapType.of(Types.IntegerType.get(), Types.StringType.get(), true),
+              "map_type"),
+          Column.of(
+              "struct_type",
+              Types.StructType.of(
+                  Types.StructType.Field.nullableField("k1", Types.IntegerType.get()),
+                  Types.StructType.Field.nullableField("k2", Types.StringType.get())),
+              null)
+        };
+
+    doWithSchema(
+        metalake.loadCatalog(DEFAULT_HIVE_CATALOG),
+        DEFAULT_HIVE_SCHEMA,
+        catalog -> {
+          String tableName = "test_desc_table";
+          String comment = "comment1";
+          catalog
+              .asTableCatalog()
+              .createTable(
+                  NameIdentifier.of(DEFAULT_HIVE_SCHEMA, "test_desc_table"),
+                  columns,
+                  comment,
+                  ImmutableMap.of("k1", "v1"));
+
+          Optional<org.apache.flink.table.catalog.Catalog> flinkCatalog =
+              tableEnv.getCatalog(DEFAULT_HIVE_CATALOG);
+          Assertions.assertTrue(flinkCatalog.isPresent());
+          try {
+            CatalogBaseTable table =
+                flinkCatalog.get().getTable(new ObjectPath(DEFAULT_HIVE_SCHEMA, tableName));
+            Assertions.assertNotNull(table);
+            Assertions.assertEquals(CatalogBaseTable.TableKind.TABLE, table.getTableKind());
+            Assertions.assertEquals(comment, table.getComment());
+
+            org.apache.flink.table.catalog.Column[] expected =
+                new org.apache.flink.table.catalog.Column[] {
+                  org.apache.flink.table.catalog.Column.physical("string_type", DataTypes.STRING())
+                      .withComment("string_type"),
+                  org.apache.flink.table.catalog.Column.physical("double_type", DataTypes.DOUBLE())
+                      .withComment("double_type"),
+                  org.apache.flink.table.catalog.Column.physical("int_type", DataTypes.INT())
+                      .withComment("int_type"),
+                  org.apache.flink.table.catalog.Column.physical(
+                          "varchar_type", DataTypes.VARCHAR(Integer.MAX_VALUE))
+                      .withComment("varchar_type"),
+                  org.apache.flink.table.catalog.Column.physical("char_type", DataTypes.CHAR(1))
+                      .withComment("char_type"),
+                  org.apache.flink.table.catalog.Column.physical(
+                          "boolean_type", DataTypes.BOOLEAN())
+                      .withComment("boolean_type"),
+                  org.apache.flink.table.catalog.Column.physical("byte_type", DataTypes.BINARY(1))
+                      .withComment("byte_type"),
+                  org.apache.flink.table.catalog.Column.physical("binary_type", DataTypes.BYTES())
+                      .withComment("binary_type"),
+                  org.apache.flink.table.catalog.Column.physical(
+                          "decimal_type", DataTypes.DECIMAL(10, 2))
+                      .withComment("decimal_type"),
+                  org.apache.flink.table.catalog.Column.physical("bigint_type", DataTypes.BIGINT())
+                      .withComment("bigint_type"),
+                  org.apache.flink.table.catalog.Column.physical("float_type", DataTypes.FLOAT())
+                      .withComment("float_type"),
+                  org.apache.flink.table.catalog.Column.physical("date_type", DataTypes.DATE())
+                      .withComment("date_type"),
+                  org.apache.flink.table.catalog.Column.physical(
+                          "timestamp_type", DataTypes.TIMESTAMP())
+                      .withComment("timestamp_type"),
+                  org.apache.flink.table.catalog.Column.physical(
+                          "smallint_type", DataTypes.SMALLINT())
+                      .withComment("smallint_type"),
+                  org.apache.flink.table.catalog.Column.physical(
+                          "array_type", DataTypes.ARRAY(DataTypes.INT()))
+                      .withComment("array_type"),
+                  org.apache.flink.table.catalog.Column.physical(
+                          "map_type", DataTypes.MAP(DataTypes.INT(), DataTypes.STRING()))
+                      .withComment("map_type"),
+                  org.apache.flink.table.catalog.Column.physical(
+                      "struct_type",
+                      DataTypes.ROW(
+                          DataTypes.FIELD("k1", DataTypes.INT()),
+                          DataTypes.FIELD("k2", DataTypes.STRING())))
+                };
+            org.apache.flink.table.catalog.Column[] actual =
+                toFlinkPhysicalColumn(table.getUnresolvedSchema().getColumns());
+            Assertions.assertArrayEquals(expected, actual);
+
+            CatalogTable catalogTable = (CatalogTable) table;
+            Assertions.assertFalse(catalogTable.isPartitioned());
+          } catch (TableNotExistException e) {
+            Assertions.fail(e);
+          }
+        },
+        false);
   }
 
   @Override
