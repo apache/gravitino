087a0bd597878d3bd82555c476159d27d0cecba5
rickyma <rickyma@tencent.com>
2024-07-26 16:51:14 +0800

    Fix codestyle

--- catalogs/catalog-hadoop/src/main/java/org/apache/gravitino/catalog/hadoop/HadoopCatalog.java
+++ catalogs/catalog-hadoop/src/main/java/org/apache/gravitino/catalog/hadoop/HadoopCatalog.java
@@ -26,8 +26,8 @@ import org.apache.gravitino.connector.capability.Capability;
 
 /**
  * Hadoop catalog is a fileset catalog that can manage filesets on the Hadoop Compatible File
- * Systems, like Local, HDFS, S3, ADLS, etc., using the Hadoop FileSystem API. It can manage filesets
- * from different Hadoop Compatible File Systems in the same catalog.
+ * Systems, like Local, HDFS, S3, ADLS, etc., using the Hadoop FileSystem API. It can manage
+ * filesets from different Hadoop Compatible File Systems in the same catalog.
  */
 public class HadoopCatalog extends BaseCatalog<HadoopCatalog> {
 
