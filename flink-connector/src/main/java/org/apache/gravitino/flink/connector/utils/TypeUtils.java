1ed19176f50a89e9f9735a65b7e4f6e97fe65281
Peidian li <38486782+coolderli@users.noreply.github.com>
2024-07-29 20:32:34 +0800

    [#3982]feat(flink-connector):Support more column data type for hive table (#4265)
    ### What changes were proposed in this pull request?
    
    - Support more columns for hive table
    
    ### Why are the changes needed?
    
    Fix: #3982 
    
    ### Does this PR introduce _any_ user-facing change?
    
    - no
    
    ### How was this patch tested?
    
    - add UTs and ITs:
    flink-connector/src/test/java/org/apache/gravitino/flink/connector/integration/test/hive/FlinkHiveCatalogIT.java
    -
    flink-connector/src/test/java/org/apache/gravitino/flink/connector/utils/TestTypeUtils.java

--- flink-connector/src/main/java/org/apache/gravitino/flink/connector/utils/TypeUtils.java
+++ flink-connector/src/main/java/org/apache/gravitino/flink/connector/utils/TypeUtils.java
@@ -19,9 +19,18 @@
 
 package org.apache.gravitino.flink.connector.utils;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.apache.flink.table.api.DataTypes;
 import org.apache.flink.table.types.DataType;
+import org.apache.flink.table.types.logical.ArrayType;
+import org.apache.flink.table.types.logical.BinaryType;
+import org.apache.flink.table.types.logical.CharType;
+import org.apache.flink.table.types.logical.DecimalType;
 import org.apache.flink.table.types.logical.LogicalType;
+import org.apache.flink.table.types.logical.MapType;
+import org.apache.flink.table.types.logical.RowType;
 import org.apache.gravitino.rel.types.Type;
 import org.apache.gravitino.rel.types.Types;
 
@@ -39,6 +48,71 @@ public class TypeUtils {
         return Types.IntegerType.get();
       case BIGINT:
         return Types.LongType.get();
+      case CHAR:
+        CharType charType = (CharType) logicalType;
+        return Types.FixedCharType.of(charType.getLength());
+      case BOOLEAN:
+        return Types.BooleanType.get();
+      case BINARY:
+        BinaryType binaryType = (BinaryType) logicalType;
+        return Types.FixedType.of(binaryType.getLength());
+      case VARBINARY:
+        return Types.BinaryType.get();
+      case DECIMAL:
+        DecimalType decimalType = (DecimalType) logicalType;
+        return Types.DecimalType.of(decimalType.getPrecision(), decimalType.getScale());
+      case TINYINT:
+        return Types.ByteType.get();
+      case SMALLINT:
+        return Types.ShortType.get();
+      case DATE:
+        return Types.DateType.get();
+      case TIME_WITHOUT_TIME_ZONE:
+        return Types.TimeType.get();
+      case TIMESTAMP_WITHOUT_TIME_ZONE:
+        return Types.TimestampType.withoutTimeZone();
+      case INTERVAL_YEAR_MONTH:
+        return Types.IntervalYearType.get();
+      case INTERVAL_DAY_TIME:
+        return Types.IntervalDayType.get();
+      case FLOAT:
+        return Types.FloatType.get();
+      case TIMESTAMP_WITH_LOCAL_TIME_ZONE:
+      case TIMESTAMP_WITH_TIME_ZONE:
+        return Types.TimestampType.withTimeZone();
+      case ARRAY:
+        ArrayType arrayType = (ArrayType) logicalType;
+        Type elementType = toGravitinoType(arrayType.getElementType());
+        return Types.ListType.of(elementType, arrayType.isNullable());
+      case MAP:
+        MapType mapType = (MapType) logicalType;
+        Type keyType = toGravitinoType(mapType.getKeyType());
+        Type valueType = toGravitinoType(mapType.getValueType());
+        return Types.MapType.of(keyType, valueType, mapType.isNullable());
+      case ROW:
+        RowType rowType = (RowType) logicalType;
+        Types.StructType.Field[] fields =
+            rowType.getFields().stream()
+                .map(
+                    field -> {
+                      LogicalType fieldLogicalType = field.getType();
+                      Type fieldType = toGravitinoType(fieldLogicalType);
+                      return Types.StructType.Field.of(
+                          field.getName(),
+                          fieldType,
+                          fieldLogicalType.isNullable(),
+                          field.getDescription().orElse(null));
+                    })
+                .toArray(Types.StructType.Field[]::new);
+        return Types.StructType.of(fields);
+      case NULL:
+        return Types.NullType.get();
+      case MULTISET:
+      case STRUCTURED_TYPE:
+      case UNRESOLVED:
+      case DISTINCT_TYPE:
+      case RAW:
+      case SYMBOL:
       default:
         throw new UnsupportedOperationException(
             "Not support type: " + logicalType.asSummaryString());
@@ -55,8 +129,80 @@ public class TypeUtils {
         return DataTypes.INT();
       case LONG:
         return DataTypes.BIGINT();
+      case FLOAT:
+        return DataTypes.FLOAT();
+      case SHORT:
+        return DataTypes.SMALLINT();
+      case DECIMAL:
+        Types.DecimalType decimalType = (Types.DecimalType) gravitinoType;
+        return DataTypes.DECIMAL(decimalType.precision(), decimalType.scale());
+      case VARCHAR:
+        Types.VarCharType varCharType = (Types.VarCharType) gravitinoType;
+        return DataTypes.VARCHAR(varCharType.length());
+      case FIXED:
+        Types.FixedType fixedType = (Types.FixedType) gravitinoType;
+        return DataTypes.BINARY(fixedType.length());
+      case FIXEDCHAR:
+        Types.FixedCharType charType = (Types.FixedCharType) gravitinoType;
+        return DataTypes.CHAR(charType.length());
+      case BINARY:
+        return DataTypes.BYTES();
+      case BYTE:
+        return DataTypes.TINYINT();
+      case BOOLEAN:
+        return DataTypes.BOOLEAN();
+      case DATE:
+        return DataTypes.DATE();
+      case TIMESTAMP:
+        Types.TimestampType timestampType = (Types.TimestampType) gravitinoType;
+        if (timestampType.hasTimeZone()) {
+          return DataTypes.TIMESTAMP_LTZ();
+        } else {
+          return DataTypes.TIMESTAMP();
+        }
+      case LIST:
+        Types.ListType listType = (Types.ListType) gravitinoType;
+        return DataTypes.ARRAY(
+            nullable(toFlinkType(listType.elementType()), listType.elementNullable()));
+      case MAP:
+        Types.MapType mapType = (Types.MapType) gravitinoType;
+        return DataTypes.MAP(
+            toFlinkType(mapType.keyType()),
+            nullable(toFlinkType(mapType.valueType()), mapType.valueNullable()));
+      case STRUCT:
+        Types.StructType structType = (Types.StructType) gravitinoType;
+        List<DataTypes.Field> fields =
+            Arrays.stream(structType.fields())
+                .map(
+                    f -> {
+                      if (f.comment() == null) {
+                        return DataTypes.FIELD(
+                            f.name(), nullable(toFlinkType(f.type()), f.nullable()));
+                      } else {
+                        return DataTypes.FIELD(
+                            f.name(), nullable(toFlinkType(f.type()), f.nullable()), f.comment());
+                      }
+                    })
+                .collect(Collectors.toList());
+        return DataTypes.ROW(fields);
+      case NULL:
+        return DataTypes.NULL();
+      case TIME:
+        return DataTypes.TIME();
+      case INTERVAL_YEAR:
+        return DataTypes.INTERVAL(DataTypes.YEAR());
+      case INTERVAL_DAY:
+        return DataTypes.INTERVAL(DataTypes.DAY());
       default:
         throw new UnsupportedOperationException("Not support " + gravitinoType.toString());
+    }
+  }
+
+  private static DataType nullable(DataType dataType, boolean nullable) {
+    if (nullable) {
+      return dataType.nullable();
+    } else {
+      return dataType.notNull();
     }
   }
 }
