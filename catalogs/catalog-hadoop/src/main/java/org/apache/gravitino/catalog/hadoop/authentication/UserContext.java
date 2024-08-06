37f47adcbe31ed6a862716cf76ea93189cfc3a7d
kartik <75984244+mrkartik00@users.noreply.github.com>
2024-08-06 08:27:17 +0530

    Update UserContext.java

--- catalogs/catalog-hadoop/src/main/java/org/apache/gravitino/catalog/hadoop/authentication/UserContext.java
+++ catalogs/catalog-hadoop/src/main/java/org/apache/gravitino/catalog/hadoop/authentication/UserContext.java
@@ -123,6 +123,8 @@ public abstract class UserContext implements Closeable {
       addUserContext(nameIdentifier, simpleUserContext);
       return simpleUserContext;
     } else if (authenticationType == AuthenticationType.KERBEROS) {
+      // if the kerberos authentication is inherited from the parent context, we will use the
+      // parent context's kerberos configuration.
       // Improvement: Check if parentContext is not null before using it
       if (parentContext != null && authenticationConfig.isSimpleAuth()) {
         KerberosUserContext kerberosUserContext = ((KerberosUserContext) parentContext).deepCopy();
