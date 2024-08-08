0b068de31c79d63d510a80374612a696e4346bcb
yuqi <yuqi@datastrato.com>
2024-08-08 11:32:59 +0800

    Fix

--- iceberg/iceberg-common/src/main/java/org/apache/gravitino/iceberg/common/utils/IcebergCatalogUtil.java
+++ iceberg/iceberg-common/src/main/java/org/apache/gravitino/iceberg/common/utils/IcebergCatalogUtil.java
@@ -99,8 +99,14 @@ public class IcebergCatalogUtil {
       Map<String, String> properties, Configuration conf) {
     try {
       KerberosClient kerberosClient = new KerberosClient(properties, conf);
-      File keytabFile =
-          kerberosClient.saveKeyTabFileFromUri(Long.valueOf(properties.get("catalog_uuid")));
+      String catalogUUID = properties.get("catalog_uuid");
+      if (catalogUUID == null) {
+        // For Iceberg rest server, we haven't set the catalog_uuid, so we set it to 0 as there is
+        // only one catalog in the rest server, so it's okay to set it to 0.
+        catalogUUID = "0";
+      }
+
+      File keytabFile = kerberosClient.saveKeyTabFileFromUri(Long.valueOf(catalogUUID));
       kerberosClient.login(keytabFile.getAbsolutePath());
       return kerberosClient;
     } catch (IOException e) {
