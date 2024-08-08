b68213f3585bd02bd3d3e0c38eec855fea7223db
Naresh-kumar-Thodupunoori <140937322+Naresh-kumar-Thodupunoori@users.noreply.github.com>
2024-08-08 07:12:00 +0000

    Changes made accordingly

--- catalogs/catalog-hadoop/src/main/java/org/apache/gravitino/catalog/hadoop/authentication/kerberos/KerberosClient.java
+++ catalogs/catalog-hadoop/src/main/java/org/apache/gravitino/catalog/hadoop/authentication/kerberos/KerberosClient.java
@@ -67,7 +67,6 @@ public class KerberosClient implements Closeable {
     UserGroupInformation.setConfiguration(hadoopConf);
     UserGroupInformation kerberosLoginUgi =
         UserGroupInformation.loginUserFromKeytabAndReturnUGI(catalogPrincipal, keytabFilePath);
-    // Replace setLoginUser with an alternative approach
     UserGroupInformation.loginUserFromKeytab(catalogPrincipal, keytabFilePath);
 
     // Refresh the cache if it's out of date.
