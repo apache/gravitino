6e5e30d61f747d492bcf9c7d9b695d40fe7bf031
Yuhui <hui@datastrato.com>
2024-08-16 19:11:05 +0800

    [#3650] fix (trino-connector): Fix failed to run the tester jdbc-mysql/00008_alter_catalog in Trino distribution environment (#4497)
    ### What changes were proposed in this pull request?
    
    Fix failed to run the tester jdbc-mysql/00008_alter_catalog in Trino
    distribution environment
    
    ### Why are the changes needed?
    
    Fix: #3650
    
    ### Does this PR introduce _any_ user-facing change?
    
    NO
    
    ### How was this patch tested?
    
    Manually test

--- integration-test/trino-it/init/hive/init.sh
+++ integration-test/trino-it/init/hive/init.sh
@@ -16,8 +16,6 @@
 # specific language governing permissions and limitations
 # under the License.
 
-#
-
 IP=$(hostname -I | awk '{print $1}')
 sed -i "s|<value>hdfs://__REPLACE__HOST_NAME:9000|<value>hdfs://${IP}:9000|g" ${HIVE_TMP_CONF_DIR}/hive-site.xml
 
