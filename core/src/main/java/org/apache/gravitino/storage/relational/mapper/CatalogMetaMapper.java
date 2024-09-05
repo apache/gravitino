e9929c875e48ec02dd001cc70b80b3de87ce5435
Qi Yu <yuqi@datastrato.com>
2024-09-06 00:05:34 +0800

    [#4832] feat(core): Add basic framework to support multiple JDBC backend. (#4833)
    ### What changes were proposed in this pull request?
    
    1. Add a framework to support multiple JDBC backend 
    2. Modify all the mapper classes related to metalake, catalog, schema,
    filesetset, table, topic and use new framework, others will use another
    PR to avoid a large PR.
    
    ### Why are the changes needed?
    
    To support more JDBC storage backend.
    
    Fix: #4832 
    
    ### Does this PR introduce _any_ user-facing change?
    
    N/A
    
    ### How was this patch tested?
    
    Existing tests.

--- core/src/main/java/org/apache/gravitino/storage/relational/mapper/CatalogMetaMapper.java
+++ core/src/main/java/org/apache/gravitino/storage/relational/mapper/CatalogMetaMapper.java
@@ -21,11 +21,11 @@ package org.apache.gravitino.storage.relational.mapper;
 
 import java.util.List;
 import org.apache.gravitino.storage.relational.po.CatalogPO;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.DeleteProvider;
+import org.apache.ibatis.annotations.InsertProvider;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.SelectProvider;
+import org.apache.ibatis.annotations.UpdateProvider;
 
 /**
  * A MyBatis Mapper for catalog meta operation SQLs.
@@ -38,149 +38,50 @@ import org.apache.ibatis.annotations.Update;
 public interface CatalogMetaMapper {
   String TABLE_NAME = "catalog_meta";
 
-  @Select(
-      "SELECT catalog_id as catalogId, catalog_name as catalogName,"
-          + " metalake_id as metalakeId, type, provider,"
-          + " catalog_comment as catalogComment, properties, audit_info as auditInfo,"
-          + " current_version as currentVersion, last_version as lastVersion,"
-          + " deleted_at as deletedAt"
-          + " FROM "
-          + TABLE_NAME
-          + " WHERE metalake_id = #{metalakeId} AND deleted_at = 0")
+  @SelectProvider(type = CatalogMetaSQLProviderFactory.class, method = "listCatalogPOsByMetalakeId")
   List<CatalogPO> listCatalogPOsByMetalakeId(@Param("metalakeId") Long metalakeId);
 
-  @Select(
-      "SELECT catalog_id as catalogId FROM "
-          + TABLE_NAME
-          + " WHERE metalake_id = #{metalakeId} AND catalog_name = #{catalogName} AND deleted_at = 0")
+  @SelectProvider(
+      type = CatalogMetaSQLProviderFactory.class,
+      method = "selectCatalogIdByMetalakeIdAndName")
   Long selectCatalogIdByMetalakeIdAndName(
       @Param("metalakeId") Long metalakeId, @Param("catalogName") String name);
 
-  @Select(
-      "SELECT catalog_id as catalogId, catalog_name as catalogName,"
-          + " metalake_id as metalakeId, type, provider,"
-          + " catalog_comment as catalogComment, properties, audit_info as auditInfo,"
-          + " current_version as currentVersion, last_version as lastVersion,"
-          + " deleted_at as deletedAt"
-          + " FROM "
-          + TABLE_NAME
-          + " WHERE metalake_id = #{metalakeId} AND catalog_name = #{catalogName} AND deleted_at = 0")
+  @SelectProvider(
+      type = CatalogMetaSQLProviderFactory.class,
+      method = "selectCatalogMetaByMetalakeIdAndName")
   CatalogPO selectCatalogMetaByMetalakeIdAndName(
       @Param("metalakeId") Long metalakeId, @Param("catalogName") String name);
 
-  @Select(
-      "SELECT catalog_id as catalogId, catalog_name as catalogName,"
-          + " metalake_id as metalakeId, type, provider,"
-          + " catalog_comment as catalogComment, properties, audit_info as auditInfo,"
-          + " current_version as currentVersion, last_version as lastVersion,"
-          + " deleted_at as deletedAt"
-          + " FROM "
-          + TABLE_NAME
-          + " WHERE catalog_id = #{catalogId} AND deleted_at = 0")
+  @SelectProvider(type = CatalogMetaSQLProviderFactory.class, method = "selectCatalogMetaById")
   CatalogPO selectCatalogMetaById(@Param("catalogId") Long catalogId);
 
-  @Insert(
-      "INSERT INTO "
-          + TABLE_NAME
-          + "(catalog_id, catalog_name, metalake_id,"
-          + " type, provider, catalog_comment, properties, audit_info,"
-          + " current_version, last_version, deleted_at)"
-          + " VALUES("
-          + " #{catalogMeta.catalogId},"
-          + " #{catalogMeta.catalogName},"
-          + " #{catalogMeta.metalakeId},"
-          + " #{catalogMeta.type},"
-          + " #{catalogMeta.provider},"
-          + " #{catalogMeta.catalogComment},"
-          + " #{catalogMeta.properties},"
-          + " #{catalogMeta.auditInfo},"
-          + " #{catalogMeta.currentVersion},"
-          + " #{catalogMeta.lastVersion},"
-          + " #{catalogMeta.deletedAt}"
-          + " )")
+  @InsertProvider(type = CatalogMetaSQLProviderFactory.class, method = "insertCatalogMeta")
   void insertCatalogMeta(@Param("catalogMeta") CatalogPO catalogPO);
 
-  @Insert(
-      "INSERT INTO "
-          + TABLE_NAME
-          + "(catalog_id, catalog_name, metalake_id,"
-          + " type, provider, catalog_comment, properties, audit_info,"
-          + " current_version, last_version, deleted_at)"
-          + " VALUES("
-          + " #{catalogMeta.catalogId},"
-          + " #{catalogMeta.catalogName},"
-          + " #{catalogMeta.metalakeId},"
-          + " #{catalogMeta.type},"
-          + " #{catalogMeta.provider},"
-          + " #{catalogMeta.catalogComment},"
-          + " #{catalogMeta.properties},"
-          + " #{catalogMeta.auditInfo},"
-          + " #{catalogMeta.currentVersion},"
-          + " #{catalogMeta.lastVersion},"
-          + " #{catalogMeta.deletedAt}"
-          + " )"
-          + " ON DUPLICATE KEY UPDATE"
-          + " catalog_name = #{catalogMeta.catalogName},"
-          + " metalake_id = #{catalogMeta.metalakeId},"
-          + " type = #{catalogMeta.type},"
-          + " provider = #{catalogMeta.provider},"
-          + " catalog_comment = #{catalogMeta.catalogComment},"
-          + " properties = #{catalogMeta.properties},"
-          + " audit_info = #{catalogMeta.auditInfo},"
-          + " current_version = #{catalogMeta.currentVersion},"
-          + " last_version = #{catalogMeta.lastVersion},"
-          + " deleted_at = #{catalogMeta.deletedAt}")
+  @InsertProvider(
+      type = CatalogMetaSQLProviderFactory.class,
+      method = "insertCatalogMetaOnDuplicateKeyUpdate")
   void insertCatalogMetaOnDuplicateKeyUpdate(@Param("catalogMeta") CatalogPO catalogPO);
 
-  @Update(
-      "UPDATE "
-          + TABLE_NAME
-          + " SET catalog_name = #{newCatalogMeta.catalogName},"
-          + " metalake_id = #{newCatalogMeta.metalakeId},"
-          + " type = #{newCatalogMeta.type},"
-          + " provider = #{newCatalogMeta.provider},"
-          + " catalog_comment = #{newCatalogMeta.catalogComment},"
-          + " properties = #{newCatalogMeta.properties},"
-          + " audit_info = #{newCatalogMeta.auditInfo},"
-          + " current_version = #{newCatalogMeta.currentVersion},"
-          + " last_version = #{newCatalogMeta.lastVersion},"
-          + " deleted_at = #{newCatalogMeta.deletedAt}"
-          + " WHERE catalog_id = #{oldCatalogMeta.catalogId}"
-          + " AND catalog_name = #{oldCatalogMeta.catalogName}"
-          + " AND metalake_id = #{oldCatalogMeta.metalakeId}"
-          + " AND type = #{oldCatalogMeta.type}"
-          + " AND provider = #{oldCatalogMeta.provider}"
-          + " AND (catalog_comment = #{oldCatalogMeta.catalogComment} "
-          + "   OR (catalog_comment IS NULL and #{oldCatalogMeta.catalogComment} IS NULL))"
-          + " AND properties = #{oldCatalogMeta.properties}"
-          + " AND audit_info = #{oldCatalogMeta.auditInfo}"
-          + " AND current_version = #{oldCatalogMeta.currentVersion}"
-          + " AND last_version = #{oldCatalogMeta.lastVersion}"
-          + " AND deleted_at = 0")
+  @UpdateProvider(type = CatalogMetaSQLProviderFactory.class, method = "updateCatalogMeta")
   Integer updateCatalogMeta(
       @Param("newCatalogMeta") CatalogPO newCatalogPO,
       @Param("oldCatalogMeta") CatalogPO oldCatalogPO);
 
-  @Update(
-      "UPDATE "
-          + TABLE_NAME
-          + " SET deleted_at = (UNIX_TIMESTAMP() * 1000.0)"
-          + " + EXTRACT(MICROSECOND FROM CURRENT_TIMESTAMP(3)) / 1000"
-          + " WHERE catalog_id = #{catalogId} AND deleted_at = 0")
+  @UpdateProvider(
+      type = CatalogMetaSQLProviderFactory.class,
+      method = "softDeleteCatalogMetasByCatalogId")
   Integer softDeleteCatalogMetasByCatalogId(@Param("catalogId") Long catalogId);
 
-  @Update(
-      "UPDATE "
-          + TABLE_NAME
-          + " SET deleted_at = (UNIX_TIMESTAMP() * 1000.0)"
-          + " + EXTRACT(MICROSECOND FROM CURRENT_TIMESTAMP(3)) / 1000"
-          + " WHERE metalake_id = #{metalakeId} AND deleted_at = 0")
+  @UpdateProvider(
+      type = CatalogMetaSQLProviderFactory.class,
+      method = "softDeleteCatalogMetasByMetalakeId")
   Integer softDeleteCatalogMetasByMetalakeId(@Param("metalakeId") Long metalakeId);
 
-  @Delete(
-      "DELETE FROM "
-          + TABLE_NAME
-          + " WHERE deleted_at > 0 AND deleted_at < #{legacyTimeline} LIMIT #{limit}")
+  @DeleteProvider(
+      type = CatalogMetaSQLProviderFactory.class,
+      method = "deleteCatalogMetasByLegacyTimeline")
   Integer deleteCatalogMetasByLegacyTimeline(
       @Param("legacyTimeline") Long legacyTimeline, @Param("limit") int limit);
 }
--- core/src/main/java/org/apache/gravitino/storage/relational/mapper/MetalakeMetaMapper.java
+++ core/src/main/java/org/apache/gravitino/storage/relational/mapper/MetalakeMetaMapper.java
@@ -21,11 +21,11 @@ package org.apache.gravitino.storage.relational.mapper;
 
 import java.util.List;
 import org.apache.gravitino.storage.relational.po.MetalakePO;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.DeleteProvider;
+import org.apache.ibatis.annotations.InsertProvider;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.SelectProvider;
+import org.apache.ibatis.annotations.UpdateProvider;
 
 /**
  * A MyBatis Mapper for metalake meta operation SQLs.
@@ -38,126 +38,41 @@ import org.apache.ibatis.annotations.Update;
 public interface MetalakeMetaMapper {
   String TABLE_NAME = "metalake_meta";
 
-  @Select(
-      "SELECT metalake_id as metalakeId, metalake_name as metalakeName,"
-          + " metalake_comment as metalakeComment, properties,"
-          + " audit_info as auditInfo, schema_version as schemaVersion,"
-          + " current_version as currentVersion, last_version as lastVersion,"
-          + " deleted_at as deletedAt"
-          + " FROM "
-          + TABLE_NAME
-          + " WHERE deleted_at = 0")
+  @SelectProvider(type = MetalakeMetaSQLProviderFactory.class, method = "listMetalakePOs")
   List<MetalakePO> listMetalakePOs();
 
-  @Select(
-      "SELECT metalake_id as metalakeId, metalake_name as metalakeName,"
-          + " metalake_comment as metalakeComment, properties,"
-          + " audit_info as auditInfo, schema_version as schemaVersion,"
-          + " current_version as currentVersion, last_version as lastVersion,"
-          + " deleted_at as deletedAt"
-          + " FROM "
-          + TABLE_NAME
-          + " WHERE metalake_name = #{metalakeName} and deleted_at = 0")
+  @SelectProvider(type = MetalakeMetaSQLProviderFactory.class, method = "selectMetalakeMetaByName")
   MetalakePO selectMetalakeMetaByName(@Param("metalakeName") String name);
 
-  @Select(
-      "SELECT metalake_id as metalakeId, metalake_name as metalakeName,"
-          + " metalake_comment as metalakeComment, properties,"
-          + " audit_info as auditInfo, schema_version as schemaVersion,"
-          + " current_version as currentVersion, last_version as lastVersion,"
-          + " deleted_at as deletedAt"
-          + " FROM "
-          + TABLE_NAME
-          + " WHERE metalake_id = #{metalakeId} and deleted_at = 0")
+  @SelectProvider(type = MetalakeMetaSQLProviderFactory.class, method = "selectMetalakeMetaById")
   MetalakePO selectMetalakeMetaById(@Param("metalakeId") Long metalakeId);
 
-  @Select(
-      "SELECT metalake_id FROM "
-          + TABLE_NAME
-          + " WHERE metalake_name = #{metalakeName} and deleted_at = 0")
+  @SelectProvider(
+      type = MetalakeMetaSQLProviderFactory.class,
+      method = "selectMetalakeIdMetaByName")
   Long selectMetalakeIdMetaByName(@Param("metalakeName") String name);
 
-  @Insert(
-      "INSERT INTO "
-          + TABLE_NAME
-          + "(metalake_id, metalake_name, metalake_comment, properties, audit_info,"
-          + " schema_version, current_version, last_version, deleted_at)"
-          + " VALUES("
-          + " #{metalakeMeta.metalakeId},"
-          + " #{metalakeMeta.metalakeName},"
-          + " #{metalakeMeta.metalakeComment},"
-          + " #{metalakeMeta.properties},"
-          + " #{metalakeMeta.auditInfo},"
-          + " #{metalakeMeta.schemaVersion},"
-          + " #{metalakeMeta.currentVersion},"
-          + " #{metalakeMeta.lastVersion},"
-          + " #{metalakeMeta.deletedAt}"
-          + " )")
+  @InsertProvider(type = MetalakeMetaSQLProviderFactory.class, method = "insertMetalakeMeta")
   void insertMetalakeMeta(@Param("metalakeMeta") MetalakePO metalakePO);
 
-  @Insert(
-      "INSERT INTO "
-          + TABLE_NAME
-          + "(metalake_id, metalake_name, metalake_comment, properties, audit_info,"
-          + " schema_version, current_version, last_version, deleted_at)"
-          + " VALUES("
-          + " #{metalakeMeta.metalakeId},"
-          + " #{metalakeMeta.metalakeName},"
-          + " #{metalakeMeta.metalakeComment},"
-          + " #{metalakeMeta.properties},"
-          + " #{metalakeMeta.auditInfo},"
-          + " #{metalakeMeta.schemaVersion},"
-          + " #{metalakeMeta.currentVersion},"
-          + " #{metalakeMeta.lastVersion},"
-          + " #{metalakeMeta.deletedAt}"
-          + " )"
-          + " ON DUPLICATE KEY UPDATE"
-          + " metalake_name = #{metalakeMeta.metalakeName},"
-          + " metalake_comment = #{metalakeMeta.metalakeComment},"
-          + " properties = #{metalakeMeta.properties},"
-          + " audit_info = #{metalakeMeta.auditInfo},"
-          + " schema_version = #{metalakeMeta.schemaVersion},"
-          + " current_version = #{metalakeMeta.currentVersion},"
-          + " last_version = #{metalakeMeta.lastVersion},"
-          + " deleted_at = #{metalakeMeta.deletedAt}")
+  @InsertProvider(
+      type = MetalakeMetaSQLProviderFactory.class,
+      method = "insertMetalakeMetaOnDuplicateKeyUpdate")
   void insertMetalakeMetaOnDuplicateKeyUpdate(@Param("metalakeMeta") MetalakePO metalakePO);
 
-  @Update(
-      "UPDATE "
-          + TABLE_NAME
-          + " SET metalake_name = #{newMetalakeMeta.metalakeName},"
-          + " metalake_comment = #{newMetalakeMeta.metalakeComment},"
-          + " properties = #{newMetalakeMeta.properties},"
-          + " audit_info = #{newMetalakeMeta.auditInfo},"
-          + " schema_version = #{newMetalakeMeta.schemaVersion},"
-          + " current_version = #{newMetalakeMeta.currentVersion},"
-          + " last_version = #{newMetalakeMeta.lastVersion}"
-          + " WHERE metalake_id = #{oldMetalakeMeta.metalakeId}"
-          + " AND metalake_name = #{oldMetalakeMeta.metalakeName}"
-          + " AND (metalake_comment = #{oldMetalakeMeta.metalakeComment} "
-          + "  OR (metalake_comment IS NULL and #{oldMetalakeMeta.metalakeComment} IS NULL))"
-          + " AND properties = #{oldMetalakeMeta.properties}"
-          + " AND audit_info = #{oldMetalakeMeta.auditInfo}"
-          + " AND schema_version = #{oldMetalakeMeta.schemaVersion}"
-          + " AND current_version = #{oldMetalakeMeta.currentVersion}"
-          + " AND last_version = #{oldMetalakeMeta.lastVersion}"
-          + " AND deleted_at = 0")
+  @UpdateProvider(type = MetalakeMetaSQLProviderFactory.class, method = "updateMetalakeMeta")
   Integer updateMetalakeMeta(
       @Param("newMetalakeMeta") MetalakePO newMetalakePO,
       @Param("oldMetalakeMeta") MetalakePO oldMetalakePO);
 
-  @Update(
-      "UPDATE "
-          + TABLE_NAME
-          + " SET deleted_at = (UNIX_TIMESTAMP() * 1000.0)"
-          + " + EXTRACT(MICROSECOND FROM CURRENT_TIMESTAMP(3)) / 1000"
-          + " WHERE metalake_id = #{metalakeId} AND deleted_at = 0")
+  @UpdateProvider(
+      type = MetalakeMetaSQLProviderFactory.class,
+      method = "softDeleteMetalakeMetaByMetalakeId")
   Integer softDeleteMetalakeMetaByMetalakeId(@Param("metalakeId") Long metalakeId);
 
-  @Delete(
-      "DELETE FROM "
-          + TABLE_NAME
-          + " WHERE deleted_at > 0 AND deleted_at < #{legacyTimeline} LIMIT #{limit}")
+  @DeleteProvider(
+      type = MetalakeMetaSQLProviderFactory.class,
+      method = "deleteMetalakeMetasByLegacyTimeline")
   Integer deleteMetalakeMetasByLegacyTimeline(
       @Param("legacyTimeline") Long legacyTimeline, @Param("limit") int limit);
 }
