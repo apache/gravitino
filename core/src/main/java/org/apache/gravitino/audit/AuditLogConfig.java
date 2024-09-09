6345c2f5a9e4dfc61e741cc2c2bec65a65bbc575
han <hanwangxin@xiaomi.com>
2024-09-09 15:22:14 +0800

    add apache header or other review modify

--- core/src/main/java/org/apache/gravitino/audit/AuditLogConfig.java
+++ core/src/main/java/org/apache/gravitino/audit/AuditLogConfig.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.gravitino.audit;
 
 import java.util.Map;
@@ -18,21 +37,21 @@ public class AuditLogConfig extends Config {
 
   static final ConfigEntry<String> AUDIT_LOG_ENABLED_CONF =
       new ConfigBuilder(AUDIT_LOG_ENABLED)
-          .doc("Gravitino event audit log writer class")
+          .doc("Gravitino audit log enable flag")
           .version(ConfigConstants.VERSION_0_5_0)
           .stringConf()
           .createWithDefault("false");
 
   static final ConfigEntry<String> WRITER_CLASS_NAME =
       new ConfigBuilder(AUDIT_LOG_WRITER_CLASS_NAME)
-          .doc("Gravitino event audit log writer class")
+          .doc("Gravitino audit log writer class name")
           .version(ConfigConstants.VERSION_0_5_0)
           .stringConf()
           .createWithDefault("");
 
   static final ConfigEntry<String> FORMATTER_CLASS_NAME =
       new ConfigBuilder(AUDIT_LOG_FORMATTER_CLASS_NAME)
-          .doc("Gravitino event audit log writer class")
+          .doc("Gravitino event log formatter class name")
           .version(ConfigConstants.VERSION_0_5_0)
           .stringConf()
           .createWithDefault("");
