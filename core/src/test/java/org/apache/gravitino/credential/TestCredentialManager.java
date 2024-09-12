8ed1b75d8ba77d521be998c70c4521fc393710d0
fanng <xiaojing@datastrato.com>
2024-09-12 11:56:26 +0800

    credential framework

---
+++ core/src/test/java/org/apache/gravitino/credential/TestCredentialManager.java
@@ -0,0 +1,42 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.apache.gravitino.credential;
+
+import java.util.Map;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.shaded.com.google.common.collect.ImmutableMap;
+
+public class TestCredentialManager {
+
+  @Test
+  void testLoadTestCredentialProvider() {
+    CredentialManager credentialManager = new CredentialManager();
+    Map<String, String> catalogProperties = ImmutableMap.of("a", "b");
+    CredentialProvider credentialProvider =
+        credentialManager.getCredentialProvider(
+            TestCredentialProvider.CREDENTIAL_TYPE, catalogProperties);
+    Assertions.assertEquals(
+        TestCredentialProvider.CREDENTIAL_TYPE, credentialProvider.credentialType());
+    Assertions.assertTrue(credentialProvider instanceof TestCredentialProvider);
+    Assertions.assertEquals(
+        catalogProperties, ((TestCredentialProvider) credentialProvider).properties);
+  }
+}
