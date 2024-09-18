00eb86f9284bde4b9f5edd50dd1d676abd7bcc1c
fanng <xiaojing@datastrato.com>
2024-09-18 16:22:00 +0800

    s3 credential

--- catalogs/catalog-common/src/main/java/org/apache/gravitino/credential/CredentialConstants.java
+++ catalogs/catalog-common/src/main/java/org/apache/gravitino/credential/CredentialConstants.java
@@ -22,5 +22,8 @@ package org.apache.gravitino.credential;
 public class CredentialConstants {
   public static final String CREDENTIAL_PROVIDER_TYPE = "credential-provider-type";
 
+  public static final String S3_TOKEN_CREDENTIAL_TYPE = "s3-token";
+  public static final String S3_SECRET_KEY_CREDENTIAL_TYPE = "s3-secret-key";
+
   private CredentialConstants() {}
 }
