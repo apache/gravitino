7a79bf6afff342013e2db3d30d58e837dc6fbdc7
theoryxu <theoryxu@tencent.com>
2024-09-19 16:53:13 +0800

    [#4370]feat(iceberg): support view interface for Iceberg REST server

--- iceberg/iceberg-rest-server/src/main/java/org/apache/gravitino/iceberg/service/rest/IcebergViewOperations.java
+++ iceberg/iceberg-rest-server/src/main/java/org/apache/gravitino/iceberg/service/rest/IcebergViewOperations.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.gravitino.iceberg.service.rest;
 
 import com.codahale.metrics.annotation.ResponseMetered;
@@ -21,7 +39,6 @@ import org.apache.gravitino.metrics.MetricNames;
 import org.apache.iceberg.catalog.TableIdentifier;
 import org.apache.iceberg.rest.RESTUtil;
 import org.apache.iceberg.rest.requests.CreateViewRequest;
-import org.apache.iceberg.rest.requests.RenameTableRequest;
 import org.apache.iceberg.rest.requests.UpdateTableRequest;
 import org.apache.iceberg.rest.responses.ListTablesResponse;
 import org.apache.iceberg.rest.responses.LoadViewResponse;
@@ -117,7 +134,7 @@ public class IcebergViewOperations {
   @Produces(MediaType.APPLICATION_JSON)
   @Timed(name = "view-exists." + MetricNames.HTTP_PROCESS_DURATION, absolute = true)
   @ResponseMetered(name = "view-exits", absolute = true)
-  public Response tableExists(
+  public Response viewExists(
       @PathParam("prefix") String prefix,
       @PathParam("namespace") String namespace,
       @PathParam("view") String view) {
@@ -127,18 +144,5 @@ public class IcebergViewOperations {
     } else {
       return IcebergRestUtils.notExists();
     }
-  }
-
-  @POST
-  @Path("rename")
-  @Produces(MediaType.APPLICATION_JSON)
-  @Timed(name = "rename-view." + MetricNames.HTTP_PROCESS_DURATION, absolute = true)
-  @ResponseMetered(name = "rename-view", absolute = true)
-  public Response renameView(
-      @PathParam("prefix") String prefix,
-      @PathParam("namespace") String namespace,
-      RenameTableRequest request) {
-    icebergTableOpsManager.getOps(prefix).renameView(request);
-    return IcebergRestUtils.noContent();
   }
 }
