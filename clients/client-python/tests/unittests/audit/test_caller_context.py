ecc0a0d54e67561ec5ac2bfea7f929d681ee58d0
xloya <982052490@qq.com>
2024-09-23 18:47:47 +0800

    set diff thread local

--- clients/client-python/tests/unittests/audit/test_caller_context.py
+++ clients/client-python/tests/unittests/audit/test_caller_context.py
@@ -14,6 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+import threading
 import unittest
 from typing import Dict
 
@@ -23,12 +24,40 @@ from gravitino.audit.caller_context import CallerContextHolder, CallerContext
 class TestCallerContext(unittest.TestCase):
 
     def test_caller_context(self):
+        thread_names_and_values = [
+            ("Thread1", {"k1": "v1", "k2": "v2"}),
+            ("Thread2", {"k11": "v11", "k21": "v21"}),
+        ]
+        test_threads = []
+        for thread_name, value in thread_names_and_values:
+            t = threading.Thread(
+                target=self._set_thread_local_context, args=(thread_name, value)
+            )
+            t.start()
+            test_threads.append(t)
+
+        for t in test_threads:
+            t.join()
+
+    def _set_thread_local_context(self, thread_name, context: Dict[str, str]):
+        caller_context: CallerContext = CallerContext(context)
+        CallerContextHolder.set(caller_context)
+
         try:
-            context: Dict = {"k1": "v1", "k2": "v2"}
-            caller_context: CallerContext = CallerContext(context)
-            CallerContextHolder.set(caller_context)
-            self.assertEqual(CallerContextHolder.get().context()["k1"], context["k1"])
-            self.assertEqual(CallerContextHolder.get().context()["k2"], context["k2"])
+            if thread_name == "Thread1":
+                self.assertEqual(
+                    CallerContextHolder.get().context()["k1"], context["k1"]
+                )
+                self.assertEqual(
+                    CallerContextHolder.get().context()["k2"], context["k2"]
+                )
+            if thread_name == "Thread2":
+                self.assertEqual(
+                    CallerContextHolder.get().context()["k11"], context["k11"]
+                )
+                self.assertEqual(
+                    CallerContextHolder.get().context()["k21"], context["k21"]
+                )
         finally:
             CallerContextHolder.remove()
 
