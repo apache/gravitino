2b8687a2fcec48863efcb99a0ef5478a0fd9ae3f
cai can <94670132+caican00@users.noreply.github.com>
2024-10-13 23:21:48 -0700

    [#3920] feat(catalog-lakehouse-paimon): Support jdbc backend for Paimon Catalog (#5087)
    ### What changes were proposed in this pull request?
    
    Support jdbc backend for Paimon Catalog
    
    ### Why are the changes needed?
    
    Fix: https://github.com/apache/gravitino/issues/3920
    
    ### Does this PR introduce _any_ user-facing change?
    yes, updated the related doc.
    
    ### How was this patch tested?
    new UT and IT.
    
    ---------
    
    Co-authored-by: caican <caican@xiaomi.com>

--- catalogs/catalog-lakehouse-paimon/build.gradle.kts
+++ catalogs/catalog-lakehouse-paimon/build.gradle.kts
@@ -122,6 +122,7 @@ dependencies {
   testImplementation(libs.junit.jupiter.api)
   testImplementation(libs.mysql.driver)
   testImplementation(libs.postgresql.driver)
+  testImplementation(libs.h2db)
   testImplementation(libs.bundles.log4j)
   testImplementation(libs.junit.jupiter.params)
   testImplementation(libs.paimon.oss)
@@ -129,6 +130,7 @@ dependencies {
   testImplementation(libs.paimon.spark)
   testImplementation(libs.testcontainers)
   testImplementation(libs.testcontainers.localstack)
+  testImplementation(libs.testcontainers.mysql)
 
   testRuntimeOnly(libs.junit.jupiter.engine)
 }
