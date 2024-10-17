2a31d65f7146f8e4ca1a5adc7d6c0f93a6ab27c7
Justin Mclean <justin@datastrato.com>
2024-10-16 17:13:31 -0700

    basic role operations

---
+++ clients/cli/src/main/java/org/apache/gravitino/cli/commands/CreateRole.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.gravitino.cli.commands;
+
+import java.util.Collections;
+import org.apache.gravitino.cli.ErrorMessages;
+import org.apache.gravitino.client.GravitinoClient;
+import org.apache.gravitino.exceptions.NoSuchMetalakeException;
+import org.apache.gravitino.exceptions.RoleAlreadyExistsException;
+
+public class CreateRole extends Command {
+  protected String metalake;
+  protected String role;
+
+  /**
+   * Create a new role.
+   *
+   * @param url The URL of the Gravitino server.
+   * @param metalake The name of the metalake.
+   * @param role The name of the role.
+   */
+  public CreateRole(String url, String metalake, String role) {
+    super(url);
+    this.metalake = metalake;
+    this.role = role;
+  }
+
+  /** Create a new role. */
+  public void handle() {
+    try {
+      GravitinoClient client = buildClient(metalake);
+      client.createRole(role, null, Collections.EMPTY_LIST);
+    } catch (NoSuchMetalakeException err) {
+      System.err.println(ErrorMessages.UNKNOWN_METALAKE);
+      return;
+    } catch (RoleAlreadyExistsException err) {
+      System.err.println(ErrorMessages.ROLE_EXISTS);
+      return;
+    } catch (Exception exp) {
+      System.err.println(exp.getMessage());
+      return;
+    }
+
+    System.out.println(role + " created");
+  }
+}
