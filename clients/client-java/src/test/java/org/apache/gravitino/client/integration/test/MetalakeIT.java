37fdc1938ec7a164707e9ef9f2dcb1d4e4dcffa0
mchades <liminghuang@datastrato.com>
2024-10-25 11:00:05 +0800

    [#5237][#5240] fix(metalake): fix list metalakes missing in-use property (#5241)
    ### What changes were proposed in this pull request?
    
     - fix list metalakes missing in-use property
     - fix NPE when null properties
    
    ### Why are the changes needed?
    
    list metalakes will show metalake details by default so need put in-use
    property in it
    
    Fix: #5240 
    Fix: #5237
    
    ### Does this PR introduce _any_ user-facing change?
    
    no
    
    ### How was this patch tested?
    
    tests added

--- clients/client-java/src/test/java/org/apache/gravitino/client/integration/test/MetalakeIT.java
+++ clients/client-java/src/test/java/org/apache/gravitino/client/integration/test/MetalakeIT.java
@@ -18,44 +18,54 @@
  */
 package org.apache.gravitino.client.integration.test;
 
+import static org.apache.gravitino.Metalake.PROPERTY_IN_USE;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.google.common.collect.ImmutableMap;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Map;
+import org.apache.gravitino.Catalog;
+import org.apache.gravitino.CatalogChange;
 import org.apache.gravitino.MetalakeChange;
 import org.apache.gravitino.NameIdentifier;
+import org.apache.gravitino.Namespace;
+import org.apache.gravitino.SchemaChange;
+import org.apache.gravitino.SupportsSchemas;
 import org.apache.gravitino.auth.AuthConstants;
 import org.apache.gravitino.client.GravitinoMetalake;
 import org.apache.gravitino.exceptions.IllegalNameIdentifierException;
 import org.apache.gravitino.exceptions.MetalakeAlreadyExistsException;
+import org.apache.gravitino.exceptions.MetalakeInUseException;
+import org.apache.gravitino.exceptions.MetalakeNotInUseException;
 import org.apache.gravitino.exceptions.NoSuchMetalakeException;
+import org.apache.gravitino.exceptions.NonEmptyMetalakeException;
+import org.apache.gravitino.file.FilesetCatalog;
+import org.apache.gravitino.file.FilesetChange;
 import org.apache.gravitino.integration.test.util.BaseIT;
+import org.apache.gravitino.integration.test.util.GravitinoITUtils;
 import org.apache.gravitino.utils.RandomNameUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MetalakeIT extends BaseIT {
   public static String metalakeNameA = RandomNameUtils.genRandomName("metalakeA");
   public static String metalakeNameB = RandomNameUtils.genRandomName("metalakeB");
 
   @BeforeEach
-  private void start() {
+  public void start() {
     // Just in case clean up is needed due to a test failure
     dropMetalakes();
   }
 
   @AfterEach
-  private void stop() {
+  public void stop() {
     dropMetalakes();
   }
 
@@ -88,6 +98,7 @@ public class MetalakeIT extends BaseIT {
     client.createMetalake(metalakeNameA, "metalake A comment", Collections.emptyMap());
     GravitinoMetalake metaLakeA = client.loadMetalake(metalakeNameA);
     assertEquals(metaLakeA.name(), metalakeNameA);
+    assertEquals("true", metaLakeA.properties().get(PROPERTY_IN_USE));
 
     // metalake does not exist
     NameIdentifier noexist = NameIdentifier.of(metalakeNameB);
@@ -150,6 +161,7 @@ public class MetalakeIT extends BaseIT {
     assertEquals(metalakeNameA, metalake.name());
     assertEquals("metalake A comment", metalake.comment());
     assertEquals(AuthConstants.ANONYMOUS_USER, metalake.auditInfo().creator());
+    assertEquals("true", metalake.properties().get(PROPERTY_IN_USE));
 
     // Test metalake name already exists
     Map<String, String> emptyMap = Collections.emptyMap();
@@ -210,11 +222,119 @@ public class MetalakeIT extends BaseIT {
     assertTrue(client.dropMetalake(metalakeNameA, true));
   }
 
+  @Test
+  public void testMetalakeAvailable() {
+    String metalakeName = RandomNameUtils.genRandomName("test_metalake");
+    client.createMetalake(metalakeName, null, null);
+    // test load metalake
+    GravitinoMetalake metalake = client.loadMetalake(metalakeName);
+    Assertions.assertEquals(metalakeName, metalake.name());
+    Assertions.assertEquals("true", metalake.properties().get(PROPERTY_IN_USE));
+
+    // test list metalakes
+    GravitinoMetalake[] metalakes = client.listMetalakes();
+    Assertions.assertEquals(1, metalakes.length);
+    Assertions.assertEquals(metalakeName, metalakes[0].name());
+    Assertions.assertEquals("true", metalakes[0].properties().get(PROPERTY_IN_USE));
+
+    Exception exception =
+        assertThrows(MetalakeInUseException.class, () -> client.dropMetalake(metalakeName));
+    Assertions.assertTrue(exception.getMessage().contains("please disable it first"));
+
+    // create a catalog under the metalake
+    String catalogName = GravitinoITUtils.genRandomName("test_catalog");
+    Catalog catalog =
+        metalake.createCatalog(
+            catalogName, Catalog.Type.FILESET, "hadoop", "catalog comment", ImmutableMap.of());
+    Assertions.assertEquals("true", catalog.properties().get(Catalog.PROPERTY_IN_USE));
+
+    Assertions.assertDoesNotThrow(() -> client.disableMetalake(metalakeName));
+    GravitinoMetalake loadedMetalake = client.loadMetalake(metalakeName);
+    Assertions.assertEquals("false", loadedMetalake.properties().get(PROPERTY_IN_USE));
+
+    exception =
+        assertThrows(
+            MetalakeNotInUseException.class,
+            () -> client.alterMetalake(metalakeName, MetalakeChange.updateComment("new comment")));
+    Assertions.assertTrue(exception.getMessage().contains("please enable it first"));
+
+    // test catalog operations under non-in-use metalake
+    Assertions.assertThrows(
+        MetalakeNotInUseException.class,
+        () ->
+            loadedMetalake.createCatalog(
+                catalogName, Catalog.Type.FILESET, "dummy", null, Collections.emptyMap()));
+    Assertions.assertThrows(MetalakeNotInUseException.class, loadedMetalake::listCatalogs);
+    Assertions.assertThrows(
+        MetalakeNotInUseException.class, () -> loadedMetalake.loadCatalog(catalogName));
+    Assertions.assertThrows(
+        MetalakeNotInUseException.class, () -> loadedMetalake.dropCatalog(catalogName));
+    Assertions.assertThrows(
+        MetalakeNotInUseException.class,
+        () -> loadedMetalake.alterCatalog(catalogName, CatalogChange.rename("dummy")));
+    Assertions.assertThrows(
+        MetalakeNotInUseException.class, () -> loadedMetalake.disableCatalog(catalogName));
+    Assertions.assertThrows(
+        MetalakeNotInUseException.class, () -> loadedMetalake.enableCatalog(catalogName));
+    Assertions.assertThrows(
+        MetalakeNotInUseException.class,
+        () ->
+            loadedMetalake.testConnection(
+                catalogName, Catalog.Type.FILESET, "dummy", null, Collections.emptyMap()));
+    Assertions.assertThrows(
+        MetalakeNotInUseException.class, () -> loadedMetalake.catalogExists(catalogName));
+
+    // test schema operations under non-in-use metalake
+    SupportsSchemas schemaOps = catalog.asSchemas();
+    Assertions.assertThrows(MetalakeNotInUseException.class, schemaOps::listSchemas);
+    Assertions.assertThrows(
+        MetalakeNotInUseException.class, () -> schemaOps.createSchema("dummy", null, null));
+    Assertions.assertThrows(MetalakeNotInUseException.class, () -> schemaOps.loadSchema("dummy"));
+    Assertions.assertThrows(
+        MetalakeNotInUseException.class,
+        () -> schemaOps.alterSchema("dummy", SchemaChange.removeProperty("dummy")));
+    Assertions.assertThrows(
+        MetalakeNotInUseException.class, () -> schemaOps.dropSchema("dummy", false));
+
+    // test fileset operations under non-in-use catalog
+    FilesetCatalog filesetOps = catalog.asFilesetCatalog();
+    Assertions.assertThrows(
+        MetalakeNotInUseException.class, () -> filesetOps.listFilesets(Namespace.of("dummy")));
+    Assertions.assertThrows(
+        MetalakeNotInUseException.class,
+        () -> filesetOps.loadFileset(NameIdentifier.of("dummy", "dummy")));
+    Assertions.assertThrows(
+        MetalakeNotInUseException.class,
+        () ->
+            filesetOps.createFileset(NameIdentifier.of("dummy", "dummy"), null, null, null, null));
+    Assertions.assertThrows(
+        MetalakeNotInUseException.class,
+        () -> filesetOps.dropFileset(NameIdentifier.of("dummy", "dummy")));
+    Assertions.assertThrows(
+        MetalakeNotInUseException.class,
+        () -> filesetOps.getFileLocation(NameIdentifier.of("dummy", "dummy"), "dummy"));
+    Assertions.assertThrows(
+        MetalakeNotInUseException.class,
+        () ->
+            filesetOps.alterFileset(
+                NameIdentifier.of("dummy", "dummy"), FilesetChange.removeComment()));
+
+    Assertions.assertThrows(
+        NonEmptyMetalakeException.class, () -> client.dropMetalake(metalakeName));
+
+    Assertions.assertDoesNotThrow(() -> client.enableMetalake(metalakeName));
+    Assertions.assertTrue(loadedMetalake.dropCatalog(catalogName, true));
+
+    Assertions.assertDoesNotThrow(() -> client.disableMetalake(metalakeName));
+    Assertions.assertTrue(client.dropMetalake(metalakeName));
+    Assertions.assertFalse(client.dropMetalake(metalakeName));
+  }
+
   public void dropMetalakes() {
     GravitinoMetalake[] metaLakes = client.listMetalakes();
     for (GravitinoMetalake metalake : metaLakes) {
       assertDoesNotThrow(() -> client.disableMetalake(metalake.name()));
-      assertTrue(client.dropMetalake(metalake.name()));
+      assertTrue(client.dropMetalake(metalake.name(), true));
     }
 
     // Reload metadata from backend to check if the drop operations are applied
