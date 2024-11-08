45be919f5a9bfc5f857065582f559541bc6849e6
Justin Mclean <justin@datastrato.com>
2024-11-08 14:33:29 +1100

    fix missed merge

--- clients/cli/src/main/java/org/apache/gravitino/cli/commands/DeleteUser.java
+++ clients/cli/src/main/java/org/apache/gravitino/cli/commands/DeleteUser.java
@@ -30,11 +30,7 @@ public class DeleteUser extends Command {
   protected final String user;
 
   /**
-<<<<<<< HEAD
    * Delete a user.
-=======
-   * Deletes a user.
->>>>>>> main
    *
    * @param url The URL of the Gravitino server.
    * @param ignoreVersions If true don't check the client/server versions match.
