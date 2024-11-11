14cc1e6d0ff5b739ff872c2227e5f601ab4141d7
Justin Mclean <justin@datastrato.com>
2024-11-11 14:44:36 +1100

    fix role description

--- clients/cli/src/main/java/org/apache/gravitino/cli/GravitinoOptions.java
+++ clients/cli/src/main/java/org/apache/gravitino/cli/GravitinoOptions.java
@@ -73,7 +73,7 @@ public class GravitinoOptions {
     options.addOption(createArgOption("l", USER, "user name"));
     options.addOption(createArgOption("g", GROUP, "group name"));
     options.addOption(createArgOption("t", TAG, "tag name"));
-    options.addOption(createArgOption("r", ROLE, "tag name"));
+    options.addOption(createArgOption("r", ROLE, "role name"));
 
     return options;
   }
