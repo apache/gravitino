618fa7d9bf44b435f71dac44d44153eb208b013b
Rory <roryqi@apache.org>
2024-11-26 14:53:48 +0800

    [#5671] fix(auth): Auth IT should add Iceberg and Paimon dependency

--- core/src/main/java/org/apache/gravitino/connector/authorization/SupportsConverter.java
+++
@@ -1,9 +0,0 @@
-package org.apache.gravitino.connector.authorization;
-
-import org.apache.gravitino.authorization.SecurableObject;
-
-import java.util.List;
-
-public interface SupportsConverter {
-    List<AuthorizationSecurableObject> convertToAuthorizationObject(ExtendedSecurableObject object);
-}
