145d7bda9d5d8d9a56707e0a2a64b9b1bb7134a1
Jerry Shao <jshao@apache.org>
2024-11-27 16:03:26 +0800

    Add storage schema for model (part-1)

---
+++ core/src/main/java/org/apache/gravitino/storage/relational/mapper/provider/base/ModelMetaBaseSQLProvider.java
@@ -0,0 +1,137 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.gravitino.storage.relational.mapper.provider.base;
+
+import org.apache.gravitino.storage.relational.mapper.ModelMetaMapper;
+import org.apache.gravitino.storage.relational.po.ModelPO;
+import org.apache.ibatis.annotations.Param;
+
+public class ModelMetaBaseSQLProvider {
+
+  public String insertModelMeta(@Param("modelMeta") ModelPO modelPO) {
+    return "INSERT INTO "
+        + ModelMetaMapper.TABLE_NAME
+        + "(model_id, model_name, metalake_id, catalog_id, schema_id,"
+        + " model_comment, model_properties, model_latest_version, audit_info, deleted_at)"
+        + " VALUES (#{modelMeta.modelId}, #{modelMeta.modelName}, #{modelMeta.metalakeId},"
+        + " #{modelMeta.catalogId}, #{modelMeta.schemaId}, #{modelMeta.modelComment},"
+        + " #{modelMeta.modelProperties}, #{modelMeta.modelLatestVersion}, #{modelMeta.auditInfo},"
+        + " #{modelMeta.deletedAt})";
+  }
+
+  public String insertModelMetaOnDuplicateKeyUpdate(@Param("modelMeta") ModelPO modelPO) {
+    return "INSERT INTO "
+        + ModelMetaMapper.TABLE_NAME
+        + "(model_id, model_name, metalake_id, catalog_id, schema_id,"
+        + " model_comment, model_properties, model_latest_version, audit_info, deleted_at)"
+        + " VALUES (#{modelMeta.modelId}, #{modelMeta.modelName}, #{modelMeta.metalakeId},"
+        + " #{modelMeta.catalogId}, #{modelMeta.schemaId}, #{modelMeta.modelComment},"
+        + " #{modelMeta.modelProperties}, #{modelMeta.modelLatestVersion}, #{modelMeta.auditInfo},"
+        + " #{modelMeta.deletedAt})"
+        + " ON DUPLICATE KEY UPDATE"
+        + " model_name = #{modelMeta.modelName},"
+        + " metalake_id = #{modelMeta.metalakeId},"
+        + " catalog_id = #{modelMeta.catalogId},"
+        + " schema_id = #{modelMeta.schemaId},"
+        + " model_comment = #{modelMeta.modelComment},"
+        + " model_properties = #{modelMeta.modelProperties},"
+        + " model_latest_version = #{modelMeta.modelLatestVersion},"
+        + " audit_info = #{modelMeta.auditInfo},"
+        + " deleted_at = #{modelMeta.deletedAt}";
+  }
+
+  public String listModelPOsBySchemaId(@Param("schemaId") Long schemaId) {
+    return "SELECT model_id as modelId, model_name as modelName, metalake_id as metalakeId,"
+        + " catalog_id as catalogId, schema_id as schemaId, model_comment as modelComment,"
+        + " model_properties as modelProperties, model_latest_version as"
+        + " modelLatestVersion, audit_info as auditInfo, deleted_at as deletedAt"
+        + " FROM "
+        + ModelMetaMapper.TABLE_NAME
+        + " WHERE schema_id = #{schemaId} AND deleted_at = 0";
+  }
+
+  public String selectModelMetaBySchemaIdAndModelName(
+      @Param("schemaId") Long schemaId, @Param("modelName") String modelName) {
+    return "SELECT model_id as modelId, model_name as modelName, metalake_id as metalakeId,"
+        + " catalog_id as catalogId, schema_id as schemaId, model_comment as modelComment,"
+        + " model_properties as modelProperties, model_latest_version as"
+        + " modelLatestVersion, audit_info as auditInfo, deleted_at as deletedAt"
+        + " FROM "
+        + ModelMetaMapper.TABLE_NAME
+        + " WHERE schema_id = #{schemaId} AND model_name = #{modelName} AND deleted_at = 0";
+  }
+
+  public String selectModelIdBySchemaIdAndModelName(
+      @Param("schemaId") Long schemaId, @Param("modelName") String modelName) {
+    return "SELECT model_id"
+        + " FROM "
+        + ModelMetaMapper.TABLE_NAME
+        + " WHERE schema_id = #{schemaId} AND model_name = #{modelName} AND deleted_at = 0";
+  }
+
+  public String selectModelMetaByModelId(@Param("modelId") Long modelId) {
+    return "SELECT model_id as modelId, model_name as modelName, metalake_id as metalakeId,"
+        + " catalog_id as catalogId, schema_id as schemaId, model_comment as modelComment,"
+        + " model_properties as modelProperties, model_latest_version as "
+        + " modelLatestVersion, audit_info as auditInfo, deleted_at as deletedAt"
+        + " FROM "
+        + ModelMetaMapper.TABLE_NAME
+        + " WHERE model_id = #{modelId} AND deleted_at = 0";
+  }
+
+  public String softDeleteModelMetaBySchemaIdAndModelName(
+      @Param("schemaId") Long schemaId, @Param("modelName") String modelName) {
+    return "UPDATE "
+        + ModelMetaMapper.TABLE_NAME
+        + " SET deleted_at = (UNIX_TIMESTAMP() * 1000.0)"
+        + " + EXTRACT(MICROSECOND FROM CURRENT_TIMESTAMP(3)) / 1000"
+        + " WHERE schema_id = #{schemaId} AND model_name = #{modelName} AND deleted_at = 0";
+  }
+
+  public String softDeleteModelMetasByCatalogId(@Param("catalogId") Long catalogId) {
+    return "UPDATE "
+        + ModelMetaMapper.TABLE_NAME
+        + " SET deleted_at = (UNIX_TIMESTAMP() * 1000.0)"
+        + " + EXTRACT(MICROSECOND FROM CURRENT_TIMESTAMP(3)) / 1000"
+        + " WHERE catalog_id = #{catalogId} AND deleted_at = 0";
+  }
+
+  public String softDeleteModelMetasByMetalakeId(@Param("metalakeId") Long metalakeId) {
+    return "UPDATE "
+        + ModelMetaMapper.TABLE_NAME
+        + " SET deleted_at = (UNIX_TIMESTAMP() * 1000.0)"
+        + " + EXTRACT(MICROSECOND FROM CURRENT_TIMESTAMP(3)) / 1000"
+        + " WHERE metalake_id = #{metalakeId} AND deleted_at = 0";
+  }
+
+  public String softDeleteModelMetasBySchemaId(@Param("schemaId") Long schemaId) {
+    return "UPDATE "
+        + ModelMetaMapper.TABLE_NAME
+        + " SET deleted_at = (UNIX_TIMESTAMP() * 1000.0)"
+        + " + EXTRACT(MICROSECOND FROM CURRENT_TIMESTAMP(3)) / 1000"
+        + " WHERE schema_id = #{schemaId} AND deleted_at = 0";
+  }
+
+  public String deleteModelMetasByLegacyTimeline(
+      @Param("legacyTimeline") Long legacyTimeline, @Param("limit") int limit) {
+    return "DELETE FROM "
+        + ModelMetaMapper.TABLE_NAME
+        + " WHERE deleted_at > 0 AND deleted_at < #{legacyTimeline} LIMIT #{limit}";
+  }
+}
---
+++ core/src/main/java/org/apache/gravitino/storage/relational/po/ModelVersionPO.java
@@ -0,0 +1,130 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.gravitino.storage.relational.po;
+
+import com.google.common.base.Preconditions;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
+
+@EqualsAndHashCode
+@Getter
+public class ModelVersionPO {
+
+  private Long modelId;
+
+  private Long metalakeId;
+
+  private Long catalogId;
+
+  private Long schemaId;
+
+  private Integer modelVersion;
+
+  private String modelVersionComment;
+
+  private String modelVersionProperties;
+
+  private String modelVersionUri;
+
+  private String auditInfo;
+
+  private Long deletedAt;
+
+  private ModelVersionPO() {}
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  public static class Builder {
+
+    private final ModelVersionPO modelVersionPO;
+
+    private Builder() {
+      modelVersionPO = new ModelVersionPO();
+    }
+
+    public Builder withModelId(Long modelId) {
+      modelVersionPO.modelId = modelId;
+      return this;
+    }
+
+    public Builder withMetalakeId(Long metalakeId) {
+      modelVersionPO.metalakeId = metalakeId;
+      return this;
+    }
+
+    public Builder withCatalogId(Long catalogId) {
+      modelVersionPO.catalogId = catalogId;
+      return this;
+    }
+
+    public Builder withSchemaId(Long schemaId) {
+      modelVersionPO.schemaId = schemaId;
+      return this;
+    }
+
+    public Builder withModelVersion(Integer modelVersion) {
+      modelVersionPO.modelVersion = modelVersion;
+      return this;
+    }
+
+    public Builder withModelVersionComment(String modelVersionComment) {
+      modelVersionPO.modelVersionComment = modelVersionComment;
+      return this;
+    }
+
+    public Builder withModelVersionProperties(String modelVersionProperties) {
+      modelVersionPO.modelVersionProperties = modelVersionProperties;
+      return this;
+    }
+
+    public Builder withModelVersionUri(String modelVersionUri) {
+      modelVersionPO.modelVersionUri = modelVersionUri;
+      return this;
+    }
+
+    public Builder withAuditInfo(String auditInfo) {
+      modelVersionPO.auditInfo = auditInfo;
+      return this;
+    }
+
+    public Builder withDeletedAt(Long deletedAt) {
+      modelVersionPO.deletedAt = deletedAt;
+      return this;
+    }
+
+    public ModelVersionPO build() {
+      Preconditions.checkArgument(modelVersionPO.modelId != null, "Model id is required");
+      Preconditions.checkArgument(modelVersionPO.metalakeId != null, "Metalake id is required");
+      Preconditions.checkArgument(modelVersionPO.catalogId != null, "Catalog id is required");
+      Preconditions.checkArgument(modelVersionPO.schemaId != null, "Schema id is required");
+      Preconditions.checkArgument(modelVersionPO.modelVersion != null, "Model version is required");
+      Preconditions.checkArgument(
+          StringUtils.isNotBlank(modelVersionPO.modelVersionUri),
+          "Model version uri cannot be empty");
+      Preconditions.checkArgument(
+          StringUtils.isNotBlank(modelVersionPO.auditInfo), "Audit info cannot be empty");
+      Preconditions.checkArgument(modelVersionPO.deletedAt != null, "Deleted at is required");
+
+      return modelVersionPO;
+    }
+  }
+}
