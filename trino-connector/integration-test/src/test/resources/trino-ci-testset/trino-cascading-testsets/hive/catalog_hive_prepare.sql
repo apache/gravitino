a2154a4381295c60b82e133beb43e897a35eaaa8
danhuawang <danhua@datastrato.com>
2024-11-28 18:17:42 +0800

    remove blank line

--- trino-connector/integration-test/src/test/resources/trino-ci-testset/trino-cascading-testsets/hive/catalog_hive_prepare.sql
+++ trino-connector/integration-test/src/test/resources/trino-ci-testset/trino-cascading-testsets/hive/catalog_hive_prepare.sql
@@ -21,19 +21,3 @@ CREATE TABLE gt_hive1_1.gt_datatype.tb03 (name char);
 INSERT INTO gt_hive1_1.gt_datatype.tb03 VALUES ('a');
 
 <RETRY WITH NOT EXISTS> SELECT * FROM gt_hive1.gt_datatype.tb03;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
