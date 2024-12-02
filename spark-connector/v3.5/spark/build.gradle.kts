a83a4066d38c9fb0bad4da54a240489b0cc3f40f
caican <caican@xiaomi.com>
2024-12-02 20:49:27 +0800

    fix

--- spark-connector/v3.5/spark/build.gradle.kts
+++ spark-connector/v3.5/spark/build.gradle.kts
@@ -157,6 +157,7 @@ tasks.test {
     dependsOn(":catalogs:catalog-lakehouse-iceberg:jar")
     dependsOn(":catalogs:catalog-hive:jar")
     dependsOn(":iceberg:iceberg-rest-server:jar")
+    dependsOn(":catalogs:catalog-lakehouse-paimon:jar")
   }
 }
 
