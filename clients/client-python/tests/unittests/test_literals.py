8a28e7d7364722a4184c992396a2be5af53156fd
Xun <liuxun@apache.org>
2024-12-03 15:13:27 +0800

    fix CI

--- clients/client-python/tests/unittests/test_literals.py
+++ clients/client-python/tests/unittests/test_literals.py
@@ -14,7 +14,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-import decimal
 import unittest
 from datetime import date, time, datetime
 from decimal import Decimal
