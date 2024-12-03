72eb9ad0dadbb59e4e00a50c6d3b94574169ddff
fanng <xiaojing@datastrato.com>
2024-12-03 19:25:51 +0800

    split interface

--- api/src/main/java/org/apache/gravitino/credential/SupportsCredentials.java
+++ api/src/main/java/org/apache/gravitino/credential/SupportsCredentials.java
@@ -19,29 +19,59 @@
 
 package org.apache.gravitino.credential;
 
-import java.util.Optional;
+import com.google.common.base.Preconditions;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.gravitino.exceptions.NoSuchCredentialException;
 
 /** Interface to get credentials. */
 public interface SupportsCredentials {
+
   /**
-   * Retrieves an array of {@link Credential} objects based on the specified credential type, like
-   * s3-token, s3-secret-key which defined in the specific credentials.
+   * Retrieves an array of {@link Credential} objects.
    *
-   * @param credentialType The server will return credentials according to the configuration if
-   *     credential type is empty, or else will return the credential with the specified credential
-   *     type.
-   * @return An array of {@link Credential} objects. If the {@code credentialType} is not empty,
-   *     there will be at most one credential in the array. If empty, in most cases the array only
-   *     contains one credential. If the object like {@link org.apache.gravitino.file.Fileset}
-   *     contains multi locations for different storages like HDFS, S3, the array will contain multi
-   *     credentials. The array could be empty if you request a credential for a catalog but the
-   *     credential provider couldn't generate the credential for the catalog, like S3 token
-   *     credential provider only generate credential for the specific object like {@link
+   * @return An array of {@link Credential} objects. In most cases the array only contains one
+   *     credential. If the object like {@link org.apache.gravitino.file.Fileset} contains multi
+   *     locations for different storages like HDFS, S3, the array will contain multi credentials.
+   *     The array could be empty if you request a credential for a catalog but the credential
+   *     provider couldn't generate the credential for the catalog, like S3 token credential
+   *     provider only generate credential for the specific object like {@link
    *     org.apache.gravitino.file.Fileset}, {@link org.apache.gravitino.rel.Table}. There will be
    *     at most one credential for one credential type.
-   * @throws NoSuchCredentialException If the specified credential type cannot be found in the
-   *     server side.
    */
-  Credential[] getCredentials(Optional<String> credentialType) throws NoSuchCredentialException;
+  Credential[] getCredentials() throws NoSuchCredentialException;
+
+  /**
+   * Retrieves an {@link Credential} object based on the specified credential type,
+   *
+   * @param credentialType The type of the credential like s3-token, s3-secret-key which defined in
+   *     the specific credentials.
+   * @return An {@link Credential} object with the specific credential type.
+   * @throws NoSuchCredentialException If the specific credential cannot be found.
+   */
+  default Credential getCredential(String credentialType) throws NoSuchCredentialException {
+    Preconditions.checkArgument(
+        StringUtils.isNotBlank(credentialType), "Credential type should not be empty");
+    Credential[] credentials = getCredentials();
+    if (credentials.length == 0) {
+      throw new NoSuchCredentialException(
+          "No credential found for the credential type: " + credentialType);
+    }
+
+    List<Credential> filteredCredentials =
+        Arrays.stream(credentials)
+            .filter(credential -> credentialType.equals(credential.credentialType()))
+            .collect(Collectors.toList());
+    if (filteredCredentials.isEmpty()) {
+      throw new NoSuchCredentialException(
+          "No credential found for the credential type: " + credentialType);
+    } else if (credentials.length > 1) {
+      throw new IllegalStateException(
+          "Multi credentials found for the credential type:" + credentialType);
+    }
+
+    return filteredCredentials.get(0);
+  }
 }
