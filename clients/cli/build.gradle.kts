2c44ff72a467cfeec851ee9660b135b214bd385f
yuqi <yuqi@datastrato.com>
2024-12-03 18:06:52 +0800

    fix again.

--- clients/cli/build.gradle.kts
+++ clients/cli/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
   testImplementation(libs.junit.jupiter.api)
   testImplementation(libs.junit.jupiter.params)
   testImplementation(libs.mockito.core)
+  testImplementation(libs.postgresql.driver)
   testImplementation(libs.testcontainers)
 
   testImplementation(project(":core")) {
