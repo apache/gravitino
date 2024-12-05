10fc5cda8e69a2beec8a24bde3a2ba4eb9367f3c
SophieTech88 <yun.sun7119@gmail.com>
2024-12-04 18:14:47 -0800

    [#5201] feat(client-python): Implement expressions in python client (#5646)
    ### What changes were proposed in this pull request?
    Implement expression from java, including:
    - Expression.java
    - FunctionExpression.java
    - NamedReference.java
    - UnparsedExpression.java
    - literals/
    
    convert to python client, and add unit test for each class.
    
    ### Why are the changes needed?
    
    We need to support the expressions in python client
    
    Fix: #5201
    
    ### Does this PR introduce _any_ user-facing change?
    No
    
    ### How was this patch tested?
    Need to pass all unit tests.
    
    ---------
    
    Co-authored-by: Xun <liuxun@apache.org>

---
+++ clients/client-python/tests/unittests/test_unparsed_expression.py
@@ -0,0 +1,34 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import unittest
+from gravitino.api.expressions.unparsed_expression import UnparsedExpressionImpl
+
+
+class TestUnparsedExpression(unittest.TestCase):
+    def test_unparsed_expression_creation(self):
+        expr = UnparsedExpressionImpl("some_expression")
+        self.assertEqual(expr.unparsed_expression(), "some_expression")
+        self.assertEqual(
+            str(expr), "UnparsedExpressionImpl{unparsedExpression='some_expression'}"
+        )
+
+    def test_unparsed_expression_equality(self):
+        expr1 = UnparsedExpressionImpl("some_expression")
+        expr2 = UnparsedExpressionImpl("some_expression")
+        self.assertEqual(expr1, expr2)
+        self.assertEqual(hash(expr1), hash(expr2))
