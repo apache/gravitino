215d2f15be2e76c5307436348b47ebb2573d7b79
fanng <xiaojing@datastrato.com>
2024-12-06 17:49:21 +0800

    Initialize

--- clients/client-python/gravitino/utils/credential_utils.py
+++ clients/client-python/gravitino/utils/credential_utils.py
@@ -16,6 +16,9 @@
 # under the License.
 
 from gravitino.api.credential.credential import Credential
+from gravitino.api.credential.gcs_token_credential import GCSTokenCredential
+from gravitino.api.credential.oss_token_credential import OSSTokenCredential
+from gravitino.api.credential.s3_secret_key_credential import S3SecretKeyCredential
 from gravitino.api.credential.s3_token_credential import S3TokenCredential
 
 class CredentialUtils:
@@ -23,4 +26,10 @@ class CredentialUtils:
     def to_credential(credential_type: str, credential_info: dict, expire_time_in_ms: int) -> Credential:
         if credential_type == S3TokenCredential.S3_TOKEN_CREDENTIAL_TYPE:
             return S3TokenCredential(credential_info, expire_time_in_ms)
+        if credential_type == S3SecretKeyCredential.S3_SECRET_KEY_CREDENTIAL_TYPE:
+            return S3SecretKeyCredential(credential_info, expire_time_in_ms)
+        if credential_type == GCSTokenCredential.GCS_TOKEN_CREDENTIAL_TYPE:
+            return GCSTokenCredential(credential_info, expire_time_in_ms)
+        if credential_type == OSSTokenCredential.OSS_TOKEN_CREDENTIAL_TYPE:
+            return OSSTokenCredential(credential_info, expire_time_in_ms)
         raise NotImplementedError(f"Credential type {credential_type} is not supported")
