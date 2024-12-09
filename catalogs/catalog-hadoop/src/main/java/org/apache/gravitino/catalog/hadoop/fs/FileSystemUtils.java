649bb25cb9ae65f903eeed59e12b93073e3f1590
yuqi <yuqi@datastrato.com>
2024-12-09 11:30:05 +0800

    Add a comma.

--- catalogs/catalog-hadoop/src/main/java/org/apache/gravitino/catalog/hadoop/fs/FileSystemUtils.java
+++ catalogs/catalog-hadoop/src/main/java/org/apache/gravitino/catalog/hadoop/fs/FileSystemUtils.java
@@ -63,7 +63,7 @@ public class FileSystemUtils {
               if (resultMap.containsKey(fileSystemProvider.scheme())) {
                 throw new UnsupportedOperationException(
                     String.format(
-                        "File system provider: '%s' with scheme '%s' already exists in the provider list "
+                        "File system provider: '%s' with scheme '%s' already exists in the provider list,"
                             + "please make sure the file system provider scheme is unique.",
                         fileSystemProvider.getClass().getName(), fileSystemProvider.scheme()));
               }
