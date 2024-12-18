a4190e1f12932718e30cb37ac3df6ec69b41e989
Jerry Shao <jerryshao@datastrato.com>
2024-12-18 18:23:02 +0800

    [#5739] feat(model-catalog): Implement the model catalog logic (#5848)
    ### What changes were proposed in this pull request?
    
    This PR adds the model catalog implementation.
    
    ### Why are the changes needed?
    
    This is a part of work to support model management in Gravitino.
    
    Fix: #5739 
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Added UTs to cover the changes.

--- api/src/main/java/org/apache/gravitino/model/ModelCatalog.java
+++ api/src/main/java/org/apache/gravitino/model/ModelCatalog.java
@@ -79,12 +79,11 @@ public interface ModelCatalog {
    * @param properties The properties of the model. The properties are optional and can be null or
    *     empty.
    * @return The registered model object.
+   * @throws NoSuchSchemaException If the schema does not exist.
    * @throws ModelAlreadyExistsException If the model already registered.
    */
-  default Model registerModel(NameIdentifier ident, String comment, Map<String, String> properties)
-      throws ModelAlreadyExistsException {
-    return registerModel(ident, null, new String[0], comment, properties);
-  }
+  Model registerModel(NameIdentifier ident, String comment, Map<String, String> properties)
+      throws NoSuchSchemaException, ModelAlreadyExistsException;
 
   /**
    * Register a model in the catalog if the model is not existed, otherwise the {@link
@@ -99,16 +98,22 @@ public interface ModelCatalog {
    * @param properties The properties of the model. The properties are optional and can be null or
    *     empty.
    * @return The registered model object.
+   * @throws NoSuchSchemaException If the schema does not exist when register a model.
    * @throws ModelAlreadyExistsException If the model already registered.
    * @throws ModelVersionAliasesAlreadyExistException If the aliases already exist in the model.
    */
-  Model registerModel(
+  default Model registerModel(
       NameIdentifier ident,
       String uri,
       String[] aliases,
       String comment,
       Map<String, String> properties)
-      throws ModelAlreadyExistsException, ModelVersionAliasesAlreadyExistException;
+      throws NoSuchSchemaException, ModelAlreadyExistsException,
+          ModelVersionAliasesAlreadyExistException {
+    Model model = registerModel(ident, comment, properties);
+    linkModelVersion(ident, uri, aliases, comment, properties);
+    return model;
+  }
 
   /**
    * Delete the model from the catalog. If the model does not exist, return false. Otherwise, return
@@ -197,11 +202,10 @@ public interface ModelCatalog {
    * @param comment The comment of the model version. The comment is optional and can be null.
    * @param properties The properties of the model version. The properties are optional and can be
    *     null or empty.
-   * @return The model version object.
    * @throws NoSuchModelException If the model does not exist.
    * @throws ModelVersionAliasesAlreadyExistException If the aliases already exist in the model.
    */
-  ModelVersion linkModelVersion(
+  void linkModelVersion(
       NameIdentifier ident,
       String uri,
       String[] aliases,
