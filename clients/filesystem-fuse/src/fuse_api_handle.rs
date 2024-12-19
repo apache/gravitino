b7e9b078d99c2a0079fc3d1492f9ac64db04ab63
Eric Chang <e850506@gmail.com>
2024-12-20 06:51:58 +0800

    revert unnecesary change

--- clients/filesystem-fuse/src/fuse_api_handle.rs
+++ clients/filesystem-fuse/src/fuse_api_handle.rs
@@ -555,8 +555,7 @@ impl<T: RawFileSystem> Filesystem for FuseApiHandle<T> {
     }
 }
 
-fn fstat_to_file_attr(file_st: &FileStat, context: &FileSystemContext) -> FileAttr {
-    debug!("file_st: {:?}, context: {:?}", file_st, context);
+const fn fstat_to_file_attr(file_st: &FileStat, context: &FileSystemContext) -> FileAttr {
     debug_assert!(file_st.file_id != 0 && file_st.parent_file_id != 0);
     let perm = match file_st.kind {
         Directory => context.default_dir_perm,
