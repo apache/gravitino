2bd54653b022630e694a7969b7935261d2475afb
jun <oren.ccl@gmail.com>
2024-12-22 19:29:02 +0800

    fix pylint

--- clients/client-python/gravitino/utils/credential_factory.py
+++ clients/client-python/gravitino/utils/credential_factory.py
@@ -17,16 +17,16 @@
 
 from typing import Dict
 
-from gravitino.api.credential.adls_token_credential import ADLSTokenCredential
-from gravitino.api.credential.azure_account_key_credential import (
-    AzureAccountKeyCredential,
-)
 from gravitino.api.credential.credential import Credential
 from gravitino.api.credential.gcs_token_credential import GCSTokenCredential
 from gravitino.api.credential.oss_token_credential import OSSTokenCredential
 from gravitino.api.credential.s3_secret_key_credential import S3SecretKeyCredential
 from gravitino.api.credential.s3_token_credential import S3TokenCredential
 from gravitino.api.credential.oss_secret_key_credential import OSSSecretKeyCredential
+from gravitino.api.credential.adls_token_credential import ADLSTokenCredential
+from gravitino.api.credential.azure_account_key_credential import (
+    AzureAccountKeyCredential,
+)
 
 
 class CredentialFactory:
@@ -34,21 +34,28 @@ class CredentialFactory:
     def create(
         credential_type: str, credential_info: Dict[str, str], expire_time_in_ms: int
     ) -> Credential:
+        credential = None
+
         if credential_type == S3TokenCredential.S3_TOKEN_CREDENTIAL_TYPE:
-            return S3TokenCredential(credential_info, expire_time_in_ms)
-        if credential_type == S3SecretKeyCredential.S3_SECRET_KEY_CREDENTIAL_TYPE:
-            return S3SecretKeyCredential(credential_info, expire_time_in_ms)
-        if credential_type == GCSTokenCredential.GCS_TOKEN_CREDENTIAL_TYPE:
-            return GCSTokenCredential(credential_info, expire_time_in_ms)
-        if credential_type == OSSTokenCredential.OSS_TOKEN_CREDENTIAL_TYPE:
-            return OSSTokenCredential(credential_info, expire_time_in_ms)
-        if credential_type == OSSSecretKeyCredential.OSS_SECRET_KEY_CREDENTIAL_TYPE:
-            return OSSSecretKeyCredential(credential_info, expire_time_in_ms)
-        if credential_type == ADLSTokenCredential.ADLS_SAS_TOKEN_CREDENTIAL_TYPE:
-            return ADLSTokenCredential(credential_info, expire_time_in_ms)
-        if (
+            credential = S3TokenCredential(credential_info, expire_time_in_ms)
+        elif credential_type == S3SecretKeyCredential.S3_SECRET_KEY_CREDENTIAL_TYPE:
+            credential = S3SecretKeyCredential(credential_info, expire_time_in_ms)
+        elif credential_type == GCSTokenCredential.GCS_TOKEN_CREDENTIAL_TYPE:
+            credential = GCSTokenCredential(credential_info, expire_time_in_ms)
+        elif credential_type == OSSTokenCredential.OSS_TOKEN_CREDENTIAL_TYPE:
+            credential = OSSTokenCredential(credential_info, expire_time_in_ms)
+        elif credential_type == OSSSecretKeyCredential.OSS_SECRET_KEY_CREDENTIAL_TYPE:
+            credential = OSSSecretKeyCredential(credential_info, expire_time_in_ms)
+        elif credential_type == ADLSTokenCredential.ADLS_SAS_TOKEN_CREDENTIAL_TYPE:
+            credential = ADLSTokenCredential(credential_info, expire_time_in_ms)
+        elif (
             credential_type
             == AzureAccountKeyCredential.AZURE_ACCOUNT_KEY_CREDENTIAL_TYPE
         ):
-            return AzureAccountKeyCredential(credential_info, expire_time_in_ms)
-        raise NotImplementedError(f"Credential type {credential_type} is not supported")
+            credential = AzureAccountKeyCredential(credential_info, expire_time_in_ms)
+        else:
+            raise NotImplementedError(
+                f"Credential type {credential_type} is not supported"
+            )
+
+        return credential
--- clients/client-python/tests/unittests/test_credential_factory.py
+++ clients/client-python/tests/unittests/test_credential_factory.py
@@ -19,16 +19,16 @@
 
 import unittest
 
-from gravitino.api.credential.adls_token_credential import ADLSTokenCredential
-from gravitino.api.credential.azure_account_key_credential import (
-    AzureAccountKeyCredential,
-)
 from gravitino.api.credential.gcs_token_credential import GCSTokenCredential
 from gravitino.api.credential.oss_token_credential import OSSTokenCredential
 from gravitino.api.credential.s3_secret_key_credential import S3SecretKeyCredential
 from gravitino.api.credential.s3_token_credential import S3TokenCredential
 from gravitino.utils.credential_factory import CredentialFactory
 from gravitino.api.credential.oss_secret_key_credential import OSSSecretKeyCredential
+from gravitino.api.credential.adls_token_credential import ADLSTokenCredential
+from gravitino.api.credential.azure_account_key_credential import (
+    AzureAccountKeyCredential,
+)
 
 
 class TestCredentialFactory(unittest.TestCase):
