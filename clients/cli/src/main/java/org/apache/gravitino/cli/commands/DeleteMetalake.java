b81e2860f028a8bbee2c850cbf1ab2269e22160f
Lord of Abyss <103809695+Abyss-lord@users.noreply.github.com>
2024-12-22 19:23:26 +0800

    Update clients/cli/src/main/java/org/apache/gravitino/cli/commands/DeleteMetalake.java
    Co-authored-by: Qiming Teng <tengqm@outlook.com>

--- clients/cli/src/main/java/org/apache/gravitino/cli/commands/DeleteMetalake.java
+++ clients/cli/src/main/java/org/apache/gravitino/cli/commands/DeleteMetalake.java
@@ -58,7 +58,7 @@ public class DeleteMetalake extends Command {
     } catch (NoSuchMetalakeException err) {
       exitWithError(ErrorMessages.UNKNOWN_METALAKE);
     } catch (MetalakeInUseException inUseException) {
-      System.err.println(metalake + " in use, please use disable command disable it first.");
+      System.err.println(metalake + " in use, please disable it first.");
     } catch (Exception exp) {
       exitWithError(exp.getMessage());
     }
