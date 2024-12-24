69f3224916492476d5327d7f9f2046b889163f27
Eric Chang <e850506@gmail.com>
2024-12-24 10:48:10 +0800

    feat(client-python): porting partitions from java client

---
+++ clients/client-python/gravitino/api/expressions/partitions/identity_partition.py
@@ -0,0 +1,56 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from abc import abstractmethod
+from typing import List, Any
+from .partition import Partition
+
+class IdentityPartition(Partition):
+    """
+    An identity partition represents a result of identity partitioning. For example, for Hive
+    partition
+
+    ```
+    PARTITION (dt='2008-08-08',country='us')
+    ```
+
+    its partition name is "dt=2008-08-08/country=us", field names are [["dt"], ["country"]] and
+    values are ["2008-08-08", "us"].
+
+    This class is evolving and may change in future versions.
+    """
+
+    @abstractmethod
+    def field_names(self) -> List[List[str]]:
+        """
+        Returns the field names of the identity partition.
+
+        Returns:
+            List[List[str]]: A list of lists representing the field names of the identity partition.
+        """
+        pass
+
+    @abstractmethod
+    def values(self) -> List[Any]:
+        """
+        Returns the values of the identity partition. The values are in the same order as the field
+        names.
+
+        Returns:
+            List[Any]: A list representing the values of the identity partition.
+        """
+        pass
