824e3a9b9a5dbeb1c35337afebe3538ee6bdee1d
Rory <roryqi@apache.org>
2024-12-26 15:30:04 +0800

    fix error

--- server/src/test/java/org/apache/gravitino/server/web/TestConfigServlet.java
+++ server/src/test/java/org/apache/gravitino/server/web/TestConfigServlet.java
@@ -23,6 +23,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.gravitino.server.ServerConfig;
 import org.junit.jupiter.api.Test;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 public class TestConfigServlet {
 
   @Test
