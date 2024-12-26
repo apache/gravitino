06af45ccdfbf1cfe337b42a36eea7f3765262d06
FANNG <xiaojing@datastrato.com>
2024-12-26 12:19:27 +0800

    [#5620] feat(fileset): Support credential vending for fileset catalog (#5682)
    ### What changes were proposed in this pull request?
    
    Support credential vending for fileset catalog
    
    1. add `credential-providers` properties for the fileset catalog,
    schema, and fileset.
    2. try to get `credential-providers` from the order of fileset, schema,
    and catalog.
    3. The user could set multi-credential providers
    
    
    ### Why are the changes needed?
    
    Fix: #5620 
    
    ### Does this PR introduce _any_ user-facing change?
    
    will add document after this PR is merged
    
    ### How was this patch tested?
    Add IT and test with local setup Gravitino server

--- core/src/main/java/org/apache/gravitino/credential/CredentialUtils.java
+++ core/src/main/java/org/apache/gravitino/credential/CredentialUtils.java
@@ -19,14 +19,75 @@
 
 package org.apache.gravitino.credential;
 
+import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableSet;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import org.apache.gravitino.utils.PrincipalUtils;
 
 public class CredentialUtils {
+
+  private static final Splitter splitter = Splitter.on(",");
+
   public static Credential vendCredential(CredentialProvider credentialProvider, String[] path) {
     PathBasedCredentialContext pathBasedCredentialContext =
         new PathBasedCredentialContext(
-            PrincipalUtils.getCurrentUserName(), ImmutableSet.copyOf(path), ImmutableSet.of());
+            PrincipalUtils.getCurrentUserName(), ImmutableSet.copyOf(path), Collections.emptySet());
     return credentialProvider.getCredential(pathBasedCredentialContext);
+  }
+
+  public static Map<String, CredentialProvider> loadCredentialProviders(
+      Map<String, String> catalogProperties) {
+    Set<String> credentialProviders =
+        CredentialUtils.getCredentialProvidersByOrder(() -> catalogProperties);
+
+    return credentialProviders.stream()
+        .collect(
+            Collectors.toMap(
+                String::toString,
+                credentialType ->
+                    CredentialProviderFactory.create(credentialType, catalogProperties)));
+  }
+
+  /**
+   * Get Credential providers from properties supplier.
+   *
+   * <p>If there are multiple properties suppliers, will try to get the credential providers in the
+   * input order.
+   *
+   * @param propertiesSuppliers The properties suppliers.
+   * @return A set of credential providers.
+   */
+  public static Set<String> getCredentialProvidersByOrder(
+      Supplier<Map<String, String>>... propertiesSuppliers) {
+
+    for (Supplier<Map<String, String>> supplier : propertiesSuppliers) {
+      Map<String, String> properties = supplier.get();
+      Set<String> providers = getCredentialProvidersFromProperties(properties);
+      if (!providers.isEmpty()) {
+        return providers;
+      }
+    }
+
+    return Collections.emptySet();
+  }
+
+  private static Set<String> getCredentialProvidersFromProperties(Map<String, String> properties) {
+    if (properties == null) {
+      return Collections.emptySet();
+    }
+
+    String providers = properties.get(CredentialConstants.CREDENTIAL_PROVIDERS);
+    if (providers == null) {
+      return Collections.emptySet();
+    }
+    return splitter
+        .trimResults()
+        .omitEmptyStrings()
+        .splitToStream(providers)
+        .collect(Collectors.toSet());
   }
 }
