0c516e26abccfcf418787f0ba4ec00a2b432c757
Jerry Shao <jerryshao@datastrato.com>
2024-12-31 14:15:57 +0800

    [#5872] feat(client): Add model management Java client API (#6003)
    ### What changes were proposed in this pull request?
    
    This PR adds the Java client API for model management.
    
    ### Why are the changes needed?
    
    This is a part of work of model management.
    
    Fix: #5872 
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    UT added.

--- clients/client-java/src/main/java/org/apache/gravitino/client/ErrorHandlers.java
+++ clients/client-java/src/main/java/org/apache/gravitino/client/ErrorHandlers.java
@@ -41,12 +41,16 @@ import org.apache.gravitino.exceptions.InUseException;
 import org.apache.gravitino.exceptions.MetalakeAlreadyExistsException;
 import org.apache.gravitino.exceptions.MetalakeInUseException;
 import org.apache.gravitino.exceptions.MetalakeNotInUseException;
+import org.apache.gravitino.exceptions.ModelAlreadyExistsException;
+import org.apache.gravitino.exceptions.ModelVersionAliasesAlreadyExistException;
 import org.apache.gravitino.exceptions.NoSuchCatalogException;
 import org.apache.gravitino.exceptions.NoSuchCredentialException;
 import org.apache.gravitino.exceptions.NoSuchFilesetException;
 import org.apache.gravitino.exceptions.NoSuchGroupException;
 import org.apache.gravitino.exceptions.NoSuchMetadataObjectException;
 import org.apache.gravitino.exceptions.NoSuchMetalakeException;
+import org.apache.gravitino.exceptions.NoSuchModelException;
+import org.apache.gravitino.exceptions.NoSuchModelVersionException;
 import org.apache.gravitino.exceptions.NoSuchPartitionException;
 import org.apache.gravitino.exceptions.NoSuchRoleException;
 import org.apache.gravitino.exceptions.NoSuchSchemaException;
@@ -218,6 +222,15 @@ public class ErrorHandlers {
    */
   public static Consumer<ErrorResponse> ownerErrorHandler() {
     return OwnerErrorHandler.INSTANCE;
+  }
+
+  /**
+   * Creates an error handler specific to Model operations.
+   *
+   * @return A Consumer representing the Model error handler.
+   */
+  public static Consumer<ErrorResponse> modelErrorHandler() {
+    return ModelErrorHandler.INSTANCE;
   }
 
   private ErrorHandlers() {}
@@ -980,6 +993,69 @@ public class ErrorHandlers {
 
         case ErrorConstants.INTERNAL_ERROR_CODE:
           throw new RuntimeException(errorMessage);
+
+        default:
+          super.accept(errorResponse);
+      }
+    }
+  }
+
+  /** Error handler specific to Model operations. */
+  @SuppressWarnings("FormatStringAnnotation")
+  private static class ModelErrorHandler extends RestErrorHandler {
+
+    private static final ModelErrorHandler INSTANCE = new ModelErrorHandler();
+
+    @Override
+    public void accept(ErrorResponse errorResponse) {
+      String errorMsg = formatErrorMessage(errorResponse);
+
+      switch (errorResponse.getCode()) {
+        case ErrorConstants.ILLEGAL_ARGUMENTS_CODE:
+          throw new IllegalArgumentException(errorMsg);
+
+        case ErrorConstants.NOT_FOUND_CODE:
+          if (errorResponse.getType().equals(NoSuchSchemaException.class.getSimpleName())) {
+            throw new NoSuchSchemaException(errorMsg);
+          } else if (errorResponse.getType().equals(NoSuchModelException.class.getSimpleName())) {
+            throw new NoSuchModelException(errorMsg);
+          } else if (errorResponse
+              .getType()
+              .equals(NoSuchModelVersionException.class.getSimpleName())) {
+            throw new NoSuchModelVersionException(errorMsg);
+          } else {
+            throw new NotFoundException(errorMsg);
+          }
+
+        case ErrorConstants.ALREADY_EXISTS_CODE:
+          if (errorResponse.getType().equals(ModelAlreadyExistsException.class.getSimpleName())) {
+            throw new ModelAlreadyExistsException(errorMsg);
+          } else if (errorResponse
+              .getType()
+              .equals(ModelVersionAliasesAlreadyExistException.class.getSimpleName())) {
+            throw new ModelVersionAliasesAlreadyExistException(errorMsg);
+          } else {
+            throw new AlreadyExistsException(errorMsg);
+          }
+
+        case ErrorConstants.FORBIDDEN_CODE:
+          throw new ForbiddenException(errorMsg);
+
+        case ErrorConstants.INTERNAL_ERROR_CODE:
+          throw new RuntimeException(errorMsg);
+
+        case ErrorConstants.NOT_IN_USE_CODE:
+          if (errorResponse.getType().equals(CatalogNotInUseException.class.getSimpleName())) {
+            throw new CatalogNotInUseException(errorMsg);
+
+          } else if (errorResponse
+              .getType()
+              .equals(MetalakeNotInUseException.class.getSimpleName())) {
+            throw new MetalakeNotInUseException(errorMsg);
+
+          } else {
+            throw new NotInUseException(errorMsg);
+          }
 
         default:
           super.accept(errorResponse);
