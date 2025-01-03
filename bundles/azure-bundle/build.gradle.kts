995a1d535f4d3510f4f4a638a9b75aa4237f6aec
Jerry Shao <jerryshao@datastrato.com>
2025-01-03 15:15:53 +0800

    Update build.gradle.kts

--- bundles/azure-bundle/build.gradle.kts
+++ bundles/azure-bundle/build.gradle.kts
@@ -42,7 +42,6 @@ tasks.withType(ShadowJar::class.java) {
   relocate("com.fasterxml", "org.apache.gravitino.azure.shaded.com.fasterxml")
   relocate("com.google.common", "org.apache.gravitino.azure.shaded.com.google.common")
   relocate("org.eclipse.jetty", "org.apache.gravitino.azure.shaded.org.eclipse.jetty")
-
   mergeServiceFiles()
 }
 
