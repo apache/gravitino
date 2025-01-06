16261b1257b5a5c2b08d7d658bf8023562c6e31a
pancx <p_lord@163.com>
2025-01-06 09:39:17 +0800

    [#6087] fix(CLI): Refactor the validation logic of catalog
    Add `validate` method to Command, and refactor the validation code.

--- clients/cli/src/main/java/org/apache/gravitino/cli/commands/SetCatalogProperty.java
+++ clients/cli/src/main/java/org/apache/gravitino/cli/commands/SetCatalogProperty.java
@@ -74,4 +74,12 @@ public class SetCatalogProperty extends Command {
 
     System.out.println(catalog + " property set.");
   }
+
+  @Override
+  public Command validate() {
+    if (property == null && value == null) exitWithError("Missing --property and --value options.");
+    if (property == null) exitWithError(ErrorMessages.MISSING_PROPERTY);
+    if (value == null) exitWithError(ErrorMessages.MISSING_VALUE);
+    return this;
+  }
 }
