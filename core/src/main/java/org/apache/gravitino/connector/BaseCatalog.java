9e6f68d108cd4f59627f83d273637199182f508c
roryqi <roryqi@apache.org>
2025-01-15 19:21:09 +0800

    [#6245] fix(authz): Authorization should use classloader to create the plugin (#6246)
    Authorization should use classloader to create the plugin
    
    Fix: #6245
    
    No.
    
    I tested it manually.

--- core/src/main/java/org/apache/gravitino/connector/BaseCatalog.java
+++ core/src/main/java/org/apache/gravitino/connector/BaseCatalog.java
@@ -205,8 +205,12 @@ public abstract class BaseCatalog<T extends BaseCatalog>
             LOG.info("Authorization provider is not set!");
             return;
           }
-          try (BaseAuthorization<?> authorization =
-              BaseAuthorization.createAuthorization(classLoader, authorizationProvider)) {
+          try {
+            BaseAuthorization<?> authorization =
+                BaseAuthorization.createAuthorization(classLoader, authorizationProvider);
+
+            // Load the authorization plugin with the class loader of the catalog.
+            // Because the JDBC authorization plugin may load JDBC driver using the class loader.
             authorizationPlugin =
                 classLoader.withClassLoader(
                     cl ->
