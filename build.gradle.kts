f359517d58f82138556ffa5ded9e3aea8b662591
Rory <roryqi@apache.org>
2025-02-21 11:12:19 +0800

    Add comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -181,6 +181,9 @@ allprojects {
       param.environment("GRAVITINO_CI_RANGER_DOCKER_IMAGE", "apache/gravitino-ci:ranger-0.1.1")
       param.environment("GRAVITINO_CI_KAFKA_DOCKER_IMAGE", "apache/kafka:3.7.0")
       param.environment("GRAVITINO_CI_LOCALSTACK_DOCKER_IMAGE", "localstack/localstack:latest")
+
+      // Disable Ryuk for integration tests
+      // Ryuk need privileged mode, if we want to rootless or run non-privileged mode, we need to disable it.
       param.environment("TESTCONTAINERS_RYUK_DISABLED", "true")
 
       val dockerRunning = project.rootProject.extra["dockerRunning"] as? Boolean ?: false
