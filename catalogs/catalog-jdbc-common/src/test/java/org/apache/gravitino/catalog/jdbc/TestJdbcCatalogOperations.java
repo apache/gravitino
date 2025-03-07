1aa9feb5ec01294982bfc57ff2c280042f495716
sunxiaojian <sunxiaojian926@163.com>
2025-03-07 20:39:16 +0800

    fixed jdbc config

--- catalogs/catalog-jdbc-common/src/test/java/org/apache/gravitino/catalog/jdbc/TestJdbcCatalogOperations.java
+++ catalogs/catalog-jdbc-common/src/test/java/org/apache/gravitino/catalog/jdbc/TestJdbcCatalogOperations.java
@@ -19,13 +19,20 @@
 package org.apache.gravitino.catalog.jdbc;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import java.sql.SQLException;
+import java.util.HashMap;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.gravitino.Catalog;
 import org.apache.gravitino.NameIdentifier;
+import org.apache.gravitino.catalog.jdbc.config.JdbcConfig;
 import org.apache.gravitino.catalog.jdbc.converter.SqliteColumnDefaultValueConverter;
 import org.apache.gravitino.catalog.jdbc.converter.SqliteExceptionConverter;
 import org.apache.gravitino.catalog.jdbc.converter.SqliteTypeConverter;
 import org.apache.gravitino.catalog.jdbc.operation.SqliteDatabaseOperations;
 import org.apache.gravitino.catalog.jdbc.operation.SqliteTableOperations;
+import org.apache.gravitino.catalog.jdbc.utils.DataSourceUtils;
 import org.apache.gravitino.exceptions.GravitinoRuntimeException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -50,5 +57,21 @@ public class TestJdbcCatalogOperations {
                 "sqlite",
                 "comment",
                 ImmutableMap.of()));
+  }
+
+  @Test
+  public void testConfigTestOnBorrow() throws SQLException {
+    HashMap<String, String> properties = Maps.newHashMap();
+    properties.put(JdbcConfig.JDBC_DRIVER.getKey(), "org.sqlite.JDBC");
+    properties.put(JdbcConfig.JDBC_URL.getKey(), "jdbc:sqlite::memory:");
+    properties.put(JdbcConfig.USERNAME.getKey(), "test");
+    properties.put(JdbcConfig.PASSWORD.getKey(), "test");
+    properties.put(JdbcConfig.TEST_ON_BORROW.getKey(), "false");
+
+    DataSource dataSource =
+        Assertions.assertDoesNotThrow(() -> DataSourceUtils.createDataSource(properties));
+    Assertions.assertTrue(dataSource instanceof org.apache.commons.dbcp2.BasicDataSource);
+    Assertions.assertTrue(((BasicDataSource) dataSource).getTestOnBorrow() == false);
+    ((BasicDataSource) dataSource).close();
   }
 }
