1f975b72d5fa33324f095c37c19b5ec0c47f02cf
sunxiaojian <sunxiaojian926@163.com>
2025-03-11 15:52:15 +0800

    fixed

--- core/src/main/java/org/apache/gravitino/storage/relational/service/RoleMetaService.java
+++ core/src/main/java/org/apache/gravitino/storage/relational/service/RoleMetaService.java
@@ -24,8 +24,6 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import java.io.IOException;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
