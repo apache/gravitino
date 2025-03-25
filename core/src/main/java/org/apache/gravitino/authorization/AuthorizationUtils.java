b898a7c20991a4fd15caf3000e4f0c18b622f1a2
Rory <he@datastrato.com>
2025-03-25 20:36:30 +0800

    style

--- core/src/main/java/org/apache/gravitino/authorization/AuthorizationUtils.java
+++ core/src/main/java/org/apache/gravitino/authorization/AuthorizationUtils.java
@@ -27,7 +27,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.gravitino.Catalog;
 import org.apache.gravitino.Entity;
