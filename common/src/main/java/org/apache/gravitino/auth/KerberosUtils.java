ff800773abca62c2d2deb53721fc09e3a351ddcb
gavin.wang <gracejianbo@163.com>
2025-03-26 11:32:53 +0800

    Fix the code formatting

--- common/src/main/java/org/apache/gravitino/auth/KerberosUtils.java
+++ common/src/main/java/org/apache/gravitino/auth/KerberosUtils.java
@@ -124,11 +124,11 @@ public class KerberosUtils {
         options.put("storeKey", "true");
       } else {
         options.put("useTicketCache", "true");
-	String ticketCache = System.getenv("KRB5CCNAME");
-	if (ticketCache != null) {
+        String ticketCache = System.getenv("KRB5CCNAME");
+        if (ticketCache != null) {
           options.put("ticketCache", ticketCache);
-	}
-	options.put("renewTGT", "true");
+        }
+        options.put("renewTGT", "true");
       }
       options.put("principal", principal);
       options.put("doNotPrompt", "true");
