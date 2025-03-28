1b514bff3675fd5a125dbaa83958d064008b78dd
Rory <he@datastrato.com>
2025-03-28 10:05:32 +0800

    fix the minor

--- core/src/test/java/org/apache/gravitino/utils/TestNamespaceUtil.java
+++ core/src/test/java/org/apache/gravitino/utils/TestNamespaceUtil.java
@@ -24,6 +24,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestNamespaceUtil {
+
   @Test
   public void testCheckNamespace() {
     Namespace a = Namespace.of("a");
