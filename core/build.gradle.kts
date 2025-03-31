090ab37fab82108fad9bc966062173851e6b3c63
fanng <xiaojing@datastrato.com>
2025-03-31 11:29:44 +0800

    remove dep

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -37,7 +37,9 @@ dependencies {
   implementation(libs.guava)
   implementation(libs.h2db)
   implementation(libs.mybatis)
-  implementation(libs.openlineage.java)
+  implementation(libs.openlineage.java) {
+    isTransitive = false
+  }
   implementation(libs.jackson.datatype.jdk8)
   implementation(libs.jackson.datatype.jsr310)
   implementation(libs.jackson.databind)
