01d7cb8e21a7837907bf1894f5f9832a0e45e96b
pancx <p_lord@163.com>
2025-03-31 20:01:18 +0800

    [#6797] feat(core): Uniform CLI output format
    Uniform CLI output format for Model, User and Group commands.

--- clients/cli/src/main/java/org/apache/gravitino/cli/outputs/PlainFormat.java
+++ clients/cli/src/main/java/org/apache/gravitino/cli/outputs/PlainFormat.java
@@ -25,7 +25,10 @@ import org.apache.gravitino.Audit;
 import org.apache.gravitino.Catalog;
 import org.apache.gravitino.Metalake;
 import org.apache.gravitino.Schema;
+import org.apache.gravitino.authorization.Group;
+import org.apache.gravitino.authorization.User;
 import org.apache.gravitino.cli.CommandContext;
+import org.apache.gravitino.model.Model;
 import org.apache.gravitino.rel.Column;
 import org.apache.gravitino.rel.Table;
 
@@ -57,6 +60,18 @@ public abstract class PlainFormat<T> extends BaseOutputFormat<T> {
       new TablePlainFormat(context).output((Table) entity);
     } else if (entity instanceof Table[]) {
       new TableListPlainFormat(context).output((Table[]) entity);
+    } else if (entity instanceof Model) {
+      new ModelDetailPlainFormat(context).output((Model) entity);
+    } else if (entity instanceof Model[]) {
+      new ModelListPlainFormat(context).output((Model[]) entity);
+    } else if (entity instanceof User) {
+      new UserDetailsPlainFormat(context).output((User) entity);
+    } else if (entity instanceof User[]) {
+      new UserListPlainFormat(context).output((User[]) entity);
+    } else if (entity instanceof Group) {
+      new GroupDetailsPlainFormat(context).output((Group) entity);
+    } else if (entity instanceof Group[]) {
+      new GroupListPlainFormat(context).output((Group[]) entity);
     } else if (entity instanceof Audit) {
       new AuditPlainFormat(context).output((Audit) entity);
     } else if (entity instanceof Column[]) {
@@ -271,6 +286,126 @@ public abstract class PlainFormat<T> extends BaseOutputFormat<T> {
         data.append(System.lineSeparator());
       }
       return NEWLINE_JOINER.join(header, data.toString());
+    }
+  }
+
+  /**
+   * Format a {@link Model} instance with detailed information. Output format: model_name,
+   * model_comment and latest_version
+   */
+  static final class ModelDetailPlainFormat extends PlainFormat<Model> {
+
+    /**
+     * Creates a new {@link PlainFormat} with the specified command context.
+     *
+     * @param context the {@link CommandContext} instance.
+     */
+    public ModelDetailPlainFormat(CommandContext context) {
+      super(context);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getOutput(Model model) {
+      return String.format(
+          "Model name %s, comment: %s, latest version: %s%n",
+          model.name(), model.comment(), model.latestVersion());
+    }
+  }
+
+  /** Format an array of {@link Model} instances with their names. Output format: model_name */
+  static final class ModelListPlainFormat extends PlainFormat<Model[]> {
+
+    /**
+     * Creates a new {@link PlainFormat} with the specified command context.
+     *
+     * @param context the {@link CommandContext} instance.
+     */
+    public ModelListPlainFormat(CommandContext context) {
+      super(context);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getOutput(Model[] models) {
+      return NEWLINE_JOINER.join(
+          Arrays.stream(models).map(Model::name).collect(Collectors.toList()));
+    }
+  }
+
+  /** Format a {@link User} instance with their details. Output format: username, role */
+  static final class UserDetailsPlainFormat extends PlainFormat<User> {
+
+    /**
+     * Creates a new {@link PlainFormat} with the specified command context.
+     *
+     * @param context the {@link CommandContext} instance.
+     */
+    public UserDetailsPlainFormat(CommandContext context) {
+      super(context);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getOutput(User user) {
+      return String.format("username: %s, role: %s", user.name(), COMMA_JOINER.join(user.roles()));
+    }
+  }
+
+  /** Format an array of {@link User} instances with their names. Output format: username */
+  static final class UserListPlainFormat extends PlainFormat<User[]> {
+
+    /**
+     * Creates a new {@link PlainFormat} with the specified command context.
+     *
+     * @param context the {@link CommandContext} instance.
+     */
+    public UserListPlainFormat(CommandContext context) {
+      super(context);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getOutput(User[] users) {
+      return NEWLINE_JOINER.join(Arrays.stream(users).map(User::name).collect(Collectors.toList()));
+    }
+  }
+
+  /** Format a {@link Group} instance with their details. Output format: group name, role */
+  static final class GroupDetailsPlainFormat extends PlainFormat<Group> {
+    /**
+     * Constructs a new {@link GroupDetailsPlainFormat} instance.
+     *
+     * @param context the {@link CommandContext} instance.
+     */
+    public GroupDetailsPlainFormat(CommandContext context) {
+      super(context);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getOutput(Group group) {
+      return String.format(
+          "group name: %s, role: %s", group.name(), COMMA_JOINER.join(group.roles()));
+    }
+  }
+
+  /** Format an array of {@link Group} instances with their names. Output format: group name */
+  static final class GroupListPlainFormat extends PlainFormat<Group[]> {
+    /**
+     * Constructs a new {@link GroupListPlainFormat} instance.
+     *
+     * @param context the {@link CommandContext} instance.
+     */
+    public GroupListPlainFormat(CommandContext context) {
+      super(context);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getOutput(Group[] groups) {
+      return NEWLINE_JOINER.join(
+          Arrays.stream(groups).map(Group::name).collect(Collectors.toList()));
     }
   }
 }
