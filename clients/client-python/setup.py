f4cfdc72cfdf9debdc2b0088d268bb4dfed2eb62
Justin Mclean <justin@datastrato.com>
2025-04-03 07:22:03 +0800

    for 0.8.1 release

--- clients/client-python/setup.py
+++ clients/client-python/setup.py
@@ -27,7 +27,7 @@ except FileNotFoundError:
 setup(
     name="apache-gravitino",
     description="Python lib/client for Apache Gravitino",
-    version="0.9.0",
+    version="0.8.1",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Apache Software Foundation",
