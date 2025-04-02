0d1ee0dfc784d01c319198c638c942e902a83df0
Tian Lu <tian.lu.xyz@gmail.com>
2025-04-02 21:22:16 +0200

    fix(spark-connector): filter out all pg gravitino table properties

---
+++ spark-connector/spark-common/src/main/java/org/apache/gravitino/spark/connector/jdbc/postgersql/PostgreSqlPropertiesConverter.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.gravitino.spark.connector.jdbc.postgersql;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.gravitino.spark.connector.PropertiesConverter;
+import org.apache.gravitino.spark.connector.jdbc.JdbcPropertiesConstants;
+
+public class PostgreSqlPropertiesConverter implements PropertiesConverter {
+
+  public static class PostgresPropertiesConverterHolder {
+    private static final PostgreSqlPropertiesConverter INSTANCE =
+        new PostgreSqlPropertiesConverter();
+  }
+
+  private PostgreSqlPropertiesConverter() {}
+
+  public static PostgreSqlPropertiesConverter getInstance() {
+    return PostgreSqlPropertiesConverter.PostgresPropertiesConverterHolder.INSTANCE;
+  }
+
+  private static final Map<String, String> GRAVITINO_CONFIG_TO_JDBC =
+      ImmutableMap.of(
+          JdbcPropertiesConstants.GRAVITINO_JDBC_URL,
+          JdbcPropertiesConstants.SPARK_JDBC_URL,
+          JdbcPropertiesConstants.GRAVITINO_JDBC_USER,
+          JdbcPropertiesConstants.SPARK_JDBC_USER,
+          JdbcPropertiesConstants.GRAVITINO_JDBC_PASSWORD,
+          JdbcPropertiesConstants.SPARK_JDBC_PASSWORD,
+          JdbcPropertiesConstants.GRAVITINO_JDBC_DRIVER,
+          JdbcPropertiesConstants.SPARK_JDBC_DRIVER);
+
+  @Override
+  public Map<String, String> toSparkCatalogProperties(Map<String, String> properties) {
+    Preconditions.checkArgument(properties != null, "Jdbc Catalog properties should not be null");
+    HashMap<String, String> jdbcProperties = new HashMap<>();
+    properties.forEach(
+        (key, value) -> {
+          if (GRAVITINO_CONFIG_TO_JDBC.containsKey(key)) {
+            jdbcProperties.put(GRAVITINO_CONFIG_TO_JDBC.get(key), value);
+          }
+        });
+    return jdbcProperties;
+  }
+
+  @Override
+  public Map<String, String> toGravitinoTableProperties(Map<String, String> properties) {
+    // PG doesn't support table properties.
+    return new HashMap<>();
+  }
+
+  @Override
+  public Map<String, String> toSparkTableProperties(Map<String, String> properties) {
+    return new HashMap<>(properties);
+  }
+}
--- spark-connector/spark-common/src/main/java/org/apache/gravitino/spark/connector/version/CatalogNameAdaptor.java
+++ spark-connector/spark-common/src/main/java/org/apache/gravitino/spark/connector/version/CatalogNameAdaptor.java
@@ -55,12 +55,24 @@ public class CatalogNameAdaptor {
           "3.5",
           "org.apache.gravitino.spark.connector.jdbc.GravitinoJdbcCatalogSpark35");
 
+  private static final Map<String, String> pgCatalogNames =
+      ImmutableMap.of(
+          "3.3",
+          "org.apache.gravitino.spark.connector.jdbc.postgresql.GravitinoPostgreSqlCatalogSpark33",
+          "3.4",
+          "org.apache.gravitino.spark.connector.jdbc.postgresql.GravitinoPostgreSqlCatalogSpark34",
+          "3.5",
+          "org.apache.gravitino.spark.connector.jdbc.postgresql.GravitinoPostgreSqlCatalogSpark35");
+
   private static String sparkVersion() {
     return package$.MODULE$.SPARK_VERSION();
   }
 
   private static String getCatalogName(String provider, int majorVersion, int minorVersion) {
     if (provider.startsWith("jdbc")) {
+      if (provider.startsWith("jdbc-postgresql")) {
+        return pgCatalogNames.get(String.format("%d.%d", majorVersion, minorVersion));
+      }
       return jdbcCatalogNames.get(String.format("%d.%d", majorVersion, minorVersion));
     }
     String key =
