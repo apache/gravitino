e5273a9329c10ee5377c7fc9a2807c281820e074
FANNG <xiaojing@datastrato.com>
2025-04-08 12:18:50 +0800

    [#6779] feat(core): Support lineage framework in Gravitino (#6782)
    ### What changes were proposed in this pull request?
    
    Support lineage framework in Gravitino, lineage endpoint and lineage
    sink manager will be proposed in separate PR.
    
    Total workflow draft PR: https://github.com/apache/gravitino/pull/6723
    
    The main work flow:
    1. Gravitino server creates lineage service which manages lineage source
    and lineage sinks.
    2. lineage source implementation receives lineage run event and
    dispatches to lineage service.
    3. lineage service process the run event and dispatch to lineage sink
    manager.
    4. lineage sink manager manges the life cycle of link lineage sinks,
    will dispatch run event to lineage sinks.
    
    ### Why are the changes needed?
    
    Fix: #6779 
    
    ### Does this PR introduce _any_ user-facing change?
    no
    
    ### How was this patch tested?
    setup Spark&Marquez environment and test the work flow.

---
+++ lineage/build.gradle.kts
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+plugins {
+  id("java")
+}
+
+dependencies {
+  implementation(project(":core"))
+  implementation(project(":server-common"))
+  implementation(libs.commons.lang3)
+  implementation(libs.guava)
+  implementation(libs.slf4j.api)
+  implementation(libs.jackson.datatype.jdk8)
+  implementation(libs.jackson.datatype.jsr310)
+  implementation(libs.jackson.databind)
+  implementation(libs.openlineage.java) {
+    isTransitive = false
+  }
+
+  testImplementation(libs.junit.jupiter.api)
+  testImplementation(libs.junit.jupiter.params)
+
+  testRuntimeOnly(libs.junit.jupiter.engine)
+}
