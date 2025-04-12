cb911c03d71b878621f3f0b5bf03679bbe391e78
George T. C. Lai <tsungchih.hd@gmail.com>
2025-04-13 00:46:07 +0800

    test: add unit tests for serializing struct type
    add unit tests for serializing struct type
    
    apache/gravitino#5202
    
    Signed-off-by: George T. C. Lai <tsungchih.hd@gmail.com>

--- clients/client-python/tests/unittests/json_serdes/test_type_serializer.py
+++ clients/client-python/tests/unittests/json_serdes/test_type_serializer.py
@@ -37,3 +37,35 @@ class TestTypeSerializer(unittest.TestCase):
     def test_serialize_primitive_and_none_type(self):
         for simple_string, type_ in self._primitive_and_none_types.items():
             self.assertEqual(TypeSerializer.serialize(data=type_), simple_string)
+
+    def test_serialize_struct_type_of_primitive_and_none_types(self):
+        types = self._primitive_and_none_types.values()
+        fields = [
+            Types.StructType.Field.not_null_field(
+                name=f"field_{field_idx}",
+                field_type=type_,
+                comment=f"comment {field_idx}" if field_idx % 2 == 0 else None,
+            )
+            for type_, field_idx in zip(types, range(len(types)))
+        ]
+
+        struct_type = Types.StructType.of(*fields)
+        serialized_result = TypeSerializer.serialize(struct_type)
+        serialized_fields = serialized_result.get(SerdesUtils.FIELDS)
+
+        self.assertEqual(serialized_result.get(SerdesUtils.TYPE), SerdesUtils.STRUCT)
+        for field, serialized_field in zip(fields, serialized_fields):
+            self.assertEqual(
+                serialized_field.get(SerdesUtils.STRUCT_FIELD_NAME), field.name()
+            )
+            self.assertEqual(
+                serialized_field.get(SerdesUtils.TYPE),
+                SerdesUtils.write_data_type(field.type()),
+            )
+            self.assertEqual(
+                serialized_field.get(SerdesUtils.STRUCT_FIELD_NULLABLE),
+                field.nullable(),
+            )
+            self.assertEqual(
+                serialized_field.get(SerdesUtils.STRUCT_FIELD_COMMENT), field.comment()
+            )
