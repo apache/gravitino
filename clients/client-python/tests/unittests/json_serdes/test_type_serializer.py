f2aedf5b8af0f01d366170d54c7b69b3c083add1
George T. C. Lai <tsungchih.hd@gmail.com>
2025-04-13 11:41:52 +0800

    test: add unit tests for TypesSerializer
    add unit tests for TypesSerializer
    
    apache/gravitino#5202
    
    Signed-off-by: George T. C. Lai <tsungchih.hd@gmail.com>

---
+++ clients/client-python/tests/unittests/json_serdes/test_type_serializer.py
@@ -0,0 +1,39 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import unittest
+
+from gravitino.api.types.types import Types
+from gravitino.utils.json_serdes import TypeSerializer
+from gravitino.utils.json_serdes._helper.serdes_utils import SerdesUtils
+
+
+class TestTypeSerializer(unittest.TestCase):
+    def setUp(self):
+        self._primitive_and_none_types = {
+            **SerdesUtils.TYPES,
+            **{
+                "decimal(10,2)": Types.DecimalType.of(10, 2),
+                "fixed(10)": Types.FixedType.of(10),
+                "char(10)": Types.FixedCharType.of(10),
+                "varchar(10)": Types.VarCharType.of(10),
+            },
+        }
+
+    def test_serialize_primitive_and_none_type(self):
+        for simple_string, type_ in self._primitive_and_none_types.items():
+            self.assertEqual(TypeSerializer.serialize(data=type_), simple_string)
