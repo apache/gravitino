e97ecb578d3d684170048c470beb7c797b4b5b7e
Tian Lu <ltakamoriaaa@gmail.com>
2025-04-23 03:43:29 +0200

    [#6697] fix(spark): fix throw exception during PG table creation in spark connector (#6824)
    ### What changes were proposed in this pull request?
    
    Filtering out `owner` table properties, as table properties not
    supported in PG and will cause issue in:
    https://github.com/apache/gravitino/issues/6697 during PG table creation
    
    ### Why are the changes needed?
    
    fix https://github.com/apache/gravitino/issues/6697
    
    ### Does this PR introduce _any_ user-facing change?
    
    No
    
    ### How was this patch tested?
    
    Tested in local environment, following the steps in
    https://github.com/apache/gravitino/issues/6697#issuecomment-2746218039
    
    ```
    spark-sql ()> create table public.a(a int);
    Time taken: 0.483 seconds
    spark-sql ()> 
    ```
    
    <img width="1403" alt="image"
    src="https://github.com/user-attachments/assets/21e1e557-ac7c-4ebb-8dae-c1d6dddbbd19"
    />
    
    TODO integration test.

--- spark-connector/v3.4/spark/src/main/java/org/apache/gravitino/spark/connector/jdbc/postgresql/GravitinoPostgreSqlCatalogSpark34.java
+++ spark-connector/v3.4/spark/src/main/java/org/apache/gravitino/spark/connector/jdbc/postgresql/GravitinoPostgreSqlCatalogSpark34.java
@@ -16,19 +16,15 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+package org.apache.gravitino.spark.connector.jdbc.postgresql;
 
-package org.apache.gravitino.spark.connector.jdbc;
+import org.apache.gravitino.spark.connector.PropertiesConverter;
+import org.apache.gravitino.spark.connector.jdbc.GravitinoJdbcCatalog;
+import org.apache.gravitino.spark.connector.jdbc.JdbcPropertiesConverter;
 
-public class JdbcPropertiesConstants {
-
-  public static final String GRAVITINO_JDBC_USER = "jdbc-user";
-  public static final String GRAVITINO_JDBC_PASSWORD = "jdbc-password";
-  public static final String GRAVITINO_JDBC_DRIVER = "jdbc-driver";
-  public static final String GRAVITINO_JDBC_URL = "jdbc-url";
-  public static final String GRAVITINO_JDBC_DATABASE = "jdbc-database";
-
-  public static final String SPARK_JDBC_URL = "url";
-  public static final String SPARK_JDBC_USER = "user";
-  public static final String SPARK_JDBC_PASSWORD = "password";
-  public static final String SPARK_JDBC_DRIVER = "driver";
+public class GravitinoPostgreSqlCatalogSpark34 extends GravitinoJdbcCatalog {
+  @Override
+  protected PropertiesConverter getPropertiesConverter() {
+    return JdbcPropertiesConverter.getPGInstance();
+  }
 }
