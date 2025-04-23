ae46ef7f7160d385c77280eb54e33ac3287fca8f
Lord of Abyss <103809695+Abyss-lord@users.noreply.github.com>
2025-04-23 14:08:34 +0800

    [#6525] feature(core): Support update comment operations for model alteration. (#7041)
    ### What changes were proposed in this pull request?
    
    Support update comment operations for model alteration.
    
    ### Why are the changes needed?
    
    Fix: #6525 
    
    ### Does this PR introduce _any_ user-facing change?
    
    User can update a comment of a model.
    
    ### How was this patch tested?
    
    local test + ut + it.
    
    `bin/gcli.sh model update -m demo_metalake --name
    model_catalog.schema.model2 --comment 'new_comment'`
    
    
    ![image](https://github.com/user-attachments/assets/d4f64e33-e307-4b2a-a225-0b598deac994)
    
    `bin/gcli.sh model update -m demo_metalake --name
    model_catalog.schema.model2 --comment ''`
    
    
    ![image](https://github.com/user-attachments/assets/bfe3872b-9167-412c-8b84-7c0b003fb5a6)

--- core/src/test/java/org/apache/gravitino/catalog/TestModelOperationDispatcher.java
+++ core/src/test/java/org/apache/gravitino/catalog/TestModelOperationDispatcher.java
@@ -339,7 +339,7 @@ public class TestModelOperationDispatcher extends TestOperationDispatcher {
   @Test
   void testRemoveModelProperty() {
     String schemaName = "test_remove_model_property_schema";
-    String modelName = "test_update_model_property";
+    String modelName = "test_remove_model_property";
     String modelComment = "model which tests update property";
     NameIdentifier schemaIdent = NameIdentifier.of(metalake, catalog, schemaName);
     schemaOperationDispatcher.createSchema(
@@ -362,6 +362,37 @@ public class TestModelOperationDispatcher extends TestOperationDispatcher {
     Assertions.assertEquals(modelName, alteredModel.name());
     Assertions.assertEquals(modelComment, alteredModel.comment());
     Assertions.assertEquals(ImmutableMap.of("k2", "v2"), alteredModel.properties());
+  }
+
+  @Test
+  void testUpdateModelComment() {
+    String schemaName = "test_update_model_comment_schema";
+    String modelName = "test_update_model_comment";
+    String modelComment = "model which tests update property";
+    String newModelComment = "new model comment";
+    Map<String, String> props = ImmutableMap.of("k1", "v1", "k2", "v2");
+
+    NameIdentifier schemaIdent = NameIdentifier.of(metalake, catalog, schemaName);
+    schemaOperationDispatcher.createSchema(
+        schemaIdent, "schema comment", ImmutableMap.of("k1", "v1", "k2", "v2"));
+
+    NameIdentifier modelIdent =
+        NameIdentifierUtil.ofModel(metalake, catalog, schemaName, modelName);
+
+    Model model = modelOperationDispatcher.registerModel(modelIdent, modelComment, props);
+
+    // validate registered model
+    Assertions.assertEquals(modelName, model.name());
+    Assertions.assertEquals(modelComment, model.comment());
+    Assertions.assertEquals(props, model.properties());
+
+    ModelChange change = ModelChange.updateComment(newModelComment);
+    Model alteredModel = modelOperationDispatcher.alterModel(modelIdent, change);
+
+    // validate updated model
+    Assertions.assertEquals(modelName, alteredModel.name());
+    Assertions.assertEquals(newModelComment, alteredModel.comment());
+    Assertions.assertEquals(props, alteredModel.properties());
   }
 
   @Test
