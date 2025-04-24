25f0d4ea414d30e39e033487cadb4c5f46055ea7
yangyang zhong <35210666+hdygxsj@users.noreply.github.com>
2025-04-24 10:58:05 +0800

    [#6841] feat: Introduce an authorization framework. (#6842)
    ### What changes were proposed in this pull request?
    
    It only introduces the authorization framework without including any
    implementation.
    
    ### Why are the changes needed?
    
    Fix: #6841
    
    ### Does this PR introduce _any_ user-facing change?
    
    none
    
    ### How was this patch tested?
    
    none

---
+++ server-common/src/main/java/org/apache/gravitino/server/authorization/GravitinoAuthorizer.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.gravitino.server.authorization;
+
+import java.io.Closeable;
+import java.security.Principal;
+import org.apache.gravitino.MetadataObject;
+import org.apache.gravitino.authorization.Privilege;
+
+/** Used for metadata authorization. */
+public interface GravitinoAuthorizer extends Closeable {
+
+  /**
+   * After instantiating the GravitinoAuthorizer, execute the initialize method to perform a series
+   * of initialization operations, such as loading privilege policies and so on.
+   */
+  void initialize();
+
+  /**
+   * Perform authorization and return the authorization result.
+   *
+   * @param principal the user principal
+   * @param metalake the metalake
+   * @param metadataObject the metadataObject.
+   * @param privilege for example, CREATE_CATALOG, CREATE_TABLE, etc.
+   * @return authorization result.
+   */
+  boolean authorize(
+      Principal principal,
+      String metalake,
+      MetadataObject metadataObject,
+      Privilege.Name privilege);
+}
