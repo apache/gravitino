2e6b5fee24b0e8768fd99e54ed033b0b5f554c0e
sunxiaojian <sunxiaojian926@163.com>
2025-04-28 22:20:18 +0800

    fixed

--- spark-connector/spark-common/src/test/java/org/apache/gravitino/spark/connector/integration/test/hive/SparkHiveCatalogIT.java
+++ spark-connector/spark-common/src/test/java/org/apache/gravitino/spark/connector/integration/test/hive/SparkHiveCatalogIT.java
@@ -34,11 +34,12 @@ import org.apache.hadoop.fs.Path;
 import org.apache.spark.sql.connector.catalog.TableCatalog;
 import org.apache.spark.sql.types.DataTypes;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-// @Tag("gravitino-docker-test")
+@Tag("gravitino-docker-test")
 public abstract class SparkHiveCatalogIT extends SparkCommonIT {
 
   @Override
