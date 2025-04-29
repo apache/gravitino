cb1ee52426fc321114c1ad08d4bce5ad9eea9044
yangyang zhong <35210666+hdygxsj@users.noreply.github.com>
2025-04-29 10:05:50 +0800

    [#6774] feat(authz): Support initialize GravitinoAuthorizer (#7062)
    ### What changes were proposed in this pull request?
    
    Support initialize GravitinoAuthorizer
    
    ### Why are the changes needed?
    
    Fix: #6774
    
    ### Does this PR introduce _any_ user-facing change?
    
    None
    
    ### How was this patch tested?
    
    1、org.apache.gravitino.server.authorization.TestGravitinoAuthorizerProvider
    2、org.apache.gravitino.server.authorization.TestAllowAuthorizer

--- server/src/main/java/org/apache/gravitino/server/GravitinoServer.java
+++ server/src/main/java/org/apache/gravitino/server/GravitinoServer.java
@@ -19,6 +19,8 @@
 package org.apache.gravitino.server;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 import java.util.HashSet;
 import java.util.Properties;
 import javax.servlet.Servlet;
@@ -39,6 +41,7 @@ import org.apache.gravitino.metalake.MetalakeDispatcher;
 import org.apache.gravitino.metrics.MetricsSystem;
 import org.apache.gravitino.metrics.source.MetricsSource;
 import org.apache.gravitino.server.authentication.ServerAuthenticator;
+import org.apache.gravitino.server.authorization.GravitinoAuthorizer;
 import org.apache.gravitino.server.authorization.GravitinoAuthorizerProvider;
 import org.apache.gravitino.server.web.ConfigServlet;
 import org.apache.gravitino.server.web.HttpServerMetricsSource;
@@ -172,7 +175,12 @@ public class GravitinoServer extends ResourceConfig {
     server.join();
   }
 
-  public void stop() {
+  public void stop() throws IOException {
+    GravitinoAuthorizer gravitinoAuthorizer =
+        GravitinoAuthorizerProvider.getInstance().getGravitinoAuthorizer();
+    if (gravitinoAuthorizer != null) {
+      gravitinoAuthorizer.close();
+    }
     server.stop();
     gravitinoEnv.shutdown();
     if (lineageService != null) {
