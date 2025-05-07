fccb4aaa5470ff77e2753d443f1e771a4bd4a0d1
fanng <xiaojing@datastrato.com>
2025-05-07 15:42:06 +0800

    jdbc

--- dev/docker/gravitino/gravitino-dependency.sh
+++ dev/docker/gravitino/gravitino-dependency.sh
@@ -21,19 +21,53 @@ set -ex
 gravitino_dir="$(dirname "${BASH_SOURCE-$0}")"
 gravitino_dir="$(cd "${gravitino_dir}">/dev/null; pwd)"
 gravitino_home="$(cd "${gravitino_dir}/../../..">/dev/null; pwd)"
+gravitino_package_dir="${gravitino_dir}/packages/gravitino"
+gravitino_staging_dir="${gravitino_package_dir}/staging"
+gravitino_iceberg_rest_dir="${gravitino_package_dir}/iceberg-rest-server/libs/"
 
 # Build the Gravitino project
-${gravitino_home}/gradlew clean build -x test
+# ${gravitino_home}/gradlew clean build -x test -x rat
 
-rm -rf ${gravitino_home}/distribution
+# rm -rf ${gravitino_home}/distribution
 # Prepare compile Gravitino packages
-${gravitino_home}/gradlew compileDistribution -x test
+# ${gravitino_home}/gradlew compileDistribution -x test -x rat
 
 # Removed old packages, Avoid multiple re-executions using the wrong file
 rm -rf "${gravitino_dir}/packages"
 mkdir -p "${gravitino_dir}/packages"
 
-cp -r "${gravitino_home}/distribution/package" "${gravitino_dir}/packages/gravitino"
+cp -r "${gravitino_home}/distribution/package" "${gravitino_package_dir}"
+mkdir -p "${gravitino_staging_dir}"
+
+echo "Start to download the jar package"
+
+mysql_driver="mysql-connector-java-8.0.27.jar"
+wget "https://repo1.maven.org/maven2/mysql/mysql-connector-java/8.0.27/$mysql_driver" -O "${gravitino_staging_dir}/${mysql_driver}"
+cp "${gravitino_staging_dir}/${mysql_driver}" "${gravitino_package_dir}/catalogs/jdbc-mysql/libs/"
+cp "${gravitino_staging_dir}/${mysql_driver}" "${gravitino_package_dir}/catalogs/lakehouse-iceberg/libs/"
+cp "${gravitino_staging_dir}/${mysql_driver}" "${gravitino_iceberg_rest_dir}"
+cp "${gravitino_staging_dir}/${mysql_driver}" "${gravitino_package_dir}/libs/"
+
+pg_driver="postgresql-42.7.0.jar"
+wget "https://jdbc.postgresql.org/download/$pg_driver" -O "${gravitino_staging_dir}/${pg_driver}"
+cp "${gravitino_staging_dir}/${pg_driver}" "${gravitino_package_dir}/catalogs/jdbc-postgresql/libs/"
+cp "${gravitino_staging_dir}/${pg_driver}" "${gravitino_package_dir}/catalogs/lakehouse-iceberg/libs/"
+cp "${gravitino_staging_dir}/${pg_driver}" "${gravitino_iceberg_rest_dir}"
+
+iceberg_version="1.6.1"
+iceberg_aws_bundle="iceberg-aws-bundle-${iceberg_version}.jar"
+wget "https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-aws-bundle/${iceberg_version}/${iceberg_aws_bundle}" -O "${gravitino_staging_dir}/${iceberg_aws_bundle}"
+cp "${gravitino_staging_dir}/${iceberg_aws_bundle}" "${gravitino_iceberg_rest_dir}"
+
+iceberg_gcp_bundle="iceberg-gcp-bundle-${iceberg_version}.jar"
+wget "https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-gcp-bundle/${iceberg_version}/${iceberg_gcp_bundle}" -O "${gravitino_staging_dir}/${iceberg_gcp_bundle}"
+cp "${gravitino_staging_dir}/${iceberg_gcp_bundle}" "${gravitino_iceberg_rest_dir}"
+
+iceberg_azure_bundle="iceberg-azure-bundle-${iceberg_version}.jar"
+wget "https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-azure-bundle/${iceberg_version}/${iceberg_azure_bundle}" -O "${gravitino_staging_dir}/${iceberg_azure_bundle}"
+cp "${gravitino_staging_dir}/${iceberg_azure_bundle}" "${gravitino_iceberg_rest_dir}"
+
+echo "Finish downloading"
 
 mkdir -p "${gravitino_dir}/packages/gravitino/bin"
 cp "${gravitino_dir}/rewrite_gravitino_server_config.py" "${gravitino_dir}/packages/gravitino/bin/"
@@ -44,6 +78,11 @@ cp ${gravitino_home}/bundles/aliyun-bundle/build/libs/*.jar "${gravitino_dir}/pa
 cp ${gravitino_home}/bundles/aws-bundle/build/libs/*.jar "${gravitino_dir}/packages/gravitino/catalogs/hadoop/libs"
 cp ${gravitino_home}/bundles/gcp-bundle/build/libs/*.jar "${gravitino_dir}/packages/gravitino/catalogs/hadoop/libs"
 cp ${gravitino_home}/bundles/azure-bundle/build/libs/*.jar "${gravitino_dir}/packages/gravitino/catalogs/hadoop/libs"
+
+cp ${gravitino_home}/bundles/aws/build/libs/*.jar "${gravitino_iceberg_rest_dir}"
+cp ${gravitino_home}/bundles/gcp/build/libs/*.jar "${gravitino_iceberg_rest_dir}"
+cp ${gravitino_home}/bundles/azure/build/libs/*.jar "${gravitino_iceberg_rest_dir}"
+cp ${gravitino_home}/bundles/aliyun-bundle/build/libs/*.jar "${gravitino_iceberg_rest_dir}"
 
 # Keeping the container running at all times
 cat <<EOF >> "${gravitino_dir}/packages/gravitino/bin/gravitino.sh"
