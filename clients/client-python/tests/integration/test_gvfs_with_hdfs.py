ce9ba181730821c869471d7579047d025644fc48
Lord of Abyss <103809695+Abyss-lord@users.noreply.github.com>
2025-05-17 10:06:39 +0800

    [MINOR] feat(*): Rebase from main (#7173)
    Rebase from main
    
    rebase
    
    no
    
    local test.
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Signed-off-by: George T. C. Lai <tsungchih.hd@gmail.com>
    Co-authored-by: yangyang zhong <35210666+hdygxsj@users.noreply.github.com>
    Co-authored-by: FANNG <xiaojing@datastrato.com>
    Co-authored-by: Eric Chang <e850506@gmail.com>
    Co-authored-by: roryqi <he@datastrato.com>
    Co-authored-by: gavin.wang <gracejianbo@163.com>
    Co-authored-by: Kang <zhoukangcn@gmail.com>
    Co-authored-by: Mini Yu <yuqi@datastrato.com>
    Co-authored-by: Zhengke Zhou <madzhou1@gmail.com>
    Co-authored-by: mchades <liminghuang@datastrato.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Jerry Shao <jerryshao@datastrato.com>
    Co-authored-by: Justin Mclean <justin@classsoftware.com>
    Co-authored-by: Danhua Wang <danhua@datastrato.com>
    Co-authored-by: yunchi <yunchipang@gmail.com>
    Co-authored-by: RickyMa <rickyma@tencent.com>
    Co-authored-by: Yuhui <hui@datastrato.com>
    Co-authored-by: Qiming Teng <tengqm@outlook.com>
    Co-authored-by: Tian Lu <ltakamoriaaa@gmail.com>
    Co-authored-by: tian bao <2011xuesong@gmail.com>
    Co-authored-by: Jimmy Lee <55496001+waukin@users.noreply.github.com>
    Co-authored-by: Brijesh Thummar <brijeshthummar02@gmail.com>
    Co-authored-by: Qian Xia <qian@datastrato.com>
    Co-authored-by: Xiaojian Sun <sunxiaojian926@163.com>
    Co-authored-by: Cyber Star <lifangyu.mail@163.com>
    Co-authored-by: George T. C. Lai <tsungchih.hd@gmail.com>
    Co-authored-by: AndreVale69 <57899285+AndreVale69@users.noreply.github.com>
    Co-authored-by: Drinkaiii <168957924+Drinkaiii@users.noreply.github.com>
    Co-authored-by: Sébastien Brochet <blinkseb@users.noreply.github.com>
    Co-authored-by: Sébastien Brochet <sebastien.brochet@data-circle.eu>

--- clients/client-python/tests/integration/test_gvfs_with_hdfs.py
+++ clients/client-python/tests/integration/test_gvfs_with_hdfs.py
@@ -160,9 +160,7 @@ class TestGvfsWithHDFS(IntegrationTestEnv):
             schema_name=cls.schema_name, comment="", properties={}
         )
 
-        cls.fileset_storage_location: str = (
-            f"hdfs://{cls.hdfs_container.get_ip()}:9000/{cls.catalog_name}/{cls.schema_name}/{cls.fileset_name}"
-        )
+        cls.fileset_storage_location: str = f"hdfs://{cls.hdfs_container.get_ip()}:9000/{cls.catalog_name}/{cls.schema_name}/{cls.fileset_name}"
         cls.fileset_gvfs_location = (
             f"gvfs://fileset/{cls.catalog_name}/{cls.schema_name}/{cls.fileset_name}"
         )
