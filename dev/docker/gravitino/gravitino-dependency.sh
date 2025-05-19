dff4d488cd3183eb053ddea0bf9f7e4a4793bc92
fanng <xiaojing@datastrato.com>
2025-05-19 14:21:58 +0800

    fix comment

--- dev/docker/gravitino/gravitino-dependency.sh
+++ dev/docker/gravitino/gravitino-dependency.sh
@@ -30,7 +30,7 @@ ${gravitino_home}/gradlew clean build -x test
 
 rm -rf ${gravitino_home}/distribution
 # Prepare compile Gravitino packages
-${gravitino_home}/gradlew compileDistribution -x test -x rat
+${gravitino_home}/gradlew compileDistribution -x test
 
 # Removed old packages, Avoid multiple re-executions using the wrong file
 rm -rf "${gravitino_dir}/packages"
