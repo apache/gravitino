3aaa7dff8483cdb43af9b633a0290dd80c553f73
yangyang zhong <35210666+hdygxsj@users.noreply.github.com>
2025-05-19 20:23:13 +0800

    spotless apply

--- server-common/build.gradle.kts
+++ server-common/build.gradle.kts
@@ -47,7 +47,6 @@ dependencies {
   implementation(libs.ognl)
   implementation(libs.prometheus.servlet)
 
-
   testImplementation(libs.commons.io)
   testImplementation(libs.junit.jupiter.api)
   testImplementation(libs.junit.jupiter.params)
