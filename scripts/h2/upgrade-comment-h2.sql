2709e8eb536e07d177b09828f85d1a5c1b5c8a24
pancx <p_lord@163.com>
2025-05-27 20:08:58 +0800

    DRAFT Change Comment type
    Change Comment type

---
+++ scripts/h2/upgrade-comment-h2.sql
@@ -0,0 +1,26 @@
+--
+-- Licensed to the Apache Software Foundation (ASF) under one
+-- or more contributor license agreements.  See the NOTICE file--
+--  distributed with this work for additional information
+-- regarding copyright ownership.  The ASF licenses this file
+-- to you under the Apache License, Version 2.0 (the
+-- "License"). You may not use this file except in compliance
+-- with the License.  You may obtain a copy of the License at
+--
+--  http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing,
+-- software distributed under the License is distributed on an
+-- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+-- KIND, either express or implied.  See the License for the
+-- specific language governing permissions and limitations
+-- under the License.
+--
+
+ALTER TABLE metalake_meta ALTER COLUMN metalake_comment SET DATA TYPE TEXT;
+ALTER TABLE catalog_meta ALTER COLUMN catalog_comment SET DATA TYPE TEXT;
+ALTER TABLE schema_meta ALTER COLUMN schema_comment SET DATA TYPE TEXT;
+ALTER TABLE fileset_version_info ALTER COLUMN fileset_comment SET DATA TYPE TEXT;
+ALTER TABLE topic_meta ALTER COLUMN comment SET DATA TYPE TEXT;
+ALTER TABLE tag_meta ALTER COLUMN tag_comment SET DATA TYPE TEXT;
+ALTER TABLE table_column_version_info ALTER COLUMN column_comment SET DATA TYPE TEXT;
