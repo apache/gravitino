9e3ce5d98a22e72b6ca2a887c65d485b3cadc74a
yangyang zhong <35210666+hdygxsj@users.noreply.github.com>
2025-05-29 21:03:49 +0800

    [#6788] feat(authz): Introduce MetadataFilterHelper to filter the result data (#7167)
    ### What changes were proposed in this pull request?
    
    Introduce MetadataFilterHelper to filter the result data obtained from
    list requests.
    
    ### Why are the changes needed?
    
    Fix: #6788
    
    ### Does this PR introduce _any_ user-facing change?
    
    None
    
    ### How was this patch tested?
    
    org.apache.gravitino.server.authorization.TestMetadataFilterHelper
    
    ---------
    
    Co-authored-by: KyleLin0927 <kylelin0927@gmail.com>

--- server-common/src/main/java/org/apache/gravitino/server/authorization/MetadataFilterHelper.java
+++ server-common/src/main/java/org/apache/gravitino/server/authorization/MetadataFilterHelper.java
@@ -17,10 +17,17 @@
 
 package org.apache.gravitino.server.authorization;
 
-import com.google.errorprone.annotations.DoNotCall;
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.gravitino.Entity;
 import org.apache.gravitino.MetadataObject;
 import org.apache.gravitino.NameIdentifier;
+import org.apache.gravitino.authorization.Privilege;
 import org.apache.gravitino.server.authorization.expression.AuthorizationExpressionEvaluator;
+import org.apache.gravitino.utils.NameIdentifierUtil;
+import org.apache.gravitino.utils.PrincipalUtils;
 
 /**
  * MetadataFilterHelper performs permission checks on the list data returned by the REST API based
@@ -34,28 +41,89 @@ public class MetadataFilterHelper {
   /**
    * Call {@link GravitinoAuthorizer} to filter the metadata list
    *
-   * @param metadataType for example, CATALOG, SCHEMA,TABLE, etc.
+   * @param entityType for example, CATALOG, SCHEMA,TABLE, etc.
    * @param privilege for example, CREATE_CATALOG, CREATE_TABLE, etc.
    * @param metadataList metadata list.
    * @return metadata List that the user has permission to access.
    */
-  @DoNotCall
-  public static NameIdentifier[] filter(
-      MetadataObject.Type metadataType, String privilege, NameIdentifier[] metadataList) {
-    throw new UnsupportedOperationException();
+  public static NameIdentifier[] filterByPrivilege(
+      String metalake,
+      Entity.EntityType entityType,
+      String privilege,
+      NameIdentifier[] metadataList) {
+    GravitinoAuthorizer gravitinoAuthorizer =
+        GravitinoAuthorizerProvider.getInstance().getGravitinoAuthorizer();
+    Principal currentPrincipal = PrincipalUtils.getCurrentPrincipal();
+    return Arrays.stream(metadataList)
+        .filter(
+            metaDataName ->
+                gravitinoAuthorizer.authorize(
+                    currentPrincipal,
+                    metalake,
+                    NameIdentifierUtil.toMetadataObject(metaDataName, entityType),
+                    Privilege.Name.valueOf(privilege)))
+        .toArray(NameIdentifier[]::new);
   }
 
   /**
    * Call {@link AuthorizationExpressionEvaluator} to filter the metadata list
    *
+   * @param metalake metalake
    * @param expression authorization expression
-   * @param metadataType for example, CATALOG, SCHEMA,TABLE, etc.
+   * @param entityType for example, CATALOG, SCHEMA,TABLE, etc.
    * @param nameIdentifiers metaData list.
    * @return metadata List that the user has permission to access.
    */
-  @DoNotCall
   public static NameIdentifier[] filterByExpression(
-      String expression, MetadataObject.Type metadataType, NameIdentifier[] nameIdentifiers) {
-    throw new UnsupportedOperationException();
+      String metalake,
+      String expression,
+      Entity.EntityType entityType,
+      NameIdentifier[] nameIdentifiers) {
+    AuthorizationExpressionEvaluator authorizationExpressionEvaluator =
+        new AuthorizationExpressionEvaluator(expression);
+    return Arrays.stream(nameIdentifiers)
+        .filter(
+            metaDataName -> {
+              Map<MetadataObject.Type, NameIdentifier> nameIdentifierMap =
+                  spiltMetadataNames(metalake, entityType, metaDataName);
+              return authorizationExpressionEvaluator.evaluate(nameIdentifierMap);
+            })
+        .toArray(NameIdentifier[]::new);
+  }
+
+  /**
+   * Extract the parent metadata from NameIdentifier. For example, when given a Table
+   * NameIdentifier, it returns a map containing the Table itself along with its parent Schema and
+   * Catalog.
+   *
+   * @param metalake metalake
+   * @param entityType metadata type
+   * @param nameIdentifier metadata name
+   * @return A map containing the metadata object and all its parent objects, keyed by their types
+   */
+  private static Map<MetadataObject.Type, NameIdentifier> spiltMetadataNames(
+      String metalake, Entity.EntityType entityType, NameIdentifier nameIdentifier) {
+    Map<MetadataObject.Type, NameIdentifier> nameIdentifierMap = new HashMap<>();
+    nameIdentifierMap.put(MetadataObject.Type.METALAKE, NameIdentifierUtil.ofMetalake(metalake));
+    switch (entityType) {
+      case CATALOG:
+        nameIdentifierMap.put(MetadataObject.Type.CATALOG, nameIdentifier);
+        break;
+      case SCHEMA:
+        nameIdentifierMap.put(MetadataObject.Type.SCHEMA, nameIdentifier);
+        nameIdentifierMap.put(
+            MetadataObject.Type.CATALOG, NameIdentifierUtil.getCatalogIdentifier(nameIdentifier));
+        break;
+      case TABLE:
+        nameIdentifierMap.put(MetadataObject.Type.TABLE, nameIdentifier);
+        nameIdentifierMap.put(
+            MetadataObject.Type.SCHEMA, NameIdentifierUtil.getSchemaIdentifier(nameIdentifier));
+        nameIdentifierMap.put(
+            MetadataObject.Type.CATALOG, NameIdentifierUtil.getCatalogIdentifier(nameIdentifier));
+        break;
+      default:
+        break;
+    }
+    return nameIdentifierMap;
   }
 }
