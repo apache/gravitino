d5c20068e0cd3e4c3653c65a2ae675519e3b3011
yangyang zhong <35210666+hdygxsj@users.noreply.github.com>
2025-05-29 21:03:49 +0800

    [#6788] feat(authz): Introduce MetadataFilterHelper to filter the result data (#7167)
    ### What changes were proposed in this pull request?
    
    Introduce MetadataFilterHelper to filter the result data obtained from
    list requests.
    
    ### Why are the changes needed?
    
    Fix: #6788
    
    ### Does this PR introduce _any_ user-facing change?
    
    None
    
    ### How was this patch tested?
    
    org.apache.gravitino.server.authorization.TestMetadataFilterHelper
    
    ---------
    
    Co-authored-by: KyleLin0927 <kylelin0927@gmail.com>

--- server-common/src/test/java/org/apache/gravitino/server/authorization/expression/TestAuthorizationExpressionEvaluator.java
+++ server-common/src/test/java/org/apache/gravitino/server/authorization/expression/TestAuthorizationExpressionEvaluator.java
@@ -21,17 +21,13 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
 
-import java.io.IOException;
-import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 import org.apache.gravitino.MetadataObject;
 import org.apache.gravitino.NameIdentifier;
 import org.apache.gravitino.UserPrincipal;
-import org.apache.gravitino.authorization.Privilege;
-import org.apache.gravitino.server.authorization.GravitinoAuthorizer;
 import org.apache.gravitino.server.authorization.GravitinoAuthorizerProvider;
+import org.apache.gravitino.server.authorization.MockGravitinoAuthorizer;
 import org.apache.gravitino.utils.NameIdentifierUtil;
 import org.apache.gravitino.utils.PrincipalUtils;
 import org.junit.jupiter.api.Assertions;
@@ -101,52 +97,5 @@ public class TestAuthorizationExpressionEvaluator {
           MetadataObject.Type.METALAKE, NameIdentifierUtil.ofMetalake("metalakeWithOwner"));
       Assertions.assertTrue(authorizationExpressionEvaluator.evaluate(metadataNames));
     }
-  }
-
-  private static class MockGravitinoAuthorizer implements GravitinoAuthorizer {
-
-    @Override
-    public void initialize() {}
-
-    @Override
-    public boolean authorize(
-        Principal principal,
-        String metalake,
-        MetadataObject metadataObject,
-        Privilege.Name privilege) {
-      if (!("tester".equals(principal.getName()) && "testMetalake".equals(metalake))) {
-        return false;
-      }
-      String name = metadataObject.name();
-      MetadataObject.Type type = metadataObject.type();
-      if (type == MetadataObject.Type.CATALOG
-          && "testCatalog".equals(name)
-          && privilege == Privilege.Name.USE_CATALOG) {
-        return true;
-      }
-      if (type == MetadataObject.Type.SCHEMA
-          && "testSchema".equals(name)
-          && privilege == Privilege.Name.USE_SCHEMA) {
-        return true;
-      }
-      return type == MetadataObject.Type.TABLE
-          && "testTable".equals(name)
-          && privilege == Privilege.Name.SELECT_TABLE;
-    }
-
-    @Override
-    public boolean isOwner(Principal principal, String metalake, MetadataObject metadataObject) {
-      if (!("tester".equals(principal.getName()) && "metalakeWithOwner".equals(metalake))) {
-        return false;
-      }
-      return Objects.equals(metadataObject.type(), MetadataObject.Type.METALAKE)
-          && Objects.equals("metalakeWithOwner", metadataObject.name());
-    }
-
-    @Override
-    public void handleRolePrivilegeChange(Long roleId) {}
-
-    @Override
-    public void close() throws IOException {}
   }
 }
