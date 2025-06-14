e85863b7bf72922941fa2d5d88b0c22d546dc4fc
Lord of Abyss <103809695+Abyss-lord@users.noreply.github.com>
2025-06-15 01:25:41 +0800

    [#7177] feat(core): Implement cache integration with relational (#7354)
    ### What changes were proposed in this pull request?
    
    Change:
    - Updated EntityCache interface: `get` no longer delegates to underlying
    store
    - Enhanced RelationalEntityStore to support entity caching
    - Revised multiple test cases to reflect caching behavior and validate
    correctness
    
    Tasks:
    - [X] Implement the methods related to the `SupportsEntityStoreCache`
    and `SupportsRelationEntityCache` interface in `CaffeineEntityCache`.
    - [X] Implement cache integration with relational store and add basic
    unit tests.
    - [ ] Add unit tests for both index and cache.
    - [ ] Use JCStress to perform multi-threaded testing on
    `CaffeineEntityCache` and the `CacheIndex`.
    
    ### Why are the changes needed?
    
    Fix: #7177
    
    ### Does this PR introduce _any_ user-facing change?
    
    no
    
    ### How was this patch tested?
    
    local test.

--- core/src/main/java/org/apache/gravitino/cache/BaseEntityCache.java
+++ core/src/main/java/org/apache/gravitino/cache/BaseEntityCache.java
@@ -20,28 +20,11 @@
 package org.apache.gravitino.cache;
 
 import com.google.common.base.Preconditions;
-import java.util.Collections;
-import java.util.EnumMap;
 import java.util.List;
-import java.util.Map;
 import org.apache.gravitino.Config;
 import org.apache.gravitino.Entity;
-import org.apache.gravitino.EntityStore;
 import org.apache.gravitino.HasIdentifier;
 import org.apache.gravitino.NameIdentifier;
-import org.apache.gravitino.meta.BaseMetalake;
-import org.apache.gravitino.meta.CatalogEntity;
-import org.apache.gravitino.meta.ColumnEntity;
-import org.apache.gravitino.meta.FilesetEntity;
-import org.apache.gravitino.meta.ModelEntity;
-import org.apache.gravitino.meta.ModelVersionEntity;
-import org.apache.gravitino.meta.RoleEntity;
-import org.apache.gravitino.meta.SchemaEntity;
-import org.apache.gravitino.meta.TableEntity;
-import org.apache.gravitino.meta.TagEntity;
-import org.apache.gravitino.meta.TopicEntity;
-import org.apache.gravitino.meta.UserEntity;
-import org.apache.gravitino.storage.relational.RelationalEntityStore;
 
 /**
  * An abstract class that provides a basic implementation for the {@link EntityCache} interface.
@@ -49,57 +32,17 @@ import org.apache.gravitino.storage.relational.RelationalEntityStore;
  * other entity cache implementations.
  */
 public abstract class BaseEntityCache implements EntityCache {
-  private static final Map<Entity.EntityType, Class<?>> ENTITY_CLASS_MAP;
-  // The entity store used by the cache, initialized through the constructor.
-  protected final RelationalEntityStore entityStore;
   protected final Config cacheConfig;
-
-  static {
-    Map<Entity.EntityType, Class<?>> map = new EnumMap<>(Entity.EntityType.class);
-    map.put(Entity.EntityType.METALAKE, BaseMetalake.class);
-    map.put(Entity.EntityType.CATALOG, CatalogEntity.class);
-    map.put(Entity.EntityType.SCHEMA, SchemaEntity.class);
-    map.put(Entity.EntityType.TABLE, TableEntity.class);
-    map.put(Entity.EntityType.FILESET, FilesetEntity.class);
-    map.put(Entity.EntityType.MODEL, ModelEntity.class);
-    map.put(Entity.EntityType.TOPIC, TopicEntity.class);
-    map.put(Entity.EntityType.TAG, TagEntity.class);
-    map.put(Entity.EntityType.MODEL_VERSION, ModelVersionEntity.class);
-    map.put(Entity.EntityType.COLUMN, ColumnEntity.class);
-    map.put(Entity.EntityType.USER, UserEntity.class);
-    map.put(Entity.EntityType.GROUP, Entity.class);
-    map.put(Entity.EntityType.ROLE, RoleEntity.class);
-    ENTITY_CLASS_MAP = Collections.unmodifiableMap(map);
-  }
 
   /**
    * Constructs a new {@link BaseEntityCache} instance.
    *
-   * @param entityStore The entity store to be used by the cache.
+   * @param config The cache configuration
    */
-  public BaseEntityCache(Config config, EntityStore entityStore) {
+  public BaseEntityCache(Config config) {
     Preconditions.checkArgument(config != null, "Config must not be null");
-    Preconditions.checkArgument(entityStore != null, "EntityStore must not be null");
 
     this.cacheConfig = config;
-    this.entityStore = (RelationalEntityStore) entityStore;
-  }
-
-  /**
-   * Returns the class of the entity based on its type.
-   *
-   * @param type The entity type
-   * @return The class of the entity
-   * @throws IllegalArgumentException if the entity type is not supported
-   */
-  @SuppressWarnings("unchecked")
-  public static <E extends Entity & HasIdentifier> Class<E> getEntityClass(Entity.EntityType type) {
-    Preconditions.checkArgument(type != null, "EntityType must not be null");
-
-    Class<?> clazz = ENTITY_CLASS_MAP.get(type);
-    Preconditions.checkArgument(clazz != null, "Unsupported EntityType: " + type);
-
-    return (Class<E>) clazz;
   }
 
   /**
@@ -108,7 +51,7 @@ public abstract class BaseEntityCache implements EntityCache {
    * @param entity The {@link Entity} instance.
    * @return The {@link NameIdentifier} of the entity
    */
-  protected static NameIdentifier getIdentFromEntity(Entity entity) {
+  public static NameIdentifier getIdentFromEntity(Entity entity) {
     validateEntityHasIdentifier(entity);
     HasIdentifier hasIdentifier = (HasIdentifier) entity;
 
@@ -133,7 +76,7 @@ public abstract class BaseEntityCache implements EntityCache {
    * @param <E> The type of the entities in the new list.
    */
   @SuppressWarnings("unchecked")
-  protected static <E extends Entity & HasIdentifier> List<E> convertEntity(List<Entity> entities) {
+  public static <E extends Entity & HasIdentifier> List<E> convertEntities(List<Entity> entities) {
     entities.forEach(BaseEntityCache::validateEntityHasIdentifier);
 
     return (List<E>) (List<? extends Entity>) entities;
@@ -147,7 +90,7 @@ public abstract class BaseEntityCache implements EntityCache {
    * @param <E> The type of the new entity.
    */
   @SuppressWarnings("unchecked")
-  protected static <E extends Entity & HasIdentifier> E convertEntity(Entity entity) {
+  public static <E extends Entity & HasIdentifier> E convertEntity(Entity entity) {
     validateEntityHasIdentifier(entity);
 
     return (E) entity;
