261ff297e782b4c45ca5429217a2691113282e40
George T. C. Lai <tsungchih.hd@gmail.com>
2025-07-06 15:23:16 +0800

    test: add unit tests for writing LiteralDTO as function arg of SerdesUtils
    apache/gravitino#5202
    
    Signed-off-by: George T. C. Lai <tsungchih.hd@gmail.com>

---
+++ clients/client-python/tests/unittests/dto/rel/test_serdes_utils.py
@@ -0,0 +1,60 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import unittest
+from enum import Enum
+from unittest.mock import patch
+
+from gravitino.api.types.types import Types
+from gravitino.dto.rel.expressions.json_serdes._helper.serdes_utils import SerdesUtils
+from gravitino.dto.rel.expressions.literal_dto import LiteralDTO
+
+
+class MockArgType(str, Enum):
+    INVALID_ARG_TYPE = "invalid_arg_type"
+
+
+class TestExpressionSerdesUtils(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls._literal_dto = (
+            LiteralDTO.builder()
+            .with_data_type(data_type=Types.StringType.get())
+            .with_value(value="test_string")
+            .build()
+        )
+
+    def test_write_function_arg_invalid_arg_type(self):
+        mock_dto = (
+            LiteralDTO.builder()
+            .with_data_type(data_type=Types.StringType.get())
+            .with_value(value="test")
+            .build()
+        )
+        with patch.object(
+            mock_dto, "arg_type", return_value=MockArgType.INVALID_ARG_TYPE
+        ):
+            self.assertRaises(ValueError, SerdesUtils.write_function_arg, arg=mock_dto)
+
+    def test_write_function_arg_literal_dto(self):
+        result = SerdesUtils.write_function_arg(arg=self._literal_dto)
+        expected_result = {
+            SerdesUtils.EXPRESSION_TYPE: self._literal_dto.arg_type().name.lower(),
+            SerdesUtils.DATA_TYPE: self._literal_dto.data_type().simple_string(),
+            SerdesUtils.LITERAL_VALUE: self._literal_dto.value(),
+        }
+        self.assertDictEqual(result, expected_result)
