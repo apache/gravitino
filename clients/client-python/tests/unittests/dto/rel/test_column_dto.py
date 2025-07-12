5cf083dd28d8f9e9d13a59a6214dec0aaeed33bb
George T. C. Lai <tsungchih.hd@gmail.com>
2025-07-12 11:30:28 +0800

    test: add unit tests for ColumnDTO serdes with default value literal
    apache/gravitino#5202
    
    Signed-off-by: George T. C. Lai <tsungchih.hd@gmail.com>

--- clients/client-python/tests/unittests/dto/rel/test_column_dto.py
+++ clients/client-python/tests/unittests/dto/rel/test_column_dto.py
@@ -17,12 +17,16 @@
 
 import json
 import unittest
+from itertools import product
 
 from gravitino.api.column import Column
 from gravitino.api.types.json_serdes import TypeSerdes
 from gravitino.api.types.json_serdes._helper.serdes_utils import SerdesUtils
 from gravitino.api.types.types import Types
 from gravitino.dto.rel.column_dto import ColumnDTO
+from gravitino.dto.rel.expressions.json_serdes.column_default_value_serdes import (
+    ColumnDefaultValueSerdes,
+)
 from gravitino.dto.rel.expressions.literal_dto import LiteralDTO
 from gravitino.exceptions.base import IllegalArgumentException
 
@@ -214,3 +218,44 @@ class TestColumnDTO(unittest.TestCase):
                 deserialized_column_dto.default_value(), Column.DEFAULT_VALUE_NOT_SET
             )
             self.assertEqual(serialized_json, deserialized_json)
+
+    def test_column_dto_serdes_with_default_value_literal(self):
+        expected_dict = {
+            "name": "",
+            "type": "",
+            "comment": "",
+            "defaultValue": None,
+            "nullable": False,
+            "autoIncrement": False,
+        }
+        for data_type, default_value_type in product(
+            self._supported_types, self._supported_types
+        ):
+            default_value = (
+                LiteralDTO.builder()
+                .with_data_type(default_value_type)
+                .with_value(default_value_type.simple_string())
+                .build()
+            )
+            column_dto = (
+                ColumnDTO.builder()
+                .with_name(name=str(data_type.name()))
+                .with_data_type(data_type=data_type)
+                .with_default_value(default_value=default_value)
+                .with_comment(comment=data_type.simple_string())
+                .build()
+            )
+            expected_dict["name"] = str(data_type.name())
+            expected_dict["type"] = TypeSerdes.serialize(data_type)
+            expected_dict["comment"] = data_type.simple_string()
+            expected_dict["defaultValue"] = ColumnDefaultValueSerdes.serialize(
+                value=default_value
+            )
+            expected_dict["nullable"] = True
+            expected_dict["autoIncrement"] = False
+
+            serialized_result = column_dto.to_json()
+            deserialized_dto = ColumnDTO.from_json(serialized_result)
+
+            self.assertDictEqual(json.loads(serialized_result), expected_dict)
+            self.assertEqual(deserialized_dto, column_dto)
