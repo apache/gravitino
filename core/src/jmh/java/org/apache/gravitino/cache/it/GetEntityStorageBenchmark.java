8cc977534f8af646766ab7d58f51568e70326f29
Lord of Abyss <p_lord@163.com>
2025-07-15 17:59:01 +0800

    [#7546] feat(*): Add JMH E2E Performance Test
    Add JMH E2E Performance Test.

---
+++ core/src/jmh/java/org/apache/gravitino/cache/it/GetEntityStorageBenchmark.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.gravitino.cache.it;
+
+import java.io.IOException;
+import org.apache.gravitino.Entity;
+import org.apache.gravitino.EntityStore;
+import org.apache.gravitino.HasIdentifier;
+import org.apache.gravitino.cache.BenchmarkHelper;
+import org.apache.gravitino.meta.ModelEntity;
+import org.openjdk.jmh.annotations.Benchmark;
+
+/**
+ * Benchmark for testing the performance of the {@code EntityStore.get()} method.
+ *
+ * <p>This benchmark randomly selects a preloaded entity and retrieves it from the {@link
+ * EntityStore} using its name identifier and type. It then validates the retrieved entity against
+ * the expected one.
+ *
+ * <p>The test dataset is initialized by {@link AbstractEntityStorageBenchmark}, and the benchmark
+ * is designed to simulate typical read-access patterns in metadata-intensive workloads.
+ *
+ * <p>Benchmark results reflect the efficiency and correctness of metadata retrieval under varying
+ * entity counts.
+ *
+ * @see org.apache.gravitino.EntityStore#get(org.apache.gravitino.NameIdentifier, Entity.EntityType,
+ *     Class)
+ */
+public class GetEntityStorageBenchmark<E extends Entity & HasIdentifier>
+    extends AbstractEntityStorageBenchmark {
+
+  /**
+   * Benchmark for getting an entity from the store.
+   *
+   * @return the entity from store.
+   */
+  @Benchmark
+  public Entity benchmarkGet() {
+    int idx = random.nextInt(entities.size());
+    Entity sampleEntity = (Entity) entities.get(idx);
+
+    try {
+      ModelEntity entityFromStore =
+          store.get(
+              BenchmarkHelper.getIdentFromEntity(sampleEntity),
+              sampleEntity.type(),
+              ModelEntity.class);
+      return validateEntity((Entity) entities.get(idx), entityFromStore);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
