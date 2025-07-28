3ecd6592d6248eb0c1bf59b5e7948f2e64271a2a
Jarvis <jarvis@apache.org>
2025-07-28 19:30:47 +0800

    [#3302][Sub-Task] StarRocks catalog Table/DB ops (#7738)
    ### What changes were proposed in this pull request?
    
    add StarRocks Catalog Implement
    
    ### Why are the changes needed?
    
    To support StarRocks Catalog.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No
    
    ### How was this patch tested?
    
    By E2E test, the test is in another pr
    
    https://github.com/apache/gravitino/pull/7792

--- catalogs/catalog-jdbc-starrocks/src/main/java/org/apache/gravitino/catalog/starrocks/converter/StarRocksColumnDefaultValueConverter.java
+++ catalogs/catalog-jdbc-starrocks/src/main/java/org/apache/gravitino/catalog/starrocks/converter/StarRocksColumnDefaultValueConverter.java
@@ -18,10 +18,29 @@
  */
 package org.apache.gravitino.catalog.starrocks.converter;
 
-import org.apache.commons.lang3.NotImplementedException;
+import static org.apache.gravitino.catalog.starrocks.converter.StarRocksTypeConverter.BIGINT;
+import static org.apache.gravitino.catalog.starrocks.converter.StarRocksTypeConverter.CHAR;
+import static org.apache.gravitino.catalog.starrocks.converter.StarRocksTypeConverter.DATE;
+import static org.apache.gravitino.catalog.starrocks.converter.StarRocksTypeConverter.DATETIME;
+import static org.apache.gravitino.catalog.starrocks.converter.StarRocksTypeConverter.DECIMAL;
+import static org.apache.gravitino.catalog.starrocks.converter.StarRocksTypeConverter.DOUBLE;
+import static org.apache.gravitino.catalog.starrocks.converter.StarRocksTypeConverter.FLOAT;
+import static org.apache.gravitino.catalog.starrocks.converter.StarRocksTypeConverter.INT;
+import static org.apache.gravitino.catalog.starrocks.converter.StarRocksTypeConverter.SMALLINT;
+import static org.apache.gravitino.catalog.starrocks.converter.StarRocksTypeConverter.TINYINT;
+import static org.apache.gravitino.catalog.starrocks.converter.StarRocksTypeConverter.VARCHAR;
+import static org.apache.gravitino.rel.Column.DEFAULT_VALUE_NOT_SET;
+import static org.apache.gravitino.rel.Column.DEFAULT_VALUE_OF_CURRENT_TIMESTAMP;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import org.apache.gravitino.catalog.jdbc.converter.JdbcColumnDefaultValueConverter;
 import org.apache.gravitino.catalog.jdbc.converter.JdbcTypeConverter;
 import org.apache.gravitino.rel.expressions.Expression;
+import org.apache.gravitino.rel.expressions.UnparsedExpression;
+import org.apache.gravitino.rel.expressions.literals.Literals;
+import org.apache.gravitino.rel.types.Decimal;
+import org.apache.gravitino.rel.types.Types;
 
 public class StarRocksColumnDefaultValueConverter extends JdbcColumnDefaultValueConverter {
 
@@ -31,6 +50,52 @@ public class StarRocksColumnDefaultValueConverter extends JdbcColumnDefaultValue
       String columnDefaultValue,
       boolean isExpression,
       boolean nullable) {
-    throw new NotImplementedException("To be implemented in the future");
+    if (columnDefaultValue == null) {
+      return nullable ? Literals.NULL : DEFAULT_VALUE_NOT_SET;
+    }
+
+    if (columnDefaultValue.equalsIgnoreCase(NULL)) {
+      return Literals.NULL;
+    }
+
+    if (isExpression) {
+      if (columnDefaultValue.equals(CURRENT_TIMESTAMP)) {
+        return DEFAULT_VALUE_OF_CURRENT_TIMESTAMP;
+      }
+      // The parsing of Doris expressions is complex, so we are not currently undertaking the
+      // parsing.
+      return UnparsedExpression.of(columnDefaultValue);
+    }
+
+    switch (columnType.getTypeName().toLowerCase()) {
+      case TINYINT:
+        return Literals.byteLiteral(Byte.valueOf(columnDefaultValue));
+      case SMALLINT:
+        return Literals.shortLiteral(Short.valueOf(columnDefaultValue));
+      case INT:
+        return Literals.integerLiteral(Integer.valueOf(columnDefaultValue));
+      case BIGINT:
+        return Literals.longLiteral(Long.valueOf(columnDefaultValue));
+      case FLOAT:
+        return Literals.floatLiteral(Float.valueOf(columnDefaultValue));
+      case DOUBLE:
+        return Literals.doubleLiteral(Double.valueOf(columnDefaultValue));
+      case DECIMAL:
+        return Literals.decimalLiteral(
+            Decimal.of(columnDefaultValue, columnType.getColumnSize(), columnType.getScale()));
+      case DATE:
+        return Literals.dateLiteral(LocalDate.parse(columnDefaultValue, DATE_TIME_FORMATTER));
+      case DATETIME:
+        return CURRENT_TIMESTAMP.equals(columnDefaultValue)
+            ? DEFAULT_VALUE_OF_CURRENT_TIMESTAMP
+            : Literals.timestampLiteral(
+                LocalDateTime.parse(columnDefaultValue, DATE_TIME_FORMATTER));
+      case VARCHAR:
+        return Literals.of(columnDefaultValue, Types.VarCharType.of(columnType.getColumnSize()));
+      case CHAR:
+        return Literals.of(columnDefaultValue, Types.FixedCharType.of(columnType.getColumnSize()));
+      default:
+        throw new IllegalArgumentException("Unknown data columnType for literal: " + columnType);
+    }
   }
 }
