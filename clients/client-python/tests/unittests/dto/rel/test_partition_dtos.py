0d382e437012223058beeda85594798b1a9b702f
George T. C. Lai <tsungchih.hd@gmail.com>
2025-08-02 15:17:05 +0800

    test: add unit tests for RangePartitionDTO
    apache/gravitino#5199
    
    Signed-off-by: George T. C. Lai <tsungchih.hd@gmail.com>

--- clients/client-python/tests/unittests/dto/rel/test_partition_dtos.py
+++ clients/client-python/tests/unittests/dto/rel/test_partition_dtos.py
@@ -5,6 +5,7 @@ from gravitino.dto.rel.expressions.literal_dto import LiteralDTO
 from gravitino.dto.rel.partitions.identity_partition_dto import IdentityPartitionDTO
 from gravitino.dto.rel.partitions.list_partition_dto import ListPartitionDTO
 from gravitino.dto.rel.partitions.partition_dto import PartitionDTO
+from gravitino.dto.rel.partitions.range_partition_dto import RangePartitionDTO
 
 
 class TestPartitionDTOs(unittest.TestCase):
@@ -52,9 +53,9 @@ class TestPartitionDTOs(unittest.TestCase):
         self.assertListEqual(dto.values(), values)
         self.assertDictEqual(dto.properties(), properties)
 
-        self.assertTrue(dto == dto)
         self.assertTrue(dto == similar_dto)
         self.assertFalse(dto == different_dto)
+        self.assertFalse(dto == "dummy_string")
 
         self.assertEqual(len(dtos), 2)
         self.assertEqual(dtos[dto], 2)
@@ -110,9 +111,61 @@ class TestPartitionDTOs(unittest.TestCase):
         self.assertListEqual(dto.lists(), lists)
         self.assertDictEqual(dto.properties(), properties)
 
-        self.assertTrue(dto == dto)
         self.assertTrue(dto == similar_dto)
         self.assertFalse(dto == different_dto)
+        self.assertFalse(dto == "dummy_string")
+
+        self.assertEqual(len(dtos), 2)
+        self.assertEqual(dtos[dto], 2)
+
+    def test_range_partition_dto(self):
+        partition_name = "p20250808"
+        properties = {}
+        upper = (
+            LiteralDTO.builder()
+            .with_data_type(data_type=Types.DateType.get())
+            .with_value(value="2025-08-08")
+            .build()
+        )
+        lower = (
+            LiteralDTO.builder()
+            .with_data_type(data_type=Types.NullType.get())
+            .with_value(value="null")
+            .build()
+        )
+        dto = RangePartitionDTO(
+            name=partition_name,
+            properties=properties,
+            upper=upper,
+            lower=lower,
+        )
+
+        similar_dto = RangePartitionDTO(
+            name=partition_name,
+            properties=properties,
+            upper=upper,
+            lower=lower,
+        )
+
+        different_dto = RangePartitionDTO(
+            name="different_partition",
+            properties=properties,
+            upper=upper,
+            lower=lower,
+        )
+
+        dtos = {dto: 1, similar_dto: 2, different_dto: 3}
+
+        self.assertIsInstance(dto, RangePartitionDTO)
+        self.assertIs(dto.type(), PartitionDTO.Type.RANGE)
+        self.assertEqual(dto.name(), partition_name)
+        self.assertEqual(dto.upper(), upper)
+        self.assertEqual(dto.lower(), lower)
+        self.assertDictEqual(dto.properties(), properties)
+
+        self.assertTrue(dto == similar_dto)
+        self.assertFalse(dto == different_dto)
+        self.assertFalse(dto == "dummy_string")
 
         self.assertEqual(len(dtos), 2)
         self.assertEqual(dtos[dto], 2)
