06d35ef3a709043ace077f23b1c014fbea69f9d4
hqbhoho <qbhan@lilith.com>
2025-08-02 15:46:33 +0800

    [#7900] fix(catalogs): Fix jdbc catalogs DateTimeType column load with default value

--- catalogs/catalog-jdbc-starrocks/src/main/java/org/apache/gravitino/catalog/starrocks/converter/StarRocksColumnDefaultValueConverter.java
+++ catalogs/catalog-jdbc-starrocks/src/main/java/org/apache/gravitino/catalog/starrocks/converter/StarRocksColumnDefaultValueConverter.java
@@ -84,7 +84,7 @@ public class StarRocksColumnDefaultValueConverter extends JdbcColumnDefaultValue
         return Literals.decimalLiteral(
             Decimal.of(columnDefaultValue, columnType.getColumnSize(), columnType.getScale()));
       case DATE:
-        return Literals.dateLiteral(LocalDate.parse(columnDefaultValue, DATE_TIME_FORMATTER));
+        return Literals.dateLiteral(LocalDate.parse(columnDefaultValue, DATE_FORMATTER));
       case DATETIME:
         return CURRENT_TIMESTAMP.equals(columnDefaultValue)
             ? DEFAULT_VALUE_OF_CURRENT_TIMESTAMP
