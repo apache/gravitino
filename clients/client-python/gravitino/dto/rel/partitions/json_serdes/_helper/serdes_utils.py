afcdaa74e13fe66809ed647311ae1df8bd7e1111
George T. C. Lai <tsungchih.hd@gmail.com>
2025-08-12 23:05:07 +0800

    feat: add class SerdesUtils for PartitionDTOs
    apache/gravitino#5199
    
    Signed-off-by: George T. C. Lai <tsungchih.hd@gmail.com>

---
+++ clients/client-python/gravitino/dto/rel/partitions/json_serdes/_helper/serdes_utils.py
@@ -0,0 +1,52 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from typing import Any, Dict, cast
+
+from gravitino.dto.rel.expressions.json_serdes._helper.serdes_utils import (
+    SerdesUtils as ExpressionsSerdesUtils,
+)
+from gravitino.dto.rel.partitions.identity_partition_dto import IdentityPartitionDTO
+from gravitino.dto.rel.partitions.partition_dto import PartitionDTO
+from gravitino.utils.serdes import SerdesUtilsBase
+
+
+class SerdesUtils(SerdesUtilsBase):
+    @classmethod
+    def write_partition(cls, value: PartitionDTO) -> Dict[str, Any]:
+        result = {
+            cls.PARTITION_TYPE: value.type().value,
+            cls.PARTITION_NAME: value.name(),
+        }
+        dto_data = {}
+        dto_type = value.type()
+
+        if dto_type not in PartitionDTO.Type:
+            raise IOError(f"Unknown partition type: {value.type()}")
+
+        if dto_type is PartitionDTO.Type.IDENTITY:
+            dto = cast(IdentityPartitionDTO, value)
+            dto_data = {
+                cls.FIELD_NAMES: dto.field_names(),
+                cls.IDENTITY_PARTITION_VALUES: [
+                    ExpressionsSerdesUtils.write_function_arg(arg=arg)
+                    for arg in dto.values()
+                ],
+            }
+
+        result.update(dto_data)
+        return result
