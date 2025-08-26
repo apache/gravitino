cc826de28bd011f1e4c5193072338ec8aeeaeddd
George T. C. Lai <tsungchih.hd@gmail.com>
2025-08-26 23:54:58 +0800

    feat: add list partitioning DTO serdes
    apache/gravitino#5199
    
    Signed-off-by: George T. C. Lai <tsungchih.hd@gmail.com>

--- clients/client-python/gravitino/dto/rel/partitioning/json_serdes/partitioning_serdes.py
+++ clients/client-python/gravitino/dto/rel/partitioning/json_serdes/partitioning_serdes.py
@@ -28,6 +28,7 @@ from gravitino.dto.rel.partitioning.hour_partitioning_dto import HourPartitionin
 from gravitino.dto.rel.partitioning.identity_partitioning_dto import (
     IdentityPartitioningDTO,
 )
+from gravitino.dto.rel.partitioning.list_partitioning_dto import ListPartitioningDTO
 from gravitino.dto.rel.partitioning.month_partitioning_dto import MonthPartitioningDTO
 from gravitino.dto.rel.partitioning.partitioning import (
     Partitioning,
@@ -37,6 +38,8 @@ from gravitino.dto.rel.partitioning.truncate_partitioning_dto import (
     TruncatePartitioningDTO,
 )
 from gravitino.dto.rel.partitioning.year_partitioning_dto import YearPartitioningDTO
+from gravitino.dto.rel.partitions.json_serdes._helper.serdes_utils import SerdesUtils
+from gravitino.dto.rel.partitions.list_partition_dto import ListPartitionDTO
 from gravitino.utils.precondition import Precondition
 from gravitino.utils.serdes import SerdesUtilsBase
 
@@ -88,6 +91,16 @@ class PartitioningSerdes(SerdesUtilsBase, JsonSerializable[Partitioning]):
                 cls.WIDTH: partitioning.width(),
                 cls.FIELD_NAME: partitioning.field_name(),
             }
+        if strategy is Partitioning.Strategy.LIST:
+            partitioning = cast(ListPartitioningDTO, data_type)
+            return {
+                **result,
+                cls.FIELD_NAMES: partitioning.field_names(),
+                cls.ASSIGNMENTS_NAME: [
+                    SerdesUtils.write_partition(list_partition_dto)
+                    for list_partition_dto in partitioning.assignments()
+                ],
+            }
 
         raise IOError(f"Unknown partitioning strategy: {strategy}")
 
@@ -119,4 +132,21 @@ class PartitioningSerdes(SerdesUtilsBase, JsonSerializable[Partitioning]):
                 int(data[cls.WIDTH]),
                 data.get(cls.FIELD_NAME, []),
             )
+        if strategy is Partitioning.Strategy.LIST:
+            field_names = data[cls.FIELD_NAMES]
+            assignments_data = data.get(cls.ASSIGNMENTS_NAME, [])
+            Precondition.check_argument(
+                isinstance(assignments_data, list),
+                f"Cannot parse list partitioning from non-array assignments: {assignments_data}",
+            )
+            assignments = []
+            for assignment in assignments_data:
+                partition_dto = SerdesUtils.read_partition(assignment)
+                Precondition.check_argument(
+                    isinstance(partition_dto, ListPartitionDTO),
+                    f"Cannot parse list partitioning from non-list assignment: {assignment}",
+                )
+                assignments.append(partition_dto)
+            return ListPartitioningDTO(field_names, assignments)
+
         raise IOError(f"Unknown partitioning strategy: {data[cls.STRATEGY]}")
