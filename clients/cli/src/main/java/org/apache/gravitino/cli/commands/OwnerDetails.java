ac4347413fdea82dbf5b7c539ecd8f471c4d3caa
Aamir017 <skaamir2005@gmail.com>
2025-08-28 12:18:22 +0530

    Fixed the issue

--- clients/cli/src/main/java/org/apache/gravitino/cli/commands/OwnerDetails.java
+++ clients/cli/src/main/java/org/apache/gravitino/cli/commands/OwnerDetails.java
@@ -1,20 +1,21 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
-
 package org.apache.gravitino.cli.commands;
 
 import java.util.Optional;
@@ -34,6 +35,14 @@ public class OwnerDetails extends Command {
   protected final String entity;
   protected final MetadataObject.Type entityType;
 
+   /**
+   * Displays the owner of an entity.
+   *
+   * @param context The command context.
+   * @param metalake The name of the metalake.
+   * @param entity The name of the entity.
+   * @param entityType The type entity.
+   */
   public OwnerDetails(CommandContext context, String metalake, String entity, String entityType) {
     super(context);
     this.metalake = metalake;
@@ -54,6 +63,7 @@ public class OwnerDetails extends Command {
     }
   }
 
+  /** Displays the owner of an entity. */
   @Override
   public void handle() {
     if (entityType == null) {
