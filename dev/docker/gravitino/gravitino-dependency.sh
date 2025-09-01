cfed23566b38d27c42de4ebaaaf3a3bd439229fc
fanng <xiaojing@datastrato.com>
2025-09-01 20:00:09 +0800

    fix comment

--- dev/docker/gravitino/gravitino-dependency.sh
+++ dev/docker/gravitino/gravitino-dependency.sh
@@ -114,7 +114,6 @@ find ${gravitino_home}/bundles/aliyun-bundle/build/libs/ -name 'gravitino-aliyun
 
 download_gcs_connector
 
-
 # Keeping the container running at all times
 cat <<EOF >> "${gravitino_dir}/packages/gravitino/bin/gravitino.sh"
 
