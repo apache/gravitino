d184eee45e7bf14966e65ba8c9b0083a2eeddb15
qbhan <qbhan@lilith.com>
2025-09-02 16:49:43 +0800

    [#6403] improvement(trino-connector): Support SQL UPDATE/DELETE with jdbc catalog (#8188)
    ### What changes were proposed in this pull request?
    
    Support SQL UPDATE/DELETE with MySQL/PostgreSQL
    
    ### Why are the changes needed?
    
    Fix: #6403
    
    ### Does this PR introduce _any_ user-facing change?
    
    No
    
    ### How was this patch tested?
    
    local tests
    
    Co-authored-by: Yuhui <hui@datastrato.com>

---
+++ trino-connector/integration-test/src/test/resources/trino-ci-testset/testsets/jdbc-mysql/00013_delete_table.sql
@@ -0,0 +1,26 @@
+CREATE SCHEMA gt_mysql.gt_db1;
+
+CREATE TABLE gt_mysql.gt_db1.tb01 (
+    name varchar(200),
+    salary int
+);
+
+INSERT INTO gt_mysql.gt_db1.tb01(name, salary) VALUES ('bob', 14), ('tom', 12), ('nancy', 15), ('hoho', 16);
+
+SELECT * FROM gt_mysql.gt_db1.tb01 ORDER BY name;
+
+DELETE FROM gt_mysql.gt_db1.tb01 WHERE salary=14;
+
+SELECT * FROM gt_mysql.gt_db1.tb01 ORDER BY name;
+
+DELETE FROM gt_mysql.gt_db1.tb01 WHERE salary<16;
+
+SELECT * FROM gt_mysql.gt_db1.tb01 ORDER BY name;
+
+DELETE FROM gt_mysql.gt_db1.tb01;
+
+SELECT count(*) FROM gt_mysql.gt_db1.tb01;
+
+DROP TABLE gt_mysql.gt_db1.tb01;
+
+DROP SCHEMA gt_mysql.gt_db1;
