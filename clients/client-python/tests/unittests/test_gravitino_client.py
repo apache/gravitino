2bda4523bf6499695627e261b81e335a9efd24c2
Jerry Shao <jshao@apache.org>
2025-09-02 16:25:49 +0800

    Fix the ut

--- clients/client-python/tests/unittests/test_gravitino_client.py
+++ clients/client-python/tests/unittests/test_gravitino_client.py
@@ -34,7 +34,7 @@ class TestMetalake(unittest.TestCase):
             request_headers=expected_headers,
         )
         self.assertEqual(
-            expected_headers, gravitino_admin_client._rest_client.request_headers
+            expected_headers["k1"], gravitino_admin_client._rest_client.request_headers["k1"]
         )
 
         gravitino_client = GravitinoClient(
@@ -43,7 +43,7 @@ class TestMetalake(unittest.TestCase):
             request_headers=expected_headers,
         )
         self.assertEqual(
-            expected_headers, gravitino_client._rest_client.request_headers
+            expected_headers["k1"], gravitino_client._rest_client.request_headers["k1"]
         )
 
     def test_gravitino_client_timeout(self, *mock_methods):
