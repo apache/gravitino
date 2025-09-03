e0c3a687205b3f96f6a6bb116fb48f118a00fa36
Eipi-Tong <3378732929@qq.com>
2025-09-02 20:01:19 -0500

    Fix dorisUtils, set default bucket group to be 1 for "Hash(field) BUCKETS" clause

--- catalogs/catalog-jdbc-doris/src/test/java/org/apache/gravitino/catalog/doris/utils/TestDorisUtils.java
+++ catalogs/catalog-jdbc-doris/src/test/java/org/apache/gravitino/catalog/doris/utils/TestDorisUtils.java
@@ -184,19 +184,11 @@ public class TestDorisUtils {
         "CREATE TABLE `testTable` (\n`col1` date NOT NULL\n) ENGINE=OLAP\n PARTITION BY RANGE(`col1`)\n()\n DISTRIBUTED BY HASH(`col1`) BUCKETS AUTO";
     Distribution distribution2 = DorisUtils.extractDistributionInfoFromSql(createTableSqlWithAuto);
     assertEquals(distribution2.number(), -1);
-  }
 
-  @Test
-  public void testDistributionWithoutBucketClauseDefaultsToOne() {
-    String createTableSql =
+    String createTableSqlWithoutBuckets =
         "CREATE TABLE `testTable` (\n`col1` date NOT NULL\n) ENGINE=OLAP\n PARTITION BY RANGE(`col1`)\n()\n DISTRIBUTED BY HASH(`col1`)";
-    Distribution distribution = DorisUtils.extractDistributionInfoFromSql(createTableSql);
-    assertEquals(distribution.number(), 1);
-
-    String createTableSqlWithEmptyBuckets =
-        "CREATE TABLE `testTable` (\n`col1` date NOT NULL\n) ENGINE=OLAP\n PARTITION BY RANGE(`col1`)\n()\n DISTRIBUTED BY HASH(`col1`) BUCKETS";
-    Distribution distribution2 =
-        DorisUtils.extractDistributionInfoFromSql(createTableSqlWithEmptyBuckets);
-    assertEquals(distribution2.number(), 1);
+    Distribution distribution3 =
+        DorisUtils.extractDistributionInfoFromSql(createTableSqlWithoutBuckets);
+    assertEquals(distribution3.number(), 1);
   }
 }
