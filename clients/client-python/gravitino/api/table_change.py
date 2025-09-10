d9317b84daf48b5a2b92db736556bcfcc593115a
George T. C. Lai <tsungchih.hd@gmail.com>
2025-09-10 22:39:37 +0800

    feat: add add column
    apache/gravitino#5198
    
    Signed-off-by: George T. C. Lai <tsungchih.hd@gmail.com>

--- clients/client-python/gravitino/api/table_change.py
+++ clients/client-python/gravitino/api/table_change.py
@@ -15,11 +15,15 @@
 # specific language governing permissions and limitations
 # under the License.
 
-from abc import ABC
+from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
-from typing import final
+from typing import Optional, final
 
 from dataclasses_json import config
+
+from gravitino.api.column import Column
+from gravitino.api.expressions.expression import Expression
+from gravitino.api.types.type import Type
 
 
 class TableChange(ABC):
@@ -81,6 +85,47 @@ class TableChange(ABC):
             RemoveProperty: A `TableChange` for the addition.
         """
         return TableChange.RemoveProperty(property_name)
+
+    @staticmethod
+    def add_column(
+        field_name: list[str],
+        data_type: Type,
+        comment: Optional[str] = None,
+        position: Optional["TableChange.ColumnPosition"] = None,
+        nullable: bool = True,
+        auto_increment: bool = False,
+        default_value: Optional[Expression] = None,
+    ) -> "AddColumn":
+        """Create a `TableChange` for adding a column.
+
+        Args:
+            field_name (list[str]):
+                Field name of the new column.
+            data_type (Type):
+                The new column's data type.
+            comment (Optional[str]):
+                The new field's comment string, defaults to `None`.
+            position (Optional[TableChange.ColumnPosition]):
+                The new column's position, defaults to `None`.
+            nullable (bool):
+                The new column's nullable.
+            auto_increment (bool):
+                The new column's autoIncrement.
+            default_value (Expression):
+                The new column's default value.
+
+        Returns:
+            AddColumn: A `TableChange` for the addition.
+        """
+        return AddColumn(
+            field_name,
+            data_type,
+            comment,
+            position,
+            nullable,
+            auto_increment,
+            default_value,
+        )
 
     @final
     @dataclass(frozen=True)
@@ -207,6 +252,21 @@ class TableChange(ABC):
             """
             return Default()
 
+    class ColumnChange(ABC):
+        """The interface for all column changes.
+
+        Column changes are used to modify the schema of a table.
+        """
+
+        @abstractmethod
+        def field_name(self) -> list[str]:
+            """Retrieves the field name of the column to be modified.
+
+            Returns:
+                list[str]:
+                    A list of strings representing the field name.
+            """
+
 
 @final
 @dataclass(frozen=True)
@@ -257,3 +317,111 @@ class Default(TableChange.ColumnPosition):
 
     def __str__(self):
         return "DEFAULT"
+
+
+@final
+@dataclass(frozen=True)
+class AddColumn(TableChange.ColumnChange):
+    """A TableChange to add a field.
+
+    The implementation may need to back-fill all the existing data to add this new column,
+    or remember the column default value specified here and let the reader fill the column value
+    when reading existing data that do not have this new column.
+
+    If the field already exists, the change must result in an `IllegalArgumentException`.
+    If the new field is nested and its parent does not exist or is not a struct, the change must
+    result in an `IllegalArgumentException`.
+    """
+
+    _field_name: list[str] = field(metadata=config(field_name="field_name"))
+    _data_type: Type = field(metadata=config(field_name="data_type"))
+    _comment: Optional[str] = field(default=None, metadata=config(field_name="comment"))
+    _position: Optional[TableChange.ColumnPosition] = field(
+        default=None, metadata=config(field_name="position")
+    )
+    _nullable: bool = field(default=True, metadata=config(field_name="nullable"))
+    _auto_increment: bool = field(
+        default=False, metadata=config(field_name="auto_increment")
+    )
+    _default_value: Optional[Expression] = field(
+        default=None,
+        metadata=config(field_name="default_value"),
+    )
+
+    def get_field_name(self) -> list[str]:
+        """Retrieves the field name of the new column.
+
+        Returns:
+            list[str]: The field name of the new column.
+        """
+        return self._field_name
+
+    def get_data_type(self) -> Type:
+        """Retrieves the data type of the new column.
+
+        Returns:
+            Type: The data type of the new column.
+        """
+        return self._data_type
+
+    def get_comment(self) -> Optional[str]:
+        """Retrieves the comment for the new column.
+
+        Returns:
+            str: comment for the new column.
+        """
+        return self._comment
+
+    def get_position(self) -> Optional[TableChange.ColumnPosition]:
+        """Retrieves the position where the new column should be added.
+
+        Returns:
+            TableChange.ColumnPosition:
+                The position of the column.
+        """
+        return self._position
+
+    def is_nullable(self) -> bool:
+        """Checks if the new column is nullable.
+
+        Returns:
+            bool: `True` if the column is nullable; `False` otherwise.
+        """
+        return self._nullable
+
+    def is_auto_increment(self) -> bool:
+        """Checks if the new column is auto-increment.
+
+        Returns:
+            bool: `True` if the column is auto-increment; `False` otherwise.
+        """
+        return self._auto_increment
+
+    def get_default_value(self) -> Expression:
+        """Retrieves the default value of the new column.
+
+        Returns:
+            Expression: The default value of the column.
+        """
+        return (
+            self._default_value if self._default_value else Column.DEFAULT_VALUE_NOT_SET
+        )
+
+    def field_name(self) -> list[str]:
+        return self._field_name
+
+    def __hash__(self) -> int:
+        base_tuple = (
+            self._data_type,
+            self._comment,
+            self._position,
+            self._nullable,
+            self._auto_increment,
+            (
+                tuple(self._default_value)
+                if self._default_value == Column.DEFAULT_VALUE_NOT_SET
+                else self._default_value
+            ),
+        )
+        result = hash(base_tuple)
+        return 31 * result + hash(tuple(self._field_name))
