710e22d6d83a4edbc41b4b560154813ca5e7af1e
fanng <xiaojing@datastrato.com>
2025-09-10 20:31:14 +0800

    remove dep

--- spark-connector/v3.3/spark/build.gradle.kts
+++ spark-connector/v3.3/spark/build.gradle.kts
@@ -37,9 +37,24 @@ val scalaJava8CompatVersion: String = libs.versions.scala.java.compat.get()
 val scalaCollectionCompatVersion: String = libs.versions.scala.collection.compat.get()
 val artifactName = "${rootProject.name}-spark-${sparkMajorVersion}_$scalaVersion"
 
-if (hasProperty("configureSparkConnectorExcludes")) {
-  val configureFunc = properties["configureSparkConnectorExcludes"] as? (Project) -> Unit
-  configureFunc?.invoke(project)
+val excludedPackage = listOf("org/apache/gravitino/spark/connector/paimon/**", "org/apache/gravitino/spark/connector/integration/test/paimon/**")
+sourceSets {
+  main {
+    java {
+      srcDirs("src/main/java")
+      if (scalaVersion != "2.12") {
+        exclude(excludedPackage)
+      }
+    }
+  }
+  test {
+    java {
+      srcDirs("src/test/java")
+      if (scalaVersion != "2.12") {
+        exclude(excludedPackage)
+      }
+    }
+  }
 }
 
 dependencies {
