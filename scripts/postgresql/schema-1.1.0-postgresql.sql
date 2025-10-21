c0e40ecc8ad520f873a1815cdac4a3b53b1dcc51
yuqi <yuqi@datastrato.com>
2025-10-21 20:41:51 +0800

    fix

--- scripts/postgresql/schema-1.1.0-postgresql.sql
+++ scripts/postgresql/schema-1.1.0-postgresql.sql
@@ -774,24 +774,24 @@ CREATE TABLE IF NOT EXISTS table_version_info (
     location        VARCHAR(512) NOT NULL,
     "external"      BOOLEAN NOT NULL DEFAULT FALSE,
     properties      TEXT,
-    partition_info  TEXT,
-    distribution_info TEXT,
-    sort_order_info TEXT,
-    index_info      TEXT,
-    table_comment   TEXT,
+    partitions  TEXT,
+    distribution TEXT,
+    sort_orders TEXT,
+    indexes      TEXT,
+    "comment"   TEXT,
     current_version BIGINT,
     deleted_at      BIGINT DEFAULT 0
-    );
-COMMENT ON TABLE table_version_info IS 'table detail information including format, location, properties, partition, distribution, sort order, index and so on';
+);
+COMMENT ON TABLE table_version_info                  IS 'table detail information including format, location, properties, partition, distribution, sort order, index and so on';
 COMMENT ON COLUMN table_version_info.table_id        IS 'table id';
 COMMENT ON COLUMN table_version_info.format          IS 'table format, such as Lance, Iceberg and so on';
 COMMENT ON COLUMN table_version_info.location        IS 'table storage location';
 COMMENT ON COLUMN table_version_info."external"      IS 'whether the table is external table';
 COMMENT ON COLUMN table_version_info.properties      IS 'table properties';
-COMMENT ON COLUMN table_version_info.partition_info  IS 'table partition info';
-COMMENT on COLUMN table_version_info.distribution_info IS 'table distribution info';
-COMMENT ON COLUMN table_version_info.sort_order_info IS 'table sort order info';
-COMMENT ON COLUMN table_version_info.index_info      IS 'table index info';
-COMMENT ON COLUMN table_version_info.table_comment   IS 'table comment';
+COMMENT ON COLUMN table_version_info.partitions      IS 'table partition info';
+COMMENT on COLUMN table_version_info.distribution    IS 'table distribution info';
+COMMENT ON COLUMN table_version_info.sort_orders     IS 'table sort order info';
+COMMENT ON COLUMN table_version_info.indexes         IS 'table index info';
+COMMENT ON COLUMN table_version_info."comment"       IS 'table comment';
 COMMENT ON COLUMN table_version_info.current_version IS 'table current version';
 COMMENT ON COLUMN table_version_info.deleted_at      IS 'table deletion timestamp, 0 means not deleted';
