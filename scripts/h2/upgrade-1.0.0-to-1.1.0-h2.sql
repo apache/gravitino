4dbcf9a8212d617019367cc7a41e04f45f38f3ce
yuqi <yuqi@datastrato.com>
2025-10-22 20:48:20 +0800

    Change field `partitions` to `partitioning`.

--- scripts/h2/upgrade-1.0.0-to-1.1.0-h2.sql
+++ scripts/h2/upgrade-1.0.0-to-1.1.0-h2.sql
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS `table_version_info` (
     `table_id`        BIGINT(20) UNSIGNED NOT NULL COMMENT 'table id',
     `format`          VARCHAR(64) NOT NULL COMMENT 'table format, such as Lance, Iceberg and so on',
     `properties`      CLOB DEFAULT NULL COMMENT 'table properties',
-    `partitions`  MEDIUMTEXT DEFAULT NULL COMMENT 'table partition info',
+    `partitioning`  MEDIUMTEXT DEFAULT NULL COMMENT 'table partition info',
     `distribution` MEDIUMTEXT DEFAULT NULL COMMENT 'table distribution info',
     `sort_orders` MEDIUMTEXT DEFAULT NULL COMMENT 'table sort order info',
     `indexes` MEDIUMTEXT DEFAULT NULL COMMENT 'table index info',
--- scripts/postgresql/schema-1.1.0-postgresql.sql
+++ scripts/postgresql/schema-1.1.0-postgresql.sql
@@ -772,7 +772,7 @@ CREATE TABLE IF NOT EXISTS table_version_info (
     table_id        BIGINT PRIMARY KEY,
     format          VARCHAR(64) NOT NULL,
     properties      TEXT,
-    partitions  TEXT,
+    partitioning  TEXT,
     distribution TEXT,
     sort_orders TEXT,
     indexes      TEXT,
@@ -785,7 +785,7 @@ COMMENT ON TABLE table_version_info                  IS 'table detail informatio
 COMMENT ON COLUMN table_version_info.table_id        IS 'table id';
 COMMENT ON COLUMN table_version_info.format          IS 'table format, such as Lance, Iceberg and so on';
 COMMENT ON COLUMN table_version_info.properties      IS 'table properties';
-COMMENT ON COLUMN table_version_info.partitions      IS 'table partition info';
+COMMENT ON COLUMN table_version_info.partitioning      IS 'table partition info';
 COMMENT on COLUMN table_version_info.distribution    IS 'table distribution info';
 COMMENT ON COLUMN table_version_info.sort_orders     IS 'table sort order info';
 COMMENT ON COLUMN table_version_info.indexes         IS 'table index info';
