c00004794240369de9e5cb7f35cd88d67f05918c
mchades <liminghuang@datastrato.com>
2025-10-23 16:39:34 +0800

    add lance-rest-server framework and support gravitino ns ops

--- lance/lance-rest-server/build.gradle.kts
+++ lance/lance-rest-server/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
   implementation(project(":server-common")) {
     exclude("*")
   }
+
   implementation(project(":lance:lance-common"))
 
   implementation(libs.bundles.jetty)
@@ -43,6 +44,7 @@ dependencies {
   implementation(libs.bundles.log4j)
   implementation(libs.bundles.metrics)
   implementation(libs.bundles.prometheus)
+  implementation(libs.lance.namespace.core)
   implementation(libs.metrics.jersey2)
   implementation(libs.guava)
   implementation(libs.jackson.annotations)
