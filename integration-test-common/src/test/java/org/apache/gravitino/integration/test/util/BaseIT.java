9b47908a4653645a6ab454570c8443aace536d6e
mchades <liminghuang@datastrato.com>
2025-11-04 00:34:40 +0800

    fix dep

--- integration-test-common/src/test/java/org/apache/gravitino/integration/test/util/BaseIT.java
+++ integration-test-common/src/test/java/org/apache/gravitino/integration/test/util/BaseIT.java
@@ -351,10 +351,13 @@ public class BaseIT {
       miniGravitino.start();
       serverConfig = miniGravitino.getServerConfig();
     } else {
-      customConfigs.put(
-          LANCE_CONFIG_PREFIX + METALAKE_NAME.getKey(),
-          GravitinoITUtils.genRandomName("LanceRESTService_metalake"));
-      rewriteGravitinoServerConfig();
+      if (!ignoreLanceAuxRestService) {
+        customConfigs.put(
+            LANCE_CONFIG_PREFIX + METALAKE_NAME.getKey(),
+            GravitinoITUtils.genRandomName("LanceRESTService_metalake"));
+        rewriteGravitinoServerConfig();
+      }
+
       serverConfig.loadFromFile(GravitinoServer.CONF_FILE);
 
       setupJdbcDrivers();
