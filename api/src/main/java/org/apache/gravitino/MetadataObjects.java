7944f4ff92006ed49ed609276150ee376ea67670
yangyang zhong <35210666+hdygxsj@users.noreply.github.com>
2025-11-12 15:01:51 +0800

    [#9036] feat(authz): add document and api for tag access control (#9035)
    ### What changes were proposed in this pull request?
    
    add document for tag access control
    
    ### Why are the changes needed?
    
    Fix: #9036
    
    ### Does this PR introduce _any_ user-facing change?
    
    None
    
    ### How was this patch tested?
    
    org.apache.gravitino.authorization.TestSecurableObjects

--- api/src/main/java/org/apache/gravitino/MetadataObjects.java
+++ api/src/main/java/org/apache/gravitino/MetadataObjects.java
@@ -73,8 +73,9 @@ public class MetadataObjects {
         names.size() != 1
             || type == MetadataObject.Type.CATALOG
             || type == MetadataObject.Type.METALAKE
-            || type == MetadataObject.Type.ROLE,
-        "If the length of names is 1, it must be the CATALOG, METALAKE, or ROLE type");
+            || type == MetadataObject.Type.ROLE
+            || type == MetadataObject.Type.TAG,
+        "If the length of names is 1, it must be the CATALOG, METALAKE,TAG, or ROLE type");
 
     Preconditions.checkArgument(
         names.size() != 2 || type == MetadataObject.Type.SCHEMA,
@@ -155,6 +156,9 @@ public class MetadataObjects {
     List<String> parts = DOT_SPLITTER.splitToList(fullName);
     if (type == MetadataObject.Type.ROLE) {
       return MetadataObjects.of(Collections.singletonList(fullName), MetadataObject.Type.ROLE);
+    }
+    if (type == MetadataObject.Type.TAG) {
+      return MetadataObjects.of(Collections.singletonList(fullName), MetadataObject.Type.TAG);
     }
 
     return MetadataObjects.of(parts, type);
