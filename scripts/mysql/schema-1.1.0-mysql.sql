b9f05e38466e74ada855cc21a47b4740344809ce
youngyjd <youngyjd@gmail.com>
2025-11-13 22:03:52 +0000

    sql scripts license header

--- scripts/mysql/schema-1.1.0-mysql.sql
+++ scripts/mysql/schema-1.1.0-mysql.sql
@@ -1,22 +1,3 @@
-/*
- *  Licensed to the Apache Software Foundation (ASF) under one
- *  or more contributor license agreements.  See the NOTICE file
- *  distributed with this work for additional information
- *  regarding copyright ownership.  The ASF licenses this file
- *  to you under the Apache License, Version 2.0 (the
- *  "License"); you may not use this file except in compliance
- *  with the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
- */
-
 --
 -- Licensed to the Apache Software Foundation (ASF) under one
 -- or more contributor license agreements.  See the NOTICE file--
@@ -454,4 +435,3 @@ CREATE TABLE IF NOT EXISTS `table_version_info` (
      PRIMARY KEY (table_id),
      UNIQUE KEY `uk_table_id_deleted_at` (`table_id`, `deleted_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT 'table detail information including format, location, properties, partition, distribution, sort order, index and so on';
-
