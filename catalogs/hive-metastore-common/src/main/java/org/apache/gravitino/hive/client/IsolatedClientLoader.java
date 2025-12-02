5c1349bf92a36220be50afab059f2511049be393
yuhui <hui@datastrato.com>
2025-12-02 22:11:43 +0800

    Fix

--- catalogs/hive-metastore-common/src/main/java/org/apache/gravitino/hive/client/IsolatedClientLoader.java
+++ catalogs/hive-metastore-common/src/main/java/org/apache/gravitino/hive/client/IsolatedClientLoader.java
@@ -32,8 +32,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.stream.Collectors;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.gravitino.exceptions.GravitinoRuntimeException;
-import org.datanucleus.util.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
