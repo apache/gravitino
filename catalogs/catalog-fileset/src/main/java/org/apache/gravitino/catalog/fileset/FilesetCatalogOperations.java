5821c282b0e1daccb970a6c9ffea9a20c70f855f
yuqi <yuqi@datastrato.com>
2025-12-04 22:18:41 +0800

    add test

--- catalogs/catalog-fileset/src/main/java/org/apache/gravitino/catalog/fileset/FilesetCatalogOperations.java
+++ catalogs/catalog-fileset/src/main/java/org/apache/gravitino/catalog/fileset/FilesetCatalogOperations.java
@@ -142,8 +142,10 @@ public class FilesetCatalogOperations extends ManagedSchemaOperations
 
   private final ThreadPoolExecutor fileSystemExecutor =
       new ThreadPoolExecutor(
-          Math.max(2, Runtime.getRuntime().availableProcessors() * 2),
-          Math.max(2, Runtime.getRuntime().availableProcessors() * 2),
+          // Core pool size max is 12
+          Math.min(Math.max(2, Runtime.getRuntime().availableProcessors()), 12),
+          // The Maximum pool size max is 24
+          Math.min(Math.max(2, Runtime.getRuntime().availableProcessors() * 2), 24),
           5L,
           TimeUnit.SECONDS,
           new ArrayBlockingQueue<>(1000),
