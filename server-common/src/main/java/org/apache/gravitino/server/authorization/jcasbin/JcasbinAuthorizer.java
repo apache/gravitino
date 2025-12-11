3989c3a4d4ea5fbb660fd2cf52a62b7026e549ca
yangyang zhong <35210666+hdygxsj@users.noreply.github.com>
2025-12-11 11:27:31 +0800

    [#9291] improvement(authz): Add comment explaining why deny privileges are injected into allowEnforcer (#9449)
    ### What changes were proposed in this pull request?
    
    Add comment explaining why deny privileges are injected into
    allowEnforcer
    
    ### Why are the changes needed?
    
    Fix: #9291 
    
    ### Does this PR introduce _any_ user-facing change?
    
    None
    
    ### How was this patch tested?
    
    None

--- server-common/src/main/java/org/apache/gravitino/server/authorization/jcasbin/JcasbinAuthorizer.java
+++ server-common/src/main/java/org/apache/gravitino/server/authorization/jcasbin/JcasbinAuthorizer.java
@@ -535,6 +535,14 @@ public class JcasbinAuthorizer implements GravitinoAuthorizer {
                   .toUpperCase(java.util.Locale.ROOT),
               AuthConstants.ALLOW);
         }
+        // Since different roles of a user may simultaneously hold both "allow" and "deny"
+        // permissions
+        // for the same privilege on a given MetadataObject, the allowEnforcer must also incorporate
+        // the "deny" privilege to ensure that the authorize method correctly returns false in such
+        // cases. For example, if role1 has an "allow" privilege for SELECT_TABLE on table1, while
+        // role2 has a "deny" privilege for the same action on table1, then a user assigned both
+        // roles should receive a false result when calling the authorize method.
+
         allowEnforcer.addPolicy(
             String.valueOf(roleEntity.id()),
             securableObject.type().name(),
