ad2b9b3514ca86b9f2b5816d61d5220e841938c0
Mini Yu <yuqi@datastrato.com>
2025-12-25 18:51:57 +0800

    [#9474] improvement(server): Reduce the number of calling `catalogInuse` (#9475)
    ### What changes were proposed in this pull request?
    
    User proxy to reduce the number of calling `catalogInuse`.
    
    ### Why are the changes needed?
    
    To improve API performance.
    
    Fix: #9474 
    
    ### Does this PR introduce _any_ user-facing change?
    
    N/A.
    
    ### How was this patch tested?
    
    Existing test.

--- core/src/main/java/org/apache/gravitino/GravitinoEnv.java
+++ core/src/main/java/org/apache/gravitino/GravitinoEnv.java
@@ -19,6 +19,7 @@
 package org.apache.gravitino;
 
 import com.google.common.base.Preconditions;
+import java.lang.reflect.Proxy;
 import org.apache.gravitino.audit.AuditLogManager;
 import org.apache.gravitino.authorization.AccessControlDispatcher;
 import org.apache.gravitino.authorization.AccessControlManager;
@@ -37,6 +38,7 @@ import org.apache.gravitino.catalog.FilesetOperationDispatcher;
 import org.apache.gravitino.catalog.ModelDispatcher;
 import org.apache.gravitino.catalog.ModelNormalizeDispatcher;
 import org.apache.gravitino.catalog.ModelOperationDispatcher;
+import org.apache.gravitino.catalog.OperationDispatcherInterceptor;
 import org.apache.gravitino.catalog.PartitionDispatcher;
 import org.apache.gravitino.catalog.PartitionNormalizeDispatcher;
 import org.apache.gravitino.catalog.PartitionOperationDispatcher;
@@ -537,14 +539,28 @@ public class GravitinoEnv {
 
     SchemaOperationDispatcher schemaOperationDispatcher =
         new SchemaOperationDispatcher(catalogManager, entityStore, idGenerator);
-    SchemaHookDispatcher schemaHookDispatcher = new SchemaHookDispatcher(schemaOperationDispatcher);
+    SchemaDispatcher schemaDispatcherProxy =
+        (SchemaDispatcher)
+            Proxy.newProxyInstance(
+                SchemaDispatcher.class.getClassLoader(),
+                new Class[] {SchemaDispatcher.class},
+                new OperationDispatcherInterceptor(
+                    schemaOperationDispatcher, catalogManager, entityStore));
+    SchemaHookDispatcher schemaHookDispatcher = new SchemaHookDispatcher(schemaDispatcherProxy);
     SchemaNormalizeDispatcher schemaNormalizeDispatcher =
         new SchemaNormalizeDispatcher(schemaHookDispatcher, catalogManager);
     this.schemaDispatcher = new SchemaEventDispatcher(eventBus, schemaNormalizeDispatcher);
 
     TableOperationDispatcher tableOperationDispatcher =
         new TableOperationDispatcher(catalogManager, entityStore, idGenerator);
-    TableHookDispatcher tableHookDispatcher = new TableHookDispatcher(tableOperationDispatcher);
+    TableDispatcher tableDispatcherProxy =
+        (TableDispatcher)
+            Proxy.newProxyInstance(
+                TableDispatcher.class.getClassLoader(),
+                new Class[] {TableDispatcher.class},
+                new OperationDispatcherInterceptor(
+                    tableOperationDispatcher, catalogManager, entityStore));
+    TableHookDispatcher tableHookDispatcher = new TableHookDispatcher(tableDispatcherProxy);
     TableNormalizeDispatcher tableNormalizeDispatcher =
         new TableNormalizeDispatcher(tableHookDispatcher, catalogManager);
     this.tableDispatcher = new TableEventDispatcher(eventBus, tableNormalizeDispatcher);
@@ -553,28 +569,55 @@ public class GravitinoEnv {
     //  partition doesn't have ownership, so we don't need it now.
     PartitionOperationDispatcher partitionOperationDispatcher =
         new PartitionOperationDispatcher(catalogManager, entityStore, idGenerator);
+    PartitionDispatcher partitionDispatcherProxy =
+        (PartitionDispatcher)
+            Proxy.newProxyInstance(
+                PartitionDispatcher.class.getClassLoader(),
+                new Class[] {PartitionDispatcher.class},
+                new OperationDispatcherInterceptor(
+                    partitionOperationDispatcher, catalogManager, entityStore));
     PartitionNormalizeDispatcher partitionNormalizeDispatcher =
-        new PartitionNormalizeDispatcher(partitionOperationDispatcher, catalogManager);
+        new PartitionNormalizeDispatcher(partitionDispatcherProxy, catalogManager);
     this.partitionDispatcher = new PartitionEventDispatcher(eventBus, partitionNormalizeDispatcher);
 
     FilesetOperationDispatcher filesetOperationDispatcher =
         new FilesetOperationDispatcher(catalogManager, entityStore, idGenerator);
-    FilesetHookDispatcher filesetHookDispatcher =
-        new FilesetHookDispatcher(filesetOperationDispatcher);
+    FilesetDispatcher filesetDispatcherProxy =
+        (FilesetDispatcher)
+            Proxy.newProxyInstance(
+                FilesetDispatcher.class.getClassLoader(),
+                new Class[] {FilesetDispatcher.class},
+                new OperationDispatcherInterceptor(
+                    filesetOperationDispatcher, catalogManager, entityStore));
+    FilesetHookDispatcher filesetHookDispatcher = new FilesetHookDispatcher(filesetDispatcherProxy);
     FilesetNormalizeDispatcher filesetNormalizeDispatcher =
         new FilesetNormalizeDispatcher(filesetHookDispatcher, catalogManager);
     this.filesetDispatcher = new FilesetEventDispatcher(eventBus, filesetNormalizeDispatcher);
 
     TopicOperationDispatcher topicOperationDispatcher =
         new TopicOperationDispatcher(catalogManager, entityStore, idGenerator);
-    TopicHookDispatcher topicHookDispatcher = new TopicHookDispatcher(topicOperationDispatcher);
+    TopicDispatcher topicDispatcherProxy =
+        (TopicDispatcher)
+            Proxy.newProxyInstance(
+                TopicDispatcher.class.getClassLoader(),
+                new Class[] {TopicDispatcher.class},
+                new OperationDispatcherInterceptor(
+                    topicOperationDispatcher, catalogManager, entityStore));
+    TopicHookDispatcher topicHookDispatcher = new TopicHookDispatcher(topicDispatcherProxy);
     TopicNormalizeDispatcher topicNormalizeDispatcher =
         new TopicNormalizeDispatcher(topicHookDispatcher, catalogManager);
     this.topicDispatcher = new TopicEventDispatcher(eventBus, topicNormalizeDispatcher);
 
     ModelOperationDispatcher modelOperationDispatcher =
         new ModelOperationDispatcher(catalogManager, entityStore, idGenerator);
-    ModelHookDispatcher modelHookDispatcher = new ModelHookDispatcher(modelOperationDispatcher);
+    ModelDispatcher modelDispatcherProxy =
+        (ModelDispatcher)
+            Proxy.newProxyInstance(
+                ModelDispatcher.class.getClassLoader(),
+                new Class[] {ModelDispatcher.class},
+                new OperationDispatcherInterceptor(
+                    modelOperationDispatcher, catalogManager, entityStore));
+    ModelHookDispatcher modelHookDispatcher = new ModelHookDispatcher(modelDispatcherProxy);
     ModelNormalizeDispatcher modelNormalizeDispatcher =
         new ModelNormalizeDispatcher(modelHookDispatcher, catalogManager);
     this.modelDispatcher = new ModelEventDispatcher(eventBus, modelNormalizeDispatcher);
