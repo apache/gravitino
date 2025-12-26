41f8356ecaccf3ee0a7b8142374c87052195a55d
fanng <xiaojing@datastrato.com>
2025-12-26 11:03:19 +0800

    split PR

--- catalogs/catalog-lakehouse-iceberg/src/main/java/org/apache/gravitino/catalog/lakehouse/iceberg/IcebergCatalogOperations.java
+++ catalogs/catalog-lakehouse-iceberg/src/main/java/org/apache/gravitino/catalog/lakehouse/iceberg/IcebergCatalogOperations.java
@@ -449,15 +449,30 @@ public class IcebergCatalogOperations implements CatalogOperations, SupportsSche
   private Table renameTable(NameIdentifier tableIdent, TableChange.RenameTable renameTable)
       throws NoSuchTableException, IllegalArgumentException {
     try {
+      Namespace destinationNamespace = tableIdent.namespace();
+      if (renameTable.getNewSchemaName().isPresent()) {
+        String[] namespaceLevels = tableIdent.namespace().levels();
+        String[] destinationLevels = Arrays.copyOf(namespaceLevels, namespaceLevels.length);
+        destinationLevels[destinationLevels.length - 1] = renameTable.getNewSchemaName().get();
+        NameIdentifier destinationSchemaIdent = NameIdentifier.of(destinationLevels);
+        if (!schemaExists(destinationSchemaIdent)) {
+          throw new NoSuchSchemaException(
+              "Iceberg Schema (database) does not exist %s", destinationSchemaIdent);
+        }
+        destinationNamespace = Namespace.of(destinationLevels);
+      }
+
       RenameTableRequest renameTableRequest =
           RenameTableRequest.builder()
               .withSource(IcebergCatalogWrapperHelper.buildIcebergTableIdentifier(tableIdent))
               .withDestination(
                   IcebergCatalogWrapperHelper.buildIcebergTableIdentifier(
-                      tableIdent.namespace(), renameTable.getNewName()))
+                      destinationNamespace, renameTable.getNewName()))
               .build();
       icebergCatalogWrapper.renameTable(renameTableRequest);
-      return loadTable(NameIdentifier.of(tableIdent.namespace(), renameTable.getNewName()));
+      return loadTable(
+          NameIdentifier.of(
+              ArrayUtils.add(destinationNamespace.levels(), renameTable.getNewName())));
     } catch (org.apache.iceberg.exceptions.NoSuchTableException e) {
       throw new NoSuchTableException(e, ICEBERG_TABLE_DOES_NOT_EXIST_MSG, tableIdent.name());
     }
