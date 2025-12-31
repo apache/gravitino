15e1513ea28ba3189e0256145ef3e8b8aeaac725
Lord of Abyss <p_lord@163.com>
2025-12-31 09:26:58 +0800

    split test file.

--- clients/client-python/tests/unittests/dto/responses/test_responses.py
+++ clients/client-python/tests/unittests/dto/responses/test_responses.py
@@ -35,12 +35,6 @@ from gravitino.dto.responses.partition_name_list_response import (
 )
 from gravitino.dto.responses.partition_response import PartitionResponse
 from gravitino.dto.responses.table_response import TableResponse
-from gravitino.dto.responses.tag_response import (
-    TagListResponse,
-    TagNamesListResponse,
-    TagResponse,
-)
-from gravitino.dto.tag_dto import TagDTO
 from gravitino.exceptions.base import IllegalArgumentException
 
 
@@ -498,68 +492,6 @@ class TestResponses(unittest.TestCase):
         resp: PartitionListResponse = PartitionListResponse.from_json(json_string)
         resp.validate()
         self.assertListEqual(resp.get_partitions(), partitions)
-
-    def test_tag_response(self) -> None:
-        tag_dto = TagDTO.builder().name("tag1").comment("comment1").build()
-        tag_resp = TagResponse(0, tag_dto)
-
-        tag_resp.validate()
-
-        # test serialization
-        ser_json = _json.dumps(tag_resp.to_dict())
-        deser_dict = _json.loads(ser_json)
-
-        self.assertEqual(tag_dto, tag_resp.tag())
-        self.assertEqual(0, deser_dict["code"])
-        self.assertIsNotNone(deser_dict.get("tag"))
-        self.assertEqual("tag1", deser_dict["tag"]["name"])
-        self.assertEqual("comment1", deser_dict["tag"]["comment"])
-
-        tag_dto_invalid = TagDTO.builder().build()
-        with self.assertRaises(ValueError):
-            tag_resp = TagResponse(tag_dto_invalid)
-            tag_resp.validate()
-
-    def test_tag_name_list_response(self) -> None:
-        tag_names = ["tag1", "tag2", "tag3"]
-        tag_name_list_resp = TagNamesListResponse(0, tag_names)
-
-        tag_name_list_resp.validate()
-
-        # test serialization
-        ser_json = _json.dumps(tag_name_list_resp.to_dict())
-        deser_dict = _json.loads(ser_json)
-
-        self.assertEqual(0, deser_dict["code"])
-        self.assertIsNotNone(deser_dict.get("names"))
-        self.assertEqual(3, len(deser_dict["names"]))
-        self.assertListEqual(tag_names, deser_dict["names"])
-
-    def test_tag_list_response(self) -> None:
-        tag_dto1 = TagDTO.builder().name("tag1").comment("comment1").build()
-        tag_dto2 = TagDTO.builder().name("tag2").comment("comment2").build()
-        tag_dto3 = TagDTO.builder().name("tag3").comment("comment3").build()
-
-        tag_list_resp = TagListResponse(0, [tag_dto1, tag_dto2, tag_dto3])
-
-        tag_list_resp.validate()
-
-        # test serialization
-        ser_json = _json.dumps(tag_list_resp.to_dict())
-        deser_dict = _json.loads(ser_json)
-
-        self.assertEqual(0, deser_dict["code"])
-        self.assertIsNotNone(deser_dict.get("tags"))
-        self.assertEqual(3, len(deser_dict["tags"]))
-
-        self.assertEqual("tag1", deser_dict["tags"][0]["name"])
-        self.assertEqual("comment1", deser_dict["tags"][0]["comment"])
-
-        self.assertEqual("tag2", deser_dict["tags"][1]["name"])
-        self.assertEqual("comment2", deser_dict["tags"][1]["comment"])
-
-        self.assertEqual("tag3", deser_dict["tags"][2]["name"])
-        self.assertEqual("comment3", deser_dict["tags"][2]["comment"])
 
     def test_table_response(self):
         json_string = f"""
