985817e66cd7dead5070666d870a0ebf2805d493
yuhui <hui@datastrato.com>
2026-01-15 10:32:15 +0800

    Fix

--- trino-connector/trino-connector-452-465/src/main/java/org/apache/gravitino/trino/connector/GravitinoMetadata452.java
+++ trino-connector/trino-connector-452-465/src/main/java/org/apache/gravitino/trino/connector/GravitinoMetadata452.java
@@ -20,9 +20,11 @@ package org.apache.gravitino.trino.connector;
 
 import io.airlift.slice.Slice;
 import io.trino.spi.connector.ConnectorInsertTableHandle;
+import io.trino.spi.connector.ConnectorMergeTableHandle;
 import io.trino.spi.connector.ConnectorOutputMetadata;
 import io.trino.spi.connector.ConnectorSession;
 import io.trino.spi.connector.ConnectorTableHandle;
+import io.trino.spi.connector.RetryMode;
 import io.trino.spi.statistics.ComputedStatistics;
 import java.util.Collection;
 import java.util.List;
@@ -50,6 +52,27 @@ public class GravitinoMetadata452 extends GravitinoMetadata {
     return internalMetadata.finishInsert(
         session,
         GravitinoHandle.unWrap(insertHandle),
+        sourceTableHandles.stream().map(GravitinoHandle::unWrap).collect(Collectors.toList()),
+        fragments,
+        computedStatistics);
+  }
+
+  @Override
+  public ConnectorMergeTableHandle beginMerge(
+      ConnectorSession session, ConnectorTableHandle tableHandle, RetryMode retryMode) {
+    return internalMetadata.beginMerge(session, GravitinoHandle.unWrap(tableHandle), retryMode);
+  }
+
+  @Override
+  public void finishMerge(
+      ConnectorSession session,
+      ConnectorMergeTableHandle mergeTableHandle,
+      List<ConnectorTableHandle> sourceTableHandles,
+      Collection<Slice> fragments,
+      Collection<ComputedStatistics> computedStatistics) {
+    internalMetadata.finishMerge(
+        session,
+        GravitinoHandle.unWrap(mergeTableHandle),
         sourceTableHandles.stream().map(GravitinoHandle::unWrap).collect(Collectors.toList()),
         fragments,
         computedStatistics);
