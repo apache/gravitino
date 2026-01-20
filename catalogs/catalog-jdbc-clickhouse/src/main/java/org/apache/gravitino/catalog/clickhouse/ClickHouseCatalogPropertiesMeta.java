0fab6056697dfe20426a0c19425f7b6c1dbe3c8d
yuqi <yuqi@datastrato.com>
2026-01-20 16:56:08 +0800

    fix

---
+++ catalogs/catalog-jdbc-clickhouse/src/main/java/org/apache/gravitino/catalog/clickhouse/ClickHouseCatalogPropertiesMeta.java
@@ -0,0 +1,78 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.apache.gravitino.catalog.clickhouse;
+
+import static org.apache.gravitino.connector.PropertyEntry.booleanPropertyEntry;
+import static org.apache.gravitino.connector.PropertyEntry.stringPropertyEntry;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import java.util.List;
+import java.util.Map;
+import org.apache.gravitino.catalog.jdbc.JdbcCatalogPropertiesMetadata;
+import org.apache.gravitino.connector.PropertyEntry;
+
+public class ClickHouseCatalogPropertiesMeta extends JdbcCatalogPropertiesMetadata {
+
+  private static final Map<String, PropertyEntry<?>> PROPERTIES_METADATA;
+
+  static {
+    List<PropertyEntry<?>> propertyEntries =
+        ImmutableList.of(
+            stringPropertyEntry(
+                ClickHouseConfig.CK_CLUSTER_NAME.getKey(),
+                ClickHouseConfig.CK_CLUSTER_NAME.getDoc(),
+                false /* required */,
+                false /* immutable */,
+                null /* defaultValue */,
+                false /* hidden */,
+                false /* reserved */),
+            booleanPropertyEntry(
+                ClickHouseConfig.CK_ON_CLUSTER.getKey(),
+                ClickHouseConfig.CK_ON_CLUSTER.getDoc(),
+                false /* required */,
+                false /* immutable */,
+                false /* defaultValue */,
+                false /* hidden */,
+                false /* reserved */),
+            stringPropertyEntry(
+                ClickHouseConfig.CK_CLUSTER_SHARDING_KEY.getKey(),
+                ClickHouseConfig.CK_CLUSTER_SHARDING_KEY.getDoc(),
+                false /* required */,
+                false /* immutable */,
+                null /* defaultValue */,
+                false /* hidden */,
+                false /* reserved */));
+
+    PROPERTIES_METADATA = Maps.uniqueIndex(propertyEntries, PropertyEntry::getName);
+  }
+
+  @Override
+  protected Map<String, PropertyEntry<?>> specificPropertyEntries() {
+    Map<String, PropertyEntry<?>> superMap = super.specificPropertyEntries();
+    ImmutableMap<String, PropertyEntry<?>> result =
+        new ImmutableMap.Builder<String, PropertyEntry<?>>()
+            .putAll(superMap)
+            .putAll(PROPERTIES_METADATA)
+            .build();
+    return result;
+  }
+}
---
+++ catalogs/catalog-jdbc-clickhouse/src/main/java/org/apache/gravitino/catalog/clickhouse/ClickHouseConfig.java
@@ -0,0 +1,53 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.apache.gravitino.catalog.clickhouse;
+
+import static org.apache.gravitino.catalog.clickhouse.ClickhouseConstants.CLUSTER_NAME;
+import static org.apache.gravitino.catalog.clickhouse.ClickhouseConstants.CLUSTER_SHARDING_KEY;
+import static org.apache.gravitino.catalog.clickhouse.ClickhouseConstants.ON_CLUSTER;
+
+import org.apache.gravitino.config.ConfigBuilder;
+import org.apache.gravitino.config.ConfigConstants;
+import org.apache.gravitino.config.ConfigEntry;
+
+public class ClickHouseConfig {
+
+  // Constants part
+  public static final ConfigEntry<String> CK_CLUSTER_NAME =
+      new ConfigBuilder(CLUSTER_NAME)
+          .doc("Cluster name for ClickHouse distributed tables")
+          .version(ConfigConstants.VERSION_1_2_0)
+          .stringConf()
+          .create();
+
+  public static final ConfigEntry<Boolean> CK_ON_CLUSTER =
+      new ConfigBuilder(ON_CLUSTER)
+          .doc("Whether to use 'ON CLUSTER' clause when creating tables in ClickHouse")
+          .version(ConfigConstants.VERSION_1_2_0)
+          .booleanConf()
+          .createWithDefault(false);
+
+  public static final ConfigEntry<String> CK_CLUSTER_SHARDING_KEY =
+      new ConfigBuilder(CLUSTER_SHARDING_KEY)
+          .doc("Sharding key for ClickHouse distributed tables")
+          .version(ConfigConstants.VERSION_1_2_0)
+          .stringConf()
+          .create();
+}
