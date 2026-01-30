c6a5fc464d7e610c47f428858589c5006ae005fb
yuhui <hui@datastrato.com>
2026-01-30 16:33:34 +0800

    Fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -63,7 +63,11 @@ include("iceberg:iceberg-rest-server")
 include("lance:lance-common")
 include("lance:lance-rest-server")
 include("authorizations:authorization-ranger", "authorizations:authorization-common", "authorizations:authorization-chain")
-include("trino-connector:trino-connector", "trino-connector:integration-test")
+include(
+  "trino-connector:trino-connector",
+  "trino-connector:trino-connector-435-439",
+  "trino-connector:integration-test"
+)
 include("spark-connector:spark-common")
 if (scalaVersion == "2.12") {
   // flink only support scala 2.12
