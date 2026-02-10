f6450d8996cf61c462f3dd27008dec1ef912097c
mchades <liminghuang@datastrato.com>
2026-02-10 18:25:37 +0800

    feat(python-client): add function DTOs, request/response, and error handler
    Add the data transfer layer for function support:
    - FunctionDTO, FunctionColumnDTO, FunctionDefinitionDTO, FunctionImplDTO,
      FunctionParamDTO, FunctionResourcesDTO
    - FunctionRegisterRequest, FunctionUpdateRequest, FunctionUpdatesRequest
    - FunctionResponse, FunctionListResponse
    - FunctionErrorHandler for REST error mapping
    - Unit tests for DTO serialization and request validation

---
+++ clients/client-python/gravitino/dto/requests/function_register_request.py
@@ -0,0 +1,106 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from dataclasses import dataclass, field
+from typing import List, Optional
+
+from dataclasses_json import config
+
+from gravitino.api.function.function_type import FunctionType
+from gravitino.dto.function.function_definition_dto import FunctionDefinitionDTO
+from gravitino.exceptions.base import IllegalArgumentException
+from gravitino.rest.rest_message import RESTRequest
+
+
+def _encode_function_type(func_type: FunctionType) -> str:
+    """Encode FunctionType to string."""
+    if func_type is None:
+        return None
+    return func_type.value
+
+
+def _decode_function_type(func_type_str: str) -> FunctionType:
+    """Decode string to FunctionType."""
+    if func_type_str is None:
+        return None
+    return FunctionType.from_string(func_type_str)
+
+
+@dataclass
+class FunctionRegisterRequest(RESTRequest):
+    """Represents a request to register a function."""
+
+    _name: str = field(metadata=config(field_name="name"))
+    _function_type: FunctionType = field(
+        metadata=config(
+            field_name="functionType",
+            encoder=_encode_function_type,
+            decoder=_decode_function_type,
+        )
+    )
+    _deterministic: bool = field(metadata=config(field_name="deterministic"))
+    _definitions: List[FunctionDefinitionDTO] = field(
+        metadata=config(field_name="definitions")
+    )
+    _comment: Optional[str] = field(default=None, metadata=config(field_name="comment"))
+
+    def __init__(
+        self,
+        name: str,
+        function_type: FunctionType,
+        deterministic: bool,
+        definitions: List[FunctionDefinitionDTO],
+        comment: Optional[str] = None,
+    ):
+        self._name = name
+        self._function_type = function_type
+        self._deterministic = deterministic
+        self._definitions = definitions
+        self._comment = comment
+
+    def validate(self):
+        """Validates the request.
+
+        Raises:
+            IllegalArgumentException: If the request is invalid.
+        """
+        if not self._name:
+            raise IllegalArgumentException(
+                "'name' field is required and cannot be empty"
+            )
+        if self._function_type is None:
+            raise IllegalArgumentException("'functionType' field is required")
+        if not self._definitions:
+            raise IllegalArgumentException(
+                "'definitions' field is required and cannot be empty"
+            )
+
+        # Validate each definition has appropriate return type/columns based on function type
+        for definition in self._definitions:
+            if self._function_type == FunctionType.TABLE:
+                if (
+                    not definition.return_columns()
+                    or len(definition.return_columns()) == 0
+                ):
+                    raise IllegalArgumentException(
+                        "'returnColumns' is required in each definition for TABLE function type"
+                    )
+            else:
+                if definition.return_type() is None:
+                    raise IllegalArgumentException(
+                        "'returnType' is required in each definition for SCALAR or AGGREGATE function type"
+                    )
---
+++ clients/client-python/tests/unittests/dto/test_function_dto.py
@@ -0,0 +1,149 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License a
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""Tests for Function DTOs."""
+
+import unittest
+
+from gravitino.api.function.function_type import FunctionType
+from gravitino.api.rel.types.types import Types
+from gravitino.dto.audit_dto import AuditDTO
+from gravitino.dto.function.function_column_dto import FunctionColumnDTO
+from gravitino.dto.function.function_definition_dto import FunctionDefinitionDTO
+from gravitino.dto.function.function_dto import FunctionDTO
+from gravitino.dto.function.function_impl_dto import (
+    SQLImplDTO,
+    PythonImplDTO,
+    JavaImplDTO,
+)
+from gravitino.dto.function.function_param_dto import FunctionParamDTO
+from gravitino.dto.function.function_resources_dto import FunctionResourcesDTO
+
+
+class TestFunctionDTO(unittest.TestCase):
+    """Tests for Function DTOs."""
+
+    def test_function_dto(self):
+        """Test FunctionDTO serialization and deserialization."""
+        audit = AuditDTO(
+            "creator", "2022-01-01T00:00:00Z", "modifier", "2022-01-01T00:00:00Z"
+        )
+        params = [FunctionParamDTO(_name="param1", _data_type=Types.IntegerType.get())]
+        impl = SQLImplDTO(
+            _runtime="SPARK",
+            _sql="SELECT param1 + 1",
+            _resources=None,
+            _properties={},
+        )
+        definition = FunctionDefinitionDTO(
+            _parameters=params, _return_type=Types.IntegerType.get(), _impls=[impl]
+        )
+
+        function_dto = FunctionDTO(
+            _name="func1",
+            _definitions=[definition],
+            _function_type=FunctionType.SCALAR,
+            _deterministic=True,
+            _comment="comment",
+            _audit=audit,
+        )
+
+        json_str = function_dto.to_json()
+        deserialized = FunctionDTO.from_json(json_str)
+        self.assertEqual(function_dto.name(), deserialized.name())
+        self.assertEqual(function_dto.comment(), deserialized.comment())
+        self.assertEqual(
+            function_dto.audit_info().creator(), deserialized.audit_info().creator()
+        )
+        self.assertEqual(
+            function_dto.definitions()[0].impls()[0].sql(),
+            deserialized.definitions()[0].impls()[0].sql(),
+        )
+        self.assertEqual(
+            function_dto.definitions()[0].return_type(),
+            deserialized.definitions()[0].return_type(),
+        )
+
+    def test_function_definition_dto(self):
+        """Test FunctionDefinitionDTO serialization and deserialization."""
+        params = [FunctionParamDTO(_name="param1", _data_type=Types.IntegerType.get())]
+        impl = SQLImplDTO(
+            _runtime="SPARK",
+            _sql="SELECT param1 + 1",
+            _resources=None,
+            _properties={},
+        )
+        definition = FunctionDefinitionDTO(
+            _parameters=params, _return_type=Types.IntegerType.get(), _impls=[impl]
+        )
+
+        json_str = definition.to_json()
+        deserialized = FunctionDefinitionDTO.from_json(json_str)
+        self.assertEqual(
+            definition.parameters()[0].name(), deserialized.parameters()[0].name()
+        )
+        self.assertEqual(definition.impls()[0].sql(), deserialized.impls()[0].sql())
+        self.assertEqual(definition.return_type(), deserialized.return_type())
+
+    def test_function_impl_dto(self):
+        """Test FunctionImplDTO serialization and deserialization."""
+        sql_impl = SQLImplDTO(
+            _runtime="SPARK",
+            _sql="SELECT 1",
+            _resources=None,
+            _properties={},
+        )
+        json_str = sql_impl.to_json()
+        deserialized = SQLImplDTO.from_json(json_str)
+        self.assertEqual(sql_impl.sql(), deserialized.sql())
+        self.assertEqual(sql_impl.runtime(), deserialized.runtime())
+
+        python_impl = PythonImplDTO(
+            _runtime="PYTHON",
+            _handler="test_module.test_func",
+            _code_block="def test_func(): pass",
+            _resources=None,
+            _properties={},
+        )
+        json_str = python_impl.to_json()
+        deserialized = PythonImplDTO.from_json(json_str)
+        self.assertEqual(python_impl.handler(), deserialized.handler())
+        self.assertEqual(python_impl.code_block(), deserialized.code_block())
+
+        java_impl = JavaImplDTO(
+            _runtime="JAVA",
+            _class_name="com.test.TestClass",
+            _resources=None,
+            _properties={},
+        )
+        json_str = java_impl.to_json()
+        deserialized = JavaImplDTO.from_json(json_str)
+        self.assertEqual(java_impl.class_name(), deserialized.class_name())
+
+    def test_function_resources_dto(self):
+        """Test FunctionResourcesDTO serialization and deserialization."""
+        resources = FunctionResourcesDTO(_jars=["v1"], _files=None, _archives=None)
+        json_str = resources.to_json()
+        deserialized = FunctionResourcesDTO.from_json(json_str)
+        self.assertEqual(resources.jars(), deserialized.jars())
+
+    def test_function_column_dto(self):
+        """Test FunctionColumnDTO serialization and deserialization."""
+        column = FunctionColumnDTO(_name="col1", _data_type=Types.IntegerType.get())
+        json_str = column.to_json()
+        deserialized = FunctionColumnDTO.from_json(json_str)
+        self.assertEqual(column.name(), deserialized.name())
+        self.assertEqual(column.data_type(), deserialized.data_type())
