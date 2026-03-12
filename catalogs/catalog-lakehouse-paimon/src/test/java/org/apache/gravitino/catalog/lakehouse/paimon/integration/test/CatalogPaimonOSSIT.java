1195fa453c10e615d009a3a0fce94e1dc9850833
MaSai <30949009+lasdf1234@users.noreply.github.com>
2026-03-12 14:06:03 +0800

    Update CatalogPaimonOSSIT.java

--- catalogs/catalog-lakehouse-paimon/src/test/java/org/apache/gravitino/catalog/lakehouse/paimon/integration/test/CatalogPaimonOSSIT.java
+++ catalogs/catalog-lakehouse-paimon/src/test/java/org/apache/gravitino/catalog/lakehouse/paimon/integration/test/CatalogPaimonOSSIT.java
@@ -61,7 +61,7 @@ public class CatalogPaimonOSSIT extends CatalogPaimonBaseIT {
     catalogProperties.put(OSSProperties.GRAVITINO_OSS_ACCESS_KEY_ID, accessKey);
     catalogProperties.put(OSSProperties.GRAVITINO_OSS_ACCESS_KEY_SECRET, secretKey);
     catalogProperties.put(OSSProperties.GRAVITINO_OSS_ENDPOINT, endpoint);
-    catalogProperties.put(OSSProperties.GRAVITINO_OSS_IMPLEMENT, ossImpl);
+    catalogProperties.put(OSSProperties.GRAVITINO_OSS_IMPLEMENTATION, ossImpl);
 
     // Need to download the OSS dependency in the deploy mode.
     downloadOSSDependency();
