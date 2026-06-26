e4daa5d7f9193c27ba9990afb337d917ba370d05
JandyTenedora <jandy.tenedora@hotmail.com>
2026-06-26 14:56:09 +0100

    [#4211] fix(catalog-jdbc): Remove unused test import

--- catalogs/catalog-jdbc-common/src/test/java/org/apache/gravitino/catalog/jdbc/operation/TestJdbcDatabaseOperations.java
+++ catalogs/catalog-jdbc-common/src/test/java/org/apache/gravitino/catalog/jdbc/operation/TestJdbcDatabaseOperations.java
@@ -25,7 +25,6 @@ import java.nio.file.Files;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import javax.sql.DataSource;
 import org.apache.commons.io.FileUtils;
