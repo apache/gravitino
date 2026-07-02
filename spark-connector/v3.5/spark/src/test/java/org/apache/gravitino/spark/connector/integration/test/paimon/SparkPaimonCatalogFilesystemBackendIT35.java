9b310117b17209e43bdd2b06da05750b4f6149e4
羊川 <yangchuan.zy@alibaba-inc.com>
2026-07-02 10:08:11 +0800

    test(spark): use full qualified CALL syntax with catalog name

--- spark-connector/v3.5/spark/src/test/java/org/apache/gravitino/spark/connector/integration/test/paimon/SparkPaimonCatalogFilesystemBackendIT35.java
+++ spark-connector/v3.5/spark/src/test/java/org/apache/gravitino/spark/connector/integration/test/paimon/SparkPaimonCatalogFilesystemBackendIT35.java
@@ -51,11 +51,11 @@ public class SparkPaimonCatalogFilesystemBackendIT35 extends SparkPaimonCatalogF
     String fullTableName =
         String.format("%s.%s.%s", getCatalogName(), getDefaultDatabase(), tableName);
 
-    sql(String.format("USE %s.%s", getCatalogName(), getDefaultDatabase()));
-
     List<Row> result =
         getSparkSession()
-            .sql(String.format("CALL system.compact(table => '%s')", fullTableName))
+            .sql(
+                String.format(
+                    "CALL %s.system.compact(table => '%s')", getCatalogName(), fullTableName))
             .collectAsList();
     Assertions.assertFalse(result.isEmpty());
   }
