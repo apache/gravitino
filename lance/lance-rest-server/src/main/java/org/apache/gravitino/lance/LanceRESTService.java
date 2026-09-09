13e0eaa0fb418ebc7ed39646755377696d6d916c
roryqi <roryqi@apache.org>
2026-09-09 09:28:04 +0000

    [#12975] fix(lance): Preserve errors in REST responses

--- lance/lance-rest-server/src/main/java/org/apache/gravitino/lance/LanceRESTService.java
+++ lance/lance-rest-server/src/main/java/org/apache/gravitino/lance/LanceRESTService.java
@@ -34,6 +34,7 @@ import org.apache.gravitino.auxiliary.GravitinoAuxiliaryService;
 import org.apache.gravitino.lance.common.config.LanceConfig;
 import org.apache.gravitino.lance.common.ops.LanceNamespaceBackend;
 import org.apache.gravitino.lance.common.ops.NamespaceWrapper;
+import org.apache.gravitino.lance.service.LanceExceptionMapper;
 import org.apache.gravitino.lance.service.LanceHealthCheckPathMatcher;
 import org.apache.gravitino.lance.service.LanceServiceIdentityFilter;
 import org.apache.gravitino.lance.service.authorization.LanceAuthorizationMetadataFilter;
@@ -108,6 +109,7 @@ public class LanceRESTService implements GravitinoAuxiliaryService {
     ResourceConfig resourceConfig = new ResourceConfig();
     resourceConfig.register(JacksonFeature.class);
     resourceConfig.packages(LANCE_REST_SPEC_PACKAGE);
+    resourceConfig.register(LanceExceptionMapper.class);
     resourceConfig.register(
         new AbstractBinder() {
           @Override
