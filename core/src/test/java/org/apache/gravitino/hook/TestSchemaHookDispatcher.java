5fe8090a36a1e7f86a8ec9957832a8869bef217f
yuqi <yuqi@datastrato.com>
2026-09-16 19:42:17 +0800

    fix(test): add missing Mockito never import in schema hook test

--- core/src/test/java/org/apache/gravitino/hook/TestSchemaHookDispatcher.java
+++ core/src/test/java/org/apache/gravitino/hook/TestSchemaHookDispatcher.java
@@ -27,6 +27,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
